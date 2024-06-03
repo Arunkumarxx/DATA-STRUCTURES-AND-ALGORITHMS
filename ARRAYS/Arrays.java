6adb92735c17750c67ff2ef6ce561f5967408440
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-03 13:41:30 +0530

    modified

--- ARRAYS/Arrays.java
+++ ARRAYS/Arrays.java
@@ -2,10 +2,7 @@ package ARRAYS;
 public class Arrays {
     public static void main(String[] args){
         System.out.println(4);
-
         System.out.println(223);
-
-
-
+        System.out.println();
     }
 }
