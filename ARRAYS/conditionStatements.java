fe0bd00f12d602b9328ba948837198657439b231
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-20 19:51:36 +0530

    modified

--- ARRAYS/conditionStatements.java
+++ ARRAYS/conditionStatements.java
@@ -2,10 +2,14 @@ package ARRAYS;
 
 public class conditionStatements {
     public static void main(String[] args){
-        /*
-        if (condition){
-
-        }
-         */
+        int age=19;
+        String name ="Arunkumar";
+       if(age>18)
+       {
+           System.out.println(name);
+           if(age!=18 && age>18) {
+               System.out.println(name + " " + age);
+           }
+       }
     }
 }
