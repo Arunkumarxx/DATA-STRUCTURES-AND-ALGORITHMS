c65a7d6a94452343ae93ede8d7fada7e90add76b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-22 19:39:20 +0530

    modified

--- ARRAYS/conditionStatements.java
+++ ARRAYS/conditionStatements.java
@@ -2,29 +2,6 @@ package ARRAYS;
 
 public class conditionStatements {
     public static void main(String[] args){
-       int i=1;
-       do{
-           System.out.println(i);
-           i++;
-       }
-       while(i<=2);
-      /*
-      do{
-      print(i);=1
-      print(i++);
-         }
-      while(2<=2);
-
-      do{
-      print(2);
-      2++;
-      while(3<=2)
-      ...........
-      .
-      .
-      .
-      .
-
-       */
+        
     }
 }
