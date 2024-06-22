1cb84e44b16050d899d44fda8aec693a932e6058
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-22 19:33:39 +0530

    modified

--- ARRAYS/conditionStatements.java
+++ ARRAYS/conditionStatements.java
@@ -2,12 +2,12 @@ package ARRAYS;
 
 public class conditionStatements {
     public static void main(String[] args){
-       int i=2;
+       int i=1;
        do{
            System.out.println(i);
            i++;
        }
-       while(i<=10);
+       while(i<=2);
        /*
        p(1) ++
        true
