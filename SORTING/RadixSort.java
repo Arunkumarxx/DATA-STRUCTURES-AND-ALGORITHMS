0a05c343bf2f18f6ed324ac788ef2121218c357a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-28 12:55:06 +0530

    modified

--- SORTING/RadixSort.java
+++ SORTING/RadixSort.java
@@ -4,7 +4,18 @@ import java.util.Arrays;
 
 public class RadixSort {
     public static void main(String[] args) {
-        short arr [] ={3,9,3,4,6,10};
+      
+    }
+}
+
+
+
+
+
+/*
+first we needed to learn counting sort then learn radix sort
+
+  short arr [] ={3,9,3,4,6,10};
         int max=arr[0];
         for (int x:arr)
             max=Math.max(max,x);
@@ -24,5 +35,4 @@ public class RadixSort {
             else i++;
         }
         System.out.println(Arrays.toString(arr));
-    }
-}
+ */
