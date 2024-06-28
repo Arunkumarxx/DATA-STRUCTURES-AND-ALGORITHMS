99979095410123681e21632ea3b8769a0bafc668
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-28 15:22:22 +0530

    modified

--- SORTING/RadixSort.java
+++ SORTING/RadixSort.java
@@ -17,7 +17,8 @@ public class RadixSort {
             freq[i]+=freq[i-1];
         for (int i=arrN-1; i>=0; i--)
         {
-            
+            temp[freq[i]-1]=arr[i];
+            --freq[i];
         }
     }
 }
