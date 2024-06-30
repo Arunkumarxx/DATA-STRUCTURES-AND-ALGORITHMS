bc762ccc84af500323026a35b20ae48617f0fa89
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-30 13:31:58 +0530

    modified

--- SORTING/BucketSort.java
+++ SORTING/BucketSort.java
@@ -2,6 +2,6 @@ package SORTING;
 
 public class BucketSort {
     public static void main(String[] args) {
-        System.out.println((int)Math.floor(0.10*10));
+        System.out.println((int)Math.floor(0.*10));
     }
 }
