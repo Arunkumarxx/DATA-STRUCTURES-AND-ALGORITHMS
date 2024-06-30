63971a23467878a9b5b0d7f197beb9a75fccafac
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-30 14:51:18 +0530

    modified

--- SORTING/BucketSort.java
+++ SORTING/BucketSort.java
@@ -12,7 +12,8 @@ public class BucketSort {
 
         for (int i=0; i< arr.length;i++)
             BucketSort.get((int)(arr[i]*nbksBuckets)).add(arr[i]);
-
+        for (int i=0;i< nbksBuckets;i++)
+            Collections.sort(BucketSort.get(i));
         System.out.println(BucketSort);
     }
 }
