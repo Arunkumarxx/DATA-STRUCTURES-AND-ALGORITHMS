16eb980291717dd9fc8bfc387f2af46e4ce4e724
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-30 20:41:04 +0530

    modified

--- SORTING/CountTheNumberOfPossibleTriangles.java
+++ SORTING/CountTheNumberOfPossibleTriangles.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class CountTheNumberOfPossibleTriangles {
     public static void main(String[] args) {
         int [] arr ={1,16,20,3,4,6,12,13};
-        System.out.println(arr);
+        System.out.println(Arrays.toString(arr));
         System.out.println(Arrays.toString(arr));
     }
 }
