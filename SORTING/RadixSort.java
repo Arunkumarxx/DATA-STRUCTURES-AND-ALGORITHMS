bb6485d477d16ef127f6c068327fc8d247a0274e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-30 11:54:27 +0530

    modified

--- SORTING/RadixSort.java
+++ SORTING/RadixSort.java
@@ -1,7 +1,10 @@
+
 package SORTING;
 
 import java.util.Arrays;
-
+/*
+first we needed to learn counting sort then learn radix sort
+ */
 public class RadixSort {
 
     public static void main(String[] args) {
@@ -35,25 +38,3 @@ public class RadixSort {
 }
 
 
-
-/*
-first  needed to learn counting sort then learn radix sort
- int temp[] = new int[arr.length];
-        int max=arr[0];
-        for (int x:arr)
-            max=Math.max(max,x);
-        int [] freq = new int[max+1];
-        for (int i:arr)
-            ++freq[i];
-        for (int i=1; i< freq.length; i++)
-            freq[i]+=freq[i-1];
-        for (int i=arrN-1; i>=0; i--)
-        {
-            temp[freq[arr[i]]-1]=arr[i];
-            --freq[arr[i]];
-        }
-        for (int i=0; i< temp.length; i++)
-            arr[i]=temp[i];
-        System.out.println(Arrays.toString(arr));
-
- */
