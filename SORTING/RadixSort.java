16cd8c064702fa65d09a05b7c601dfb4749e4046
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-30 11:24:31 +0530

    modified

--- SORTING/RadixSort.java
+++ SORTING/RadixSort.java
@@ -1,5 +1,7 @@
 package SORTING;
 
+import java.util.Arrays;
+
 public class RadixSort {
 
     public static void main(String[] args) {
@@ -9,14 +11,14 @@ public class RadixSort {
         for (int x:arr)
             max=Math.max(x,max);
         int pos=1;
-//        while(max/pos >0){
-//            pos*=10;
-//            int [] count = new int[10];
-//            for (int i=0; i<arr.length; i++)
-//                ++count[arr[i]%pos];
-//
-//        }
-        System.out.println(((max/10)%10));
+        while(max/pos >0){
+            pos*=10;
+            int [] count = new int[10];
+            for (int i=0; i<arr.length; i++)
+                ++count[(arr[i]/pos)%10];
+            System.out.println(Arrays.toString(count));
+            break;
+        }
     }
 }
 
