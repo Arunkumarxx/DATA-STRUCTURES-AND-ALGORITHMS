266f1fdc2370b3e0e9f0b25555052c84dae8b7ac
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-30 14:32:07 +0530

    modified

--- SORTING/BucketSort.java
+++ SORTING/BucketSort.java
@@ -10,6 +10,9 @@ public class BucketSort {
         float [] arr = {0.897f, 0.565f, 0.656f, 0.1234f, 0.665f, 23.32f};
         int nbksBuckets= arr.length;
         Map<Integer, List<Float>> BucketSort = new HashMap<>();
-        
+        for (int i=0;i< arr.length;i++)
+        {
+            
+        }
     }
 }
