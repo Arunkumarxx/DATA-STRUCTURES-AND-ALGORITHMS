8318bac50f0057e8017270398490d0207550b720
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-30 14:00:18 +0530

    modified

--- SORTING/BucketSort.java
+++ SORTING/BucketSort.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class BucketSort {
     public static void main(String[] args) {
         float [] arr = {0.897f, 0.565f, 0.656f, 0.1234f, 0.665f, 0.3434f};
-        
+        ArrayList<ArrayList> BucketSort = new ArrayList<>();
+        for (int i=0; i<arr.length ;i++)
+
     }
 }
