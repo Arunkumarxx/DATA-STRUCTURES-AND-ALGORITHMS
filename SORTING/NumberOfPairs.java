a52a1b23925ff4ed0b46e6546023e4a3784bb92b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-03 11:48:16 +0530

    modified

--- SORTING/NumberOfPairs.java
+++ SORTING/NumberOfPairs.java
@@ -3,7 +3,15 @@ package SORTING;
 public class NumberOfPairs {
     public static void main(String[] args) {
         int [] x ={2,1,6};
+        int n1=x.length;
         int [] y={1,5};
-        
+        int n2=y.length;
+        for (int i=0; i<n1;i++)
+        {
+            for (int j=0; j<n2; j++)
+            {
+                
+            }
+        }
     }
 }
