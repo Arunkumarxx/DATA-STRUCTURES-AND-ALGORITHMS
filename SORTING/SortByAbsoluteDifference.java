97fe865d62015303f16da18555035da3e8e2eabd
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-03 07:55:58 +0530

    modified

--- SORTING/SortByAbsoluteDifference.java
+++ SORTING/SortByAbsoluteDifference.java
@@ -6,10 +6,11 @@ import java.util.List;
 public class SortByAbsoluteDifference {
     public static void main(String[] args) {
         List<Integer> arrList = new LinkedList<>();
-        arrList.add(12);
-        arrList.add(432);
-        arrList.add(432);
-        arrList.add(12);
-        arrList.add(12);
+        arrList.add(10);
+        arrList.add(5);
+        arrList.add(3);
+        arrList.add(9);
+        arrList.add(2);
+        System.out.println(arrList);
     }
 }
