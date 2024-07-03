f53633b2fd053fab81775b2d79d16fcfa67e8bf9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-03 19:23:58 +0530

    modified

--- SORTING/CloserToSort.java
+++ SORTING/CloserToSort.java
@@ -3,6 +3,7 @@ package SORTING;
 public class CloserToSort {
     public static void main(String[] args) {
         int [] arr ={3,2,10,4,40,70,60,90,80,110,100};
+        int x=100;
         
     }
 }
