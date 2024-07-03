df3474db3f302c2381925cdf2691d6a891f529b0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-03 20:37:42 +0530

    modified

--- SORTING/CloserToSort.java
+++ SORTING/CloserToSort.java
@@ -2,6 +2,28 @@ package SORTING;
 
 public class CloserToSort {
     public static void main(String[] args) {
-        
+        int [] arr ={3,2,10,4,40};
+        int n= arr.length;
+        int l=0;
+        int r=n-1;
+
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(arr[mid]==x)
+                return (long)mid;
+
+            if(mid > 0 && arr[mid-1]==x)
+                return (long)mid-1;
+
+            if(mid <n-1 && arr[mid+1]==x)
+                return (long)mid+1;
+
+            if(arr[mid]<x)
+                l=mid+1;
+            else r=mid-1;
+
+        }
+        return (long)-1;
     }
 }
