05a1159af7b847022bb2ba4861eaadc7b0638a93
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-05 13:54:58 +0530

    modified

--- MATRIX/MatrixOperationsAdditionSubtractionMultiplication.java
+++ MATRIX/MatrixOperationsAdditionSubtractionMultiplication.java
@@ -1,2 +1,15 @@
-package MATRIX;public class MatrixOperationsAdditionSubtractionMultiplication {
+package MATRIX;
+
+public class MatrixOperationsAdditionSubtractionMultiplication {
+    public static void main(String[] args) {
+        int [][] arrOne ={{1,2},{4,5}};
+        int [][] arrTwo ={{5,6},{8,9}};
+        int [][] result =new int[arrOne.length][];
+        for (int i=0;i<arrOne.length; i++) {
+            result[i]= new int[arrOne[i].length];
+            for (int j = 0; j < arrOne[i].length; j++){
+                result[i][j]=arrOne[i][j]+arrTwo[i][j];
+            }
+        }
+    }
 }
