adadf0bd09b4bc41cf4fd08bdeb44223cd891ced
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-05 18:17:53 +0530

    modified

--- MATRIX/PrintMatrixInSnakePattern.java
+++ MATRIX/PrintMatrixInSnakePattern.java
@@ -2,6 +2,20 @@ package MATRIX;
 
 public class PrintMatrixInSnakePattern {
     public static void main(String[] args) {
-        
+     int [][] arr  ={
+             {1,2,3},
+             {4,5,6},
+             {7,8,9}
+     };
+     int n= arr.length;
+     
+     for (int i=0; i<n; i++)
+     {
+         if((i&1)==0)
+             for (int j=0; j<n;j++)
+             {
+
+             }
+       }
     }
 }
