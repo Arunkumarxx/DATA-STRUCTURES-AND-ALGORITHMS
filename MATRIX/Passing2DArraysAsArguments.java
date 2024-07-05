e159df6ecd34b20c5562b43099c58c95bea85555
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-05 12:15:36 +0530

    modified

--- MATRIX/Passing2DArraysAsArguments.java
+++ MATRIX/Passing2DArraysAsArguments.java
@@ -1,7 +1,13 @@
 package MATRIX;
 
 public class Passing2DArraysAsArguments {
+    static void print(int [][] arr){
+        for (int [] x:arr)
+            for (int y:x)
+                System.out.print(y+" ");
+    }
     public static void main(String[] args) {
-        
+        int [][] arr ={{}};
+        print();
     }
 }
