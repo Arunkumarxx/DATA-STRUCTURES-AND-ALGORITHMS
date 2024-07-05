ed7001aa0fb88a40145f61ce8347b0dafa40180e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-05 19:50:38 +0530

    modified

--- MATRIX/MatrixBoundaryTraversal.java
+++ MATRIX/MatrixBoundaryTraversal.java
@@ -3,9 +3,9 @@ package MATRIX;
 public class MatrixBoundaryTraversal {
     public static void main(String[] args) {
         int [][] arr ={
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
+                {1,2,3,32,3,2,432},
+                {4,5,6,23,32,3,3},
+                {7,8,9,65,65,5,56}
         };
         int n= arr.length;
         for (int i=0; i<n; i++)
