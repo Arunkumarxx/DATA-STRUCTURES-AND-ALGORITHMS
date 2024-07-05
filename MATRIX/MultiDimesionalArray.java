100ebd8e59e72d010513f19982e0dcf28eae986b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-05 11:24:14 +0530

    modified

--- MATRIX/MultiDimesionalArray.java
+++ MATRIX/MultiDimesionalArray.java
@@ -29,7 +29,7 @@ public class MultiDimesionalArray {
             System.out.print("[");
             for (int x1 : x)
                 System.out.print(x1 + " ");
-            System.out.println("]");
+            System.out.print("]");
             System.out.println("]");
         }
     }
