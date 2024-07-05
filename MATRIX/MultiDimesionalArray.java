c4d2ea539e3edc006633348e62c6307e23b34ab5
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-05 10:15:17 +0530

    modified

--- MATRIX/MultiDimesionalArray.java
+++ MATRIX/MultiDimesionalArray.java
@@ -2,7 +2,7 @@ package MATRIX;
 
 public class MultiDimesionalArray {
     public static void main(String[] args) {
-        int [][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+        int [][] arr = {{1,2,3},{23,2123,343,2},{7,8,9}};
         int n=arr.length;
         for (int i=0; i<n; i++)
         {
