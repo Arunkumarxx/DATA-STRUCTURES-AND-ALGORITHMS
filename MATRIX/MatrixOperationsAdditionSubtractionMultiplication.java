6dd67523ddc1704992638b51e4e16e28089a8e65
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-05 14:02:37 +0530

    modified

--- MATRIX/MatrixOperationsAdditionSubtractionMultiplication.java
+++ MATRIX/MatrixOperationsAdditionSubtractionMultiplication.java
@@ -3,21 +3,49 @@ package MATRIX;
 import ARRAYS.Arrays;
 
 public class MatrixOperationsAdditionSubtractionMultiplication {
-    public static void main(String[] args) {
-        int [][] arrOne ={{1,2},{4,5}};
-        int [][] arrTwo ={{5,6},{8,9}};
-        int [][] result =new int[arrOne.length][];
+    static void add(int [][] arrOne,int [][]arrTwo,int [][] result) {
         for (int i=0;i<arrOne.length; i++) {
             result[i]= new int[arrOne[i].length];
             for (int j = 0; j < arrOne[i].length; j++){
                 result[i][j]=arrOne[i][j]+arrTwo[i][j];
             }
         }
-        for (int [] x:result) {
-            for (int y : x)
-                System.out.print(y + " ");
-            System.out.println();
-            
+    }
+    static void sub(int [][] arrOne,int [][]arrTwo,int [][] result) {
+        for (int i=0;i<arrOne.length; i++) {
+            result[i]= new int[arrOne[i].length];
+            for (int j = 0; j < arrOne[i].length; j++){
+                result[i][j]=arrOne[i][j]+arrTwo[i][j];
+            }
         }
+    }
+    static void divide(int [][] arrOne,int [][]arrTwo,int [][] result) {
+        for (int i=0;i<arrOne.length; i++) {
+            result[i]= new int[arrOne[i].length];
+            for (int j = 0; j < arrOne[i].length; j++){
+                result[i][j]=arrOne[i][j]+arrTwo[i][j];
+            }
+        }
+    }
+    static void multi(int [][] arrOne,int [][]arrTwo,int [][] result) {
+        for (int i=0;i<arrOne.length; i++) {
+            result[i]= new int[arrOne[i].length];
+            for (int j = 0; j < arrOne[i].length; j++){
+                result[i][j]=arrOne[i][j]+arrTwo[i][j];
+            }
+        }
+    }
+    static void print(int [][] result) {
+        for (int [] row :result) {
+            for (int row_col : row)
+                System.out.print(row_col + " ");
+            System.out.print("\n");
+        }
+    }
+    public static void main(String[] args) {
+        int [][] arrOne ={{1,2},{4,5}};
+        int [][] arrTwo ={{5,6},{8,9}};
+        int [][] result =new int[arrOne.length][];
+
     }
 }
