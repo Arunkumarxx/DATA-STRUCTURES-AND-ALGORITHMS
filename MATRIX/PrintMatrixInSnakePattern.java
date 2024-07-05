8392c39f038859c623232c7826ba50c4744073ed
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-05 18:20:25 +0530

    modified

--- MATRIX/PrintMatrixInSnakePattern.java
+++ MATRIX/PrintMatrixInSnakePattern.java
@@ -1,5 +1,7 @@
 package MATRIX;
 
+import java.util.ArrayList;
+
 public class PrintMatrixInSnakePattern {
     public static void main(String[] args) {
      int [][] arr  ={
@@ -8,14 +10,16 @@ public class PrintMatrixInSnakePattern {
              {7,8,9}
      };
      int n= arr.length;
-     
+        ArrayList<Integer> arrayList = new ArrayList<>();
      for (int i=0; i<n; i++)
      {
          if((i&1)==0)
-             for (int j=0; j<n;j++)
-             {
-
-             }
+             for (int j = 0; j < n; j++)
+                 arrayList.add(arr[i][j]);
+         if((i&1)==1) {
+             for (int j = arr[i].length-1; j>=i;j--)
+                 arrayList.add(arr[i][j]);
+         }
        }
     }
 }
