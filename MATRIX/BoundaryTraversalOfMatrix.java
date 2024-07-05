7f0127ad8a66868e0cfdf25c6d8a0ef042a96cf3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-05 20:28:53 +0530

    modified

--- MATRIX/BoundaryTraversalOfMatrix.java
+++ MATRIX/BoundaryTraversalOfMatrix.java
@@ -6,9 +6,10 @@ public class BoundaryTraversalOfMatrix {
 
     public static void main(String[] args) {
         int [][] arr ={
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
+                       {1, 2, 3, 4 },
+                       { 5, 6, 7, 8},
+                       {9,10, 11,12},
+                       {13,14,15,16}
         };
         int n= arr.length;
         ArrayList<Integer> arrayList = new ArrayList<>();
