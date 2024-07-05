2ad864ba04c5d18b43b6800812e941ae05176964
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-05 15:19:44 +0530

    modified

--- MATRIX/AddingTwoMatrices.java
+++ MATRIX/AddingTwoMatrices.java
@@ -5,51 +5,68 @@ import java.util.Random;
 
 public class AddingTwoMatrices {
     public static void main(String[] args) {
-        Random random =new Random();
-        int [][] arrOne =new int[4][4];
-        int [][] arrTwo =new int[4][4];
-        int [] arr =new int[arrOne[0].length* arrOne.length];
-        Arrays.fill(arr,random.nextInt(1,100));
-
-        for (int i=0; i<arrOne.length; i++)
-        {
-
-
-            Arrays.fill(arrTwo[i],);
-        }
-        int [][] result = Add(arrOne,arrTwo);
-        for (int [] x:result) {
-            for (int y : x)
-                System.out.print(y + "\t");
-            System.out.println();
-        }
-
-    }
-    static void fill (int [][] arrOne,int [][] arrTwo,int start,int end) {
         Random random = new Random();
-        for (int i=0; i<arrOne.length; i++) {
-            for (int j = 0; j < arrOne[i].length; j++)
-                arr[i][j] = random.nextInt(start, end);
-        }
+        int[][] arrOne = new int[4][4];
+        int[][] arrTwo = new int[4][4];
+
+
+        fill(arrOne, 1, 100);
+        fill(arrTwo, 1, 100);
+
+
+        System.out.println("Matrix One:");
+        displayMatrix(arrOne);
+
+
+        System.out.println("\nMatrix Two:");
+        displayMatrix(arrTwo);
+
+
+        int[][] result = Add(arrOne, arrTwo);
+
+        System.out.println("Arun Kumar");
+        System.out.println("\nResult Matrix (Sum of Matrix One and Matrix Two):");
+        displayMatrix(result);
     }
-    
-    static int [][] Add(int [][] arrOne,int [][] arrTwo) {
-        int n1=arrOne.length;
-        int n2=arrTwo.length;
-        int m1=arrOne[0].length;
-        int m2=arrTwo[0].length;
 
-        if(n1!=n2||m1!=m2)
-            return new int [0][0];
-
-        int [][] result = new int[n1][m1];
-        for(int i=0; i<n1; i++)
-        {
-            for(int j=0;j<m1; j++)
-            {
-                result [i][j]=  arrOne[i][j]+arrTwo[i][j];
+    static void fill(int[][] arr, int start, int end) {
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = random.nextInt(end - start + 1) + start; // Generating random number in range [start, end]
             }
         }
+    }
+
+    static int[][] Add(int[][] arrOne, int[][] arrTwo) {
+        int n1 = arrOne.length;
+        int m1 = arrOne[0].length;
+
+
+        if (n1 != arrTwo.length || m1 != arrTwo[0].length) {
+            System.out.println("Matrices have different dimensions. Cannot perform addition.");
+            return new int[0][0];
+        }
+
+
+        int[][] result = new int[n1][m1];
+
+
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < m1; j++) {
+                result[i][j] = arrOne[i][j] + arrTwo[i][j];
+            }
+        }
+
         return result;
+    }
+
+    static void displayMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + "\t");
+            }
+            System.out.println();
+        }
     }
 }
