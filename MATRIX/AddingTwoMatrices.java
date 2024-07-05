6a300e0e7ff4d8dd7e4f30bfdc9677b90bc3bbb8
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-05 14:43:35 +0530

    modified

--- MATRIX/AddingTwoMatrices.java
+++ MATRIX/AddingTwoMatrices.java
@@ -1,10 +1,32 @@
 package MATRIX;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class AddingTwoMatrices {
     public static void main(String[] args) {
-
+        Random random = new Random();
+        random.
+        Arrays.fill();
+        Add()
     }
-    static void Add() {
-        
+    static int [][] Add(int [][] arrOne,int [][] arrTwo) {
+        int n1=arrOne.length;
+        int n2=arrTwo.length;
+        int m1=arrOne[0].length;
+        int m2=arrTwo[0].length;
+
+        if(n1!=n2||m1!=m2)
+            return new int [0][0];
+
+        int [][] result = new int[n1][m1];
+        for(int i=0; i<n1; i++)
+        {
+            for(int j=0;j<m1; j++)
+            {
+                result [i][j]=  arrOne[i][j]+arrTwo[i][j];
+            }
+        }
+        return result;
     }
 }
