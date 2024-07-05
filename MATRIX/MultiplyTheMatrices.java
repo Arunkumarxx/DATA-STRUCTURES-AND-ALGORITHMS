bc23c7769b5bdce06671c1aaf4433967cc93cd47
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-05 17:22:55 +0530

    modified

--- MATRIX/MultiplyTheMatrices.java
+++ MATRIX/MultiplyTheMatrices.java
@@ -3,21 +3,13 @@ package MATRIX;
 import ARRAYS.Arrays;
 
 public class MultiplyTheMatrices {
-    public static void main(String[] args) {
-        int [][] One = {
-                {4,8},
-                {0,2},
-                {1,6}
-        };
-        int [][] Two ={
-                {5,2},
-                {9,4}
-        };
+    static int [][]print(int [][] One,int [][]Two) {
         int n1=One.length;
         int n2=Two.length;
         int m1=One[0].length;
         int m2=Two[0].length;
-
+        if(n2!=m1)
+            return new int [0][0];
         int [][] result  =new int[n1][m2];
         for (int i=0; i<n1; i++)
         {
@@ -29,10 +21,22 @@ public class MultiplyTheMatrices {
                 }
             }
         }
-        for (int x[]:result) {
-            for (int y : x)
-                System.out.print(y + " ");
-            System.out.println();
-        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int [][] One = {
+                {4,8},
+                {0,2},
+                {1,6}
+        };
+        int [][] Two ={
+                {5,2},
+                {9,4}
+        };
+       int [][] result = print(One,Two);
+       for (int [] x:result)
+           for (int y:x)
+               System.out.print(y+" ");
+
     }
 }
