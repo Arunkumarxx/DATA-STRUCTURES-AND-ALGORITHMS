e746811b386cee99429c325918c1880a31feb37a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-05 16:57:47 +0530

    modified

--- MATRIX/MultiplyTheMatrices.java
+++ MATRIX/MultiplyTheMatrices.java
@@ -16,12 +16,12 @@ public class MultiplyTheMatrices {
         int m1=One[0].length;
         int m2=Two[0].length;
 
-        int [][] result  =new int[n1][];
+        int [][] result  =new int[n1][m1];
         for (int i=0; i<n1; i++)
         {
             for (int j=0; j<m1; j++)
             {
-                result[i]=new int[j];
+//                result[i]=new int[j];
                 for (int k=0;k<m1; k++)
                 {
                     result[i][j]+=One[j][k]*Two[k][j];
