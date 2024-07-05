82d3b7741a52848404155ba5b8c8a42439d3ba10
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-05 16:42:51 +0530

    modified

--- MATRIX/MultiplyTheMatrices.java
+++ MATRIX/MultiplyTheMatrices.java
@@ -2,6 +2,19 @@ package MATRIX;
 
 public class MultiplyTheMatrices {
     public static void main(String[] args) {
+        int [][] One = {
+                {4,8},
+                {0,2},
+                {1,6}
+        };
+        int [][] Two ={
+                {5,2},
+                {9,4}
+        };
+        int n1=One.length;
+        int n2=Two.length;
+        int m1=One[0].length;
+        int m2=Two[0].length;
         
     }
 }
