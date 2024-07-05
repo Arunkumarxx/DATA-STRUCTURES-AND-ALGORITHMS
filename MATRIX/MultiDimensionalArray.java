75e4f8747ab2a172f23648f90c262785b69c7f07
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-05 11:36:04 +0530

    modified

--- MATRIX/MultiDimensionalArray.java
+++ MATRIX/MultiDimensionalArray.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public class MultiDimensionalArray {
     public static void main(String[] args) {
         MatrixPrinter.printMatrix();
-        RandomMatrixGenerator.generateRandomMatrix();;
+        RandomMatrixGenerator.generateRandomMatrix();
     }
 }
 
