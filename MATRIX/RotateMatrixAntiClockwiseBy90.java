b843ec3025f854dfd81041c34aa0ae98e108bccf
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-06 12:24:47 +0530

    modified

--- MATRIX/RotateMatrixAntiClockwiseBy90.java
+++ MATRIX/RotateMatrixAntiClockwiseBy90.java
@@ -7,6 +7,19 @@ public class RotateMatrixAntiClockwiseBy90 {
                 {4,5,6},
                 {7,8,9}
         };
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for (int i=0;  i<n; i++)
+        {
+            int start=0;
+            int end=m-1;
+            while(start<end)
+            {
+                int temp=matrix[i][start];
+                matrix[i][start]=matrix[i][end];
+                matrix[i][end]=temp;
+            }
+        }
         
     }
 }
