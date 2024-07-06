4d97f19b65eabcaa7f785cb5b7c05f3a04424b7e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-06 10:47:50 +0530

    modified

--- MATRIX/TransposeOfAMatrix.java
+++ MATRIX/TransposeOfAMatrix.java
@@ -6,7 +6,18 @@ public class TransposeOfAMatrix {
                            {2, 2, 2, 2},
                            {3, 3, 3, 3},
                            {4, 4, 4, 4}};
-        
 
+        int n=matrix.length;
+        for (int i=0; i<n;i++)
+        {
+            int k=0;
+            for (int j=0; j<matrix[i].length && k<n;j++)
+            {
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][k];
+                matrix[i][k++]=temp;
+            }
+        }
+        
     }
 }
