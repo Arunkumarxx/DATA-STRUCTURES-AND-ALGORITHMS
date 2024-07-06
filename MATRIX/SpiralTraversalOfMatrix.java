809c7fdec162b76ee6a8153cb4449f42d0b3cb86
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-06 16:09:06 +0530

    modified

--- MATRIX/SpiralTraversalOfMatrix.java
+++ MATRIX/SpiralTraversalOfMatrix.java
@@ -5,20 +5,10 @@ import java.util.ArrayList;
 public class SpiralTraversalOfMatrix {
     public static void main(String[] args) {
         int [][] matrix = {
-                {1,2,3,4},
-                {5,6,7,8},
-                {9,10,11,12},
-                {13,14,15,16}
+                {1,2,3,4}
         };
         int n=matrix.length;
-        if(n==1)
-        {
-            ArrayList<Integer> arrayList =new ArrayList<>();
-            for (int i=0; i<matrix[0].length; i++){
-                arrayList.add(matrix[0][i]);
-            }
-            return arrayList;
-        }
+
         int top=0,bottom=n-1;
         int left=0 ,right=matrix[0].length-1;
         ArrayList<Integer> arrayList =new ArrayList<>();
