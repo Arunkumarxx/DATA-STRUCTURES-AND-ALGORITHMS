fcfb226e29f859ffdbb77eca73f8ce70794e4fb8
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-06 12:42:42 +0530

    modified

--- MATRIX/RotateMatrixAntiClockwiseBy90.java
+++ MATRIX/RotateMatrixAntiClockwiseBy90.java
@@ -1,12 +1,17 @@
 package MATRIX;
 
+import java.util.Random;
+
 public class RotateMatrixAntiClockwiseBy90 {
     public static void main(String[] args) {
-        int [][] matrix = {
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
-        };
+        int [][] matrix= new int[8][8];
+        for (int i=0; i<matrix.length; i++)
+        {
+            for (int j=0; j< matrix[i].length; j++) {
+                Random random =new Random();
+                matrix[i][j] =random.nextInt();
+            }
+        }
         int n=matrix.length;
         int m=matrix[0].length;
         for (int i=0;  i<n; i++)
