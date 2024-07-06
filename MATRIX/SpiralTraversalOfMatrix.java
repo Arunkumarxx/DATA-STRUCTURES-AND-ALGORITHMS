1a327d8dafc6b0ef7e956097707ce6762ae3b10f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-06 15:16:42 +0530

    modified

--- MATRIX/SpiralTraversalOfMatrix.java
+++ MATRIX/SpiralTraversalOfMatrix.java
@@ -2,6 +2,12 @@ package MATRIX;
 
 public class SpiralTraversalOfMatrix {
     public static void main(String[] args) {
+        int [][] matrix = {
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}
+        };
         
     }
 }
