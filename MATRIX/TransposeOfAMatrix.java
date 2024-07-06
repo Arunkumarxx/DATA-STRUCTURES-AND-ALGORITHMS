d8889aebb3acc0e026a0322c3c59d57c30e9504e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-06 10:43:23 +0530

    modified

--- MATRIX/TransposeOfAMatrix.java
+++ MATRIX/TransposeOfAMatrix.java
@@ -2,6 +2,11 @@ package MATRIX;
 
 public class TransposeOfAMatrix {
     public static void main(String[] args) {
+        int [][] matrix = {{1, 1, 1, 1},
+                           {2, 2, 2, 2},
+                           {3, 3, 3, 3},
+                           {4, 4, 4, 4}};
         
+
     }
 }
