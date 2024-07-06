af7b073bcec43618903fa429ddde7184533e7388
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-06 16:53:37 +0530

    modified

--- MATRIX/SearchInRowWiseAndColumnWiseSortedMatrix.java
+++ MATRIX/SearchInRowWiseAndColumnWiseSortedMatrix.java
@@ -2,6 +2,12 @@ package MATRIX;
 
 public class SearchInRowWiseAndColumnWiseSortedMatrix {
     public static void main(String[] args) {
-        int [] [] matrix ={{}};
+        int [] [] matrix = {
+                {10, 20, 30, 40},
+                {15, 25, 35, 45},
+                {27, 29, 37, 48},
+                {32, 33, 39, 50}
+        };
+        
     }
 }
