5f9e07e928b4ba66a47f5375a96dcfafb4fba7de
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-06 16:56:32 +0530

    modified

--- MATRIX/SearchInRowWiseAndColumnWiseSortedMatrix.java
+++ MATRIX/SearchInRowWiseAndColumnWiseSortedMatrix.java
@@ -1,6 +1,15 @@
 package MATRIX;
 
 public class SearchInRowWiseAndColumnWiseSortedMatrix {
+    static int[] SearchElementBruteForce(int [][] matrix,int x) {
+        int n=matrix.length;
+        for (int i=0; i<n; i++){
+            for (int j=0; j<matrix[0].length; j++)
+            {
+                if(matrix[i][j]==x) return new int []{i,j};
+            }
+        }
+    }
     public static void main(String[] args) {
         int [] [] matrix = {
                 {10, 20, 30, 40},
@@ -8,6 +17,7 @@ public class SearchInRowWiseAndColumnWiseSortedMatrix {
                 {27, 29, 37, 48},
                 {32, 33, 39, 50}
         };
-        
+      int [] res=  SearchElementBruteForce(matrix,29);
+        System.out.println(res);
     }
 }
