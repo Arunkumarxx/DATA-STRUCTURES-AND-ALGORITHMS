b191678b896a57cc79e1df3b77984c47811759f0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-07 18:01:27 +0530

    modified

--- MATRIX/ReversingTheColumnsOfAMatrix.java
+++ MATRIX/ReversingTheColumnsOfAMatrix.java
@@ -2,6 +2,12 @@ package MATRIX;
 
 public class ReversingTheColumnsOfAMatrix {
     public static void main(String[] args) {
+        int [][] matrix = {
+                { 1,  2,  3},
+                { 5,  6,  7},
+                {11, 10,  9},
+                {13, 14, 15}
+        };
         
     }
 }
