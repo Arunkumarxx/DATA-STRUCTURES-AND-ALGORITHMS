b99b971c027fab7c71be24aacc4408263579f788
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-07 18:58:20 +0530

    modified

--- MATRIX/BooleanMatrix.java
+++ MATRIX/BooleanMatrix.java
@@ -2,6 +2,12 @@ package MATRIX;
 
 public class BooleanMatrix {
     public static void main(String[] args) {
+        int matrix[][] = {
+                { 1, 0, 0},
+                { 1, 0, 0},
+                { 1, 0, 0},
+                { 0, 0, 0}
+        };
         
     }
 }
