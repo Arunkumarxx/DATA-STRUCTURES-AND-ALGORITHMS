5132a47386a89048b63648853f658e62eb355f44
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-07 18:05:14 +0530

    modified

--- MATRIX/ReversingTheColumnsOfAMatrix.java
+++ MATRIX/ReversingTheColumnsOfAMatrix.java
@@ -8,6 +8,27 @@ public class ReversingTheColumnsOfAMatrix {
                 {11, 10,  9},
                 {13, 14, 15}
         };
-        
+        int i=0;
+        int n=matrix.length;
+        while(i<n)
+        {
+            int start=0;
+            int end= matrix[i].length-1;
+            while(start<=end)
+            {
+                int temp=matrix[i][start];
+                matrix[i][start]=matrix[i][end];
+                matrix[i][end]=temp;
+                ++start;
+                --end;
+            }
+            ++i;
+        }
+        for (int [] temp:matrix) {
+            for (int ele : temp)
+                System.out.print(ele);
+            System.out.println();
+        }
     }
+
 }
