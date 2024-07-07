fe71846b017831abd02a2f7d6a703b23fccceab5
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-07 17:53:48 +0530

    modified

--- MATRIX/ExchangeMatrixColumns.java
+++ MATRIX/ExchangeMatrixColumns.java
@@ -4,10 +4,8 @@ public class ExchangeMatrixColumns {
 
     public static void main(String[] args) {
        int  matrix[][] = {
-            {1, 2, 3, 4},
-            {5, 6, 7, 8},
-            {9, 10, 11, 12},
-            {13, 14, 15,16}
+                       {4, 3, 2},
+                       {1, 5, 6}
        };
        int n= matrix.length;
        int m1= matrix[0].length-1;
