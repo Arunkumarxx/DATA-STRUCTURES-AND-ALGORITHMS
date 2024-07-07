fee841713dae41b8be153539902835b46a199afa
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-07 19:01:39 +0530

    modified

--- MATRIX/BooleanMatrix.java
+++ MATRIX/BooleanMatrix.java
@@ -6,8 +6,9 @@ public class BooleanMatrix {
                 { 1, 0, 0},
                 { 1, 0, 0},
                 { 1, 0, 0},
-                { 0, 0, 0}
-        };
-        
+                { 0, 0, 0},
+                { 0, 1, 0}
+        };while ()
+        for (int i=0; i<n)
     }
 }
