5f52f336f720cc718122785d1fd461bc0d642214
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-07 17:30:38 +0530

    modified

--- MATRIX/SumOfUpperAndLowerTriangles.java
+++ MATRIX/SumOfUpperAndLowerTriangles.java
@@ -16,11 +16,11 @@ public class SumOfUpperAndLowerTriangles {
             for (int j=0;j< matrix[i].length; j++)
             {
                     if(i<=j) {
-                        System.out.print(matrix[i][j]+"+ ");
+                        System.out.print(matrix[i][j]+" + ");
                         sumOne += matrix[i][j];
                     }
                     if(i>=j) {
-                        System.out.print(matrix[i][j]+"+ ");
+                        System.out.print(matrix[i][j]+" + ");
                         sumTwo += matrix[i][j];
                     }
             }
