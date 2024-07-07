3c012ddb37453644f28b33ff4ca46a8377009ce0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-07 11:18:39 +0530

    modified

--- MATRIX/MedianOfARowWiseSortedMatrixJava.java
+++ MATRIX/MedianOfARowWiseSortedMatrixJava.java
@@ -1,38 +1,39 @@
 package MATRIX;
 
 public class MedianOfARowWiseSortedMatrixJava {
-    static int MedianOfRow(int [][] matrix) {
-        int n= matrix.length;
-        int m=matrix[0].length;
-        int median=(n*m)/2;
-        for (int i=0; i<n; i++)
-        {
+    static int MedianOfRow(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int totalElements = n * m;
+        int medianIndex = totalElements / 2;
 
-            for (int j=0; j<m; j++)
-            {
-                int count=0;
-                int ele=matrix[i][j];
-                for (int k=0; k<n; k++)
-                {
-                    for (int s=0; s<matrix[k].length; s++)
-                    {
-                        if(matrix[i][j]<ele)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int ele = matrix[i][j];
+                int count = 0;
+
+                for (int k = 0; k < n; k++) {
+                    for (int s = 0; s < m; s++) {
+                        if (matrix[k][s] < ele) {
                             count++;
+                        }
                     }
-
                 }
-                if(count==median)
+
+                if (count == medianIndex) {
                     return ele;
+                }
             }
         }
-        return -1;
+        return -1; // This case should not occur if the matrix is non-empty and properly sorted.
     }
+
     public static void main(String[] args) {
-        int [][] matrix ={
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
         };
-        System.out.println(MedianOfRow(matrix));
+        System.out.println(MedianOfRow(matrix)); // Expected output: 5
     }
 }
