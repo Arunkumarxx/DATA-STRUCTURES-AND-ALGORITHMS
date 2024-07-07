22d7eeff24ae461f321c49c916ddd0116e87fe28
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-07 13:59:48 +0530

    modified

--- MATRIX/MedianOfARowWiseSortedMatrixJava.java
+++ MATRIX/MedianOfARowWiseSortedMatrixJava.java
@@ -4,7 +4,7 @@ public class MedianOfARowWiseSortedMatrixJava {
     static int MedianOfRowBruteForce(int [][] matrix) {
         int n= matrix.length;
         int m=matrix[0].length;
-        int median=(n*m+1)/2;
+        int median=(n*m)/2;
 
         for (int i=0; i<n; i++)
         {
