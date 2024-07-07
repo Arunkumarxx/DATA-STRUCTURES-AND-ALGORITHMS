0796f84738ab0301697f22b3bdf3802490113179
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-07 17:31:55 +0530

    modified

--- MATRIX/SumOfUpperAndLowerTriangles.java
+++ MATRIX/SumOfUpperAndLowerTriangles.java
@@ -25,7 +25,10 @@ public class SumOfUpperAndLowerTriangles {
                     }
             }
         }
-        System.out.println(sumOne+" "+sumTwo+" "+(sumOne+sumTwo));
+        System.out.println();
+        System.out.println("SumOne"+sumOne);
+        System.out.println("SumTwo"+sumTwo);
+        System.out.println("Total"+sumOne+sumTwo);
     }
 
 }
