2098927a908414658317513ef6af69d677ea439b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-07 15:57:12 +0530

    modified

--- MATRIX/MedianOfARowWiseSortedMatrixJava.java
+++ MATRIX/MedianOfARowWiseSortedMatrixJava.java
@@ -57,22 +57,14 @@ public class MedianOfARowWiseSortedMatrixJava {
             int count=0;
             for (int i=0;i<n; i++)
             {
-                count=
+                count=countSmallestNumber(matrix[i][])
             }
         }
 
         return -1;
     }
     static int countSmallestNumber(int [][] matrix,int mid) {
-        int count=0;
-        for (int i=0; i<matrix.length; i++)
-        {
-            for (int j=0; j<matrix[0].length; j++) {
-                if (matrix[i][j] <= mid) count++;
-                else break;
-            }
-        }
-        return count;
+
     }
     public static void main(String[] args) {
         int [][] matrix ={
