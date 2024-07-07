a800c4286dd8b64e35f4a0fede953a9eceabb6e3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-07 16:22:02 +0530

    modified

--- MATRIX/MedianOfARowWiseSortedMatrixJava.java
+++ MATRIX/MedianOfARowWiseSortedMatrixJava.java
@@ -73,8 +73,11 @@ public class MedianOfARowWiseSortedMatrixJava {
         while(low<=high)
         {
             int mid=(low+high)/2;
-            
+            if(arr[mid]<=target)
+                low=mid+1;
+            else high=mid-1;
         }
+        return low;
 
     }
     public static void main(String[] args) {
