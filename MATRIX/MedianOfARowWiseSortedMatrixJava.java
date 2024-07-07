a26d9fb5624abef15104d27b44b2afb201634ff0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-07 10:55:46 +0530

    modified

--- MATRIX/MedianOfARowWiseSortedMatrixJava.java
+++ MATRIX/MedianOfARowWiseSortedMatrixJava.java
@@ -12,9 +12,21 @@ public class MedianOfARowWiseSortedMatrixJava {
         int median=matrix.length/2;
         for (int i=0; i<n; i++)
         {
+            int count=0;
             for (int j=m; j>=0; j--)
             {
+                int ele=matrix[i][j];
+                for (int k=0; k<n; k++)
+                {
+                    for (int s=0; s<matrix[k].length; s++)
+                    {
+                        if(count>n/2)
+                            break;
+                        if(matrix[i][j]<ele)
+                            count++;
 
+                    }
+                }
             }
         }
     }
