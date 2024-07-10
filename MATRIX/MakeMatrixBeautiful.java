07cc8203a4a2ca5c0950de37e5178506cf78fe14
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-10 05:55:14 +0530

    modified

--- MATRIX/MakeMatrixBeautiful.java
+++ MATRIX/MakeMatrixBeautiful.java
@@ -1,4 +1,19 @@
 package MATRIX;
 
 public class MakeMatrixBeautiful {
+    public static void main(String[] args) {
+        int matrix[][] = {
+                {1, 2},
+                {3, 4}
+        };
+        int max=matrix[0][0];
+        int n= matrix.length;
+        for (int i=0; i<n; i++)
+        {
+            for (int j=0; j<matrix[0].length; j++)
+            {
+
+            }
+        }
+    }
 }
