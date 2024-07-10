b2184fe6e555fa614e3951b14a3ceae2973d3d91
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-10 06:13:03 +0530

    modified

---
+++ MATRIX/MakeMatrixBeautiful.java
@@ -0,0 +1,28 @@
+package MATRIX;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MakeMatrixBeautiful {
+    public static void main(String[] args) {
+        int matrix[][] = {
+                {1, 2},
+                {3, 4}
+        };
+        int n= matrix.length;
+        int [] row=new int[n];
+        int [] cols = new int[matrix[0].length];
+        // row max value
+        for (int i=0; i<n; i++)
+        {
+            for (int j=0; j<matrix[0].length; j++)
+            {
+                row[i]+=matrix[i][j];
+            }
+        }
+        int i=0;
+        int j=0;
+        
+        System.out.println(Arrays.toString(cols));
+    }
+}
