553513d8cdf050c7f92fcc03354142294a0406c5
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-12 08:10:18 +0530

    modified

--- MATRIX/MakeMatrixBeautiful.java
+++ MATRIX/MakeMatrixBeautiful.java
@@ -6,12 +6,22 @@ import java.util.Arrays;
 public class MakeMatrixBeautiful {
     public static void main(String[] args) {
         int matrix[][] = {
-                {1, 2},
-                {3, 4}
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
         };
         int n= matrix.length;
-        int [] rowSum =new int[matrix[0].length];
+        int [] rowSum =new int[n];
         int [] colSum = new int[n];
-        
+         for (int i=0; i<n; i++)
+         {
+             for (int j=0; j<matrix[0].length; j++)
+             {
+                 rowSum[i]+=matrix[i][j];
+                 colSum[j]+=matrix[i][j];
+             }
+         }
+        System.out.println(Arrays.toString(rowSum));
+        System.out.println(Arrays.toString(colSum));
     }
 }
