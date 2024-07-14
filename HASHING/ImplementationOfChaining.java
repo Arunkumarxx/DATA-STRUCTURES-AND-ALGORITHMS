d633d81ec5bf1293ff1a553c22ae8a3ccde2aae1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-14 15:56:13 +0530

    modified

--- HASHING/ImplementationOfChaining.java
+++ HASHING/ImplementationOfChaining.java
@@ -1,9 +1,11 @@
 package HASHING;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class ImplementationOfChaining {
     public static void main(String[] args) {
-        ArrayList<Integer,List<>>
+        ArrayList<LinkedList<Integer>> HashTable = new ArrayList<>(7);
+        System.out.println(HashTable);
     }
 }
