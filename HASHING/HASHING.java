88d600417ddf1604703620f0edc69513e6957023
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-14 14:42:12 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -4,6 +4,7 @@ public class HASHING {
     public static void main(String[] args){
         int [] key= {50,21,58,17,15,48,56,22,23,25};
         int n= key.length;
+        System.out.println(n);
         for (int i=0; i<n; i++)
         {
             System.out.print((key[i] % n)+" ");
