5406ea36c0c54f7ea8bd9c6185853e2b1a14fab3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-14 12:04:17 +0530

    modified

--- HASHING/LinearProbingInHashing.java
+++ HASHING/LinearProbingInHashing.java
@@ -1,7 +1,13 @@
 package HASHING;
 
+import java.util.Arrays;
+
 public class LinearProbingInHashing {
     public static void main(String[] args) {
-        
+        int [] arr ={4,14,24,44};
+        int [] hashTable = new int[10];
+        for (int i=0; i<hashTable.length;i++)
+            --hashTable[i];
+        System.out.println(Arrays.toString(hashTable));
     }
 }
