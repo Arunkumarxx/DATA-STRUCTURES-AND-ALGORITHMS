dd934211b88097d27ebdd94f40111554eeadb819
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-14 15:26:48 +0530

    modified

--- HASHING/SeparateChainHashing.java
+++ HASHING/SeparateChainHashing.java
@@ -4,8 +4,12 @@ import java.util.LinkedList;
 
 public class SeparateChainHashing {
     public static void main(String[] args){
+        int [] arr ={50,21,58,17,15,48,56,22,23,25};
+
         int n=10;
-           LinkedList<Integer>[]  HashTable;
+
+        LinkedList<Integer>[]  HashTable ;
+
 
     }
 }
