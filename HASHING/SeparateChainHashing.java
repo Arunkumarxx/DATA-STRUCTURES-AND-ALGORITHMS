a34ba873523e6fdea1ee50a15af0e4a4cf6e2923
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-14 15:23:15 +0530

    modified

--- HASHING/SeparateChainHashing.java
+++ HASHING/SeparateChainHashing.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 public class SeparateChainHashing {
     public static void main(String[] args){
         int n=10;
-        int [] LinkedList<Integer> hash =new LinkedList<Integer>();
+           LinkedList<Integer>[]  HashTable;
+           
     }
 }
