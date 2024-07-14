13ff5e98c921a8b38d6d777145bdfd7f57c37b16
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-14 10:46:08 +0530

    modified

--- HASHING/DirectAddressTable.java
+++ HASHING/DirectAddressTable.java
@@ -4,11 +4,10 @@ public class DirectAddressTable {
     public static void main(String[] args) {
         int max=999;
         boolean [] values = new boolean[999];
-        // search
+        // search O()
         // insert
         // delete
         int element=23;
-        values[23]=true;
         if(values[element]==true)
             System.out.println("Yes Element Found");
         else System.out.println("Not Found");
