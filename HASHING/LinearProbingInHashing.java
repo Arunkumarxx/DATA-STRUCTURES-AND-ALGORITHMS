c7e57d9ee52f49dfc1ce48211739c63fc66820ce
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-14 13:48:54 +0530

    modified

--- HASHING/LinearProbingInHashing.java
+++ HASHING/LinearProbingInHashing.java
@@ -4,36 +4,30 @@ import java.util.Arrays;
 
 public class LinearProbingInHashing {
     public static void main(String[] args) {
-        int[] arr = {4, 14, 24, 44};
-        int n = arr.length;
-        int[] hashTable = new int[10];
-
-        // Initialize the hash table with -1
-        Arrays.fill(hashTable, -1);
-
-        for (int i = 0; i < n; i++) {
-            int ind = arr[i] % 10;
-            if (hashTable[ind] == arr[i]) {
-                continue;
-            }
-            if (hashTable[ind] == -1) {
-                hashTable[ind] = arr[i];
-            } else {
-                findPosAndInsert(arr[i], hashTable, ind);
-            }
+        int [] arr ={4,14,24,44};
+        int n= arr.length;
+        int [] hashTable = new int[10];
+        for (int i=0; i<hashTable.length;++i)
+            --hashTable[i];
+        for (int i=0; i<n; i++)
+        {
+            int ind=(arr[i]%hashTable.length);
+           if(hashTable[ind]==arr[i])
+               continue;
+           if(hashTable[ind]==-1)
+               hashTable[ind]=arr[i];
+           else findPosAndInsert(hashTable,arr[i],ind);
         }
-
         System.out.println(Arrays.toString(hashTable));
     }
-
-    static void findPosAndInsert(int value, int[] hashTable, int ind) {
-        int index = ind;
-        while (hashTable[index] != -1) {
-            index = (index + 1) % hashTable.length;
-            if (hashTable[index] == value) {
+    static void findPosAndInsert(int [] hastTable,int value,int ind) {
+        int index=ind;
+        while(hastTable[index]!=-1)
+        {
+            index+=(index+1)% hastTable.length;
+            if(hastTable[index]==value)
                 return;
-            }
         }
-        hashTable[index] = value;
+        hastTable[index]=value;
     }
 }
