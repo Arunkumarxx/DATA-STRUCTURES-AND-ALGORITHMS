7ca68e97c37477d8d6a664b2a52e9c1118607ffc
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-14 10:50:44 +0530

    modified

--- HASHING/DirectAddressTable.java
+++ HASHING/DirectAddressTable.java
@@ -1,16 +1,26 @@
 package HASHING;
 
 public class DirectAddressTable {
+    static void search(boolean [] arr,int key) {
+        if(key < arr.length && arr[key]==true )
+            System.out.println("Yes Element Found");
+        else System.out.println("Not Found");
+    }
+    static void insert(boolean [] arr,int key) {
+        if(key <arr.length && arr[key]==true)
+            System.out.println("Replaced New Value");
+        else System.out.println("Inserted Element "+key);
+    }
+    static void delete ( boolean [] arr ,int key) {
+        if(key < arr.length)
+            System.out.println();
+    }
     public static void main(String[] args) {
         int max=999;
         boolean [] values = new boolean[999];
-        // search O(1)
-        // insert O(1)
+         search(values,23);  // search O(1)
+            // insert O(1)
         // delete O(1)
-        int element=23;
-        if(values[element]==true)
-            System.out.println("Yes Element Found");
-        else System.out.println("Not Found");
-        
+
     }
 }
