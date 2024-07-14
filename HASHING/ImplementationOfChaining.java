da1e364a1f824213660a39bfdbb41c1ff09df959
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-14 16:04:39 +0530

    modified

--- HASHING/ImplementationOfChaining.java
+++ HASHING/ImplementationOfChaining.java
@@ -2,13 +2,20 @@ package HASHING;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class ImplementationOfChaining {
     public static void main(String[] args) {
         ArrayList<LinkedList<Integer>> HashTable = new ArrayList<>();
+        int key=7;
+        int [] insertValue= {10,20,15,7};
         int n=HashTable.size();
-        for (int i=0; i<n; ++i)
-            HashTable.add(new LinkedList<>());
         
+
+
+    }
+    static void insert(int [] insertValue , ArrayList<LinkedList<Integer>> HashTable,int key) {
+        for (int i=0; i< insertValue.length; ++i)
+            HashTable.get(i%key).add(insertValue[i]);
     }
 }
