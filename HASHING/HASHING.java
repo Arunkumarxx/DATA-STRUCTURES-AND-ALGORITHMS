e18ea06e29df354a162b895b4e9a319405bb8e35
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-14 11:17:01 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -2,6 +2,6 @@ package HASHING;
 
 public class HASHING {
     public static void main(String[] args) {
-        System.out.println("a2".hashCode());
+        System.out.println("2".hashCode());
     }
 }
