c30a37a2618044ce39b4f7d5754dd0824506fd5c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-16 03:46:12 +0530

    modified

--- HASHING/QuadraticProbing.java
+++ HASHING/QuadraticProbing.java
@@ -2,6 +2,7 @@ package HASHING;
 
 public class QuadraticProbing {
     public static void main(String[] args) {
+        QuadraticProbing Quadratic = new QuadraticProbing();
         int [] arr = {21,10,32,43};
         short  hashSize=11;
         int [] hashTable  =new int[hashSize];
@@ -12,8 +13,14 @@ public class QuadraticProbing {
 
         for (int i=0; i<n; ++i)
         {
-            
+            if(hashTable[i]==arr[i])
+                continue;
+            else if(hashTable[i]==-1)
+                hashTable[i]=arr[i];
+            else Quadratic.QuadraticFindAndInsert();
         }
-
+    }
+    void QuadraticFindAndInsert() {
+        
     }
 }
