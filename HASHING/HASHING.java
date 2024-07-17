5b1d748ac4b1b2de1016c493ab86e85723150c04
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-17 20:58:32 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -15,8 +15,8 @@ public class HASHING {
         System.out.println(res);
         System.out.println();
         int startOne=0;
-        int endOne=(n-1)/2;
-        int startTwo=(n+1)/2;
+        int endOne=(n)/2;
+        int startTwo=(n)/2;
         int endTwo=n-1;
         boolean isEndOneIsOdd=endOne%2==0?false:true;
         boolean isstartTwoIsOdd=endOne%2==0?false:true;
