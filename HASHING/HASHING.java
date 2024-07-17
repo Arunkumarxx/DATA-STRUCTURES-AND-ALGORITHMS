bfee34e43e1d99686fa684960dc5a0f343788191
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-17 18:17:45 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -1,9 +1,13 @@
 package HASHING;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class HASHING {
     public static void main(String[] args) {
-
+        int [] arr = new int[100];
+        for (int i=0; i< arr.length;++i)
+            arr[i]+=i+1;
+        System.out.println(Arrays.toString(arr));
     }
 }
