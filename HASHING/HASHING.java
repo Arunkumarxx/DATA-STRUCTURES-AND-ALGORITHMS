d3ae31ff6521031dba8f10403c8b09fb2cbf508b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-17 19:46:27 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -5,32 +5,6 @@ import java.util.HashMap;
 
 public class HASHING {
     public static void main(String[] args) {
-        int[] arr = new int[9];
-        int n = arr.length;
-        int res=0;
-        int count=1;
-        for (int i = 0; i < arr.length; ++i)
-            arr[i]=count;
 
-        for (int i = 0; i < n; i++)
-            res += arr[i];
-        System.out.println(res);
-        int  startOne = (0);
-        int  endOne =(((n)/ 2));
-        int  startTwo =((n)/ 2)+1;
-        int  endTwo = ((n)-1);
-        int sum = 0;
-        System.out.println();
-
-        int itr=0;
-        while (startOne < endOne && startTwo <endTwo) {
-            sum += (arr[++startOne] + arr[--endOne] + arr[++startTwo] + arr[--endTwo]);
-        }
-        while(startOne<endOne)
-                sum+=arr[++startOne]+arr[--endOne];
-
-        while(startTwo<endTwo)
-                sum+=arr[++startTwo]+arr[--endTwo];
-        System.out.println(sum);
     }
 }
