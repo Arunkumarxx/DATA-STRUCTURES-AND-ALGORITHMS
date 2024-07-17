7acd312474e8b3a9f3bc942059a43072b84dd18c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-17 16:32:42 +0530

    modified

--- HASHING/CheckEqualArrays.java
+++ HASHING/CheckEqualArrays.java
@@ -1,2 +1,26 @@
-package HASHING;public class CheckEqualArrays {
+package HASHING;
+
+import java.util.HashMap;
+
+public class CheckEqualArrays {
+
+    public static void main(String[] args) {
+        int [] arr1 = {1,2,5,4,0};
+        int [] arr2 = {2,4,5,0,1};
+        int n1= arr1.length;
+        int n2= arr1.length;
+        HashMap<Integer,Integer> Freq =new HashMap<>();
+        int i=0, j=0;
+        while(i<n1 && j<n2)
+        {
+            Freq.put(arr1[i],Freq.get(i)+1);
+            Freq.put(arr2[i],Freq.get(i)+1);
+            i++;
+            j++;
+        }
+        while(i<n1)
+            ;
+        while(j<n2)
+            ;
+    }
 }
