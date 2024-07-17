4623b0edddba6b90a648e41dc13f8cd61766a48e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-17 16:47:03 +0530

    modified

--- HASHING/CheckEqualArrays.java
+++ HASHING/CheckEqualArrays.java
@@ -23,8 +23,7 @@ public class CheckEqualArrays {
             Freq.put(arr1[i],Freq.get(arr1[i])==null?1:Freq.get(arr1[i])+1);
         while (j<n2)
             Freq.put(arr2[j],Freq.get(arr2[j])==null?1:Freq.get(arr2[j])+1);
-
-        System.out.println(Freq);
+        
         Iterator iterator = Freq.values().iterator();
         while(iterator.hasNext()) {
             if((Integer)2!=iterator.next())
