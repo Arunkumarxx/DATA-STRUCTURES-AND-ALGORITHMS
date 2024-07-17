5f28c270b54789c6eceb44798786808a963aea67
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-17 18:27:36 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -14,7 +14,6 @@ public class HASHING {
         for (int i=0; i<n; i++)
             res+=arr[i];
         System.out.println(res);
-        System.out.println(res);
         int startOne=((n/100)*0)-1;
         int endOne=   ((n/100)*49)+1;
         int startTwo =((n/100)*50)-1;
