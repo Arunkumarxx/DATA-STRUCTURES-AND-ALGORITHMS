3a4d9c87d0aea1dcb6fb9fff41636edf128eb2f1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-17 16:37:00 +0530

    modified

--- HASHING/CheckEqualArrays.java
+++ HASHING/CheckEqualArrays.java
@@ -23,6 +23,7 @@ public class CheckEqualArrays {
         while (j<n2)
             Freq.put(arr2[i],Freq.get(i)==null?1:Freq.get(i)+1);
 
+        System.out.println(Freq);
         for (int k=0; k<Freq.size(); k++)
         {
             if(Freq.get(i)!=2) {
@@ -30,6 +31,7 @@ public class CheckEqualArrays {
                 break;
             }
         }
+        System.out.println("True");
 
     }
 }
