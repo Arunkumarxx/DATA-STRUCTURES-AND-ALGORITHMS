1a93a71a9d9d36552b30a87ac4b4f788fa181826
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-17 19:41:55 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -18,10 +18,10 @@ public class HASHING {
         int  endOne =((n / 2))-1;
         int  startTwo =(n / 2);
         int  endTwo = ((n)-1);
-//        System.out.println(startOne);
-//        System.out.println(endOne);
-//        System.out.println(startTwo);
-//        System.out.println(endTwo);
+        System.out.println(startOne);
+        System.out.println(endOne);
+        System.out.println(startTwo);
+        System.out.println(endTwo);
         int sum = 0;
         System.out.println();
 
