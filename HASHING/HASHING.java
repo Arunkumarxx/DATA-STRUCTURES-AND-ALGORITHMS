0342a190672d4d171259d7b8269b0cbacf756659
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-17 21:19:56 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class HASHING {
     public static void main(String[] args) {
-        int[] arr = new int[7];
+        int[] arr = new int[8];
         int n = arr.length;
         int res=0;
         int count=1;
