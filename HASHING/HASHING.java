fa7b09b14067996a729d7215b413736da7bbcce3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-17 21:13:15 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class HASHING {
     public static void main(String[] args) {
-        int[] arr = new int[12];
+        int[] arr = new int[13];
         int n = arr.length;
         int res=0;
         int count=1;
@@ -15,33 +15,24 @@ public class HASHING {
         for (int i = 0; i < n; i++)
             res += arr[i];
         System.out.println(res);
-        System.out.println(Arrays.toString(arr));
+        int sum=0;
         System.out.println();
         int startOne=0;
-        int endOne=(n)/2;
-        int startTwo= (int) Math.ceil((n+1)/2d);
-        int endTwo=n-1;
-        boolean isEndOneIsOdd=endOne%2==0?false:true;
-        boolean isstartTwoIsOdd=endOne%2==0?false:true;
+        int endOne=(n-1)/2;
+        int startTwo=(n)/2;
+        int endTwo=n%2==0?n-1:n-2;
+        if(n%2==1)
+            sum+=arr[n-1];
         System.out.println(startOne);
         System.out.println(endOne);
         System.out.println(startTwo);
         System.out.println(endTwo);
-        int sum=0;
+
         int itr=0;
         while(startOne<endOne &&  startTwo<endTwo) {
             sum = sum+ arr[++startOne] + arr[--endOne] + arr[++startTwo] + arr[--endTwo];
             itr++;
         }
-        while(startOne<endOne)
-            sum+=arr[++startOne]+arr[--endOne];
-        while(startTwo<endTwo)
-            sum+=arr[++startTwo]+arr[--endTwo];
-
-        if(isstartTwoIsOdd)
-            sum+=arr[startTwo];
-        if(isEndOneIsOdd)
-            sum+=arr[startOne];
 
         System.out.println(sum);
 
