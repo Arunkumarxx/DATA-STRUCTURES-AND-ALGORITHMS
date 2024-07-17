2504c78093e7033939f1a09cf146c02665ca5938
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-17 20:35:09 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -28,15 +28,10 @@ public class HASHING {
         System.out.println(endTwo);
         int sum=0;
         while(startOne<endOne &&  startTwo<endTwo) {
-            sum = sum+ arr[startOne] + arr[endOne] + arr[startTwo] + arr[endTwo];
-            ++startOne;
-            ++startTwo;
-            --endOne;
-            --endTwo;
+            sum = sum+ arr[++startOne] + arr[--endOne] + arr[++startTwo] + arr[--endTwo];
         }
-        if(startOne==endOne && startTwo==endTwo) {
+        if(startOne==endOne && startTwo==endTwo)
             sum += arr[startOne] + arr[startTwo];
-        }
         System.out.println(sum);
 
     }
