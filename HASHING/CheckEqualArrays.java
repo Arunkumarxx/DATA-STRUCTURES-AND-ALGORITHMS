6797c8fae12dc1ec4d32663dd80e1047ce63dbac
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-17 16:40:02 +0530

    modified

--- HASHING/CheckEqualArrays.java
+++ HASHING/CheckEqualArrays.java
@@ -13,20 +13,20 @@ public class CheckEqualArrays {
         int i=0, j=0;
         while(i<n1 && j<n2)
         {
-            Freq.put(arr1[i],Freq.get(i)==null?1:Freq.get(i)+1);
-            Freq.put(arr2[j],Freq.get(j)==null?1:Freq.get(j)+1);
+            Freq.put(arr1[i],Freq.get(arr1[i])==null?1:Freq.get(arr1[i])+1);
+            Freq.put(arr2[j],Freq.get(arr2[j])==null?1:Freq.get(arr2[j])+1);
             i++;
             j++;
         }
         while(i<n1)
-            Freq.put(arr1[i],Freq.get(i)==null?1:Freq.get(i)+1);
+            Freq.put(arr1[i],Freq.get(arr1[i])==null?1:Freq.get(arr1[i])+1);
         while (j<n2)
-            Freq.put(arr2[j],Freq.get(j)==null?1:Freq.get(j)+1);
+            Freq.put(arr2[j],Freq.get(arr2[j])==null?1:Freq.get(arr2[j])+1);
 
         System.out.println(Freq);
         for (int k=0; k<Freq.size(); k++)
         {
-            if(Freq.get(i)!=2) {
+            if(Freq.get(k)!=2) {
                 System.out.println("False");
                 break;
             }
