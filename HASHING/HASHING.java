7186181dae03a713ae549a0e3670a2623a012000
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-17 19:38:36 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -1,41 +1,37 @@
 package HASHING;
 
-import java.util.Arrays;
-import java.util.HashMap;
-
 public class HASHING {
     public static void main(String[] args) {
         int[] arr = new int[5];
         int n = arr.length;
-        int res=0;
+        int res = 0;
+
         for (int i = 0; i < arr.length; ++i)
-            arr[i]=1;
+            arr[i] = 1;
 
         for (int i = 0; i < n; i++)
             res += arr[i];
-        System.out.println(res);
-        int  startOne = (0);
-        int  endOne =((n / 2))-1;
-        int  startTwo =(n / 2);
-        int  endTwo = ((n)-1);
-//        System.out.println(startOne);
-//        System.out.println(endOne);
-//        System.out.println(startTwo);
-//        System.out.println(endTwo);
-        int sum = 0;
-        System.out.println();
+        System.out.println("Total Sum: " + res);
 
-        int itr=0;
+        int startOne = 0;
+        int endOne = (n / 2) - 1;
+        int startTwo = n / 2;
+        int endTwo = n - 1;
+
+        int sum = 0;
+
         while (startOne <= endOne && startTwo <= endTwo) {
             sum += (arr[startOne++] + arr[endOne--] + arr[startTwo++] + arr[endTwo--]);
-            itr++;
         }
-        while(startOne<endOne)
-                sum+=arr[startOne++]+arr[endOne--];
 
-        while(startTwo<endTwo)
-                sum+=arr[startTwo++]+arr[endTwo--];
+        while (startOne <= endOne) {
+            sum += arr[startOne++] + arr[endOne--];
+        }
 
-        System.out.println(sum);
+        while (startTwo <= endTwo) {
+            sum += arr[startTwo++] + arr[endTwo--];
+        }
+
+        System.out.println("Segment Sum: " + sum);
     }
 }
