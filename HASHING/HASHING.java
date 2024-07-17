f3d668dad364fdcbc0d5bf5405168a711e0122ec
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-17 19:56:22 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -5,24 +5,6 @@ import java.util.HashMap;
 
 public class HASHING {
     public static void main(String[] args) {
-        int[] arr = new int[9];
-        int n = arr.length;
-        int res=0;
-        int count=1;
-        for (int i = 0; i < arr.length; ++i)
-            arr[i]=count;
 
-        for (int i = 0; i < n; i++)
-            res += arr[i];
-        System.out.println(res);
-
-        int left=0;
-        int leftBreak=((n-1)/100)*25;
-        int leftMid=leftBreak+1;
-        int leftMidBreak=((n-2)/100)*50;
-        int mid=((n-1)/100)*50;
-        int rightMid=((n-1)/100)*75;
-        int right=n-1;
-        while(left<leftBreak && leftMid<mid &&  )
     }
 }
