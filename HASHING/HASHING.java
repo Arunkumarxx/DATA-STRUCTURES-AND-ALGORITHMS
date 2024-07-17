df4b9fb59e3b66b5df9b5e0424c9fc7352a1d60b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-17 19:06:02 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -5,46 +5,28 @@ import java.util.HashMap;
 
 public class HASHING {
     public static void main(String[] args) {
-        int [] arr = new int[50];
-        int n= arr.length;
-        int res=0;
-        for (int i=0; i< arr.length;++i)
-             arr[i]+=i+1;
+        int[] arr = new int[50];
+        int n = arr.length;
+        int res = 0;
+        for (int i = 0; i < arr.length; ++i)
+            arr[i] += i + 1;
 
-        for (int i=0; i<n; i++)
-            res+=arr[i];
+        for (int i = 0; i < n; i++)
+            res += arr[i];
         System.out.println(res);
-        int startOne=(0);
-        int endOne=   ((n/2)-1);
-        int startTwo =((n/2));
-        int endTwo =((n-1));
-        System.out.println();
-        System.out.println(startOne);
-        System.out.println(endOne);
-        System.out.println(startTwo);
-        System.out.println(endTwo);
-        System.out.println();
-        int sum=0;
-        int NofItr=0;
-        while(startOne<endOne && startTwo<endTwo) {
+        int startOne = (0);
+        int endOne = ((n / 2) - 1);
+        int startTwo = ((n / 2));
+        int endTwo = ((n - 1));
+        int sum = 0;
+
+        while (startOne < endOne && startTwo < endTwo) {
             sum += (arr[startOne] + arr[endOne] + arr[startTwo] + arr[endTwo]);
-        ++startOne;
-        --endOne;
-        ++startTwo;
-        --endTwo;
+            ++startOne;
+            --endOne;
+            ++startTwo;
+            --endTwo;
 
         }
-//        while(startOne<endOne && startTwo<endTwo) {
-//            System.out.println(arr[startOne] +" "+ arr[endOne] +" "+ arr[startTwo] +" "+ arr[endTwo]);
-//
-//        }
-        System.out.println(sum);
-//        System.out.println(NofItr);
     }
 }
-/*
-0
-49
-50
-99
- */
