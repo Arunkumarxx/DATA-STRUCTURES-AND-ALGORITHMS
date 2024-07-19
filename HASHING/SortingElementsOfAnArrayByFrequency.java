ef537f1330f80104fef290e964b6918d062f33ad
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-19 21:17:45 +0530

    modified

--- HASHING/SortingElementsOfAnArrayByFrequency.java
+++ HASHING/SortingElementsOfAnArrayByFrequency.java
@@ -9,7 +9,7 @@ public class SortingElementsOfAnArrayByFrequency {
         for (int x:arr)
             hashMap.put(x, hashMap.getOrDefault(x,0)+1);
         List<Map.Entry <Integer,Integer>> entryList = new ArrayList<>(hashMap.entrySet());
-        entryList.sort((a,b)->a.getValue().compareTo(b.getValue()));
+        entryList.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue()));
         System.out.println(entryList);
         System.out.println(hashMap);
     }
