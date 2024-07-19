89e641a898595c78db19a49541f1375d99fb26d6
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-19 14:58:17 +0530

    modified

--- HASHING/CountDistinctElements.java
+++ HASHING/CountDistinctElements.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class CountDistinctElements {
     public static void main(String[] args) {
-        int [] arr ={1,2,3,3,4,4,5A};
+        int [] arr ={1,2,3,3,4,4,5};
         HashSet<Integer> freq = new HashSet();
         int start=0;
         int end= arr.length-1;
