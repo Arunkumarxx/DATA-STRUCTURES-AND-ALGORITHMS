7ed869a1c911e31f1ad5680a59bf20e5ec907ba4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-19 12:55:19 +0530

    modified

--- HASHING/HashingForPairTwo.java
+++ HASHING/HashingForPairTwo.java
@@ -7,14 +7,15 @@ public class HashingForPairTwo {
         int[] arr1 =
     {73, 23, 39, 93, 39, 80, 91, 58, 59, 92, 16, 89, 57, 12, 3, 35, 73, 56, 29};
         int target=47;
+        int n= arr1.length;
         HashSet<Integer> hashSet = new HashSet<>();
-        for (int i=0; i< arr1.length; i++)
-        {
-            for (int j=0; j< arr1.length; j++)
-            {
-                if(arr1[i]+arr1[j]==target)
-                    System.out.println(arr1[i]+","+arr1[j]);
-            }
-        }
+       for (int i=0; i<n; ++i)
+       {
+           
+       }
     }
 }
+/*
+12,35
+35,12
+ */
