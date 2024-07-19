05cb7768d61a78549bc4cbd355f35295286fdf5f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-19 11:55:26 +0530

    modified

--- HASHING/HashingForPairTwo.java
+++ HASHING/HashingForPairTwo.java
@@ -1,7 +1,11 @@
 package HASHING;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class HashingForPairTwo {
     public static void main(String[] args) {
+        int [] arr ={1,2,3,4,5,6,7,8,9,10};
         
     }
 }
