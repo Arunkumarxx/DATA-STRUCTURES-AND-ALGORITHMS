b7854e10e6eb31b8cfa7bae497303bfa4eb99fed
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-19 10:31:31 +0530

    modified

--- HASHING/HashingForPairOne.java
+++ HASHING/HashingForPairOne.java
@@ -7,12 +7,11 @@ public class HashingForPairOne {
     private int [] PaidCheckBruteForce(int [] arr,int n,int target){
         Arrays.sort(arr);
         int start=0 ,end=n-1;
-        int sum=0;
         while(start<end)
         {
-            if(sum==(arr[start]+arr[end]))
+            if(target==(arr[start]+arr[end]))
                 return new int []{ start , end };
-            if(sum<(arr[start]+arr[end]))
+            if(target<(arr[start]+arr[end]))
                 ++start;
             else  --end;
         }
@@ -24,7 +23,6 @@ public class HashingForPairOne {
         for (int i=0 ;i< arr.length; ++i)
             arr[i]=i+1;
         HashingForPairOne obj = new HashingForPairOne();
-        System.out.println(  Arrays.toString(obj.PaidCheckBruteForce(arr, arr.length,14)));
-//        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(obj.PaidCheckBruteForce(arr, arr.length,14)));
     }
 }
