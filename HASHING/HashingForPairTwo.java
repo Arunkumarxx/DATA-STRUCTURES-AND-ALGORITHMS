cddbf25dc076b6e478d419ca77cf79a1122c2538
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-19 12:52:06 +0530

    modified

--- HASHING/HashingForPairTwo.java
+++ HASHING/HashingForPairTwo.java
@@ -1,9 +1,13 @@
 package HASHING;
 
+import java.util.HashSet;
+
 public class HashingForPairTwo {
     public static void main(String[] args) {
         int[] arr1 =
     {73, 23, 39, 93, 39, 80, 91, 58, 59, 92, 16, 89, 57, 12, 3, 35, 73, 56, 29};
+        int target=47;
+        HashSet<Integer> hashSet = new HashSet<>();
         
     }
 }
