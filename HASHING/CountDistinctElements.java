66d9fa0c660aab34fa6376124b2ffbd6eae63b71
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-19 14:47:08 +0530

    modified

--- HASHING/CountDistinctElements.java
+++ HASHING/CountDistinctElements.java
@@ -10,6 +10,7 @@ public class CountDistinctElements {
         int end= arr.length-1;
         while(start<end)
         {
+            freq.put(arr[end],arr[end]);
             if(!(freq.getOrDefault(arr[start],0)>1))
                 freq.put(arr[start],arr[start]);
             if(!(freq.get(arr[end])>1))
