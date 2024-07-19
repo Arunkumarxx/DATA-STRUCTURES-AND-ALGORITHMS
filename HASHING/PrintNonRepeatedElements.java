8235b76cfd114516036188fe783cabfde0759ce5
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-19 20:36:39 +0530

    modified

--- HASHING/PrintNonRepeatedElements.java
+++ HASHING/PrintNonRepeatedElements.java
@@ -9,29 +9,12 @@ public class PrintNonRepeatedElements {
         int [] arr ={1,1,2,2,3,3,4,5,6,7};
         int n= arr.length;
         HashMap<Integer,Integer> hashMap = new HashMap<>();
-        int start=0;
-        int end=n-1;
-        while(start<end)
-        {
-            hashMap.put(arr[start],hashMap.getOrDefault(arr[start],0)+1);
-            hashMap.put(arr[end],hashMap.getOrDefault(arr[end],0)+1);
-            ++start;
-            --end;
-        }
-        if(start==end) hashMap.put(arr[start],hashMap.getOrDefault(arr[start],0)+1);
-
-        ArrayList<Integer> result= new ArrayList<>();
-        int i=0;
-        int j=n-1;
-        while(i<j)
-        {
-           result.add(hashMap.get(i));
-           result.add(hashMap.get(j));
-            ++i;
-            --j;
-        }
-        if(i==j) hashMap.put(arr[i],hashMap.getOrDefault(arr[i],0)+1);
-
-        System.out.println(result);
+        for (int i=0; i<n; ++i)
+            hashMap.put(i,arr[i]);
+        ArrayList<Integer> arrayList =new ArrayList<>();
+        for (int i=0; i<n; ++i)
+            if(hashMap.get(i)==1)
+                arrayList.add(hashMap.get(i));
+        System.out.println(arrayList);
     }
 }
