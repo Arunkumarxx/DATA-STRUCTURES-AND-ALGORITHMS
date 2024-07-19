8cd3a2e385c4f46c3c9b65b35d56dd84ad4e3f77
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-19 22:11:31 +0530

    modified

--- HASHING/FrequenciesOfArrayElements.java
+++ HASHING/FrequenciesOfArrayElements.java
@@ -1,7 +1,13 @@
 package HASHING;
 
+import java.util.HashMap;
+
 public class FrequenciesOfArrayElements {
     public static void main(String[] args) {
-        int [] arr ={}
+        int [] arr ={10, 20, 20, 10, 10, 20, 5, 20};
+        HashMap<Integer,Integer> hashMap =new HashMap<>();
+        for(int x:arr)
+            hashMap.put(x,hashMap.getOrDefault(x,0)+1);
+        System.out.println(hashMap);
     }
 }
