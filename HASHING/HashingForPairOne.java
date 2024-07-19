4afc23308adce6493e9c06453b6a14d301a1949b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-19 10:57:12 +0530

    modified

--- HASHING/HashingForPairOne.java
+++ HASHING/HashingForPairOne.java
@@ -1,33 +1,34 @@
 package HASHING;
 
 import java.util.Arrays;
-import java.util.Random;
 
 public class HashingForPairOne {
-    static int x=213;
+    static int x = 213;
+
     static void m() {
         System.out.println("Arun");
     }
-    private int [] PaidCheckBruteForce(int [] arr,int n,int target){
-        Arrays.sort(arr);
-        int start=0 ,end=n-1;
-        while(start<end)
-        {
-            if(target==(arr[start]+arr[end]))
-                return new int[]{start+1, end+1};
-            if((arr[start]+arr[end])<target)
-                ++start;
-            else  --end;
-        }
-        return new int [] {};
-    }
-    public static void main(String[] args) {
-        int [] arr = new int [10];
 
-        for (int i=0 ;i< arr.length; ++i)
-            arr[i]=i+1;
+    private int[] PaidCheckBruteForce(int[] arr, int n, int target) {
+        Arrays.sort(arr);
+        int start = 0, end = n - 1;
+        while (start < end) {
+            if (target == (arr[start] + arr[end]))
+                return new int[]{start + 1, end + 1};
+            if ((arr[start] + arr[end]) < target)
+                ++start;
+            else --end;
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+
+        for (int i = 0; i < arr.length; ++i)
+            arr[i] = i + 1;
         HashingForPairOne obj = new HashingForPairOne();
         System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(obj.PaidCheckBruteForce(arr, arr.length,14)));
+        System.out.println(Arrays.toString(obj.PaidCheckBruteForce(arr, arr.length, 14)));
     }
 }
