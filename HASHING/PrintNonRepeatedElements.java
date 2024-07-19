08ccf9867ca0513133c7811d0c88906b6048893a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-19 20:17:49 +0530

    modified

--- HASHING/PrintNonRepeatedElements.java
+++ HASHING/PrintNonRepeatedElements.java
@@ -5,8 +5,14 @@ import java.util.List;
 
 public class PrintNonRepeatedElements {
     public static void main(String[] args) {
-        int [] arr ={}
+        int [] arr ={1,1,2,2,3,3,4,5,6,7};
         List<List<Integer>> arrayList = new ArrayList<>();
-
+        int n= arr.length;
+        int start=0;
+        int end=n-1;
+        while(start<end)
+        {
+            arrayList.add(new ArrayList<>());
+        }
      }
 }
