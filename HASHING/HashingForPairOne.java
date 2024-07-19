3d22fac821d4a9198f23ee945a4825665ae8957c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-19 10:31:55 +0530

    modified

--- HASHING/HashingForPairOne.java
+++ HASHING/HashingForPairOne.java
@@ -23,6 +23,7 @@ public class HashingForPairOne {
         for (int i=0 ;i< arr.length; ++i)
             arr[i]=i+1;
         HashingForPairOne obj = new HashingForPairOne();
+        System.out.println(Arrays.toString(arr));
         System.out.println(Arrays.toString(obj.PaidCheckBruteForce(arr, arr.length,14)));
     }
 }
