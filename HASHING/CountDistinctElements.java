81f815b03d50243674f752680908d775dc23917e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-19 14:37:09 +0530

    modified

--- HASHING/CountDistinctElements.java
+++ HASHING/CountDistinctElements.java
@@ -1,2 +1,10 @@
-package HASHING;public class CountDistinctElements {
+package HASHING;
+
+import java.util.HashMap;
+
+public class CountDistinctElements {
+    public static void main(String[] args) {
+        int [] arr ={15,12,13,12,13,18};
+        HashMap< >
+    }
 }
