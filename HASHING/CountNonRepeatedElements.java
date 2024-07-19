4bd279e3f4a33b7808f81195ad0bff1f9fe4340d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-19 19:43:47 +0530

    modified

--- HASHING/CountNonRepeatedElements.java
+++ HASHING/CountNonRepeatedElements.java
@@ -1,6 +1,37 @@
 package HASHING;
 
+import java.util.HashMap;
+
 public class CountNonRepeatedElements {
+    static long countNonRepeated(int arr[], int n)
+    {
+        // add your code
+        HashMap<Integer,Integer> hashMap = new HashMap ();
+        int start=0;
+        int end=n-1;
+        while(start<end)
+        {
+            hashMap.put(arr[start],hashMap.getOrDefault(arr[start],0)+1);
+
+            hashMap.put(arr[end],hashMap.getOrDefault(arr[end],0)+1);
+
+            ++start;
+            --end;
+        }
+        int i=0;
+        int j=n-1;
+        int count=0;
+        while(i<j)
+        {
+            if(hashMap.get(arr[i])==1)
+                ++count;
+            if(hashMap.get(arr[j])==1)
+                ++count;
+            ++i;
+            --j;
+        }
+        return count;
+    }
     public static void main(String[] args) {
 
     }
