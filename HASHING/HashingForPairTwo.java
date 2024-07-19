759d6f5e438e6c6cb426b4e1e95543dfae3a9086
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-19 12:58:44 +0530

    modified

--- HASHING/HashingForPairTwo.java
+++ HASHING/HashingForPairTwo.java
@@ -4,14 +4,19 @@ import java.util.HashSet;
 
 public class HashingForPairTwo {
     public static void main(String[] args) {
-        int[] arr1 =
+        int[] arr =
     {73, 23, 39, 93, 39, 80, 91, 58, 59, 92, 16, 89, 57, 12, 3, 35, 73, 56, 29};
         int target=47;
-        int n= arr1.length;
+        int n= arr.length;
         HashSet<Integer> hashSet = new HashSet<>();
+        HashSet<Integer> visitedPaid = new HashSet<>();
        for (int i=0; i<n; ++i)
        {
-           
+           int pair= target-arr[i];
+           if(hashSet.contains(pair))
+           {
+               String Pairs=Math.min(arr[i],pair)+","+Math.max(arr[i],pair);
+           }
        }
     }
 }
