978661c0e891ad1cb162d32725cb2776be6a718b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-19 10:22:22 +0530

    modified

--- HASHING/HashingForPairOne.java
+++ HASHING/HashingForPairOne.java
@@ -1,7 +1,17 @@
 package HASHING;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class HashingForPairOne {
+    private void PaidCheck(){
+
+    }
     public static void main(String[] args) {
-        
+        int [] arr = new int [100];
+        Random random = new Random();
+        for (int x:arr)
+            x=random.nextInt(1,10);
+        System.out.println(Arrays.toString(arr));
     }
 }
