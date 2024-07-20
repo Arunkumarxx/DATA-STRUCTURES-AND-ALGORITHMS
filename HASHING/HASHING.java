b66e9d6f067cc92b5f6869a700b6e92f911ca188
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-20 12:57:25 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -1,8 +1,34 @@
 package HASHING;
 
+import ARRAYS.Arrays;
+
 public class HASHING {
     public static void main(String[] args) {
         String str= "practice";
+        int start=0;
+        int end=str.length()-1;
+        char [] chars  =new char[str.length()];
+        for (int i=0 ;i<str.length(); ++i)
+            chars[i]=str.charAt(i);
+        while(true)
+        {
+            char c1=chars[start];
+            while(start<end && (c1!='a' && c1!='e' && c1!='i' && c1!='o' && c1!='u'))
+                ++start;
+
+            char c2=chars[end];
+            while(start<end && (c2!='a' && c2!='e' && c2!='i' && c2!='o' && c2!='u'))
+                --end;
+
+            char temp=chars[start];
+            chars[start]=chars[end];
+            chars[end]=temp;
+
+            ++start;
+            --end;
+            if(start==end)
+                return;
+        }
         
     }
 }
