3c49a516786c330e55477f3f07b8bb9802511f8f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-20 14:12:24 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -4,6 +4,20 @@ import java.util.Arrays;
 
 public class HASHING {
     public static void main(String[] args) {
-        
+
+    }
+    String removeVowels(String S) {
+        // code here
+        String str="";
+        for(int i=0; i<s.length();++i)
+        {
+            if(!(isVowel(s.charAt(i))))
+                str+=s.charAt(i);
+        }
+        return str;
+    }
+
+    private static boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 }
