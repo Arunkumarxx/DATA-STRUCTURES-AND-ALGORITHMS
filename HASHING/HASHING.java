6bbb82e853cea65081067c592054ff7dc39e9d04
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-20 11:30:33 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -1,18 +1,7 @@
 package HASHING;
 
-
-import java.util.Arrays;
-
 public class HASHING {
     public static void main(String[] args) {
-        int n=-342;
-        long res=0;
-
-        while(n!=0)
-        {
-            res=(res*10)+(n%10);
-            n=n/10;
-        }
-        System.out.println(res);
+        
     }
 }
