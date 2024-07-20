b243cf436276e77aee92d5905adccd26584c6fcf
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-20 13:23:30 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -7,4 +7,19 @@ public class HASHING {
         System.out.println(97-65);
         System.out.println((int)'B');
     }
+    public String transform(String s)
+    {
+        // code here
+        String str="";
+        int n=s.length();
+        for(int i=1; i<n-1; ++i)
+        {
+            if(s.charAt(i-1)==' ' || s.charAt(i+1)==' ')
+            {
+                str+= (char)((s.charAt(i)+32));
+            }
+            str+=s.charAt(i);
+        }
+        return str;
+    }
 }
