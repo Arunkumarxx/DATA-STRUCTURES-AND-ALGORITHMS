282248bc5d7c99a048564d82491baba80363243e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-20 13:48:15 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -4,43 +4,6 @@ import java.util.Arrays;
 
 public class HASHING {
     public static void main(String[] args) {
-        System.out.println(97-65);
-        System.out.println((int)'B');
-        System.out.println( transform("welcome arun kumar"));
-        long [] arr ={23};
-        int n= arr.length;;
-        Arrays.sort(arr);
-        long x= (n&1)==0?arr[n/2]:arr[(n/2)+1];
-
-    }
-    public long leftElement(long arr[], long n)
-    {
-        // Your code goes here
-        Arrays.sort(arr);
-        if((n&1)==0)
-        {
-            return (long)arr[(int)n/2];
-        }
-        else return (long) arr[(int)(n/2)+1];
-
-    }
-    public static String transform(String s)
-    {
-        // code here
-        String str="";
-        int n=s.length();
-        String te=s.charAt(0)+"";
-        str+=te.toUpperCase();
-        for(int i=1; i<n; ++i)
-        {
-            if(s.charAt(i-1)==' ')
-            {
-                String temp=s.charAt(i)+"";
-                str+=temp.toUpperCase();
-            }
-           else  str+=s.charAt(i);
-        }
-
-        return str;
+        
     }
 }
