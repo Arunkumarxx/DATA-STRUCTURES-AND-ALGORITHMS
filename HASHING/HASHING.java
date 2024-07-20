216725b82025bca5fabb586553017c718ee23925
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-20 13:19:50 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -5,6 +5,6 @@ import ARRAYS.Arrays;
 public class HASHING {
     public static void main(String[] args) {
         System.out.println(97-65);
-        System.out.println((int)'b');
+        System.out.println((int)'B');
     }
 }
