a2997af5def9850c3b97a7815feaeb279920903c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-20 11:06:13 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class HASHING {
     public static void main(String[] args) {
-        int n=-1023;
+        int n=-123;
         long res=0;
 
         while(n!=0)
