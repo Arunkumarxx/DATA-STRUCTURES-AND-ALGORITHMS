09834e6cdb4e986061deaadfd33ee904ad525a06
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-20 11:36:15 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -2,6 +2,17 @@ package HASHING;
 
 public class HASHING {
     public static void main(String[] args) {
-        
+        int n=10;
+        String str= Integer.toString(n);
+        int start=0;
+        int end=str.length()-1;
+        while(start<end)
+        {
+            if(!(str.charAt(start)==str.charAt(end)))
+                return "No";
+            ++start;
+            --end;
+        }
+        return "Yes";
     }
 }
