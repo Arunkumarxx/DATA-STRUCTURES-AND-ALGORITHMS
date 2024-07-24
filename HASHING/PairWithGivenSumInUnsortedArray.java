1c48dba1dcfbb78ca7dfc32bef11756776954014
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-24 13:31:13 +0530

    modified

--- HASHING/PairWithGivenSumInUnsortedArray.java
+++ HASHING/PairWithGivenSumInUnsortedArray.java
@@ -1,6 +1,22 @@
 package HASHING;
+
+import java.util.HashSet;
+
 public class PairWithGivenSumInUnsortedArray {
     public static void main(String[] args) {
-        
+        int [] arr ={3,2,8,15,-8};
+        HashSet<Integer> set = new HashSet<>();
+        int n= arr.length;
+        int sum=17;
+        for (int i=0; i<n;++i)
+        {
+            if(set.contains(sum-arr[i]))
+            {
+                System.out.println(true);
+                break;
+            }
+            set.add(arr[i]);
+        }
+
     }
 }
