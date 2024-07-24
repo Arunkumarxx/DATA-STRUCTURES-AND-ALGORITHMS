6aaff18358de4820111e36d67113e65a384224b4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-24 14:30:11 +0530

    modified

--- HASHING/PositiveNegativePair.java
+++ HASHING/PositiveNegativePair.java
@@ -8,23 +8,11 @@ public class PositiveNegativePair {
     public static void main(String[] args) {
         int [] arr = {1,3,6,-2,-1,-3,2,7};
         int n= arr.length;
-        HashMap<Integer,Integer> hashMap =new HashMap<>();
-        for (int i=0; i<n; ++i)
-        {
-            if(arr[i]<0)
-            hashMap.put(Math.abs(arr[i]),hashMap.getOrDefault(Math.abs(arr[i]),0)-1);
-            if(arr[i]>0)
-            hashMap.put(Math.abs(arr[i]),hashMap.getOrDefault(Math.abs(arr[i]),0)+1);
-        }
-        ArrayList<Integer> res =new ArrayList<>();
-        for(int i=0; i<n; ++i)
-        {
-            if(arr[i]>0 && hashMap.get(arr[i])==0) {
-                res.add(-arr[i]);
-                res.add(arr[i]);
-            }
-        }
-        System.out.println(hashMap);
-        System.out.println(res);
+      HashSet<Integer> set =new HashSet<>();
+      ArrayList<Integer> res =new ArrayList<>();
+      for (int i=0; i<n; ++i)
+      {
+          
+      }
     }
 }
