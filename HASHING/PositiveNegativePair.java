465874f6850ce8d846b189b6cc3260199fc93745
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-24 14:13:10 +0530

    modified

--- HASHING/PositiveNegativePair.java
+++ HASHING/PositiveNegativePair.java
@@ -1,15 +1,16 @@
 package HASHING;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class PositiveNegativePair {
     public static void main(String[] args) {
         int [] arr = {1,3,6,-2,-1,-3,2,7};
         int n= arr.length;
-        HashSet<Integer> set =new HashSet<>();
+        HashMap<Integer,Integer> hashMap =new HashMap<>();
         for (int i=0; i<n; ++i)
         {
-
+            hashMap.put(Math.abs(arr[i]),hashMap.getOrDefault(arr[i],0)+1);
         }
     }
 }
