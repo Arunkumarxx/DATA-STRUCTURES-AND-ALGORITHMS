c4e2bbda3258bff282c7c9d7893952fee16515ac
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-24 14:43:46 +0530

    modified

--- HASHING/PositiveNegativePair.java
+++ HASHING/PositiveNegativePair.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 
 public class PositiveNegativePair {
     public static void main(String[] args) {
-        int [] arr = {1,3,6,-2,-1,-3,-2,7};
+        int [] arr = {1,3,6,-2,-1,-3,2,7};
         int n= arr.length;
       HashSet<Integer> set =new HashSet<>();
       ArrayList<Integer> res =new ArrayList<>();
@@ -29,5 +29,7 @@ public class PositiveNegativePair {
           set.add(arr[i]);
       }
         System.out.println(res);
+      int x=-2;
+        System.out.println(-x);
     }
 }
