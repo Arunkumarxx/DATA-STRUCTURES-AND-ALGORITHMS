e293b780026f55a060ed77b9db4ec6b01f3375ff
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-24 15:25:56 +0530

    modified

--- HASHING/UnionOfTwoArrays.java
+++ HASHING/UnionOfTwoArrays.java
@@ -9,27 +9,17 @@ public class UnionOfTwoArrays {
         int []arr2 = {1, 2, 3};
         int n1= arr1.length;
         int n2= arr2.length;
-        int i=0,j=0;
         int count=0;
-        HashMap<Integer,Integer> hashMap =new HashMap<>();
+        HashSet<Integer> hashSet =new HashSet<>();
+        int i=0;
+        int j=0;
         while(i<n1 && j<n2)
         {
-            hashMap.put(arr1[i],hashMap.getOrDefault(arr1[i],0)+1);
-            hashMap.put(arr2[j],hashMap.getOrDefault(arr2[j],0)-1);
+            hashSet.add(arr1[i]);
+            hashSet.add(arr2[j]);
             ++i;
             ++j;
         }
-        while(i<n1)
-        {
-            hashMap.put(arr1[i],hashMap.getOrDefault(arr1[i],0)+1);
-            ++i;
-        }
-        while(j<n2)
-        {
-            hashMap.put(arr2[j],hashMap.getOrDefault(arr2[j],0)-1);
-            ++j;
-        }
-        System.out.println(hashMap);
-
+        
     }
 }
