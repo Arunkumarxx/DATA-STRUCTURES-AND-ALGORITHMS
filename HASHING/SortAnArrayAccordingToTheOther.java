45123ebb8b16ce818e9b2a384b4464fbe72e2017
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-24 11:04:05 +0530

    modified

--- HASHING/SortAnArrayAccordingToTheOther.java
+++ HASHING/SortAnArrayAccordingToTheOther.java
@@ -7,20 +7,21 @@ public class SortAnArrayAccordingToTheOther {
     public static void main(String[] args) {
         int [] arr1 = {2, 1, 2, 5, 7, 1, 9, 3, 6, 8, 8};
         int [] arr2 = {2,1,8,3};
-        int n1= arr1.length;
+        int n1= arr1.length;;
         int n2= arr2.length;
+        //Your code here
         HashMap<Integer,Integer> hashMap =new HashMap<>();
         for (int i=0; i<n2; ++i)
-        hashMap.put(arr2[i],0);
+            hashMap.put(arr2[i],0);
         int i=0,j=n1-1;
-
+        int [] res = new int [n1];
         while(i<j )
         {
             if(hashMap.containsKey(arr1[i])) // left pointer
-               {
+            {
                 hashMap.put(arr1[i],hashMap.getOrDefault(arr1[i],0)+1);
                 arr1[i]=Integer.MIN_VALUE;
-                }
+            }
 
             if(hashMap.containsKey(arr1[j])) // right pointer
             {
@@ -40,12 +41,14 @@ public class SortAnArrayAccordingToTheOther {
         for (int k = 0; k < n2; ++k) {
             int count = hashMap.getOrDefault(arr2[k], 0);
             while (count > 0) {
-                System.out.print(arr2[k] + " ");
+                res[h]+=arr2[k];
+                ++h;
                 hashMap.put(arr2[k], --count);
             }
         }
         for (int m=0; m<n1; ++m)
             if(arr1[m]>Integer.MIN_VALUE)
-            System.out.print(arr1[m]+" ");
+                res[h++]+arr1[m];
+        return res;
     }
 }
