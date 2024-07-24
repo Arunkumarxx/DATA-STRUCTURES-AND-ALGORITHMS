6453fc913aa3af1b4d7beebd8adfffd43f41c755
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-24 15:02:57 +0530

    modified

--- HASHING/UnionOfTwoArrays.java
+++ HASHING/UnionOfTwoArrays.java
@@ -1,4 +1,9 @@
 package HASHING;
 
 public class UnionOfTwoArrays {
+    public static void main(String[] args) {
+        int [] arr1 = {1, 2, 3, 4, 5};
+        int []arr2 = {1, 2, 3};
+        
+    }
 }
