0e74338fae1b4b55527b9be66371c1a80e4194b7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-24 11:54:11 +0530

    modified

--- HASHING/IntersectionOfTwoArrays.java
+++ HASHING/IntersectionOfTwoArrays.java
@@ -2,7 +2,8 @@ package HASHING;
 
 public class IntersectionOfTwoArrays {
     public static void main(String[] args) {
-        int [] arr =
+        int [] arr1 ={89, 24, 75, 11, 23};
+        int [] arr2 = {89}
     }
     private static int NbrOfIntersection(int a[], int b[], int n, int m) {
         return -1;
