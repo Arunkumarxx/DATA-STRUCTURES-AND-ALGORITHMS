310cb4f2209f41926d13f36984bb32e878bac54a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-24 11:18:19 +0530

    modified

--- HASHING/FirstRepeatingElement.java
+++ HASHING/FirstRepeatingElement.java
@@ -1,12 +1,21 @@
 package HASHING;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class FirstRepeatingElement {
     public static void main(String[] args) {
         int [] arr ={1,5,3,4,3,5,6};
         int n= arr.length;
-        
+        HashMap<Integer,Integer> hashMap =new HashMap<>();
+        int i=0,j=n-1;
+        while(i<j) {
+            hashMap.put(arr[i], hashMap.getOrDefault(arr[i],0)+1);
+            hashMap.put(arr[j], hashMap.getOrDefault(arr[j],0)+1);
+            ++i;
+            --j;
+        }
+        System.out.println(hashMap);
     }
 }
