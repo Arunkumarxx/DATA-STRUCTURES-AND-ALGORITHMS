62ccc83a9ec3fd11f42a0ff7b3306337f8e67858
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-24 10:30:41 +0530

    modified

--- HASHING/SortAnArrayAccordingToTheOther.java
+++ HASHING/SortAnArrayAccordingToTheOther.java
@@ -1,7 +1,12 @@
 package HASHING;
 
+import java.util.HashMap;
+
 public class SortAnArrayAccordingToTheOther {
     public static void main(String[] args) {
+        int [] arr1 = {2, 1, 2, 5, 7, 1, 9, 3, 6, 8, 8};
+        int [] arr2 = {2,1,8,3};
+        HashMap<Integer,Integer> hashMap =new HashMap<>();
         
     }
 }
