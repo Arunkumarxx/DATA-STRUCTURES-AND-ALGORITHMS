c4984134dd4ad9ff56a27be07ce7beb800bc3645
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-24 15:04:28 +0530

    modified

--- HASHING/UnionOfTwoArrays.java
+++ HASHING/UnionOfTwoArrays.java
@@ -1,9 +1,16 @@
 package HASHING;
 
+import java.util.HashSet;
+
 public class UnionOfTwoArrays {
     public static void main(String[] args) {
         int [] arr1 = {1, 2, 3, 4, 5};
         int []arr2 = {1, 2, 3};
-        
+        int n= arr1.length;
+        HashSet<Integer> set =new HashSet<>();
+        for (int i=0; i<n;++i)
+        {
+            
+        }
     }
 }
