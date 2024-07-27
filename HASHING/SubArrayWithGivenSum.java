a4e14bb82ae3eac3781596a858e7b7e63ff6fda1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-27 17:39:33 +0530

    modified

--- HASHING/SubArrayWithGivenSum.java
+++ HASHING/SubArrayWithGivenSum.java
@@ -1,2 +1,22 @@
-package HASHING;public class SubArrayWithGivenSum {
+package HASHING;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SubArrayWithGivenSum {
+    public static void main(String[] args) {
+        int [] arr ={15,2,8,10,-5,-8,6};
+        ArrayList<Integer> list  =new ArrayList<>();
+        for (int i=0; i< arr.length; ++i)
+        {
+            int sum=0;
+            for (int j=i; j< arr.length; ++j)
+            {
+                sum+=arr[j];
+            }
+            list.add(sum);
+        }
+        System.out.println(list);
+    }
+
 }
