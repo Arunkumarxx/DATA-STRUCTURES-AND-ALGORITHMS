f4954630464108c6a25d3bf954472580320958d0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-27 18:23:16 +0530

    modified

--- HASHING/SubArrayWithGivenSum.java
+++ HASHING/SubArrayWithGivenSum.java
@@ -7,23 +7,16 @@ public class SubArrayWithGivenSum {
         int i=0 ,j=0;
         int target=33;
         int sum=0;
-        while(i<=j && j<n)
+        while(j<n)
         {
-            if(sum==target)
-            {
-                System.out.println(arr[i]+" "+arr[j]);
-                break;
-            }
-            if(sum<target)
-            {
-                sum+=arr[j];
-                ++j;
-            }
-            else
-            {
-                sum-=arr[i];
-                ++i;
-            }
+           while(sum>target)
+               sum-=arr[i++];
+           if(sum==target)
+           {
+               System.out.println(i+" "+j);
+               return;
+           }
+           j++;
         }
     }
 
