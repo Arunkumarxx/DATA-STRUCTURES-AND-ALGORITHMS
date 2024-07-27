29be3e4e6a2681356c5de92af5e7072ea8b0c581
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-27 16:02:21 +0530

    modified

--- HASHING/ZeroSumSubArrays.java
+++ HASHING/ZeroSumSubArrays.java
@@ -2,8 +2,7 @@ package HASHING;
 
 public class ZeroSumSubArrays {
     public static void main(String[] args) {
-        int [] arr =
-                
+        int [] arr ={6,-1,-3,4,-2,2,4,6,-12,-7};
     }
     private  int findSubArr () {
         int count=0;
