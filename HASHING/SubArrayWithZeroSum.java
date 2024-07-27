548a03fcbabf9be431a58c34ca3e81dcb732d501
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-27 15:35:37 +0530

    modified

--- HASHING/SubArrayWithZeroSum.java
+++ HASHING/SubArrayWithZeroSum.java
@@ -1,7 +1,15 @@
 package HASHING;
 
+import java.util.HashSet;
+
 public class SubArrayWithZeroSum {
     public static void main(String[] args) {
-        int [] arr ={1,4}
+        int [] arr ={56,2,2,6,-10};
+        HashSet<Integer> hashSet =new HashSet<>();
+        int n= arr.length;
+        for (int i=0; i<n-1; ++i)
+        {
+            int sum=
+        }
     }
 }
