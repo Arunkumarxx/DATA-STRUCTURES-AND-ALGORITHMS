d3beb1b69bed0d8c9ab7f9f4a00b838a35e15a91
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-27 15:26:49 +0530

    modified

--- HASHING/SubArrayWithZeroSum.java
+++ HASHING/SubArrayWithZeroSum.java
@@ -1,2 +1,7 @@
-package HASHING;public class SubArrayWithZeroSum {
+package HASHING;
+
+public class SubArrayWithZeroSum {
+    public static void main(String[] args) {
+        int [] arr ={1,4}
+    }
 }
