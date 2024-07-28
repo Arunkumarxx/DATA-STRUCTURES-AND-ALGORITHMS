015342a414564cf001c9b0b464d36a51693d0942
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-28 13:22:56 +0530

    modified

--- HASHING/LongestSubarrayWithGivenSum.java
+++ HASHING/LongestSubarrayWithGivenSum.java
@@ -57,10 +57,10 @@ public class LongestSubarrayWithGivenSum {
             sum+=arr[i];
             if(sum==target)
                 res=i+1;
-            
+
             hashMap.put(sum,i+1);
-            if(hashMap.containsKey(target-sum) && (i+1)-hashMap.get(target-sum)>res)
-                res=(i+1)-hashMap.get(target-sum);
+            if(hashMap.containsKey(sum-target) )
+                res=Math.max(res,i+1-hashMap.get(sum-target));
             hashMap.put(hashMap.getOrDefault(sum,0),+1);
         }
         return res;
