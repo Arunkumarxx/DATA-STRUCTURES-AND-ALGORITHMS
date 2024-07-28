80f9f0405fd942848a18e900902236b933b52575
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-28 15:56:58 +0530

    modified

--- HASHING/SubarraysWithEqual1sAnd0s.java
+++ HASHING/SubarraysWithEqual1sAnd0s.java
@@ -7,22 +7,19 @@ public class SubarraysWithEqual1sAnd0s {
     public static void main(String[] args) {
         int [] arr ={1,0,0,1,0,1,1};
         int n= arr.length;
-        int maxLen=0;
+        int count=0;
         HashMap<Integer,Integer> hashMap =new HashMap<>();
-
         int sum=0;
         hashMap.put(0,-1);
         for (int i=0; i<n;++i)
         {
             sum=arr[i]==0?sum-1:sum+1;
-            if(hashMap.containsKey(sum)) {
-                maxLen = Math.max(maxLen, i - hashMap.get(sum));
-
-            }
+            if(hashMap.containsKey(sum))
+                ++count;
             if(!hashMap.containsKey(sum))
                 hashMap.put(sum,i);
-        }
-//        System.out.println(maxLen);
+            }
+        System.out.println(count);
     }
 
 }
