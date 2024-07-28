fda3d43d9d8328367dc72168213be95c27288e26
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-28 16:12:41 +0530

    modified

--- HASHING/SubarraysWithEqual1sAnd0s.java
+++ HASHING/SubarraysWithEqual1sAnd0s.java
@@ -16,10 +16,42 @@ public class SubarraysWithEqual1sAnd0s {
             sum=arr[i]==0?sum-1:sum+1;
             if(hashMap.containsKey(sum))
                 count+=hashMap.get(sum);
-            
+
                 hashMap.put(sum,hashMap.getOrDefault(sum,0)+1);
             }
         System.out.println(count);
     }
 
 }
+/*
+Subarrays with equal 1s and 0s
+Difficulty: MediumAccuracy: 57.47%Submissions: 53K+Points: 4
+Given an array containing 0s and 1s. Find the number of subarrays having equal number of 0s and 1s.
+
+Example 1:
+
+Input:
+n = 7
+A[] = {1,0,0,1,0,1,1}
+Output: 8
+Explanation: The index range for the 8
+sub-arrays are: (0, 1), (2, 3), (0, 3), (3, 4),
+(4, 5) ,(2, 5), (0, 5), (1, 6)
+Example 2:
+
+Input:
+n = 5
+A[] = {1,1,1,1,0}
+Output: 1
+Explanation: The index range for the
+subarray is (3,4).
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function countSubarrWithEqualZeroAndOne() which takes the array arr[] and the size of the array as inputs and returns the number of subarrays with equal number of 0s and 1s.
+
+Expected Time Complexity: O(n).
+Expected Auxiliary Space: O(n).
+
+Constraints:
+1 <= n <= 106
+0 <= A[i] <= 1
+ */
