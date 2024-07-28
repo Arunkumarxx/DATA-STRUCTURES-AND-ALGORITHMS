bbd4960637b918aa1fc05154475c29dbc37b08d6
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-28 12:41:16 +0530

    modified

--- HASHING/LongestSubarrayWithGivenSum.java
+++ HASHING/LongestSubarrayWithGivenSum.java
@@ -29,11 +29,15 @@ public class LongestSubarrayWithGivenSum {
         int res=0;
         for (int i=0; i<n; ++i)
         {
+            int sum=0;
+            int count=0;
             for (int j=i; j<n; ++j)
             {
-                System.out.print(arr[j]+" ");
+                sum=sum+arr[j];
+                ++count;
+                if(sum==target && count>res)
+                    res=count;
             }
-            System.out.println();
         }
         return res;
     }
