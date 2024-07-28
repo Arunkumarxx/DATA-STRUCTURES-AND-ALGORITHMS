85340503d6332ed1590fbdd76bdadaa99c7f921c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-28 15:42:47 +0530

    modified

--- HASHING/LargestSubarrayOf0sAnd1s.java
+++ HASHING/LargestSubarrayOf0sAnd1s.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class LargestSubarrayOf0sAnd1s {
 
     public static void main(String[] args) {
-        int [] arr ={1,0,0,1,0,1,1};
+        int [] arr ={0, 0, 1, 1, 0};
         System.out.println( optimal(arr, arr.length));
     }
     private static int optimal(int [] arr,int n) {
