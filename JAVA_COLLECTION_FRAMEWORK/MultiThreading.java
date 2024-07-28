7f1d3eaa62612181c09e812c0d62201459a7d7e4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-28 20:24:53 +0530

    modified

--- JAVA_COLLECTION_FRAMEWORK/MultiThreading.java
+++ JAVA_COLLECTION_FRAMEWORK/MultiThreading.java
@@ -1,29 +1,25 @@
 package JAVA_COLLECTION_FRAMEWORK;
 
+class MultiThreading extends Thread {
+    private final int[] arr;
+    private final char operation;
 
-  class multiThreading extends Thread {
-    int[] arr;
-    int n;
-    char c;
-
-    multiThreading(int[] arr, int n, char c) {
+    MultiThreading(int[] arr, char operation) {
         this.arr = arr;
-        this.n = n;
-        this.c = c;
+        this.operation = operation;
     }
 
     public void run() {
-        System.out.println("Result for " + c + ": " + method(arr, c) + " - " + Thread.currentThread().getName());
+        System.out.println("Result for " + operation + ": " + performOperation(arr, operation) + " - " + Thread.currentThread().getName());
     }
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
-        int n = arr.length;
 
-        multiThreading T1 = new multiThreading(arr, n, '+');
-        multiThreading T2 = new multiThreading(arr, n, '-');
-        multiThreading T3 = new multiThreading(arr, n, '/');
-        multiThreading T4 = new multiThreading(arr, n, '*');
+        MultiThreading T1 = new MultiThreading(arr, '+');
+        MultiThreading T2 = new MultiThreading(arr, '-');
+        MultiThreading T3 = new MultiThreading(arr, '/');
+        MultiThreading T4 = new MultiThreading(arr, '*');
 
         T1.start();  // Start thread T1
         T2.start();  // Start thread T2
@@ -31,30 +27,39 @@ package JAVA_COLLECTION_FRAMEWORK;
         T4.start();  // Start thread T4
     }
 
-    private int method(int[] arr, char job) {
-        int res = (job == '*' || job == '/') ? 1 : arr[0]; // Initialize based on operation
-        if (job == '+') {
-            for (int i = 0; i < arr.length; ++i) {
-                res += arr[i];
-            }
-        } else if (job == '-') {
-            for (int i = 1; i < arr.length; ++i) { // Start from the second element
-                res -= arr[i];
-            }
-        } else if (job == '/') {
-            for (int i = 1; i < arr.length; ++i) { // Start from the second element
-                if (arr[i] != 0) { // Prevent division by zero
-                    res /= arr[i];
-                } else {
-                    System.out.println("Division by zero encountered!");
-                    return 0; // Return 0 or handle it according to your logic
+    private int performOperation(int[] arr, char operation) {
+        int result = (operation == '*' || operation == '/') ? 1 : arr[0];
+
+        switch (operation) {
+            case '+':
+                for (int num : arr) {
+                    result += num;
                 }
-            }
-        } else if (job == '*') {
-            for (int i = 0; i < arr.length; ++i) {
-                res *= arr[i];
-            }
+                break;
+            case '-':
+                for (int i = 1; i < arr.length; i++) {
+                    result -= arr[i];
+                }
+                break;
+            case '/':
+                for (int i = 1; i < arr.length; i++) {
+                    if (arr[i] != 0) {
+                        result /= arr[i];
+                    } else {
+                        System.out.println("Division by zero encountered!");
+                        return 0; // Return 0 or handle it according to your logic
+                    }
+                }
+                break;
+            case '*':
+                for (int num : arr) {
+                    result *= num;
+                }
+                break;
+            default:
+                System.out.println("Invalid operation");
+                return 0;
         }
-        return res;
+        return result;
     }
 }
