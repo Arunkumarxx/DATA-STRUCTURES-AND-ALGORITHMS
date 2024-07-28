a0d4efa7497b500ffa09a8f3eede62e60dba92b2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-28 20:16:43 +0530

    modified

--- HASHING/LongestConsecutiveSubsequence.java
+++ HASHING/LongestConsecutiveSubsequence.java
@@ -1,10 +1,24 @@
 package HASHING;
 
-public class LongestConsecutiveSubsequence {
-
+public class LongestConsecutiveSubsequence extends Thread {
+    int [] arr;
+    int n;
+    char c;
+    LongestConsecutiveSubsequence(int [] arr,int n,char c) {
+        this.arr=arr;
+        this.n=n;
+        this.c=c;
+    }
+    public void run() {
+        System.out.println(method(arr,c));
+    }
     public static void main(String[] args) {
-        Thread thread = new Thread();
-        
+        int [] arr ={1,2,3,4,5};
+        int n= arr.length;
+        LongestConsecutiveSubsequence T1 =new LongestConsecutiveSubsequence(arr,n,'+');
+        LongestConsecutiveSubsequence T2  =new LongestConsecutiveSubsequence(arr,n,'-');
+        T1.run();
+        T2.run();
     }
     private int method(int [] arr,char job) {
         int res=0;
