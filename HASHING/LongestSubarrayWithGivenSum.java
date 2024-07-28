cb00ebf3b3c41e547017082fa0c8dc41d1ff5568
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-28 13:25:50 +0530

    modified

--- HASHING/LongestSubarrayWithGivenSum.java
+++ HASHING/LongestSubarrayWithGivenSum.java
@@ -1,8 +1,6 @@
 package HASHING;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 
 public class LongestSubarrayWithGivenSum {
     public static void main(String[] args) {
@@ -67,5 +65,37 @@ public class LongestSubarrayWithGivenSum {
     }
 }
 /*
+Longest Sub-Array with Sum K
+Difficulty: MediumAccuracy: 24.64%Submissions: 340K+Points: 4
+Given an array arr containing n integers and an integer k.
+Your task is to find the length of the longest Sub-Array with the
+sum of the elements equal to the given value k.
 
+
+Examples:
+
+
+Input :
+arr[] = {10, 5, 2, 7, 1, 9}, k = 15
+Output : 4
+Explanation:
+The sub-array is {5, 2, 7, 1}.
+
+Input :
+arr[] = {-1, 2, 3}, k = 6
+Output : 0
+Explanation:
+There is no such sub-array with sum 6.
+Expected Time Complexity: O(n).
+Expected Auxiliary Space: O(n).
+
+
+
+Constraints:
+1<=n<=105
+-105<=arr[i], K<=105
+
+
+Company Tags
+Amazon
  */
