843ebe5624b5b4e2a88f1d9c23aeac7cbed52fb2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-28 12:38:10 +0530

    modified

--- HASHING/LongestSubarrayWithGivenSum.java
+++ HASHING/LongestSubarrayWithGivenSum.java
@@ -4,22 +4,25 @@ public class LongestSubarrayWithGivenSum {
     public static void main(String[] args) {
         int [] arr ={5,8,-4,-4,9,-2,2};
         int k=0;
-        System.out.println(SubArrayWithGivenSumBruteForce(arr, arr.length,k));
+        System.out.println(BruteForce(arr, arr.length,k));
     }
-    private static int  SubArrayWithGivenSumBruteForce(int [] arr,int n,int target) {
+    private static int  BruteForce(int [] arr,int n,int target) {
+        int res=0;
         for (int i=0; i<n; ++i)
         {
-            int sum=0;
             for (int j=i; j<n; ++j)
             {
+                int sum=0;
+                int count=0;
                 for (int k=i; k<=j; k++)
                 {
-                    System.out.print(arr[k]+",");
+                    sum+=arr[k];
+                    ++count;
+                    if(sum==target && count>res)
+                        res=count;
                 }
-                System.out.println();
             }
-
         }
-        return 0;
+        return res;
     }
 }
