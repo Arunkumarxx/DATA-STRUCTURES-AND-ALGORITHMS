4c2479a472b2abdc427647293f636d581321f415
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-28 17:03:21 +0530

    modified

--- HASHING/LongestCommonSpanWithSameSumInBinaryArrays.java
+++ HASHING/LongestCommonSpanWithSameSumInBinaryArrays.java
@@ -1,7 +1,27 @@
 package HASHING;
 
+import java.util.HashMap;
+
 public class LongestCommonSpanWithSameSumInBinaryArrays {
     public static void main(String[] args) {
-        
+
+    }
+    private static int Optimal (int [] arr,int n,int target) {
+        int res=0;
+        HashMap<Integer,Integer> hashMap =new HashMap<>();
+        int sum=0;
+        hashMap.put(0,-1);
+        for (int i=0; i<n; ++i)
+        {
+            sum+=arr[i];
+            if(hashMap.containsKey(sum-target))
+            {
+                if(i-hashMap.get(sum-target)>res)
+                    res=i-hashMap.get(sum-target);
+            }
+            if(!hashMap.containsKey(sum))
+                hashMap.put(sum,i);
+        }
+        return res;
     }
 }
