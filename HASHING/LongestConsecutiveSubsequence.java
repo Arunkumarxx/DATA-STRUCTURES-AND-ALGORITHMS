28308608fd073d2b3835ac6b1d8c4f7ab35f9e79
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-28 20:06:40 +0530

    modified

--- HASHING/LongestConsecutiveSubsequence.java
+++ HASHING/LongestConsecutiveSubsequence.java
@@ -3,10 +3,23 @@ package HASHING;
 public class LongestConsecutiveSubsequence {
 
     public static void main(String[] args) {
-
+        
     }
+    private int method(int [] arr,char job) {
+        int res=0;
+        if(job=='+')
+            for(int i=0; i< arr.length ;++i)
+            res+=arr[i];
+       else  if(job=='-')
+            for(int i=0; i< arr.length ;++i)
+                res+=arr[i];
+       else if(job=='/')
+            for(int i=0; i< arr.length ;++i)
+                res/=arr[i];
+       else if(job=='*')
+            for(int i=0; i< arr.length ;++i)
+                res*=arr[i];
 
-    private void method() {
-
+            return res;
     }
 }
