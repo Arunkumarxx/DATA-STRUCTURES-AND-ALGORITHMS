4bd148b23594f51d7fe0426b510b68ba9deeb100
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-07-31 18:06:00 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -2,9 +2,6 @@ package HASHING;
 
 public class HASHING {
     public static void main(String[] args) {
-        while(true)
-        {
-            System.out.println("Arun Kumar");
-        }
+        
     }
 }
