a0106704b964bd675555cdedb64e55134ef720f4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-02 10:52:10 +0530

    modified

--- HASHING/LongestConsecutiveSubsequence.java
+++ HASHING/LongestConsecutiveSubsequence.java
@@ -51,7 +51,7 @@ public class LongestConsecutiveSubsequence {
         return longest;
     }
     public static void main(String[] args) {
-        int [] arr ={0,1,4,5};
+        int [] arr ={2,6,1,9,4,5,3};
         System.out.println(brute(arr, arr.length));
         System.out.println(optimal(arr, arr.length));
     }
