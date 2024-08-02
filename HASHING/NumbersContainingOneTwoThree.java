64b5e43638a55aadebf4a217ae590e4f35811e36
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-02 12:21:21 +0530

    modified

--- HASHING/NumbersContainingOneTwoThree.java
+++ HASHING/NumbersContainingOneTwoThree.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 public class NumbersContainingOneTwoThree {
     public static void main(String[] args) {
         int [] arr ={9, 12, 23, 45, 132};
-        
-        System.out.println(numbersContains123());
+        int n= arr.length;
+        System.out.println(numbersContains123(arr,n));
      }
-     private static ArrayList<Integer> numbersContains123() {
-
+     private static ArrayList<Integer> numbersContains123(int [] arr,int n) {
+         System.out.println(arr);
         return new ArrayList<>();
      }
 }
