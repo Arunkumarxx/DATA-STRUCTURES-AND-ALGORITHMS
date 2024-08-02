ca6ee5ffa46fd7a5069ddd83dcf3ae539558a821
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-02 18:41:33 +0530

    modified

--- HASHING/WinnerOfAnElection.java
+++ HASHING/WinnerOfAnElection.java
@@ -1,9 +1,18 @@
 package HASHING;
 
+import java.util.HashMap;
+
 public class WinnerOfAnElection {
     public static void main(String[] args) {
         String [] arr= {"lucas"," emma", "lucas", "emma", "emma", "lucas", "lucas"};
-
+        int n= arr.length;
+        System.out.println(Winner(arr,n));
     }
-    private static 
+    private static String  Winner(String [] arr,int n) {
+        HashMap<String,Integer> hashMap =new HashMap<>();
+        for(String x:arr)
+            hashMap.put(x, hashMap.getOrDefault(x,0)+1);
+        System.out.println(hashMap);
+        return "";
+    }
 }
