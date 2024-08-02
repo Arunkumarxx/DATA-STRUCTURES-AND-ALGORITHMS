40068ff99b1bd2ea9a2a27abc653732ef2fdccd9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-02 17:21:39 +0530

    modified

--- HASHING/CountDistinctElementsInEveryWindow.java
+++ HASHING/CountDistinctElementsInEveryWindow.java
@@ -1,6 +1,7 @@
 package HASHING;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CountDistinctElementsInEveryWindow {
     public static void main(String[] args) {
@@ -8,7 +9,12 @@ public class CountDistinctElementsInEveryWindow {
         System.out.println(count(arr, arr.length));
     }
     private static ArrayList<Integer> count(int [] arr,int n) {
-
+        HashMap<Integer,Integer> hashMap =new HashMap<>();
+        for (int x:arr)
+        {
+            hashMap.put(x,hashMap.getOrDefault(x,0)+1);
+        }
+        System.out.println(hashMap);
         return new ArrayList<>();
     }
 }
