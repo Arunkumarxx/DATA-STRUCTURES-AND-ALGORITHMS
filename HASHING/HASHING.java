247f13522c309d54eeadbb5622b19fb4c3652787
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-02 11:49:46 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -1,53 +1,7 @@
 package HASHING;
 
 public class HASHING {
-    private static long sumUsingSingleLoop(long[] arr) {
-        long sum = 0;
-        for(long i=0; i< arr.length; i+=2)
-        {
-            sum+=arr[(int)i];
-            sum+=arr[(int)i+1];
-        }
-        return sum;
-    }
-
-    // Method to sum elements using two-pointer approach
-    private static long sumUsingTwoPointer(long[] arr) {
-        long sum = 0;
-        int left = 0, right = arr.length - 1;
-
-        while (left <right) {
-          sum+=arr[left];
-          sum+=arr[right];
-          ++left;
-          --right;
-        }
-        if(left==right)
-            sum+=arr[left];
-        return sum;
-    }
-
     public static void main(String[] args) {
-        long[] arr = new long[Integer.MAX_VALUE/3];
-        // Populate arr with test data
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
-        }
 
-        // Measure time for single loop approach
-        long startTimeSingleLoop = System.currentTimeMillis();
-        long sumSingleLoop = sumUsingSingleLoop(arr);
-        long endTimeSingleLoop = System.currentTimeMillis();
-
-        // Measure time for two-pointer approach
-        long startTimeTwoPointer = System.currentTimeMillis();
-        long sumTwoPointer = sumUsingTwoPointer(arr);
-        long endTimeTwoPointer = System.currentTimeMillis();
-
-        // Print results
-        System.out.println("Sum using single loop: " + sumSingleLoop);
-        System.out.println("Time taken using single loop: " + (endTimeSingleLoop - startTimeSingleLoop) + " ms");
-        System.out.println("Sum using two-pointer: " + sumTwoPointer);
-        System.out.println("Time taken using two-pointer: " + (endTimeTwoPointer - startTimeTwoPointer) + " ms");
     }
 }
