66e4e4cfe680eaf3928c1828217c733bbc1fc6c1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-02 20:20:42 +0530

    modified

--- HASHING/MoreThanNbyKOccurences.java
+++ HASHING/MoreThanNbyKOccurences.java
@@ -2,6 +2,8 @@ package HASHING;
 
 public class MoreThanNbyKOccurences {
     public static void main(String[] args) {
-        int [] arr ={}
+        int [] arr ={30,10,20,20,10,20,30,30};
+
     }
+    private static 
 }
