6eeac91256736b5ee1ef14a4441f21e6d71bbd82
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-02 19:14:49 +0530

    modified

--- HASHING/WinnerOfAnElection.java
+++ HASHING/WinnerOfAnElection.java
@@ -10,31 +10,22 @@ public class WinnerOfAnElection {
     }
     private static String[]  Winner(String [] arr,int n) {
         HashMap<String,Integer> hashMap =new HashMap<>();
-        ArrayList<String> list =new ArrayList<>();
+        TreeSet<String> treeSet =new TreeSet<>();
         int max=0;
         for(int i=0; i<n; ++i) {
             hashMap.put(arr[i],hashMap.getOrDefault(arr[i],0)+1);
             if(hashMap.get(arr[i])>max) {
                 max = hashMap.get(arr[i]);
-                list.clear();
-                list.add(arr[i]);
+                treeSet.clear();
+                treeSet.add(arr[i]);
             }
             else if(hashMap.get(arr[i])==max)
             {
-                list.add(arr[i]);
+                treeSet.add(arr[i]);
             }
         }
         String [] res =new String[2];
-        if(list.size()==2)
-        {
-            char one=(list.get(0)).charAt(0);
-            char two=list.get(1).charAt(0);
-            res [0]=one>two?list.get(0):list.get(1);
-            res [1]=max+"";
-            return res;
-        }
-        res[0]=list.get(0);
-        res[1]=max+"";
+        System.out.println(treeSet);
         return res;
     }
 }
