f60c646448b70f2339af389e9b753d08f1edc7b2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-02 12:45:40 +0530

    modified

--- HASHING/NumbersContainingOneTwoThree.java
+++ HASHING/NumbersContainingOneTwoThree.java
@@ -19,22 +19,8 @@ public class NumbersContainingOneTwoThree {
     public List<Integer> filterByDigits(List<Integer> arr) {
         // code here
         ArrayList<Integer> list =new ArrayList<>();
-        for(int i=0; i<arr.size(); ++i)
-        {
-            int x=arr.get(i);
-            int sum1=0;
-            int sum2=0;
-            while(x>0)
-            {
-                sum1+=x%10;
-                if(x%10==1 || x%10==2 || x%10==3)
-                    sum2+=x%10;
-                x/=10;
-            }
-            if(sum1==sum2)
-                list.add(arr.get(i));
-        }
+        for
         return list;
-        
+
     }
 }
