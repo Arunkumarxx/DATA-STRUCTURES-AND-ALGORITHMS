816d4870e3c86e1f485f0cd480c65a205f074ee7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-02 11:08:51 +0530

    modified

--- HASHING/LongestConsecutiveSubsequence.java
+++ HASHING/LongestConsecutiveSubsequence.java
@@ -25,30 +25,11 @@ public class LongestConsecutiveSubsequence {
 
     private static int optimal (int [] arr,int n) {
         HashSet<Integer> hashSet =new HashSet<>();
-        int i=0,j=n-1;
-        while(i<j)
-        {
-            hashSet.add(arr[i]);
-            hashSet.add(arr[j]);
-            ++i;
-            --j;
-        }
-        if(i==j) hashSet.add(arr[i]);
-        int longest=1;
-        int count=1;
-        for (int k=0; k<n; ++k)
-        {
-            if(k!=n-1 && arr[k]==arr[k+1])
-                continue;
-            if(hashSet.contains(arr[k]+1))
-            {
-                ++count;
-                if(count>longest)
-                    longest=count;
-            }
-            else count=1;
-        }
-        return longest;
+
+        for(int x:arr) hashSet.add(x);
+
+        System.out.println(hashSet);
+        return -1;
     }
     public static void main(String[] args) {
         int [] arr ={1,9,3,10,4,20,2};
