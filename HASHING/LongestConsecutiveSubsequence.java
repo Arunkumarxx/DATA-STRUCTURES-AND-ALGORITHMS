687778ced15ab0dfd24a1dd5de0f54e0f2702afe
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-02 10:10:07 +0530

    modified

--- HASHING/LongestConsecutiveSubsequence.java
+++ HASHING/LongestConsecutiveSubsequence.java
@@ -4,11 +4,11 @@ import java.util.HashSet;
 
 public class LongestConsecutiveSubsequence {
     public static void main(String[] args) {
-        int [] arr ={2,6,1,2,4};
+        int [] arr ={2,6,1,9,4,5,3};
         HashSet <Integer> hashSet = new HashSet<>();
         for (int x:arr)
             hashSet.add(x);
         System.out.println(hashSet);
-        
+
     }
 }
