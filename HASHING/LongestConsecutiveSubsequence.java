5f894cee0aa39389b4c2405679c11d7ec78121fe
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-02 10:12:16 +0530

    modified

---
+++ HASHING/LongestConsecutiveSubsequence.java
@@ -0,0 +1,14 @@
+package HASHING;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class LongestConsecutiveSubsequence {
+    public static void main(String[] args) {
+        int [] arr ={1,5,3,10,4,20,2};
+        Arrays.sort(arr);
+        
+        System.out.println(arr);
+
+    }
+}
