8b0287a267fa121d1bec5486615963defe441016
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-02 11:33:28 +0530

    modified

--- HASHING/HASHING.java
+++ HASHING/HASHING.java
@@ -26,7 +26,7 @@ public class HASHING {
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[Integer.MAX_VALUE-Integer.MAX_VALUE/2];
+        int[] arr = new int[99999];
         // Populate arr with test data
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i;
