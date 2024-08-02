f89f8e14ae7219b5546912477139fefaff84ffcf
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-02 15:33:19 +0530

    modified

--- HASHING/CountDistinctElementsInEveryWindow.java
+++ HASHING/CountDistinctElementsInEveryWindow.java
@@ -1,5 +1,6 @@
 package HASHING;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CountDistinctElementsInEveryWindow {
@@ -7,13 +8,8 @@ public class CountDistinctElementsInEveryWindow {
         long s=System.currentTimeMillis();
         int [] arr ={1,2,1,3,4,2,3};
         Arrays.sort(arr);
-        Arrays.sort(arr);
-        Arrays.sort(arr);
-        Arrays.sort(arr);
-        Arrays.sort(arr);
-        Arrays.sort(arr);
-        Arrays.sort(arr);
-        Arrays.sort(arr);
+        ArrayList<Integer> integers =new ArrayList<>();
+        integers.add(23);
         long e=System.currentTimeMillis();
         System.out.println(s+"ms");
         System.out.println((e-s)+"ms");
