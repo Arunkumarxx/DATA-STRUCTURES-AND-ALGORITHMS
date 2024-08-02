efa17a2a475cf37b5b315a7f64bfd15edb1a82f0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-02 20:26:43 +0530

    modified

--- HASHING/MoreThanNbyKOccurences.java
+++ HASHING/MoreThanNbyKOccurences.java
@@ -5,13 +5,17 @@ import java.util.HashMap;
 public class MoreThanNbyKOccurences {
     public static void main(String[] args) {
         int [] arr ={30,10,20,20,10,20,30,30};
-        System.out.println(count(arr, arr.length,4));
+        System.out.println(count(arr, arr.length, 4));
     }
     private static int count(int [] arr,int n,int k) {
         HashMap<Integer,Integer> hashMap =new HashMap<>();
         for (int x:arr)
             hashMap.put(x,hashMap.getOrDefault(x,0)+1);
-        System.out.println(hashMap);
+        int count=0;
+        for(int x: hashMap.values())
+            if(hashMap.get(x)>n/k)
+                ++count;
+        System.out.println(count);
         return -1;
     }
 }
