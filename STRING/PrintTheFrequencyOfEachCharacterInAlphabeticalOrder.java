b767b692ef9b2d80a30cc186fb2213f9815a478d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-03 21:01:59 +0530

    modified

--- STRING/PrintTheFrequencyOfEachCharacterInAlphabeticalOrder.java
+++ STRING/PrintTheFrequencyOfEachCharacterInAlphabeticalOrder.java
@@ -4,14 +4,15 @@ import java.util.Arrays;
 
 class PrintTheFrequencyOfEachCharacterInAlphabeticalOrder {
     private static void PrintCharacterFreq(String str,int n) {
+
         int [] freq = new int[26];
+
         for (int i=0; i<n; ++i)
             ++freq[str.charAt(i)-'a'];
 
         for (int j=0; j<freq.length; ++j)
-        {
             System.out.println((char) (j+'a')+"->"+freq[j]);
-        }
+
     }
     public static void main(String[] args) {
         String str = "aabbccccddd";
