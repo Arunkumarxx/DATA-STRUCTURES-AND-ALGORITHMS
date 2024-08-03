8ef8b3b8a3e9aa34ad249d4681bca0a7b9a33f3f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-03 21:10:36 +0530

    modified

--- STRING/PrintTheFrequencyOfEachCharacterInAlphabeticalOrder.java
+++ STRING/PrintTheFrequencyOfEachCharacterInAlphabeticalOrder.java
@@ -18,7 +18,7 @@ class PrintTheFrequencyOfEachCharacterInAlphabeticalOrder {
         String str = "aabbccccddd";
         PrintCharacterFreq(str,str.length());
         String s1 ="arun";
-        String s2="arun";
+        String s2=s1;
         System.out.println(s1==s2);
     }
 }
