8b59542ebb73da19901fc0f58bbac52c39fc36ae
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-04 15:25:44 +0530

    modified

--- STRING/ImplementStrStr.java
+++ STRING/ImplementStrStr.java
@@ -6,9 +6,11 @@ public class ImplementStrStr {
         String key="For";
         System.out.println(Str(str,key));
     }
-    private static int Str(String str,String SearchKey) {
+    private static int Str(String str,String searchKey) {
        int ind=-1;
        int n=str.length();
+       char start=searchKey.charAt(0);
+       char end=searchKey.charAt(searchKey.length()-1);
        for(int i=0; i<n; ++i)
        {
            
