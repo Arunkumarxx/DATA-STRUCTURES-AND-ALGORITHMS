ed3daddd1164131e536af7feb6fccd6671ffa7d1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-04 15:58:54 +0530

    modified

--- STRING/ImplementStrStr.java
+++ STRING/ImplementStrStr.java
@@ -12,15 +12,20 @@ public class ImplementStrStr {
 
        char start=searchKey.charAt(0);
        char end=searchKey.charAt(searchKey.length()-1);
-
+       boolean smarked=true;
+       boolean emarked=true;
        int startInd=-1;
        int endInd=-1;
        for (int i=0; i<str.length(); ++i)
        {
-            if(str.charAt(i)==start)
-                startInd=i;
-            if(str.charAt(i)==end)
-                endInd=i;
+            if(str.charAt(i)==start && smarked) {
+                startInd = i;
+                smarked=false;
+            }
+            if(str.charAt(i)==end && emarked) {
+                endInd = i;
+                smarked=false;
+            }
        }
        if((endInd-startInd+1)!=searchKey.length() ||( startInd<0 && endInd<0))
            return -1;
