88b540ff7c3c74034a5e6e5597f2ba6b13b85bf6
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-04 15:35:37 +0530

    modified

--- STRING/ImplementStrStr.java
+++ STRING/ImplementStrStr.java
@@ -13,14 +13,12 @@ public class ImplementStrStr {
        char end=searchKey.charAt(searchKey.length()-1);
        int startInd=-1;
        int endInd=-1;
-       for (int i=0; i<n; ++i)
+       for (int i=0; i<n && (startInd<0|| endInd<0); ++i)
        {
            if(str.charAt(i)==start)
                startInd=i;
            if(str.charAt(i)==end)
                endInd=i;
-           if(startInd>-1 && endInd>-1)
-               break;
        }
         System.out.println(startInd+" "+endInd);
        return -1;
