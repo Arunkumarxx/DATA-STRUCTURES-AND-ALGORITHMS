22fb5f9623267f724747de6fa129bbb982c89598
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-04 13:11:37 +0530

    modified

--- STRING/BinaryString.java
+++ STRING/BinaryString.java
@@ -2,9 +2,13 @@ package STRING;
 
 public class BinaryString {
     public static void main(String[] args) {
-        
+        String str="1111";
+        System.out.println(BinaryString(str.length(),str));
     }
-    private static void BinaryString () {
-
+    private static int  BinaryString (int n,String str) {
+        short count=0;
+        for(int i=0; i<n; ++i)
+            if(str.charAt(i)=='1') ++count;
+        return count;
     }
 }
