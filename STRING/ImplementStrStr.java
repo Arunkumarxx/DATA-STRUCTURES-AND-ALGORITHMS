0db2c81c42b1850c57ed09032af7e6773d64e260
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-04 15:06:10 +0530

    modified

--- STRING/ImplementStrStr.java
+++ STRING/ImplementStrStr.java
@@ -9,13 +9,18 @@ public class ImplementStrStr {
     private static int Str(String str,String key) {
         int kn=key.length();
         int sn=str.length();
-        for (int i=0; i<kn; ++i)
+        int ind=0;
+        for (int i=0; i<kn-1; ++i)
         {
-            for(int j=0; j<sn-kn; ++j)
-                if(str.charAt(i)==)
+            for(int j=0; j<sn-1; ++j) {
+                if(i==0 && str.charAt(j)==key.charAt(i))
+                    ind=j;
+                if (!(str.charAt(j) == key.charAt(i) && key.charAt(i + 1) == str.charAt(j + 1)))
+                    return -1;
 
+            }
         }
 
-        return -1;
+        return ind;
     }
 }
