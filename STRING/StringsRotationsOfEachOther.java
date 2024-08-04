c757c3dc982cce8e3de4880e3a2df145adf6ca77
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-04 18:03:41 +0530

    modified

--- STRING/StringsRotationsOfEachOther.java
+++ STRING/StringsRotationsOfEachOther.java
@@ -5,22 +5,25 @@ public class StringsRotationsOfEachOther {
         String str1 ="geeksforgeeks";
         String str2 ="forgeeksgeeks";
         long time1s = System.nanoTime();
-        String rotated  =str1+str1;
+        System.out.println(areRotations(str1,str2));
+        long time1e = System.nanoTime();
+        System.out.println((time1e-time1s)/1_000_000.0+"ms");
+    }
+    private static boolean areRotations (String s1,String s2) {
+        String rotated  =s1+s1;
         int j=0;
         int count=0;
         for(int i=0; i<rotated.length(); ++i)
         {
-            if(j==str2.length()) break;
-            if(rotated.charAt(i)==str2.charAt(j))
+            if(j==s2.length()) break;
+            if(rotated.charAt(i)==s2.charAt(j))
             {
                 ++count;
                 ++j;
             }
         }
-        if(count==str2.length())
-            System.out.println("true");;
-        System.out.println("false");
-        long time1e = System.nanoTime();
-        System.out.println((time1e-time1s)/1_000_000.0+"ms");
+        if(count==s2.length())
+            return true;
+        return false;
     }
 }
