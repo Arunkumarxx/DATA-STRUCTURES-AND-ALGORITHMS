bbdd9b21dadfd765682412319768cfce73b9aa91
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-04 17:05:28 +0530

    modified

--- STRING/StringsRotationsOfEachOther.java
+++ STRING/StringsRotationsOfEachOther.java
@@ -12,7 +12,6 @@ public class StringsRotationsOfEachOther {
             stringBuilder.append(str1.charAt(i));
 
         String res=stringBuilder.toString();
-
-        System.out.println(res==str2);
+        System.out.println(res=="forgeeksgeeks");
     }
 }
