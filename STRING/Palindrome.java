e21b477e81eca84fa86bf1da2a6e49527b1a2db7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-04 16:48:29 +0530

    modified

--- STRING/Palindrome.java
+++ STRING/Palindrome.java
@@ -9,9 +9,12 @@ public class Palindrome {
 
         int start=0,end=str.length()-1;
 
-        while(start<end)
-            if(!(str.charAt(++start)==str.charAt(--end)))
+        while(start<end) {
+            if (!(str.charAt(start) == str.charAt(end)))
                 return false;
+            ++start;
+            --end;
+        }
         return true;
     }
 }
