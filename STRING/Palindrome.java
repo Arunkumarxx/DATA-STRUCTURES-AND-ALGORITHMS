ba4dc7b155f178b3f1b64760cf9efe0bfb8967ff
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-04 16:48:12 +0530

    modified

---
+++ STRING/Palindrome.java
@@ -0,0 +1,17 @@
+package STRING;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String str= "moom";
+        System.out.println(isPalindromeString(str));
+    }
+    private static boolean isPalindromeString(String str) {
+
+        int start=0,end=str.length()-1;
+
+        while(start<end)
+            if(!(str.charAt(++start)==str.charAt(--end)))
+                return false;
+        return true;
+    }
+}
