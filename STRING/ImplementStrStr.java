774d3f1c5bf1e2d725d2342f9b7f7e3a80a652c3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-04 14:34:36 +0530

    modified

--- STRING/ImplementStrStr.java
+++ STRING/ImplementStrStr.java
@@ -3,7 +3,7 @@ package STRING;
 public class ImplementStrStr {
     public static void main(String[] args) {
         String s="GeeksForGeeks";
-        String x="For";
+        String x="Fox";
         System.out.println(Str(s,x));
     }
     private static int Str(String s,String x) {
