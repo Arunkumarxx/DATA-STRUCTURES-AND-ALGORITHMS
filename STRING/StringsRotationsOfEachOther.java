1bc4c2395a0f8d747596be4f939fcdf7d5ce0556
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-04 17:19:35 +0530

    modified

--- STRING/StringsRotationsOfEachOther.java
+++ STRING/StringsRotationsOfEachOther.java
@@ -4,7 +4,7 @@ public class StringsRotationsOfEachOther {
     public static void main(String[] args) {
         String str1 ="geeksforgeeks";
         String str2 ="forgeeksgeeks";
-
+        System.out.println(areRotations(str1,str2));
     }
     private static boolean areRotations (String s1,String s2) {
         if(s1.length() !=s2.length())
