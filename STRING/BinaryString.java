6293fafeeb2da03f09656b60efff23fa9791a9d7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-04 13:24:08 +0530

    modified

--- STRING/BinaryString.java
+++ STRING/BinaryString.java
@@ -2,7 +2,7 @@ package STRING;
 
 public class BinaryString {
     public static void main(String[] args) {
-        String str="1111";
+        String str=" 01101";
         System.out.println(BinaryString(str.length(),str));
     }
     private static int  BinaryString (int n,String str) {
