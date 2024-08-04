b383688031a500660af631f880cb17a05f3e5e3d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-04 16:45:39 +0530

    modified

--- STRING/Palindrome.java
+++ STRING/Palindrome.java
@@ -7,7 +7,7 @@ public class Palindrome {
     }
     private static boolean isPalindromeString(String str) {
 
-        int start=0,end=str.length();
+        int start=0,end=str.length()-1;
         while(start<end)
         {
             if(!(str.charAt(start)==str.charAt(end)))
