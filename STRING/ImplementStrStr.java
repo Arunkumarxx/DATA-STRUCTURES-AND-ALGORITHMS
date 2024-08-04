e7ffa34c9061b3c0f628b9ba1b540dbbbdd6f88b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-04 16:19:47 +0530

    modified

--- STRING/ImplementStrStr.java
+++ STRING/ImplementStrStr.java
@@ -9,15 +9,23 @@ public class ImplementStrStr {
     private static int Str(String str,String searchKey) {
         int n1=str.length();
         int n2=searchKey.length();
+        boolean isFound=true;
         for (int i=0; i<n1; ++i)
         {
             if(str.charAt(i)==searchKey.charAt(0))
             {
                 for(int j=0;j<n2; ++j)
                 {
-                    if(str.charAt(i))
+                    if(str.charAt(i+j)!=searchKey.charAt(j))
+                    {
+                        isFound=false;
+                        break;
+                    }
                 }
             }
+            if(!(isFound))
+                return -1;
         }
+        return 
     }
 }
