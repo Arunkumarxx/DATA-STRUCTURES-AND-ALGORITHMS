8188c50f0705f4756c277cd7a97437ad155f4ba1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-04 16:49:28 +0530

    modified

--- STRING/Palindrome.java
+++ STRING/Palindrome.java
@@ -10,10 +10,9 @@ public class Palindrome {
         int start=0,end=str.length()-1;
 
         while(start<end) {
-            if (!(str.charAt(start) == str.charAt(end)))
+            if (!(str.charAt(++start) == str.charAt(--end)))
                 return false;
-            ++start;
-            --end;
+
         }
         return true;
     }
