fb4a03f6051643bd947a4519cdda604004433eac
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-04 17:08:08 +0530

    modified

---
+++ STRING/StringsRotationsOfEachOther.java
@@ -0,0 +1,16 @@
+package STRING;
+
+public class StringsRotationsOfEachOther {
+    public static void main(String[] args) {
+        String str1 ="geeksforgeeks";
+        String str2 ="forgeeksgeeks";
+        int unit=5;
+        StringBuilder reversed =new StringBuilder();
+        for(int i=unit;i<str1.length(); ++i)
+            reversed.append(str1.charAt(i));
+        for(int i=0; i<unit; ++i)
+            reversed.append(str1.charAt(i));
+        String res=reversed.toString();
+        System.out.println(str2.equals(res));
+    }
+}
