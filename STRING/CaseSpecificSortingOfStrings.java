25fd95e55dc601f710a33de1550e2250a8bece5f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 13:34:33 +0530

    modified

--- STRING/CaseSpecificSortingOfStrings.java
+++ STRING/CaseSpecificSortingOfStrings.java
@@ -1,10 +1,14 @@
 package STRING;
 
+import java.util.ArrayList;
 import java.util.Collections;
 
 public class CaseSpecificSortingOfStrings {
     public static void main(String[] args) {
         String str="aBzLkPq";
+        ArrayList<Character> arrayList =new ArrayList<>();
+        for(int i=0; i<str.length(); ++i)
+            arrayList.add(str.charAt(i));
         
     }
 }
