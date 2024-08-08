f5f68527d7ac15b6b27f210cd206020baef7581b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 14:36:34 +0530

    modified

--- STRING/CheckForAnagram.java
+++ STRING/CheckForAnagram.java
@@ -1,5 +1,8 @@
 package STRING;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class CheckForAnagram {
     public static void main(String[] args) {
         String str1 ="Arunkumar";
@@ -7,7 +10,20 @@ public class CheckForAnagram {
         System.out.println(isAnagram(str1,str2));
     }
     private static boolean isAnagram(String a, String b) {
-        
+        if(a.length()!=b.length())
+            return false;
+
+        HashMap<Character,Integer> hashMap = new HashMap<>();
+        for(int i=0;i<a.length(); ++i)
+        {
+            hashMap.put(a.charAt(i),hashMap.getOrDefault(a.charAt(i),0)+1);
+            hashMap.put(b.charAt(i),hashMap.getOrDefault(b.charAt(i),0)-1);
+        }
+        int count=0;
+        Iterator itr = hashMap.values().iterator();
+        while(itr.hasNext())
+            if((int)itr.next()==0)
+                count++;
         return false;
     }
 }
