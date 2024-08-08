2e6efc7706cab400dd42f21bf930d2ea21cf4c07
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 15:15:51 +0530

    modified

--- STRING/LeftmostRepeatingCharacter.java
+++ STRING/LeftmostRepeatingCharacter.java
@@ -11,7 +11,7 @@ public class LeftmostRepeatingCharacter {
     {
         HashMap<Character,Integer> hashMap = new HashMap<>();
         int n=Str.length();
-        for(int i=0; i<n;)
+        for(int i=0; i<n;++i)
             hashMap.put(Str.charAt(i),hashMap.getOrDefault(Str.charAt(i),0)+1);
         for(int i=0; i<n; ++i)
             if(hashMap.get(Str.charAt(i))>1)
