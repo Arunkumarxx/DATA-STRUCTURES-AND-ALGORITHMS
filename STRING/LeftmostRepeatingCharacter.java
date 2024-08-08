35d6a21ae26cd52b3e2e9ede76ecddd4959917b2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 15:16:18 +0530

    modified

--- STRING/LeftmostRepeatingCharacter.java
+++ STRING/LeftmostRepeatingCharacter.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class LeftmostRepeatingCharacter {
     public static void main(String[] args) {
-        String str="arun";
+        String str="arunn";
         System.out.println(repeatedCharacter(str));
     }
     private static boolean repeatedCharacter(String Str)
