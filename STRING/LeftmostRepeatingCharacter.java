2b7b23db136c2e98f633f6419775ffd101ef007e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 15:15:36 +0530

    modified

--- STRING/LeftmostRepeatingCharacter.java
+++ STRING/LeftmostRepeatingCharacter.java
@@ -1,8 +1,21 @@
 package STRING;
 
+import java.util.HashMap;
+
 public class LeftmostRepeatingCharacter {
     public static void main(String[] args) {
-
+        String str="geeksforgeeks";
+        System.out.println(repeatedCharacter(str));
     }
-    pir
+    private static boolean repeatedCharacter(String Str)
+    {
+        HashMap<Character,Integer> hashMap = new HashMap<>();
+        int n=Str.length();
+        for(int i=0; i<n;)
+            hashMap.put(Str.charAt(i),hashMap.getOrDefault(Str.charAt(i),0)+1);
+        for(int i=0; i<n; ++i)
+            if(hashMap.get(Str.charAt(i))>1)
+                return true;
+        return false;
+    }
 }
