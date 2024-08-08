79a67808229d8be14f6e09d8e4a0f2fbdf73dbb9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 12:19:03 +0530

    modified

--- STRING/IsSubSequence.java
+++ STRING/IsSubSequence.java
@@ -1,2 +1,12 @@
-package STRING;public class IsSubSequence {
+package STRING;
+
+public class IsSubSequence {
+    public static void main(String[] args) {
+        String str ="abc";
+        String target = "ahbgdc";
+    }
+    private static boolean subsequence (String str,String target) {
+        
+        return false;
+    }
 }
