3f68cc000897e2e90f2cda40adee2ec838c9cd76
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 10:56:14 +0530

    modified

--- STRING/IsomorphicStrings.java
+++ STRING/IsomorphicStrings.java
@@ -4,8 +4,8 @@ import java.util.HashMap;
 
 public class IsomorphicStrings {
     public static void main(String[] args) {
-        String str1= "bar";
-        String str2= "foo";
+        String str1= "aab";
+        String str2= "xxy";
         System.out.println(IsIsomorphicString(str1,str2));
     }
     private static boolean IsIsomorphicString(String str1,String str2) {
