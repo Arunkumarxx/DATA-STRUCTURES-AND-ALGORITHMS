4d0819b1c1ed1f0f1cf7c82bc3f9ead80795540f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 13:45:28 +0530

    modified

--- STRING/CaseSpecificSortingOfStrings.java
+++ STRING/CaseSpecificSortingOfStrings.java
@@ -6,11 +6,13 @@ import java.util.Collections;
 public class CaseSpecificSortingOfStrings {
     public static void main(String[] args) {
         String str="GdCaBgfR";
-        ArrayList<Character> arrayList =new ArrayList<>();
-        for(int i=0; i<str.length(); ++i) {
-            arrayList.add(str.charAt(i)<97? (char) ( str.charAt(i) + 32) :str.charAt(i));
+        int n=str.length();
+        ArrayList<Character> arrayList1 =new ArrayList<>();
+        ArrayList<Character> arrayList2 =new ArrayList<>();
+
+        for(int i=0; i<n;++i)
+        {
+            
         }
-        Collections.sort(arrayList);
-        System.out.println(arrayList);
     }
 }
