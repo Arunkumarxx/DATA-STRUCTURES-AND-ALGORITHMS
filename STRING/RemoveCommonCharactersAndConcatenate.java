e4675f7e493550f8d23ab019bd828575fb0015fc
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 18:43:14 +0530

    modified

--- STRING/RemoveCommonCharactersAndConcatenate.java
+++ STRING/RemoveCommonCharactersAndConcatenate.java
@@ -2,6 +2,12 @@ package STRING;
 
 public class RemoveCommonCharactersAndConcatenate {
     public static void main(String[] args) {
-        
+        String string1 = "aacdb";
+        String string2 = "gafd";
+        System.out.println(concatenatedString(string1,string2));
+    }
+    private static String concatenatedString(String s1,String s2)
+    {
+
     }
 }
