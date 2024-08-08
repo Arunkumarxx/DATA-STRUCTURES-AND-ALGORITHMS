fbdc485c7cd9cd7dd0c42fc4769d099742200012
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 16:24:06 +0530

    modified

--- STRING/TheModifiedString.java
+++ STRING/TheModifiedString.java
@@ -10,8 +10,16 @@ public class TheModifiedString {
         if(str.length()==3)
             return 1;
         int n=str.length();
-        for(int i=0; i<n; ++i)
-            
+        int consicutive=1;
+        int modificationCount=0;
+        for(int i=0; i<n-1; ++i) {
+            if (str.charAt(i) == str.charAt(i + 1))
+                ++cons;
+            if(cons==3) {
+                ++modificationCount;
+                cons
+            }
+        }
         return 1;
     }
 }
