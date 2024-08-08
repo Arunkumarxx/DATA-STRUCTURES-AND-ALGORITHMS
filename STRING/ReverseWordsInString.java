dea0f160c9b6e02bc716d85d96f07fb3493cda19
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 17:03:40 +0530

    modified

--- STRING/ReverseWordsInString.java
+++ STRING/ReverseWordsInString.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 
 public class ReverseWordsInString {
     public static void main(String[] args) {
-        StringBuilder stringBuilder =new StringBuilder("arun kumar");
+        StringBuilder stringBuilder =new StringBuilder("arun.kumar");
         ArrayList<ArrayList<StringBuilder>> arrayList =new ArrayList<>();
         int n=stringBuilder.length();
         StringBuilder temp =new StringBuilder();
         int k=0;
         for(int i=0; i<n; ++i)
         {
-            if(stringBuilder.charAt(i)!=' ')
+            if(stringBuilder.charAt(i)!='.')
                 temp.append(stringBuilder.charAt(i));
-            if(stringBuilder.charAt(i)==' ') {
+            if(stringBuilder.charAt(i)=='.') {
                 arrayList.add(new ArrayList<>());
                 arrayList.get(k).add(temp);
                 temp=new StringBuilder();
