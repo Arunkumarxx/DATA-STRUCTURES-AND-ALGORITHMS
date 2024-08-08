de99f0742ef3cae059b002300fbd23675822b23e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 16:45:34 +0530

    modified

--- STRING/ReverseWordsInString.java
+++ STRING/ReverseWordsInString.java
@@ -1,2 +1,8 @@
-package STRING;public class ReverseWordsInString {
+package STRING;
+
+public class ReverseWordsInString {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder =new StringBuilder("arun kumar");
+        System.out.println(stringBuilder);
+    }
 }
