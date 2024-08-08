ea5d1eb278318af4727d2f63e788a3d4b29f8f67
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 14:10:41 +0530

    modified

--- STRING/CaseSpecificSortingOfStrings.java
+++ STRING/CaseSpecificSortingOfStrings.java
@@ -1,34 +1,13 @@
 package STRING;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class CaseSpecificSortingOfStrings {
     public static void main(String[] args) {
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            PrintWriter out=new PrintWriter(System.out);
-            String[] words = br.readLine().split("\\s+");
-            int numTestCases = Integer.parseInt(words[0]);
-
-            for (int tIdx = 0; tIdx < numTestCases; tIdx++) {
-                words = br.readLine().split("\\s+");
-                int size = Integer.parseInt(words[0]);
-                String str = br.readLine();
-                String sortedStr = new CaseSpecificSortingOfStrings().caseSort(str);
-                out.println(sortedStr);
-            }
-            out.close();
-        }
-        catch (IOException e) {
-            System.out.println(e);
-        }
+        System.out.println(caseSort(""));
     }
-    public    String caseSort(String str)
+    private static   String caseSort(String str)
     {
         int n=str.length();
         ArrayList<Character> lowerCase =new ArrayList<>();
