f1a8c631ec1fc01cff092effefbad9b80f819c02
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 13:18:53 +0530

    modified

--- STRING/MaximumOccuringCharacter.java
+++ STRING/MaximumOccuringCharacter.java
@@ -10,14 +10,13 @@ public class MaximumOccuringCharacter {
     private static char getMaxOccuringChar(String str){
         int n=str.length();
         int [] arr  =new int[26];
-//        for(int i=0;i<n; ++i)
-//            arr[str.charAt(i)-'a']+=1;
+        for(int i=0;i<n; ++i)
+            arr[str.charAt(i)-'a']+=1;
 
         char maxFirstChar='a';
-        int max=0;
-        for(int i=0; i< arr.length;++i)
+        int max=arr[0];
+        for(int i=1; i< arr.length;++i)
         {
-            arr[str.charAt(i)-'a']+=1;
             if(arr[i]>max) {
                 max = arr[i];
                 maxFirstChar=(char)('a'+i);
