2600946f62e705c9510d5a39e2acb87aa05adf73
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 16:54:44 +0530

    modified

--- STRING/ReverseWordsInString.java
+++ STRING/ReverseWordsInString.java
@@ -1,8 +1,23 @@
 package STRING;
 
+import java.util.ArrayList;
+
 public class ReverseWordsInString {
     public static void main(String[] args) {
         StringBuilder stringBuilder =new StringBuilder("arun kumar");
-        System.out.println(stringBuilder.reverse());
+        ArrayList<ArrayList<StringBuilder>> arrayList =new ArrayList<>();
+        int n=stringBuilder.length();
+        StringBuilder temp =new StringBuilder();
+        int k=0;
+        for(int i=0; i<n; ++i)
+        {
+            if(stringBuilder.charAt(i)!=' ')
+                temp.append(stringBuilder.charAt(i));
+            if(stringBuilder.charAt(i)==' ') {
+                arrayList.add(k,new ArrayList<>());
+                arrayList.add(k,temp.toString());
+            }
+        }
+
     }
 }
