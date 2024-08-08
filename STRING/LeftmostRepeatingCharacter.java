80415aef45d713cb5da226721e08f03e05492237
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 15:27:30 +0530

    modified

--- STRING/LeftmostRepeatingCharacter.java
+++ STRING/LeftmostRepeatingCharacter.java
@@ -7,21 +7,17 @@ public class LeftmostRepeatingCharacter {
         String str="geeksforgeeks";
         System.out.println(repeatedCharacter(str));
     }
-    private static int repeatedCharacter(String Str)
+    static int repeatedCharacter(String Str)
     {
+        // add your code here
         HashMap<Character,Integer> hashMap = new HashMap<>();
         int n=Str.length();
-        for(int i=0; i<n;++i) {
-            if(hashMap.containsKey(Str.charAt(i)))
-                return linearSearch(Str,Str.charAt(i));
-                else hashMap.put(Str.charAt(i),hashMap.getOrDefault(Str.charAt(i),0)+1);
-        }
+        for(int i=0; i<n;++i)
+            hashMap.put(Str.charAt(i),hashMap.getOrDefault(Str.charAt(i),0)+1);
+        for(int i=0; i<n; ++i)
+            if(hashMap.get(Str.charAt(i))>1)
+                return 0;
         return -1;
     }
-    private static int linearSearch(String str, char key) {
-        for(int i =0; i<str.length(); ++i)
-            if(key==str.charAt(i))
-                return i;
-        return -1;
-    }
+
 }
