3ef36def667bcdb4b552907daddc3a506a18ecf8
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 19:46:45 +0530

    modified

--- STRING/RemoveCommonCharactersAndConcatenate.java
+++ STRING/RemoveCommonCharactersAndConcatenate.java
@@ -11,30 +11,17 @@ public class RemoveCommonCharactersAndConcatenate {
     }
     private static String concatenatedString(String s1,String s2)
     {
-        HashSet<String> hashSet1 =new HashSet<>();
-        HashSet<String > hashSet2=new HashSet<>();
-        for(int i=0; i<s1.length(); ++i)
-            hashSet1.add(String.valueOf(s1.charAt(i)));
-
-        for(int i=0; i<s2.length(); ++i)
-            hashSet2.add(String.valueOf(s2.charAt(i)));
-        ArrayList<String> duplicates =new ArrayList<>();
-       for(String ele:hashSet1)
-           if(hashSet2.contains(ele)) {
-               hashSet2.remove(ele);
-               duplicates.add(ele);
-           }
-       for(String ele:duplicates)
-           hashSet1.remove(ele);
-
-       StringBuilder stringBuilder =new StringBuilder();
-       for(int i=0; i<s1.length(); ++i)
-           if (hashSet1.contains(String.valueOf(s1.charAt(i))))
-               stringBuilder.append(s1.charAt(i));
-       for(int i=0; i<s2.length(); ++i)
-           if(hashSet2.contains(String.valueOf(s2.charAt(i))))
-               stringBuilder.append(String.valueOf(s2.charAt(i)));
-        if(stringBuilder.length()==0) return "-1";
-       return stringBuilder.toString();
+        String ans = "";
+        for(int i = 0;i<s1.length();i++){
+            char ch = s1.charAt(i);
+            if(s2.indexOf(ch)==-1)
+                ans+=ch;
+        }
+        for(int i = 0;i<s2.length();i++){
+            char ch = s2.charAt(i);
+            if(s1.indexOf(ch)==-1)
+                ans+=ch;
+        }
+        return ans==""?"-1":ans;
     }
 }
