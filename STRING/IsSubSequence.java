a22df65251c0211209b165d0ab183f67769ce200
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 12:26:21 +0530

    modified

--- STRING/IsSubSequence.java
+++ STRING/IsSubSequence.java
@@ -2,7 +2,7 @@ package STRING;
 
 public class IsSubSequence {
     public static void main(String[] args) {
-        String str ="abc";
+        String str ="axc";
         String target = "ahbgdc";
         System.out.println(subsequence(str,target));
     }
