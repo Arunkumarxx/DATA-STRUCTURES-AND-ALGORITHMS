00e1e04d920fe0ad9df638edab582bc7bfb7f93e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 14:28:32 +0530

    modified

--- STRING/CheckForAnagram.java
+++ STRING/CheckForAnagram.java
@@ -2,6 +2,12 @@ package STRING;
 
 public class CheckForAnagram {
     public static void main(String[] args) {
-
+        String str1 ="Arunkumar";
+        String str2 ="ramuknura";
+        System.out.println(isAnagram(str1,str2));
+    }
+    private static boolean isAnagram(String a, String b) {
+        
+        return false;
     }
 }
