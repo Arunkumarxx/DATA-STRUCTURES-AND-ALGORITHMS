6fd3bc6cfa7ad596379471d36985b3816d7e34f2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 19:04:52 +0530

    modified

--- STRING/RemoveCommonCharactersAndConcatenate.java
+++ STRING/RemoveCommonCharactersAndConcatenate.java
@@ -11,29 +11,48 @@ public class RemoveCommonCharactersAndConcatenate {
     }
     private static String concatenatedString(String s1,String s2)
     {
-        HashSet<String> hashSet =new HashSet<>();
+        HashSet<String> hashSet1 =new HashSet<>();
+        HashSet<String > hashSet2=new HashSet<>();
         int i=0,j=0;
         int n1=s1.length();
         int n2=s2.length();
         while(i<n1 && j<n2)
         {
-            hashSet.add(String.valueOf(s1.charAt(i)));
-            hashSet.add(String.valueOf(s2.charAt(j)));
+            if(hashSet1.contains(s1.charAt(i)) )
+                hashSet1.remove(s1.charAt(i));
+            else hashSet1.add(String.valueOf(s1.charAt(i)));
+            if(hashSet2.contains(s1.charAt(i)))
+                hashSet2.remove(s2.charAt(i));
+
+            if(hashSet1.contains(s2.charAt(j)) )
+                hashSet1.remove(s2.charAt(j));
+            else hashSet2.add(String.valueOf(s2.charAt(j)));
+            if(hashSet2.contains(s2.charAt(j)))
+                hashSet2.remove(s2.charAt(j));
+
             ++i; ++j;
         }
         while(i<n1) {
-            hashSet.add(String.valueOf(s1.charAt(i)));
+            if(hashSet1.contains(s1.charAt(i)) )
+                hashSet1.remove(s1.charAt(i));
+            else hashSet1.add(String.valueOf(s1.charAt(i)));
+            if(hashSet2.contains(s1.charAt(i)))
+                hashSet2.remove(s2.charAt(i));
             ++i;
         }
         while(j<n2)
         {
-            hashSet.add(String.valueOf(s2.charAt(j)));
+            if(hashSet1.contains(s2.charAt(j)) )
+                hashSet1.remove(s2.charAt(j));
+            else hashSet2.add(String.valueOf(s2.charAt(j)));
+            if(hashSet2.contains(s2.charAt(j)))
+                hashSet2.remove(s2.charAt(j));
             ++j;
         }
         StringBuilder stringBuilder =new StringBuilder();
-        Iterator iterator = hashSet.iterator();
-        while(iterator.hasNext())
-            stringBuilder.append(iterator.next());
-        return stringBuilder.toString();
+
+        System.out.println(hashSet1);
+        System.out.println(hashSet2);
+
     }
 }
