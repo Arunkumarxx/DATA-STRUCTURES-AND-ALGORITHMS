51cd4aede7883bf819d5d04903d3754393bd46e2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 12:30:17 +0530

    modified

--- STRING/IsSubSequence.java
+++ STRING/IsSubSequence.java
@@ -2,8 +2,8 @@ package STRING;
 
 public class IsSubSequence {
     public static void main(String[] args) {
-        String str ="";
-        String target = "";
+        String str ="cd";
+        String target = "abcd";
         System.out.println(subsequence(str,target));
     }
     private static boolean subsequence (String str,String target) {
