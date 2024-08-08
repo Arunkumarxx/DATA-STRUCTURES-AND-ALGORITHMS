72c0efdcf7b753dc1920f00e9bfc13c6c21a4602
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 16:25:42 +0530

    modified

--- STRING/TheModifiedString.java
+++ STRING/TheModifiedString.java
@@ -2,7 +2,7 @@ package STRING;
 
 public class TheModifiedString {
     public static void main(String[] args) {
-        String str ="aaaaaa";
+        String str ="aaaaa";
         System.out.println(modified(str));
     }
     private static long modified(String str)
