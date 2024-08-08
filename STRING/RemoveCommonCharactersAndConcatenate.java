188f5f2f7e363903984a93e1c7a2835d560a008c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 19:29:31 +0530

    modified

--- STRING/RemoveCommonCharactersAndConcatenate.java
+++ STRING/RemoveCommonCharactersAndConcatenate.java
@@ -19,16 +19,9 @@ public class RemoveCommonCharactersAndConcatenate {
         for(int i=0; i<s2.length(); ++i)
             hashSet2.add(String.valueOf(s2.charAt(i)));
 
-        Iterator<String> iterator = hashSet1.iterator();
-        while (iterator.hasNext()) {
-            String element = iterator.next();
-            if (hashSet2.contains(element)) {
-                hashSet2.remove(element);
-                Iterator<String> iterator2 = hashSet2.iterator();
-                while(iterator2.hasNext())
-                    hashSet2.remove;
-            }
-        }
+       for(String ele:hashSet1)
+           if(hashSet2.contains(ele))
+               hashSet2.remove(ele);
 
         System.out.println(hashSet1);
         System.out.println(hashSet2);
