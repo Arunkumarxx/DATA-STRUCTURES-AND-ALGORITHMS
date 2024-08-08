18a13d5d61a95d1d01722c853805c10c54ba7d38
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 19:36:30 +0530

    modified

--- STRING/RemoveCommonCharactersAndConcatenate.java
+++ STRING/RemoveCommonCharactersAndConcatenate.java
@@ -7,7 +7,7 @@ import java.util.Iterator;
 public class RemoveCommonCharactersAndConcatenate {
     public static void main(String[] args) {
         String string1 = "aacdb";
-        String string2 = "cxzca";
+        String string2 = "gafd";
         System.out.println(concatenatedString(string1,string2));
     }
     private static String concatenatedString(String s1,String s2)
