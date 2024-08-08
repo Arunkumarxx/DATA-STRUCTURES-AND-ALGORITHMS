ef9a4bc0722a5f62f48a9168c92aff134d32d5b9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 15:06:50 +0530

    modified

--- STRING/CheckIfAStringIsIsogramOrNot.java
+++ STRING/CheckIfAStringIsIsogramOrNot.java
@@ -1,11 +1,19 @@
 package STRING;
 
+import java.util.HashMap;
+
 public class CheckIfAStringIsIsogramOrNot {
     public static void main(String[] args) {
-
+        String str="machine";
+        System.out.println(IsogramOrNot( str));
     }
-    private static  String IsogramOrNot() {
-
-        return "";
+    private static  boolean IsogramOrNot(String  str) {
+        HashMap<Character,Integer> hashMap =new HashMap<>();
+        for(int i=0; i<str.length(); ++i) {
+            hashMap.put(str.charAt(i), hashMap.getOrDefault(str.charAt(i), 0) + 1);
+           if( hashMap.get(str.charAt(i))>0)
+               return false;
+        }
+        return true;
     }
 }
