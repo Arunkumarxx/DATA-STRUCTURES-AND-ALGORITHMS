086bbc625d902e1754a96b00d5ae8569229e2135
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 18:47:28 +0530

    modified

--- STRING/RemoveCommonCharactersAndConcatenate.java
+++ STRING/RemoveCommonCharactersAndConcatenate.java
@@ -1,5 +1,7 @@
 package STRING;
 
+import java.util.HashSet;
+
 public class RemoveCommonCharactersAndConcatenate {
     public static void main(String[] args) {
         String string1 = "aacdb";
@@ -8,6 +10,27 @@ public class RemoveCommonCharactersAndConcatenate {
     }
     private static String concatenatedString(String s1,String s2)
     {
-
+        HashSet<String> hashSet =new HashSet<>();
+        int i=0,j=0;
+        int n1=s1.length();
+        int n2=s2.length();
+        while(i<n1 && j<n2)
+        {
+            hashSet.add(String.valueOf(s1.charAt(i)));
+            hashSet.add(String.valueOf(s2.charAt(j)));
+            ++i; ++j;
+        }
+        while(i<n1) {
+            hashSet.add(String.valueOf(s1.charAt(i)));
+            ++i;
+        }
+        while(j<n2)
+        {
+            hashSet.add(String.valueOf(s2.charAt(j)));
+            ++j;
+        }
+        String result =new String();
+        System.out.println(hashSet);
+return "";
     }
 }
