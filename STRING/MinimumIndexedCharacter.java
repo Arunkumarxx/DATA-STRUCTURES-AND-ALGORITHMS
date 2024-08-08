8898886bcc13197d979894dc10c82e6e012cdfea
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 15:46:07 +0530

    modified

--- STRING/MinimumIndexedCharacter.java
+++ STRING/MinimumIndexedCharacter.java
@@ -1,11 +1,20 @@
 package STRING;
 
+import java.io.CharArrayReader;
+import java.util.HashMap;
+
 public class MinimumIndexedCharacter {
     public static void main(String[] args) {
-
+        String str ="geeksforgeeks";
+        String pattern="set";
+        System.out.println(minIndexChar(str,pattern));
     }
-    private static int minIndexChar(String str, String patt)
+    private static int minIndexChar(String str, String pattern)
     {
-        // Your code 
+        HashMap<Character,Integer> hashMap =new HashMap<>();
+        int n=str.length();
+        for(int i=0; i<n; ++i)
+            hashMap.put(str.charAt(i),hashMap.getOrDefault(str.charAt(i),0)+1);
+        System.out.println(hashMap);
     }
 }
