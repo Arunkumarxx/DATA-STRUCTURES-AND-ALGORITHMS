de22b540511eaeb7f2d22b7f48a5f7c51cd37e0f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 16:18:53 +0530

    modified

--- STRING/TheModifiedString.java
+++ STRING/TheModifiedString.java
@@ -3,6 +3,15 @@ package STRING;
 public class TheModifiedString {
     public static void main(String[] args) {
         String str ="aabbbcc";
-        
+        System.out.println(modified(str));
+    }
+    private static long modified(String str)
+    {
+        int 
+        if(str.length()==3)
+            return 1;
+        int n=str.length();
+        for(int i=0; i<n)
+        return 1;
     }
 }
