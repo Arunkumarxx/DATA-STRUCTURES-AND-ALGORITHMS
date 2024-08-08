a5f7091a77f59b1674e1ad7e2a8ad8874ed258b4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 15:57:13 +0530

    modified

--- STRING/MinimumIndexedCharacter.java
+++ STRING/MinimumIndexedCharacter.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 
 public class MinimumIndexedCharacter {
     public static void main(String[] args) {
-        String str ="adcffaet";
-        String pattern="onkl";
+        String str ="arun";
+        String pattern="u";
         System.out.println(minIndexChar(str,pattern));
     }
     private static int minIndexChar(String str, String pattern)
