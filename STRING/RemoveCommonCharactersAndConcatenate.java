4ddf71747c32fbdb8a3c7b4111c7ea973fc67788
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 19:55:20 +0530

    modified

--- STRING/RemoveCommonCharactersAndConcatenate.java
+++ STRING/RemoveCommonCharactersAndConcatenate.java
@@ -7,38 +7,10 @@ public class RemoveCommonCharactersAndConcatenate {
     public static void main(String[] args) {
         String string1 = "aacdb";
         String string2 = "gafd";
-
-        // Timing for the first method
-        long startTime1 = System.currentTimeMillis();
-        System.out.println(concatenatedString(string1, string2));
-        long endTime1 = System.currentTimeMillis();
-        long duration1 = endTime1 - startTime1;
-        System.out.println("Time taken for concatenatedString(string1, string2): " + duration1 + " milliseconds");
-
-        // Timing for the second method (assuming this method exists)
-        long startTime2 = System.currentTimeMillis();
-        System.out.println(concatenatedString(string1, string2, 1));
-        long endTime2 = System.currentTimeMillis();
-        long duration2 = endTime2 - startTime2;
-        System.out.println("Time taken for concatenatedString(string1, string2, 1): " + duration2 + " milliseconds");
-
+        System.out.println( concatenatedString(string1,string2));
     }
-    private static String concatenatedString(String s1,String s2)
-    {
-        String ans = "";
-        for(int i = 0;i<s1.length();i++){
-            char ch = s1.charAt(i);
-            if(s2.indexOf(ch)==-1)
-                ans+=ch;
-        }
-        for(int i = 0;i<s2.length();i++){
-            char ch = s2.charAt(i);
-            if(s1.indexOf(ch)==-1)
-                ans+=ch;
-        }
-        return ans==""?"-1":ans;
-    }
-    public static String concatenatedString(String s1,String s2,int n)
+
+    public static String concatenatedString(String s1,String s2)
     {
         // Your code here
         HashSet<String> hashSet1 =new HashSet<>();
