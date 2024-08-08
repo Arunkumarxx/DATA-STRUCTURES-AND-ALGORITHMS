7587122708be949f08c8167177fbf4f6d6395f9f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 13:05:17 +0530

    modified

--- STRING/MaximumOccuringCharacter.java
+++ STRING/MaximumOccuringCharacter.java
@@ -2,6 +2,16 @@ package STRING;
 
 public class MaximumOccuringCharacter {
     public static void main(String[] args) {
-        
+        String str ="testsample";
+        System.out.println(getMaxOccuringChar(str));
+    }
+    private static char getMaxOccuringChar(String str){
+        int n=str.length();
+        int [] arr  =new int[26];
+        for(int i=0;i<n; ++i)
+        {
+            
+        }
+        return ' ';
     }
 }
