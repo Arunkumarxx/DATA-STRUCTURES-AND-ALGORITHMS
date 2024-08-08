52a87ffde29edd508fefb2ef274a4d4258a8163b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 15:39:34 +0530

    modified

--- STRING/NonRepeatingCharacter.java
+++ STRING/NonRepeatingCharacter.java
@@ -1,11 +1,26 @@
 package STRING;
 
+import java.util.HashMap;
+
 public class NonRepeatingCharacter {
     public static void main(String[] args) {
-
+        String str = "hello";
+        System.out.println(nonrepeatingCharacter(str));
     }
-    
+    private static char nonrepeatingCharacter(String Str)
+    {
+        HashMap<Character,Integer> hashMap =new HashMap<>();
+        int n=Str.length();
+        for(int i=0; i<n; ++i)
+            hashMap.put(Str.charAt(i),hashMap.getOrDefault(Str.charAt(i),0)+1);
+        for(int i=0; i<n; ++i)
+         if(hashMap.get(Str.charAt(i))>1)
+               return Str.charAt(i);
+        return '$';
+    }
 }
+
+
 /*
 Non Repeating Character
 Given a string s consisting of lowercase Latin Letters. Return the first non-repeating character in s. If there is no non-repeating character, return '$'.
