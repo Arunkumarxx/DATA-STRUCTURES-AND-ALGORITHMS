87f0e124c3180d2f95d3e0089802193d84fc32a7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 10:45:58 +0530

    modified

--- STRING/IsomorphicStrings.java
+++ STRING/IsomorphicStrings.java
@@ -2,6 +2,12 @@ package STRING;
 
 public class IsomorphicStrings {
     public static void main(String[] args) {
+        String str1= "aab";
+        String str2= "xxy";
+        System.out.println(IsIsomorphicString(str1,str2));
+    }
+    private static boolean IsIsomorphicString(String str1,String str2) {
         
+        return false;
     }
 }
