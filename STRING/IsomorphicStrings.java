46953bd031c34076db372165cc6e6e34086e2975
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 11:10:44 +0530

    modified

--- STRING/IsomorphicStrings.java
+++ STRING/IsomorphicStrings.java
@@ -11,22 +11,6 @@ public class IsomorphicStrings {
     private static boolean IsIsomorphicString(String str1,String str2) {
         if(str1.length()!=str2.length())
             return false;
-        int n1=str1.length();
-        int n2=str2.length();
-        HashMap<String,String> hashMap1= new HashMap<>();
-        for(int i=0; i<n1; ++i)
-        {
-            hashMap1.put(str1.charAt(i)+"",str2.charAt(i)+"");
-        }
-        HashMap<String,String> hashMap2 =new HashMap<>();
-        for(int i=0; i<n1; ++i)
-        {
-            hashMap2.put(str2.charAt(i)+"",str1.charAt(i)+"");
-        }
-        System.out.println(hashMap1);
-        System.out.println(hashMap2);
-        if(hashMap1.size()==hashMap2.size())
-            return true;
-        return false;
+        
     }
 }
