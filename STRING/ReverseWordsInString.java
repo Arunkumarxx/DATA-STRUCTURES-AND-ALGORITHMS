ea8777ff7a41ea83d1adaf6169b01c61c27a513f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-08 17:03:56 +0530

    modified

--- STRING/ReverseWordsInString.java
+++ STRING/ReverseWordsInString.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class ReverseWordsInString {
     public static void main(String[] args) {
-        StringBuilder stringBuilder =new StringBuilder("arun.kumar");
+        StringBuilder stringBuilder =new StringBuilder("i.like.this.program.very.much");
         ArrayList<ArrayList<StringBuilder>> arrayList =new ArrayList<>();
         int n=stringBuilder.length();
         StringBuilder temp =new StringBuilder();
