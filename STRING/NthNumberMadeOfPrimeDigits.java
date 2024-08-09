9d2b13d31fa17fc056e323a789cf7cb818d1864d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-09 15:56:02 +0530

    modified

--- STRING/NthNumberMadeOfPrimeDigits.java
+++ STRING/NthNumberMadeOfPrimeDigits.java
@@ -6,7 +6,14 @@ public class NthNumberMadeOfPrimeDigits {
         System.out.println(primeDigits(n));
     }
     private static int primeDigits(int n) {
-        
+        int [] arr ={2,3,5,7};
+        StringBuilder stringBuilder =new StringBuilder();
+        while(n>0)
+        {
+            stringBuilder.insert(0,arr[(n-1)%4]);
+            n=(n-1)/4;
+        }
+        System.out.println(stringBuilder);
         return 1;
     }
 }
