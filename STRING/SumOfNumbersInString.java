d4166a7b6a3c1cea48d29e69efbf3c04c2aad821
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-09 07:26:44 +0530

    modified

--- STRING/SumOfNumbersInString.java
+++ STRING/SumOfNumbersInString.java
@@ -1,7 +1,20 @@
 package STRING;
 
+import java.util.ArrayList;
+
 public class SumOfNumbersInString {
     public static void main(String[] args) {
-        
+        String str ="1abc23";
+        System.out.println(findSum(str));
+    }
+    private static long findSum(String str)
+    {
+        ArrayList<String> arrayList =new ArrayList<>();
+        int n=str.length();
+        for(int i=0; i<n; ++i)
+        {
+            
+        }
+        return 0;
     }
 }
