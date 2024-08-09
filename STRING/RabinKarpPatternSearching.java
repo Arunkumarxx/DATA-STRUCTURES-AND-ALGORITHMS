6420699e6e6d59663d43f97382b1d9faebe18fb4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-09 19:11:17 +0530

    modified

--- STRING/RabinKarpPatternSearching.java
+++ STRING/RabinKarpPatternSearching.java
@@ -33,8 +33,6 @@ public class RabinKarpPatternSearching {
                 }
                 if(isPatternFound)
                     return true;
-                
-            }
         if(i<n1-n2)
         {
             stringHash-=(str.charAt(i)-'a'+1);
