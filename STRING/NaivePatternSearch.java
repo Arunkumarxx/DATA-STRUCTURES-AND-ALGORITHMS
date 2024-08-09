74dd969af8e010f7cfc3d39488765762a2dd3d82
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-09 11:33:21 +0530

    modified

--- STRING/NaivePatternSearch.java
+++ STRING/NaivePatternSearch.java
@@ -1,5 +1,7 @@
 package STRING;
 
+import java.util.ArrayList;
+
 public class NaivePatternSearch {
     public static void main(String[] args) {
         String str="GEEKFORGEEKS";
@@ -9,14 +11,19 @@ public class NaivePatternSearch {
     private static boolean search(String str, String pattern)
     {
         int n=str.length();
+        ArrayList<Integer> indList =new ArrayList<>();
         for(int i=0; i<n-pattern.length(); ++i)
         {
             if(str.charAt(i)==pattern.charAt(0))
             {
+                boolean coundValid=true;
                 for(int j=0; j<pattern.length(); ++j)
                 {
                     if(str.charAt(i+j)!=pattern.charAt(j))
-                        break;
+                        coundValid=false;
+                }
+                if(coundValid) {
+                    indList.add(i);
                 }
             }
         }
