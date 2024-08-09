76af66203b8db8800108db9701484a23168cc5e8
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-09 17:12:37 +0530

    modified

--- STRING/RabinKarpPatternSearching.java
+++ STRING/RabinKarpPatternSearching.java
@@ -7,7 +7,23 @@ public class RabinKarpPatternSearching {
         System.out.println(RabinKarp_PatternSearching(str,pattern));
     }
     private static  boolean RabinKarp_PatternSearching(String str,String pattern) {
-        
+       int n1=str.length();
+       int n2=pattern.length();
+
+       if(n2>n1)
+           return false;
+       int StringHash=HashValue(str);
+       int PatterHash=HashValue(str);
+
+        System.out.println(StringHash);
+        System.out.println(PatterHash);
+        return false;
+    }
+    private static int HashValue(String str) {
+        int hash=0;
+        for(char x:str.toCharArray())
+            hash+=(x-'a'+1);
+        return hash;
     }
 
 }
