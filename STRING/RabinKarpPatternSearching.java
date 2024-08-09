8a45603b474c79b1b3168a1202a6696bcbf46293
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-09 16:33:31 +0530

    modified

--- STRING/RabinKarpPatternSearching.java
+++ STRING/RabinKarpPatternSearching.java
@@ -2,8 +2,8 @@ package STRING;
 
 public class RabinKarpPatternSearching {
     public static void main(String[] args) {
-        String str= "aaBaacaadaabaaba";
-        String pattern = "aaBa";
+        String str= "aabaacaadaabaaba";
+        String pattern = "aaba";
         System.out.println(RabinKarp_PatternSearching(str,pattern));
     }
     private static  int RabinKarp_PatternSearching(String str,String pattern) {
