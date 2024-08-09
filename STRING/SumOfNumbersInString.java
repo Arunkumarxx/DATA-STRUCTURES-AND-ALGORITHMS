58ea6087d16c70b0bdd8ecaaf30ce6f705c8ab72
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-09 07:55:28 +0530

    modified

--- STRING/SumOfNumbersInString.java
+++ STRING/SumOfNumbersInString.java
@@ -12,8 +12,15 @@ public class SumOfNumbersInString {
         ArrayList<Integer> arrayList =new ArrayList<>();
         int n=str.length();
         StringBuilder stringBuilder = new StringBuilder();
-        int i=0;
-
+        for(int i=0; i<n; ++i)
+        {
+            char ch=str.charAt(i);
+            if(ch>='0' && ch<='9')
+                stringBuilder.append(ch);
+            else {
+                arrayList.add(Integer.parseInt(stringBuilder.toString()));
+            }
+        }
         return 0;
     }
 }
