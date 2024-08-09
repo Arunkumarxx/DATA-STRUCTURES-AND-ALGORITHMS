a9b65d2d7d86958e3b3633eb84a3ca95766b35c7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-09 11:31:00 +0530

    modified

--- STRING/NaivePatternSearch.java
+++ STRING/NaivePatternSearch.java
@@ -3,6 +3,23 @@ package STRING;
 public class NaivePatternSearch {
     public static void main(String[] args) {
         String str="GEEKFORGEEKS";
+        String target ="aaba";
+        System.out.println(search(str,target));
     }
-    private 
+    private static boolean search(String str, String pattern)
+    {
+        int n=str.length();
+        for(int i=0; i<n-pattern.length(); ++i)
+        {
+            if(str.charAt(i)==pattern.charAt(0))
+            {
+                for(int j=0; j<pattern.length(); ++j)
+                {
+                    if(str.charAt(i+j)!=pattern.charAt(j))
+                        break;
+                }
+            }
+        }
+        return true;
+    }
 }
