cb3fd173dbc482d872febaabd9e22e8d146dd751
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-09 16:53:44 +0530

    modified

--- STRING/RabinKarpPatternSearching.java
+++ STRING/RabinKarpPatternSearching.java
@@ -3,7 +3,7 @@ package STRING;
 public class RabinKarpPatternSearching {
     public static void main(String[] args) {
         String str= "aabaacaadaabaaba";
-        String pattern = "aaba";
+        String pattern = "Aaba";
         System.out.println(RabinKarp_PatternSearching(str,pattern));
     }
     private static  boolean RabinKarp_PatternSearching(String str,String pattern) {
