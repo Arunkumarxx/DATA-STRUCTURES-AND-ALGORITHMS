56fa4a7b4ca78935b0076ce5a6d5d22105fb1c61
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-10 20:08:07 +0530

    modified

--- STRING/KMPAlgorithmPatternSearching.java
+++ STRING/KMPAlgorithmPatternSearching.java
@@ -4,45 +4,6 @@ public class KMPAlgorithmPatternSearching {
     public static void main(String[] args) {
         String str ="aaba";
         String pattern ="aba";
-        System.out.println(kmp(str,pattern));
     }
-    private static boolean  kmp(String str,String pattern) {
-        if(pattern.length()>str.length())
-            return false;
-        int n1=str.length();
-        int n2=pattern.length();
-        int i=0 ,j=0;
-        while(i<n1 && j<n2)
-        {
-            if(j==n2-1)
-                return true;
-            if(str.charAt(i)==pattern.charAt(j))
-            {
-                ++i;
-                ++j;
-                if(j==n2)
-                    return true;
-                continue;
-            }
-            else {
-                int ind=longestPrefix(str,pattern,i,j);
-                if(ind!=-1)
-                {
-                    j=ind;
-                }
-                else
-                    j=0;
-            }
-            ++i;
-        }
-        return false;
-    }
-    private static int  longestPrefix(String str,String pattern,int i,int j) {
-        for(int k=j;k>=0; --k)
-        {
-            if(str.charAt(i)==pattern.charAt(k))
-                return k;
-        }
-        return -1;
-    }
+    private static 
 }
