6459b452b2a40004ea7bb2a15cabc900ddcbf463
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-10 10:57:17 +0530

    modified

--- STRING/KMPAlgorithmPatternSearching.java
+++ STRING/KMPAlgorithmPatternSearching.java
@@ -4,8 +4,9 @@ public class KMPAlgorithmPatternSearching {
     public static void main(String[] args) {
         String str ="aabaacaadaabaaba";
         String pattern ="aaaab";
+        System.out.println(kmp(str,pattern));
     }
-    private boolean  kmp(String str,String pattern) {
+    private static boolean  kmp(String str,String pattern) {
         int n1=str.length();
         int n2=pattern.length();
         int i=0 ,j=0;
