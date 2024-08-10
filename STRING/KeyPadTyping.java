37cfd3822d65ca4305acfc6c1794387fe405f24c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-10 09:24:47 +0530

    modified

--- STRING/KeyPadTyping.java
+++ STRING/KeyPadTyping.java
@@ -2,10 +2,12 @@ package STRING;
 
 public class KeyPadTyping {
     public static void main(String[] args) {
-        
+        String str ="geeksforgeeks";
+        System.out.println(printNumber(str,str.length()));
     }
     private static String printNumber(String s, int n)
     {
+        
         return "";
     }
 }
