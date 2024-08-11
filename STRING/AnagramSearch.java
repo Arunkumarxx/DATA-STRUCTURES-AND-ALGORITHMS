165fd00bbeb0b89f498e7b1c1641d7435c472657
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 21:02:57 +0530

    modified

--- STRING/AnagramSearch.java
+++ STRING/AnagramSearch.java
@@ -1,57 +1,61 @@
 package STRING;
 
-import java.util.Arrays;
-
 public class AnagramSearch {
 
     public static void main(String[] args) {
-        String txt="forxxorfxdofr";
-        String pattern ="for";
-        System.out.println(search(txt,pattern));
+        String txt = "forxxorfxdofr";
+        String pattern = "for";
+        System.out.println(search(txt, pattern));  // Output: 3
     }
-    private static int search(String str,String pattern) {
-        int n1=str.length();
-        int n2=pattern.length();
 
-        if(n2>n1) return 0;
+    private static int search(String txt, String pattern) {
+        int n1 = txt.length();
+        int n2 = pattern.length();
 
-        int result=0;
-        int [] windowHash  =new int[26];
-        int [] patternHash = new int[26];
+        // Edge case: if pattern is longer than the text
+        if (n2 > n1) return 0;
 
-        for(int j=0; j<n2; ++j)
-            --patternHash[pattern.charAt(j)-'a'];
+        int[] patternHash = new int[26];
+        int[] windowHash = new int[26];
+        int result = 0;
 
-        for(int i=0; i<n2; ++i)
-            ++windowHash[str.charAt(i)-'a'];
-
-        if(isMatchFound(windowHash,patternHash))
-            ++result;
-        for(int i=n2; i<n1; ++i)
-        {
-            --windowHash[str.charAt(i-n2)-'a'];
-            ++windowHash[str.charAt(i)-'a'];
-            if(isMatchFound(windowHash,patternHash))
-                ++result;
+        // Fill patternHash with frequency of characters in the pattern
+        for (int i = 0; i < n2; ++i) {
+            patternHash[pattern.charAt(i) - 'a']++;
         }
-        return result;
-    }
-    private static boolean isMatchFound(int[] windowHash, int[] patternHash) {
-        int wc=0;
-        int wh=0;
-        for (int i = 0; i < 26; ++i) {
-            if(windowHash[i]!=0 || patternHash[i]!=0)
-            {
-                if(windowHash[i]==patternHash[i])
-                {
-                    ++wc;
-                    ++wh;
-                }
 
+        // Fill windowHash with frequency of characters in the initial window
+        for (int i = 0; i < n2; ++i) {
+            windowHash[txt.charAt(i) - 'a']++;
+        }
+
+        // Check the initial window
+        if (areHashesEqual(windowHash, patternHash)) {
+            result++;
+        }
+
+        // Slide the window across the text
+        for (int i = n2; i < n1; ++i) {
+            // Add new character to the window
+            windowHash[txt.charAt(i) - 'a']++;
+            // Remove old character from the window
+            windowHash[txt.charAt(i - n2) - 'a']--;
+            // Check if the new window matches the pattern
+            if (areHashesEqual(windowHash, patternHash)) {
+                result++;
             }
         }
-        if(wc==wh)
-            return true;
-        return false;
+
+        return result;
+    }
+
+    // Helper function to check if two hash arrays are equal
+    private static boolean areHashesEqual(int[] windowHash, int[] patternHash) {
+        for (int i = 0; i < 26; ++i) {
+            if (windowHash[i] != patternHash[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
