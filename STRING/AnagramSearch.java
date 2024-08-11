2a97acc220773c4dd79b342b806513ab31e159f9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 17:31:26 +0530

    modified

--- STRING/AnagramSearch.java
+++ STRING/AnagramSearch.java
@@ -12,6 +12,7 @@ public class AnagramSearch {
         int k=pattern.length();
         int hash1=pattern.hashCode();
         int hash2=str.substring(0,pattern.length()).hashCode();
+        System.out.println(str.substring(0,2));
         System.out.println(hash1);
 
         return 0;
