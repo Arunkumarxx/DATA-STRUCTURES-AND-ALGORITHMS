e7933897edfd23492858b0929bf66df7e695cca8
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 14:18:02 +0530

    modified

--- STRING/StringRotatedByTwoPlaces.java
+++ STRING/StringRotatedByTwoPlaces.java
@@ -2,6 +2,8 @@ package STRING;
 
 public class StringRotatedByTwoPlaces {
     public static void main(String[] args) {
+        String s1 = "amazon";
+        String s2 = "azonam";
         
     }
 }
