af249109cd0c5003e67d5be7e234c9a3feb004e4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 16:14:24 +0530

    modified

--- STRING/AnagramSearch.java
+++ STRING/AnagramSearch.java
@@ -2,7 +2,7 @@ package STRING;
 
 public class AnagramSearch {
     public static void main(String[] args) {
-        String str ="bcA";
+        String str ="bca";
         String patt ="bca";
         boolean x=str.hashCode()==patt.hashCode();
         System.out.println(x);
