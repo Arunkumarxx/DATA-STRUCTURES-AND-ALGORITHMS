e14f6fec90b4eeff1a11286bf6bfb56aa170f7c1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 16:19:09 +0530

    modified

--- STRING/AnagramSearch.java
+++ STRING/AnagramSearch.java
@@ -4,6 +4,9 @@ public class AnagramSearch {
     public static void main(String[] args) {
         String str ="forxxorfxdofr";
         String patt ="for";
-        
+        int k=patt.length();
+        int hash=0;
+        int n=str.length();
+        for(int i=0; i<)
     }
 }
