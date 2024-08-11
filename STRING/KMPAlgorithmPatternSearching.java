9f021984a2cdea0ae5f936dd906e3f92305af5cc
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 10:04:28 +0530

    modified

--- STRING/KMPAlgorithmPatternSearching.java
+++ STRING/KMPAlgorithmPatternSearching.java
@@ -1,17 +1,18 @@
 package STRING;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class KMPAlgorithmPatternSearching {
     public static void main(String[] args) {
         String str ="aaba";
-        String pattern ="aba";
+        String pattern ="ababaaba";
         int [] lps =new int[str.length()];
         BuildLongestPrefixSuffix(str,str.length(),lps);
     }
     private static void BuildLongestPrefixSuffix(String str,int n,int [] lps) {
         int i=0 ,j=1;
-        System.out.println(lps.toString());
+        System.out.println(Arrays.toString(lps));
         while(i<j && j<n)
         {
             i++;
