36e5dd778dcb1990cb8dca9c60411cb37a337027
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 21:26:11 +0530

    modified

--- STRING/PanagramChecking.java
+++ STRING/PanagramChecking.java
@@ -2,7 +2,10 @@ package STRING;
 
 public class PanagramChecking {
     public static void main(String[] args) {
+        String str="";
+    }
+    public static boolean checkPangram  (String s) {
 
     }
-    
+
 }
