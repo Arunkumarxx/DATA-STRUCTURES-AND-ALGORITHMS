b52b32d9f3fad64cb3f9d66eb7a6606629f6ea2d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 16:28:55 +0530

    modified

--- STRING/AnagramSearch.java
+++ STRING/AnagramSearch.java
@@ -5,10 +5,19 @@ public class AnagramSearch {
         String str ="forxxorfxdofr";
         String patt ="for";
         int k=patt.length();
-        int hash=0;
+        int hash1=0;
+        int hash2=0;
         int n=str.length();
-        for(int i=0; i<k;++i)
-            hash+=str.charAt(i)-'a';
-        System.out.println(hash);
+
+        for(int i=0; i<k;++i) {
+            hash1 += patt.charAt(i) - 'a';
+            hash2+= str.charAt(i)-'a';
+        }
+        int count=0;
+        if(hash1== hash2)
+            ++count;
+
+        
+
     }
 }
