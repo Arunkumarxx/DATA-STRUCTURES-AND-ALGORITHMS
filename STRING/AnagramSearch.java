63aa169fcaa022dee55d30666b0bd4400e034f2e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 18:26:18 +0530

    modified

--- STRING/AnagramSearch.java
+++ STRING/AnagramSearch.java
@@ -2,9 +2,14 @@ package STRING;
 
 public class AnagramSearch {
     public static void main(String[] args) {
-        String txt= "";
+        String txt="forxxorfxdofr";
+        String pattern ="for";
     }
-    private static int search() {
+    private static int search(String str,String pattern) {
+        int n1=str.length();
+        int n2=pattern.length();
+        if(n2>n1) return 
+        int [] windowHash =new int[]
+    }
 
-    }
 }
