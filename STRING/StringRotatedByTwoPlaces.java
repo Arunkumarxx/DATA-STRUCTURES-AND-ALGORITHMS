f7f14056a2e27cd3691facce8cc09db13cefc784
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 14:35:34 +0530

    modified

--- STRING/StringRotatedByTwoPlaces.java
+++ STRING/StringRotatedByTwoPlaces.java
@@ -7,13 +7,8 @@ public class StringRotatedByTwoPlaces {
         System.out.println(isRotatedByTwoPlaces(s1,s2));
     }
     private static boolean isRotatedByTwoPlaces(String s1,String s2) {
-        StringBuilder stringBuilder =new StringBuilder(s1);
-        stringBuilder.delete(0,2);
-        stringBuilder.append(s1.charAt(0));
-        stringBuilder.append(s1.charAt(1));
-        if(stringBuilder.equals(s2))
-            return true;
-        System.out.println(stringBuilder);
+        String conditionOne= s1.substring(2)+s1.substring(0,2);
+        System.out.println(conditionOne);
         return false;
     }
 }
