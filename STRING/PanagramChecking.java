93234e610f2262419b1a6339765884e20ececc32
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 21:45:53 +0530

    modified

--- STRING/PanagramChecking.java
+++ STRING/PanagramChecking.java
@@ -8,15 +8,15 @@ public class PanagramChecking {
         System.out.println(checkPangram(str));
     }
     public static boolean checkPangram  (String s) {
-        int[] ALPHABET = new int[26];
+        int[] freq = new int[26];
         for (int i = 0; i < s.length(); ++i) {
             if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
-                ++ALPHABET[s.charAt(i) - 'a'];
+                ++freq[s.charAt(i) - 'a'];
             } else if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
-                ++ALPHABET[Character.toLowerCase(s.charAt(i)) - 'a'];
+                ++freq[Character.toLowerCase(s.charAt(i)) - 'a'];
             }
         }
-        for(int x:ALPHABET)
+        for(int x:freq)
             if(x==0)
                 return false;
         return true;
