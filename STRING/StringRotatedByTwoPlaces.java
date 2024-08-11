de720274c5052b99b77439dbe70076bfa74870c7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 14:23:01 +0530

    modified

--- STRING/StringRotatedByTwoPlaces.java
+++ STRING/StringRotatedByTwoPlaces.java
@@ -7,7 +7,7 @@ public class StringRotatedByTwoPlaces {
         System.out.println(s1.charAt(0)==s2.charAt(s2.length()-2) &&( s1.charAt(1)==s2.charAt(s2.length()-1)));
         /*
         daxjheq
-eqdaxjh
+         eqdaxjh
          */
     }
 }
