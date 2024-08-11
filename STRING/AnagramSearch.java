6cc33a81928124ec4839ed9cc9b1228441cbfcbc
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 19:11:34 +0530

    modified

--- STRING/AnagramSearch.java
+++ STRING/AnagramSearch.java
@@ -18,34 +18,17 @@ public class AnagramSearch {
         int k=pattern.length();
         if(n2>n1) return -1;
 
-        for(int i=0; i<n2; ++i) {
+        for(int i=0; i<26; ++i) {
             patternHash[pattern.charAt(i) - 'a']++;
             windowHash[str.charAt(i)-'a']++;
         }
-        int patternHashCount = 0;
-        for(int i=0; i<patternHash.length; ++i)
-            patternHashCount+=patternHash[i];
-        System.out.println(patternHashCount);
-//        System.out.println(n1-k);
-//        System.out.println(n1);
-//        System.out.println(n1-(k+1));
-
-        for(int i=1; i<=n1-k;++i)
-        {
-
-        }
-        System.out.println(Arrays.toString(windowHash));
-        System.out.println(Arrays.toString(patternHash));
-
-        return -1;
-    }
-    private static int  isPatternFoundCount() {
-        int patterCount=0;
+        int windowHashCount=0;
         for(int i=0; i<26; ++i)
         {
-            if(patternHash[i]>0 && windowHash[i]==patternHash[i])
-                ++patterCount;
+            windowHashCount+=windowHash[i];
         }
-        return patterCount;
+        System.out.println(windowHashCount);
+        return -1;
     }
+
 }
