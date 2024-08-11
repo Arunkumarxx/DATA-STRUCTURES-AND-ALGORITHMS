0b39d12945f8c491e35cecdfbe216409b852048f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 12:29:13 +0530

    modified

--- STRING/CheckIfStringsAreRotations.java
+++ STRING/CheckIfStringsAreRotations.java
@@ -2,6 +2,8 @@ package STRING;
 
 public class CheckIfStringsAreRotations {
     public static void main(String[] args) {
+        String a= "amazon";
+        String b= "azonam";
         
     }
 }
