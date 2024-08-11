f25ef834b853926b9dab07874a0ac1b0832f4993
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 17:05:05 +0530

    modified

--- STRING/AnagramSearch.java
+++ STRING/AnagramSearch.java
@@ -2,8 +2,9 @@ package STRING;
 
 public class AnagramSearch {
     public static void main(String[] args) {
-        String str = "a1b2c3d4e5f";
-        String patt = "2c3";
+        String str = "AB";
+        String patt = "ABC";
+
         System.out.println(SlidingWindowWithHashing(str,patt));
     }
     private static int SlidingWindowWithHashing(String str,String pattern) {
