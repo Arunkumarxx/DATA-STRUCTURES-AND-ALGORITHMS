350ad391f2afec6555b113394fe8899746ce8b85
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 17:20:48 +0530

    modified

--- STRING/AnagramSearch.java
+++ STRING/AnagramSearch.java
@@ -3,7 +3,7 @@ package STRING;
 public class AnagramSearch {
     public static void main(String[] args) {
         String str = "zbcoejuvpvaboyg";
-        String patt = "for";
+        String patt = "po";
         System.out.println(SlidingWindowWithHashing(str,patt));
     }
     private static int SlidingWindowWithHashing(String str,String pattern) {
