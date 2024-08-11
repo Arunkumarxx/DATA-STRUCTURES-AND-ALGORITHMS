1cf639a0ad6f73fe9cedf555e4064735ace8fdd8
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 17:58:13 +0530

    modified

--- STRING/AnagramSearch.java
+++ STRING/AnagramSearch.java
@@ -2,52 +2,13 @@ package STRING;
 
 public class AnagramSearch {
     public static void main(String[] args) {
-        String str = "gvdzvbswujtwzpvqbogzjsrfqcuuxmpczmprhmuamfuqchosbqdujaxvxpujruczfdqqzzuavomjqohpvqpaufsmcsbmruxmgbhcexuxqwhxhfttvjhyzdevfoxujdmzqhxvamqrhvamxzuouopbusubsjzqcqfcrpmpgfcdxcrzxhfbpuhacramjqzqquzjouxmprugduqocpsfsmcvvbmxzhkzjdyvfmbfoipdmeosscqzpvhurumzarpzcvmcgjuxjdqabfpqoomqfmuubhxancnafsxzbubebvnfhlmpctfwmaubbqhpqquafzucpdupcoxzjgvvfxmqrzcoushrjmmsxhujmjpfvsgqopsucozardbzqmrucpbfmucuhqqzmxavcjvofacxfbsuxbqsqhpahuzgqmmrdpvjmcomuqzupuzrrpuusahcvvmhxjzufmfzacrbjqsummrxoqzgocqpdbupqdtsmozwsradrbfapoocvuugbzxczmhuufphmscujxzqvmpsmjqqqqjupfjbqhbupfqpsmamhzcavgoqomxudvszxruzmrccumbpzrhzfpajouqcgjozvrcpuvxqaduhcmmfuqbxmqsusqbqqcjupouvomhqudhucjpmzsragmfzzxrfsmcpbxvaulikzhvlmjpnouuropmsuqoxhfmjcqprfucmxuzavjzsqpbbqzaugumvhcdjoazruqqmufbsmbcapfzmczpduprxogsjmvxhvuchuqqqvrcfoujusmbzsurmhqxmdzfopqcauzubmqpjxcgphvadhzpxxkmnjlcajmxdouvoamscqxbpgmuvphqhrfzqucbsmujpczzqurfajiobhsbwktbccspqjuzvfmbvpmqmcxzhqquxzabamgroduphcfjrsouuhhsqpqmjszfcvdbugpcoauxczxbzqvuufraqujopmmmrlrnragnldauovgnxqudpjqcbpcqpzoghscrxujmomuzffxauasqhrmmvzqvubwlmhdrpgcozczmhruqaumxspfcfjqbqvuuozbasxjpvqumvhjuuhqzadbcmgcrfuqozcmbrsmoxuspjxmpufvzqapqqrjuspcppmuhqbodmszcvmjzfgqaamxubuqhzcvruxofamvudpbxhzozocqumuqjvsfpqrmfxgujphcmcrsauzbquahqpjsqzqsxczuvubzgucbfrxjoqocmvpummpmdarfhqrgrydabsbvgzosqasqurmmxqbpzuuhrofdujxucccpfjbmmzhpqavxuhaqmfmvxmmvdqpugrscqzbrjojshcqbuozpazfpuucxzvuzuqdhmbmxcjuogzbfspvcqqmfocpuasjpramuqrhkcusmgpqzbqprvuhazdmhjmsxcqfzjfcapuxorvqobmuuwwfkeumrxouslhxwjpvmsrozdvcbjgufpaumuxfrhhzmcmxpbcsazoqjqqquutzrwotonqzzoavsvrdfhsaczqpmghpjqujbmruxqpmcfoumbcuxusjvqoaufbcumcmhmjbzxrphzqrpuqxasufqpugdoczmvuzxhhsrjizszbaczjvcoxmdzjvhfcsmaqhxpfmsuqruuumbqoqgurppbfzdxjvvcpufmsmaacqzguoppouxqjbrhmrusqquczmhzgoubzmpjpuszvovdmcauhuqpxmfxqucrbqazghsfjqrmcnwhrszzugcqcmvdqcjzbhhqmfaupsxvoamfuxmuqpburjprocuzkmqazrqvrbmmospufacgzuuxcupjhqsqbxzphcjufvdomqhgmrucummzhzxupoqcopjavmfdxfusurqqbcjzspvabaovxvguxpmuhzqjzqumcqrafrudpubbpjsqhfmszmoccibsvcowzauxsuomzahprjccgduzzbqjqsomqvvbprmhcqmxfpuuffvymutvmbqqhpzjqqamdrxmprszuucgzxcmuhfpavvjsuobocufmdhzmsuupzjccraqqxbfufomuzjgpxbcasopqvrmvqhuodjujzpgfcrzmqxuumpqqcxupmhubasqzfcrvbovmshakdwvsocqjdufqgmqqhxpbrhupoufmuaxcrzmbzpavumzcjspsalpikolqzcggddlxmiuivnqhfzozgjuvfcmpcpxqsqhcxuhabpqmujrobzaqurmsmduvkzgenayzbwfesfsgfozpeynzsummepjcjpzafbuhqmmxuqczoxqgpqbmuscrauprodhzmfvusvoqptsofucbxrbqcqmhhupzzpsvfuusfqvcajzromampugdmqojxbxayipirvvxcmbjspquxmqcosuzzapucqrbmgouqhjmuhffavdrzprqsjbmvhacuuqhmxoqbcpjpfpurzgzuzvxsaocmmfudqrxbkzbixjubvsmsjvqxcqzhudurazmmpuqpzgjhqxmucopouraffbchmxfvgzzmuuuocabmauqqmphpjfdsqqjozcrrusvcbxpekbjzvvtkbihwrbdelhlisorpjgpxudmcuhcoojhpvafxmqvsmuamcsfrzzuzqqbuqbxpefwtihobmvmqncebqteznmmyccukhdoirtdmnvrdzrmaxpjqubhcbguurmumoxqjvcuvspfqoqzshfzcapmaxquzcjucomrfxvuuzzbpbqvhmjraugpsfpmhqoqdmcsmpozmmejthbuucsxqbpzrpmuqugodrfjhzcqhzxvpavsaubfummjmcoqjarxrvzqompqmjcpxfmcpvgusafsmquuohbubhqcuzzdmpuzaqqhcsavrgfzzjxbmrchupxdsfvumbmquoupjcqohhzxbsjdpqqourmqqvccpujaufmrmovmupczfgbasxuzuzzsbmruqaqpxumozpmarccvmhbqphufxojujgvfdqcshpkclrfgvaxpfchqpzxqofmbohszagmbuzvupscmmuqdjrjruucqbquqzqchfsuxuocpmzpjsrgmvxuavzqahdubjmpfcomrdqcumpgoauxbzmjrumspbsjarmfvuqvxupozqhfchzqcbxvcklxxmpxfqawkrrtqqkoucszphqmgdxmujobvmuzsbfpmchqpfjuxaaqrucqzvrtfmiruyfixucmzfcpdvhqambrjqvuguomuphscqqxoaupszmxfrbjzufzpjsquvocazhsbgorjcfrxmbaphupqmqudmumqvzxcoaqdjuzaoxqupbcxmqmvpujhqrczsgfuhzrbucmspvmfposjuzcdmquuumvpxfbcmzogqmaaujvfcqzsbxrphrhqsxvuujuudrqhfbpcvpozzsfrmzmouaqpahxgjqcqbmcmmqfgjquxpaofhmajbbzoucrzxsvmqpsprcquhuvzmduccfuqzzumcosoqpfdujqamjvbgruhpcsxmbmrhuqzxvpasbmbvhvozmqzjuqxcdphumucrqraqcfxmupozsfagpujbkquujtyzaqhxsrmmrczuxjazbmdvuofqqzmoupvaupbccughqjspftdnpsauiuzjbqhjrxquouacumcbsovahzrpdmppqsfgumfzmvcqxkibgehsrybspcxuuzsvfazomudqqcargoxhmqrjzupjfbmbqpumhcvdvgvsuxpphzxcuobumdmrohcrfqmufqpqbzamqcujajszbwuacpiqjburmqoxgqzmppruhocbcuzfvpqvudhsxaqmzscmafujqzftaqxvmpcrzuosqjcouzbhcupzjqhvuuqsrfpmxmdmbfgaseususaqfvfmmzgzpduouqrchazphjxbjrcumxqqbcpovmfvhsmbrmdjuuzqaccxupavmfgxzpmqsoqchupqzojurbwuwlassjxqmzouujmrsbzgjqhfbazcuvuscdupvommprqhpqafxcxrombmzpcupvgqofchzxuqapmhqsqufdzuusmjjvabcrcjuyqzuqbdmxvrbammschxmuofjrzavpchcpuopzqsfgjuqubyzdfqoxhqcuqjmacrufsaqhobuzpxsdmzvpzmbrmgcpvuujnviomrruagjuvpmcjpcfvqxafmrhxsmoquushzzupqczobbdqmoirpyaoxbcofmqrvzhzppjcmxfjguvaqqbosmqudruhuzcmuaspqojhsouzasmagxfcrmmzbpdxuqphcbfpjmurcvzvquuqaqqodhwegxaopmxpzzhmqkirocqqxbmhpsovzrzafqazjcgurbmjcuhsmpfxupomvqudukwusyqrnpgaghfstqorpfoscubopvmqujbquuqzfqarxhmcacszmzjhpgxmdvuxacgbcvosmzpdmuvfhfqbuoxrpqqpjzcrumjhmqusuazojqbummhuumgbmfxvopjarxzrqsccqzpfavuudscpzhqkllznuchafrcgvaquhvcfumqbmzpuusqzboxjrzqspmopdjmxpmcqcpuqrfojbhzpuaufvmzmzbjduraqmxhsxgouscvqubpqahbzxufcmmjovcrqzcuavzqrmpoxjphsudqmfsugvtnnywlqqjbnpchczomqvmvrzgmcaxujbduufurqsmqbqxhszfappuojcoxvbbjzjrhpmuhzsazfrqdvxfumgumcqsqpmcauoqupsuudqschpqhavoabqrzmczpbjuqoxjpmrffxvugummczobhyfcvdcxzvqbpujjgmfapocxocumvfhruqamhmzqspusqruzdbbtcobxqxqhougmzbpspmuvdcfqmupjufajcvuszarzmhrqvjshxazamvcpduvqfbphbcuuczoqqgosfrpzjruxmjumqmpxcormujmcqjqsmfodusbzguaazvhqpfhmurqcpvzxubwazhughkpoxupqpbgzxauarmmbrdzfcfvuvscqqmpjmuhzjcqohuslclexozhfqxhvouaruqcpdcqubuqzcspouapjmrvfjxmmbzmsgxjkygpaiuzqfxwxnolmrbsjqquujuupafzvmammvzqqxgsphuzdmorchcpxfcbosujqucpamovmuzomdgzpfchcfbvapqzxqxqmubhsrjurtchqgwnrrxcujauhfsopxmcmjzumoaqzqvhrmfrbuppqusgvdqzbxc\n" +
-                "cujauhfsopxmcmjzumoaqzqvhrmfrbuppqusgvdqzbxc";
-        String patt = "cujauhfsopxmcmjzumoaqzqvhrmfrbuppqusgvdqzbxc";
-        System.out.println(SlidingWindowWithHashing(str,patt));
+        
     }
     private static int SlidingWindowWithHashing(String str, String pattern) {
-        int k = pattern.length();
-        int n = str.length();
-        if (k > n) {
-            return 0;
-        }
-        if (k == 1) {
-            return countOccurrences(str, pattern.charAt(0));
-        }
 
-        int hashPattern = 0;
-        int hashWindow = 0;
-        int count = 0;
-
-
-        for (int i = 0; i < k; ++i) {
-            hashPattern += pattern.charAt(i);
-            hashWindow += str.charAt(i);
-        }
-
-        if (hashPattern == hashWindow) {
-            count++;
-        }
-
-        for (int i = k; i < n; ++i) {
-            hashWindow += str.charAt(i) - str.charAt(i - k);
-            if (hashPattern == hashWindow) {
-                count++;
-            }
-        }
-
-        return count;
     }
 
     private static int countOccurrences(String str, char target) {
-        int count = 0;
-        for (int i = 0; i < str.length(); ++i) {
-            if (str.charAt(i) == target) {
-                count++;
-            }
-        }
-        return count;
+
     }
 }
