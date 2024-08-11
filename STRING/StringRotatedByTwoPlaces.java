a04c47fad31aa037e8c850546680951ed496e56d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 14:34:29 +0530

    modified

--- STRING/StringRotatedByTwoPlaces.java
+++ STRING/StringRotatedByTwoPlaces.java
@@ -4,7 +4,7 @@ public class StringRotatedByTwoPlaces {
     public static void main(String[] args) {
         String s1 = "amazon";
         String s2 = "azonam";
-
+        System.out.println(isRotatedByTwoPlaces(s1,s2));
     }
     private static boolean isRotatedByTwoPlaces(String s1,String s2) {
         StringBuilder stringBuilder =new StringBuilder(s1);
