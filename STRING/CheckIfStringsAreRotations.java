cc38b5da54c2bf9f789717fffe674f3f47ea47d7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 12:43:01 +0530

    modified

--- STRING/CheckIfStringsAreRotations.java
+++ STRING/CheckIfStringsAreRotations.java
@@ -1,13 +1,19 @@
 package STRING;
 
 public class CheckIfStringsAreRotations {
+
     public static void main(String[] args) {
         String s1="ABCD";
         String s2="CDAB";
     }
-    private static boolean isStringsAreRotations() {
+    private static boolean isStringsAreRotations(String s1,String s2) {
+        int n1=s1.length();
+        int n2=s2.length();
         StringBuilder stringBuilder =new StringBuilder();
-        
+        for(int i=0; i<n1; ++i)
+        {
+            
+        }
         return true;
     }
 }
