94367c50957ea555b071bd5175fd731416a8ea5c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 17:19:54 +0530

    modified

--- STRING/AnagramSearch.java
+++ STRING/AnagramSearch.java
@@ -21,10 +21,10 @@ public class AnagramSearch {
         int count=0;
         if(hash1== hash2)
             ++count;
-        for(int i=0; i<n-k; ++i)
+        for(int i=0; i<=n-k; ++i)
         {
             hash1-=(str.charAt(i)-'a');
-                hash1 += (str.charAt(i + k) - 'a');
+              if(i+k<n)  hash1 += (str.charAt(i + k) - 'a');
             if(hash1==hash2)
                 ++count;
         }
