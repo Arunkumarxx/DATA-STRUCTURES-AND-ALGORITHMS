824ec2d1e6fef27b6d01cfff8773c77443bfb938
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 12:55:39 +0530

    modified

--- STRING/CheckIfStringsAreRotations.java
+++ STRING/CheckIfStringsAreRotations.java
@@ -5,8 +5,8 @@ import MATRIX.SumOfUpperAndLowerTriangles;
 public class CheckIfStringsAreRotations {
 
     public static void main(String[] args) {
-        String s1="ABCD";
-        String s2="CDAB";
+        String s1="ABAAA";
+        String s2="BAAAA";
         System.out.println(isStringsAreRotations(s1,s2));
     }
     private static boolean isStringsAreRotations(String s1,String s2) {
