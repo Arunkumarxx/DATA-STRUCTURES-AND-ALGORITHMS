e73139790b448c4861a1342696cffb806d71e399
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 17:19:33 +0530

    modified

--- STRING/AnagramSearch.java
+++ STRING/AnagramSearch.java
@@ -2,7 +2,7 @@ package STRING;
 
 public class AnagramSearch {
     public static void main(String[] args) {
-        String str = "eaylnlfdxf";
+        String str = "forxxorfxdofr";
         String patt = "irc";
         System.out.println(SlidingWindowWithHashing(str,patt));
     }
