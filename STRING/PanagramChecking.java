91fc6af1598028251854e77a328eac1673fd294a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 21:27:32 +0530

    modified

--- STRING/PanagramChecking.java
+++ STRING/PanagramChecking.java
@@ -1,11 +1,14 @@
 package STRING;
 
+import java.util.HashMap;
+
 public class PanagramChecking {
+    static int [] ALPHABHET
     public static void main(String[] args) {
         String str="Bawds jog, flick quartz, vex nymph";
     }
     public static boolean checkPangram  (String s) {
-        
+
     }
 
 }
