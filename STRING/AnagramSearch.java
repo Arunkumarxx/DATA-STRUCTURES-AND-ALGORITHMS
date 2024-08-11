49e7baa1618b2be6815a62c481c6ce942b85c5c4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 18:43:19 +0530

    modified

--- STRING/AnagramSearch.java
+++ STRING/AnagramSearch.java
@@ -23,6 +23,7 @@ public class AnagramSearch {
             windowHash[str.charAt(i)-'a']++;
         }
         System.out.println(n1-k);
+        System.out.println(n);
         System.out.println(n1-(k+1));
         for(int i=1; i<=n1-k;++i)
         {
