e61b7d23c82f3f526acd7c824348f47f12b464cd
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 16:13:16 +0530

    modified

--- STRING/AnagramSearch.java
+++ STRING/AnagramSearch.java
@@ -4,6 +4,6 @@ public class AnagramSearch {
     public static void main(String[] args) {
         String str ="abc";
         String patt ="bca";
-        System.out.println("%b",str.hashCode()==patt.hashCode());
+        System.out.println("%b"str.hashCode()==patt.hashCode());
     }
 }
