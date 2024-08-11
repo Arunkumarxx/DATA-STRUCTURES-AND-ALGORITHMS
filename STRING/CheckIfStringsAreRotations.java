05716ffbd0923e235b7a8d8a77cf733c275f97be
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 13:32:49 +0530

    modified

--- STRING/CheckIfStringsAreRotations.java
+++ STRING/CheckIfStringsAreRotations.java
@@ -10,9 +10,28 @@ public class CheckIfStringsAreRotations {
         System.out.println(isStringsAreRotations(s1,s2));
     }
     private static boolean isStringsAreRotations(String s1,String s2) {
-        
-    }
-    private static void BuildLSP() {
 
+    }
+    private static void BuildLSP(String pattern,int [] lsp) {
+        int i=0;
+        lsp[i]=0;
+        int j=1;
+        int n=pattern.length();
+        while(j<n)
+        {
+            if(pattern.charAt(i)==pattern.charAt(j))
+            {
+                ++i;
+                ++j;
+            }
+            else {
+                if(i!=0)
+                    i=lsp[i-1];
+                else ++j;
+            }
+        }
+    }
+    private static boolean kmp(String str,String pattern) {
+        
     }
 }
