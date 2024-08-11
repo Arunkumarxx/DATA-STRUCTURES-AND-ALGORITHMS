f4180ba9c2a3dd0b3cedf3bf8accfdf29a7adf1c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 14:21:21 +0530

    modified

--- STRING/StringRotatedByTwoPlaces.java
+++ STRING/StringRotatedByTwoPlaces.java
@@ -4,6 +4,7 @@ public class StringRotatedByTwoPlaces {
     public static void main(String[] args) {
         String s1 = "amazon";
         String s2 = "azonam";
-        
+        System.out.println(s1.charAt(0)==s2.charAt(s2.length()-2) &&( s1.charAt(1)==s2.charAt(s2.length()-1)));
+
     }
 }
