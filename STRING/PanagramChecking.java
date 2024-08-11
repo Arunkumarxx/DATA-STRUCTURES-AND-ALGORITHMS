e3561790069b585740a6ee176a97c5752b5d6306
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 21:44:54 +0530

    modified

--- STRING/PanagramChecking.java
+++ STRING/PanagramChecking.java
@@ -4,7 +4,7 @@ public class PanagramChecking {
 
     public static void main(String[] args) {
 
-        String str="Aawds jog, flick quartz, vex nymph";
+        String str="Bawds jog, flick quartz, vex nymph";
         System.out.println(checkPangram(str));
     }
     public static boolean checkPangram  (String s) {
