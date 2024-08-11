90c8cf1d52f38c0d796f0214f5d407d53303496c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-11 17:02:58 +0530

    modified

--- STRING/AnagramSearch.java
+++ STRING/AnagramSearch.java
@@ -6,7 +6,27 @@ public class AnagramSearch {
         String patt ="ABCD";
 
     }
-    private static int SlidingWindowWithHashing(String str,String pattern,int ) {
+    private static int SlidingWindowWithHashing(String str,String pattern) {
+        int k=pattern.length();
+        int hash1=0;
+        int hash2=0;
+        int n=str.length();
 
+        for(int i=0; i<k;++i) {
+            hash1 += pattern.charAt(i) - 'a';
+            hash2+= str.charAt(i)-'a';
+        }
+        int count=0;
+        if(hash1== hash2)
+            ++count;
+        for(int i=0; i<n-k; ++i)
+        {
+            hash1-=(str.charAt(i)-'a');
+            hash1+=(str.charAt(i+k)-'a');
+            if(hash1==hash2) {
+                ++count;
+            }
+        }
+        return count;
     }
 }
