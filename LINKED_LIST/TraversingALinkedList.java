ca8db09903193f19e6ef9ad0707b6a6cc4cf89eb
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-16 19:32:21 +0530

    modified

--- LINKED_LIST/TraversingALinkedList.java
+++ LINKED_LIST/TraversingALinkedList.java
@@ -14,15 +14,10 @@ public class TraversingALinkedList {
             this.data=data;
             this.next=null;
         }
-        private static void  insertAtBegin(int data)
-        {
-            Node newNode =new Node(data);
-        }
     }
 
     public static void main(String[] args) {
-        TraversingALinkedList  list =new TraversingALinkedList();
-        insertAtBegin();
+        
     }
 
 }
