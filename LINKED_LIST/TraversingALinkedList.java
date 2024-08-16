1fa2d99230bf3d8b5773c1a773ea6a4281378e31
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-16 20:05:49 +0530

    modified

--- LINKED_LIST/TraversingALinkedList.java
+++ LINKED_LIST/TraversingALinkedList.java
@@ -1,44 +1,45 @@
 package LINKED_LIST;
 
-class Node {
-    int data;
-    Node next;
-
-    Node(int data)
-    {
-        this.data=data;
-        this.next=null;
-    }
-    Node()
-    {}
-}
-
 public class TraversingALinkedList {
-    private Node head;
-    private  void insertAtBegin (int data)
-    {
-        Node newNode = new  Node(data);
-        newNode.next=head;
-        head=newNode;
+
+    private static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        Node() {}
     }
-    private void printList()
-    {
-        Node itr=head;
-        while(itr!=null)
-        {
+
+    private Node head;
+
+
+    private void insertAtBegin(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+
+    private void printList() {
+        Node itr = head;
+        while (itr != null) {
             System.out.println(itr.data);
-            itr=itr.next;
+            itr = itr.next;
         }
     }
+
     public static void main(String[] args) {
-        TraversingALinkedList list  =new TraversingALinkedList();
-        Node node =new Node();
+        TraversingALinkedList list = new TraversingALinkedList();
+
         list.insertAtBegin(10);
         list.insertAtBegin(20);
         list.insertAtBegin(30);
         list.insertAtBegin(40);
+
         list.printList();
-
     }
-
 }
