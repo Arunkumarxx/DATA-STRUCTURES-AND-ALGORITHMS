2a8eac20025198c08d465b2b68aab79858f313b6
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-17 22:25:45 +0530

    modified

--- LINKED_LIST/DoublyLinkedListInsertionAtGivenPosition.java
+++ LINKED_LIST/DoublyLinkedListInsertionAtGivenPosition.java
@@ -1,2 +1,26 @@
-package LINKED_LIST;public class DoublyLinkedListInsertionAtGivenPosition {
+package LINKED_LIST;
+
+public class DoublyLinkedListInsertionAtGivenPosition {
+
+    private static Node head;
+    private static Node tail;
+
+    private static class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    private static void insertAtPos(int data)
+    {
+
+    }
+    public static void main(String[] args) {
+        insertAtPos();
+    }
 }
