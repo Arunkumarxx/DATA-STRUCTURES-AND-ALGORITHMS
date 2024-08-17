fa56d62e35a4d03061189dd3890f4dcf04c29ed3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-17 13:31:18 +0530

    modified

--- LINKED_LIST/InsertAtThenEndOfSinglyLinkedList.java
+++ LINKED_LIST/InsertAtThenEndOfSinglyLinkedList.java
@@ -1,7 +1,22 @@
 package LINKED_LIST;
 
 public class InsertAtThenEndOfSinglyLinkedList {
+    private static class Node
+    {
+        int data;
+        Node next;
+        Node(int data)
+        {
+            this.data=data;
+            next=null;
+        }
+        Node()
+        {
+
+        }
+    }
     public static void main(String[] args) {
-        
+
+
     }
 }
