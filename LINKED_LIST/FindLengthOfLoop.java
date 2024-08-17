34a08ee4e1cf30a047420ad866b44177a5331399
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-17 11:55:55 +0530

    modified

--- LINKED_LIST/FindLengthOfLoop.java
+++ LINKED_LIST/FindLengthOfLoop.java
@@ -69,7 +69,7 @@ public class FindLengthOfLoop {
         }
     }
     public static void main(String[] args) {
-        for(int i=1; i<=5; ++i)
+        for(int i=1; i<=10; ++i)
         {
             insertAtEnd(i);
         }
