ca4c433b7be08bde0ab15664792709443e7126cd
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-17 15:21:54 +0530

    modified

--- LINKED_LIST/DeleteFirstNodeOfSinglyLinkedList.java
+++ LINKED_LIST/DeleteFirstNodeOfSinglyLinkedList.java
@@ -34,7 +34,7 @@ public class DeleteFirstNodeOfSinglyLinkedList {
         head=head.next;
     }
     public static void main(String[] args) {
-        for(int i=0; i<=10; ++i)
+        for(int i=0; i<=2; ++i)
             insert(i);
         printList();
         DeleteFirst();
