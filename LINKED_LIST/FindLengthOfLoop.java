398b084eaf8f65952462109f959e3c2dd88e123a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-17 10:22:42 +0530

    modified

--- LINKED_LIST/FindLengthOfLoop.java
+++ LINKED_LIST/FindLengthOfLoop.java
@@ -1,2 +1,24 @@
-package LINKED_LIST;public class FindLengthOfLoop {
+package LINKED_LIST;
+
+public class FindLengthOfLoop {
+    private static class Node
+    {
+        int data;
+        Node next;
+
+        Node()
+        {
+        }
+        Node(int data)
+        {
+            this.data=data;
+        }
+    }
+    private static void insertAtBegin(int data)
+    {
+        
+    }
+    public static void main(String[] args) {
+        insertAtBegin(1);
+    }
 }
