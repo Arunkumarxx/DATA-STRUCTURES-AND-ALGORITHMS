7ad89cf64e643fef2383b67346624e0f1907d880
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-17 21:57:38 +0530

    modified

--- LINKED_LIST/InsertionAtBeginOfLinkedList.java
+++ LINKED_LIST/InsertionAtBeginOfLinkedList.java
@@ -2,6 +2,19 @@ package LINKED_LIST;
 
 public class InsertionAtBeginOfLinkedList {
 
+    private static class Node
+    {
+        int data;
+        Node prev;
+        Node next;
+
+        Node(int data)
+        {
+         this.data=data;
+         this.prev=null;
+         this.next=null;
+        }
+    }
     public static void main(String[] args) {
         
     }
