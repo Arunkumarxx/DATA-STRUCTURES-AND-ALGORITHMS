5685c4ec6e4b9b7bc63ff26ba9899242f1984acd
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-17 12:16:28 +0530

    modified

--- LINKED_LIST/RecursiveTraversalOfSinglyLinkedList.java
+++ LINKED_LIST/RecursiveTraversalOfSinglyLinkedList.java
@@ -1,8 +1,34 @@
 package LINKED_LIST;
 
 public class RecursiveTraversalOfSinglyLinkedList {
+    private static Node head;
+    private static Node tail;
 
+    private static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private static void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
     public static void main(String[] args) {
-
+        insertAtEnd(10);
+        insertAtEnd(20);
+        insertAtEnd(30);
+        insertAtEnd(40);
+        
     }
 }
