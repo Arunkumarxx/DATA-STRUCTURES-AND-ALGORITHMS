94cbec95bd9aa36e670edda4cb01a20489dcb0b4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-17 15:47:33 +0530

    modified

--- LINKED_LIST/DeleteLastOfSinglyLinkedList.java
+++ LINKED_LIST/DeleteLastOfSinglyLinkedList.java
@@ -1,2 +1,20 @@
-package LINKED_LIST;public class DeleteLastOfSinglyLinkedList {
+package LINKED_LIST;
+
+public class DeleteLastOfSinglyLinkedList {
+    private static class Node
+    {
+        int data;
+        Node next;
+
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+        Node()
+        {}
+    }
+    public static void main(String[] args) {
+        
+    }
 }
