daea0b7940dd4fb8b96881d353c360831871afc1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-17 16:22:59 +0530

    modified

--- LINKED_LIST/SearchInALinkedListIterativeAndRecursive.java
+++ LINKED_LIST/SearchInALinkedListIterativeAndRecursive.java
@@ -1,6 +1,10 @@
 package LINKED_LIST;
 
 public class SearchInALinkedListIterativeAndRecursive {
+
+    private static Node head;
+    private static Node tail;
+
     private static class Node
     {
         int data;
@@ -9,9 +13,39 @@ public class SearchInALinkedListIterativeAndRecursive {
         Node(int data)
         {
             this.data=data;
+            this.next=null;
+        }
+        Node()
+        {
+
+        }
+    }
+
+    private static void insert(int data)
+    {
+        Node newNode =new Node(data);
+        if(head==null)
+        {
+            head=newNode;
+            tail=newNode;
+        }
+        else {
+            tail.next=newNode;
+            tail=newNode;
+        }
+    }
+    private static void printList()
+    {
+        Node temp=head;
+        while(temp!=null)
+        {
+            System.out.println(temp.data+" ");
+            temp=temp.next;
         }
     }
     public static void main(String[] args) {
-
+        for(int i=1; i<=15; ++i)
+            insert(i);
+        printList();
     }
 }
