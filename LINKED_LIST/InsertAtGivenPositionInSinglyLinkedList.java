06e3e1df28af7a35da4a8050eab4e7f34dedfd3b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-17 13:44:19 +0530

    modified

--- LINKED_LIST/InsertAtGivenPositionInSinglyLinkedList.java
+++ LINKED_LIST/InsertAtGivenPositionInSinglyLinkedList.java
@@ -1,2 +1,22 @@
-package LINKED_LIST;public class InsertAtGivenPositionInSinglyLinkedList {
+package LINKED_LIST;
+
+public class InsertAtGivenPositionInSinglyLinkedList {
+    private static class Node
+    {
+        int data;
+        Node next;
+
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+        Node()
+        {
+
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
 }
