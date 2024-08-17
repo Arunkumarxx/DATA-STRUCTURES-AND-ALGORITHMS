09f8fa13ed4834210c91a40ff59c04e55c937897
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-17 13:36:01 +0530

    modified

--- LINKED_LIST/InsertAtThenEndOfSinglyLinkedList.java
+++ LINKED_LIST/InsertAtThenEndOfSinglyLinkedList.java
@@ -1,6 +1,10 @@
 package LINKED_LIST;
 
 public class InsertAtThenEndOfSinglyLinkedList {
+
+    private static Node head;
+    private static Node tail;
+
     private static class Node
     {
         int data;
@@ -15,8 +19,31 @@ public class InsertAtThenEndOfSinglyLinkedList {
 
         }
     }
+    private static void insertAtEnd(int data)
+    {
+        Node newNode= new Node(data);
+        if(head==null)
+        {
+            head=newNode;
+            tail=newNode;
+        }
+        else {
+            tail.next=newNode;
+        }
+    }
+    private static void printList()
+    {
+        Node temp=head;
+        while(temp!=null)
+        {
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
     public static void main(String[] args) {
-
-
+        insertAtEnd(10);
+        insertAtEnd(20);
+        insertAtEnd(30);
+        printList();
     }
 }
