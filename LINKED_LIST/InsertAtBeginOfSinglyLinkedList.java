d757341cbbb200f7063596c78d7cf479099e4d09
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-17 13:17:38 +0530

    modified

--- LINKED_LIST/InsertAtBeginOfSinglyLinkedList.java
+++ LINKED_LIST/InsertAtBeginOfSinglyLinkedList.java
@@ -1,6 +1,21 @@
 package LINKED_LIST;
 
 public class InsertAtBeginOfSinglyLinkedList {
+
+    private static class Node
+    {
+        int data;
+        Node next;
+        Node()
+        {
+
+        }
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+    }
     public static void main(String[] args) {
         
     }
