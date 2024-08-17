722716bb5e401f1e2186676aade4e9d47afb0033
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-17 16:19:08 +0530

    modified

--- LINKED_LIST/SearchInALinkedListIterativeAndRecursive.java
+++ LINKED_LIST/SearchInALinkedListIterativeAndRecursive.java
@@ -1,8 +1,17 @@
 package LINKED_LIST;
 
 public class SearchInALinkedListIterativeAndRecursive {
+    private static class Node
+    {
+        int data;
+        Node next;
 
+        Node(int data)
+        {
+            this.data=data;
+        }
+    }
     public static void main(String[] args) {
-        
+
     }
 }
