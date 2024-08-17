20746396072b8af66195cb5f48d3503348dab49d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-17 11:59:21 +0530

    modified

--- LINKED_LIST/FindLengthOfLoop.java
+++ LINKED_LIST/FindLengthOfLoop.java
@@ -49,7 +49,7 @@ public class FindLengthOfLoop {
             tail.next=newNode;
             tail=newNode;
         }
-        if(data==2)
+        if(data==5)
         {
             Node loopAdd = search(head,2);
             if(loopAdd!=null)
