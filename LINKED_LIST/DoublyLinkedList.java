3f72a7a6f3abbd87a0fb829e947cb3f648a8e08d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-17 21:22:03 +0530

    modified

--- LINKED_LIST/DoublyLinkedList.java
+++ LINKED_LIST/DoublyLinkedList.java
@@ -2,8 +2,27 @@ package LINKED_LIST;
 
 public class DoublyLinkedList {
 
-    public static void main(String[] args) {
+    private static class Node
+    {
+        int data;
+        Node prev;
+        Node next;
+
+        Node(int data)
+        {
+            this.data=data;
+            this.prev=null;
+            this.next=null;
+        }
+        Node()
+        {}
+    }
+    private static void insert(int data)
+    {
         
+    }
+    public static void main(String[] args) {
+        insert(10);
     }
 
 }
