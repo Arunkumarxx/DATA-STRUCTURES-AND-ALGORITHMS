042a781e3fa8ff9c184ba1a52a739a13d62811cd
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-18 12:47:17 +0530

    modified

--- LINKED_LIST/ReverseADoublyLinkedList.java
+++ LINKED_LIST/ReverseADoublyLinkedList.java
@@ -2,13 +2,52 @@ package LINKED_LIST;
 
 public class ReverseADoublyLinkedList {
 
+    private static Node head;
+    private static Node tail;
     private static class Node
     {
         int data;
-        Node head;
+        Node next;
+        Node prev;
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+            this.prev=null;
+        }
+        Node()
+        {
+
+        }
+    }
+    private static void insert(int data)
+    {
+        Node newNode =new Node(data);
+        if(head==null)
+        {
+            head=newNode;
+            tail=newNode;
+        }
+        else {
+            tail.next=newNode;
+            newNode.prev=tail;
+            tail=newNode;
+        }
+    }
+    private static void printList()
+    {
+        Node temp=head;
+        while(temp!=null)
+        {
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+        System.out.println();
     }
     public static void main(String[] args) {
-
+        for(int i=1; i<=10; ++i)
+            insert(i);
+        printList();
     }
 
 }
