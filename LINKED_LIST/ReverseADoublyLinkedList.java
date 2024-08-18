8b1350697425d2bf6d930c95c13a35d0f7d383f8
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-18 13:40:44 +0530

    modified

--- LINKED_LIST/ReverseADoublyLinkedList.java
+++ LINKED_LIST/ReverseADoublyLinkedList.java
@@ -37,7 +37,7 @@ public class ReverseADoublyLinkedList {
     private static void reverse()
     {
 
-        
+
     }
     private static void printList()
     {
--- LINKED_LIST/ReverseALinkedList.java
+++ LINKED_LIST/ReverseALinkedList.java
@@ -1,2 +1,19 @@
-package LINKED_LIST;public class ReverseALinkedList {
+package LINKED_LIST;
+
+public class ReverseALinkedList {
+    private static Node head;
+    private static class Node
+    {
+        int data;
+        Node next;
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static void main(String[] args) {
+
+
+    }
 }
