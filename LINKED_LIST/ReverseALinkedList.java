f89b70fdc9d9e016e4c315d4c8914f16f2f8a721
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-18 16:03:53 +0530

    modified

--- LINKED_LIST/ReverseALinkedList.java
+++ LINKED_LIST/ReverseALinkedList.java
@@ -26,7 +26,7 @@ public class ReverseALinkedList {
             temp.next=previous;
             temp=curr;
         }
-        head=previous;
+        
     }
     private static void insert(int data)
     {
