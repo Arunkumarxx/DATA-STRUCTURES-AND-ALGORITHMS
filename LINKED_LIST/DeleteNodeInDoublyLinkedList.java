5daf62f1c5bb7239ee6d209f3be755a81166f052
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-18 19:30:54 +0530

    modified

--- LINKED_LIST/DeleteNodeInDoublyLinkedList.java
+++ LINKED_LIST/DeleteNodeInDoublyLinkedList.java
@@ -1,2 +1,49 @@
-package LINKED_LIST;public class DeleteNodeInDoublyLinkedList {
+package LINKED_LIST;
+
+public class DeleteNodeInDoublyLinkedList {
+    private static Node head;
+    private static Node tail;
+    private static class Node
+    {
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+            this.prev=null;
+        }
+    }
+    private static void insert(int data)
+    {
+        Node newNode =new Node(data);
+        if(head==null)
+        {
+            head=newNode;
+            tail=newNode;
+        }
+        else {
+            newNode.prev=tail;
+            tail.next=newNode;
+            tail=newNode;
+        }
+    }
+    private static void printList()
+    {
+        Node temp=head;
+        while(temp!=null)
+        {
+            System.out.println(temp.data+" ");
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        for(int i=1; i<=10; ++i)
+        insert(i);
+        printList();
+    }
+
 }
