abb90dd7459db4d59a320fda5beb60dab8066147
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-18 12:39:48 +0530

    modified

--- LINKED_LIST/ReverseADoublyLinkedList.java
+++ LINKED_LIST/ReverseADoublyLinkedList.java
@@ -2,8 +2,13 @@ package LINKED_LIST;
 
 public class ReverseADoublyLinkedList {
 
+    private static class Node
+    {
+        int data;
+        Node head;
+    }
     public static void main(String[] args) {
-        
+
     }
 
 }
