3e7c3aa7a33699616e0031b613882f5eed3ba41b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-18 19:45:11 +0530

    modified

--- LINKED_LIST/DeleteNodeInDoublyLinkedList.java
+++ LINKED_LIST/DeleteNodeInDoublyLinkedList.java
@@ -61,6 +61,8 @@ public class DeleteNodeInDoublyLinkedList {
     public static void main(String[] args) {
         for(int i=1; i<=10; ++i)
         insert(i);
+        delete(2);
+        
         printList();
     }
 
