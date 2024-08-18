afc0647f10c838f3a3506b19bd7999ef8a0a8a04
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-18 12:54:33 +0530

    modified

--- LINKED_LIST/ReverseADoublyLinkedList.java
+++ LINKED_LIST/ReverseADoublyLinkedList.java
@@ -58,7 +58,7 @@ public class ReverseADoublyLinkedList {
         System.out.println();
     }
     public static void main(String[] args) {
-        for(int i=1; i<=10; ++i)
+        for(int i=1; i<=9; ++i)
             insert(i);
         printList();
         reverse();
