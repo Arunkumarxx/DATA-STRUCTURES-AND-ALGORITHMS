339cfb08ff52e0367f43b1995b0224d538b4959b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-18 20:11:05 +0530

    modified

--- LINKED_LIST/DeleteHeadOfADoublyLinkedList.java
+++ LINKED_LIST/DeleteHeadOfADoublyLinkedList.java
@@ -1,2 +1,29 @@
-package LINKED_LIST;public class DeleteHeadOfADoublyLinkedList {
+package LINKED_LIST;
+
+public class DeleteHeadOfADoublyLinkedList {
+
+    private static Node head;
+    private static Node tail;
+
+    private  static  class Node
+    {
+        int data;
+        Node head;
+        Node tail;
+
+        Node(int data)
+        {
+            this.data=data;
+            this.head=null;
+            this.tail=null;
+        }
+    }
+    private static void insert(int data)
+    {
+        
+    }
+    public static void main(String[] args) {
+
+    }
+
 }
