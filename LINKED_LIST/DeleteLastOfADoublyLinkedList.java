2f1a58bccedd4648599cda13ea2eed8a274feea7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-19 20:32:11 +0530

    modified

--- LINKED_LIST/DeleteLastOfADoublyLinkedList.java
+++ LINKED_LIST/DeleteLastOfADoublyLinkedList.java
@@ -53,7 +53,7 @@ public class DeleteLastOfADoublyLinkedList {
     }
     public static void main(String[] args) {
 
-        for(int i=1; i<=2; ++i)
+        for(int i=1; i<=1; ++i)
             insert(i);
         printList();
         deleteLast();
