8de981ad1f58ea0b506db50f9404564295526231
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-23 12:24:31 +0530

    modified

--- LINKED_LIST/InsertAtTheEndOfCircularLinkedList.java
+++ LINKED_LIST/InsertAtTheEndOfCircularLinkedList.java
@@ -1,4 +1,29 @@
 package LINKED_LIST;
 
 public class InsertAtTheEndOfCircularLinkedList {
+    private static Node head;
+    private static Node tial;
+
+    private static class Node
+    {
+        int data;
+        Node prev;
+        Node next;
+
+        Node(int data)
+        {
+            this.data=data;
+            this.prev=null;
+            this.next=null;
+        }
+    }
+    private static void printList()
+    {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
