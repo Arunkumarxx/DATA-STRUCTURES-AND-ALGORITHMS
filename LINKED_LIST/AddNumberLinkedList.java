c2e0504591f8799a9e8b1873a7a81273e35d5b95
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-23 13:11:00 +0530

    modified

--- LINKED_LIST/AddNumberLinkedList.java
+++ LINKED_LIST/AddNumberLinkedList.java
@@ -15,7 +15,23 @@ public class AddNumberLinkedList {
             this.next=null;
         }
     }
-    private static void 
+    private static void insert(int data)
+    {
+        Node newNode = new Node(data);
+        if(head==null)
+        {
+            head=newNode;
+            head.prev=head;
+            head.next=head;
+        }
+        else {
+            Node lastNode =head.prev;
+            newNode.next=head;
+            newNode.prev=lastNode;
+            lastNode.next=head;
+            head.prev=newNode;
+        }
+    }
     public static void main(String[] args) {
 
     }
