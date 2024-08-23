868050e96c1286f03e160610698d405a21c114a3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-23 11:46:36 +0530

    modified

--- LINKED_LIST/CircularLinkedListTraversal.java
+++ LINKED_LIST/CircularLinkedListTraversal.java
@@ -1,7 +1,18 @@
 package LINKED_LIST;
 
 public class CircularLinkedListTraversal {
+    private static Node head;
+    private static Node tail;
+    private static class Node
+    {
+        int data;
+        Node next;
+        Node prev;
+    }
+    private static void insert(int data)
+    {
 
+    }
     public static void main(String[] args) {
         
     }
