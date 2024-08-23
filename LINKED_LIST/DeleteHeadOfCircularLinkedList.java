712beeef9a7b19bb85a06ca0ef91c09c7ea17829
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-23 16:44:27 +0530

    modified

--- LINKED_LIST/DeleteHeadOfCircularLinkedList.java
+++ LINKED_LIST/DeleteHeadOfCircularLinkedList.java
@@ -2,8 +2,30 @@ package LINKED_LIST;
 
 public class DeleteHeadOfCircularLinkedList {
 
+    private static Node head;
+    private static class Node
+    {
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+            this.prev=null;
+        }
+    }
+    private static Node  insert(Node head,int data)
+    {
+        Node newNode =new Node(data);
+        newNode.next=head;
+        head=newNode;
+        return head;
+    }
+    private 
     public static void main(String[] args) {
-        
+
     }
 
 }
