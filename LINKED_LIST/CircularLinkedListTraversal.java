ab8de0f600ddb2da2f05e2fbbe29a1348cf5004e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-23 12:05:57 +0530

    modified

--- LINKED_LIST/CircularLinkedListTraversal.java
+++ LINKED_LIST/CircularLinkedListTraversal.java
@@ -46,8 +46,7 @@ public class CircularLinkedListTraversal {
         System.out.println();
     }
     public static void main(String[] args) {
-        for(int i=1; i<=10; ++i)
-            insert(i);
+        
         printList();
     }
 
