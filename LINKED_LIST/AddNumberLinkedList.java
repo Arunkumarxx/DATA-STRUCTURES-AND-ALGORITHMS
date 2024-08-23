41e270d9005cb03fedec7d519ae00e63faa0212e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-23 16:35:29 +0530

    modified

--- LINKED_LIST/AddNumberLinkedList.java
+++ LINKED_LIST/AddNumberLinkedList.java
@@ -97,12 +97,23 @@ public class AddNumberLinkedList {
 
 /*
 Add Number Linked Lists
-Difficulty: MediumAccuracy: 34.52%Submissions: 283K+Points: 4
-Given two numbers, num1, and num2, represented by linked lists. The task is to return the head of the linked list that represents the sum of these two numbers.
 
-For example, the number 190 will be represented by the linked list, 1->9->0->null, similarly 25 by 2->5->null. Sum of these two numbers is 190 + 25 = 215, which will be represented by 2->1->5->null. You are required to return the head of the linked list 2->1->5->null.
+Difficulty: Medium
+Accuracy: 34.52%
+Submissions: 283K+
+Points: 4
 
-Note: There can be leading zeros in the input lists, but there should not be any leading zeros in the output list.
+Given two numbers, num1, and num2, represented by linked lists.
+The task is to return the head of the linked list that represents
+the sum of these two numbers.
+
+For example, the number 190 will be represented by the linked list
+1->9->0->null, similarly 25 by 2->5->null. Sum of these two numbers
+ is 190 + 25 = 215, which will be represented by 2->1->5->null.
+ You are required to return the head of the linked list 2->1->5->null.
+
+Note: There can be leading zeros in the input lists, but there should
+ not be any leading zeros in the output list.
 
 Examples:
 
@@ -110,16 +121,18 @@ Input: num1 = 45 (4->5->null), num2 = 345 (3->4->5->null)
 Output:  3->9->0->null
 
 Explanation:
-For the given two linked list (4 5) and (3 4 5), after adding the two linked list resultant linked list will be (3 9 0).
+For the given two linked list (4 5) and (3 4 5), after adding the two
+ linked list resultant linked list will be (3 9 0).
 Input: num1 = 0063 (0->0->6->3->null), num2 = 07 (0->7->null)
 Output: 7->0->null
 
 Explanation:
-For the given two linked list (0 0 6 3) and (0 7), after adding the two linked list resultant linked list will be (7 0).
+For the given two linked list (0 0 6 3) and (0 7), after adding the two
+linked list resultant linked list will be (7 0).
+
 Expected Time Complexity: O(n+m)
-Expected Space Complexity: O(max(n,m)) for the resultant list.
+Expected Space Complexity: O(max(n,m))
 
-
-
+for the resultant list.
 
  */
