ec3837b785aa1b92a395e9ae0383dca1154cfd1b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-23 13:30:15 +0530

    modified

--- LINKED_LIST/AddNumberLinkedList.java
+++ LINKED_LIST/AddNumberLinkedList.java
@@ -1,59 +1,67 @@
 package LINKED_LIST;
 
 public class AddNumberLinkedList {
-    private static Node num1=null;
-    private static Node num2=null;
-    private static class Node
-    {
+    private static Node num1 = null; // Initialize num1
+    private static Node num2 = null; // Initialize num2
+
+    private static class Node {
         int data;
-        Node  prev;
+        Node prev;
         Node next;
 
-        Node(int data)
-        {
-            this.data=data;
-            this.prev=null;
-            this.next=null;
+        Node(int data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
         }
     }
-    private static void insert(int data,Node head)
-    {
+
+    private static void insert(int data, Node head) {
         Node newNode = new Node(data);
-        if(head==null)
-        {
-            head=newNode;
-            head.prev=head;
-            head.next=head;
-        }
-        else {
-            Node lastNode =head.prev;
-            newNode.next=head;
-            newNode.prev=lastNode;
-            lastNode.next=newNode;
-            head.prev=newNode;
+        if (head == null) {
+            head = newNode;
+            head.prev = head;
+            head.next = head;
+        } else {
+            Node lastNode = head.prev;
+            newNode.next = head;
+            newNode.prev = lastNode;
+            lastNode.next = newNode;
+            head.prev = newNode;
         }
     }
-    private static void printList(Node head)
-    {
-        if(head!=null) {
+
+    private static void printList(Node head) {
+        if (head != null) {
             Node temp = head;
             do {
                 System.out.print(temp.data + " ");
                 temp = temp.next;
-            }
-            while (temp != head);
+            } while (temp != head);
+            System.out.println();
         }
     }
+
     public static void main(String[] args) {
-        insert(4,num1);
-        insert(5,num1);
+        // Initialize lists
+        num1 = new Node(0); // Create a dummy node to start the list
+        num1.prev = num1;
+        num1.next = num1;
 
-        insert(3,num2);
-        insert(4,num2);
-        insert(5,num2);
+        num2 = new Node(0); // Create a dummy node to start the list
+        num2.prev = num2;
+        num2.next = num2;
 
+        // Insert values
+        insert(4, num1);
+        insert(5, num1);
+
+        insert(3, num2);
+        insert(4, num2);
+        insert(5, num2);
+
+        // Print lists
         printList(num1);
         printList(num2);
     }
-
 }
