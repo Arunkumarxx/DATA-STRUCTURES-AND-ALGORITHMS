2891cb3478a1bc7b9a395ca5b64242f3b4357af9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-23 13:15:12 +0530

    modified

--- LINKED_LIST/AddNumberLinkedList.java
+++ LINKED_LIST/AddNumberLinkedList.java
@@ -40,7 +40,7 @@ public class AddNumberLinkedList {
             System.out.print(temp.data+" ");
             temp=temp.prev;
         }
-        while(temp!=head);
+        while(temp!=head.prev);
     }
     public static void main(String[] args) {
         for(int i=1; i<=10; ++i)
