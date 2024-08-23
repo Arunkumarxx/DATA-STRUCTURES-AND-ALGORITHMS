d3df9a921dbaa9082886cca4a19be6fe977527b2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-23 12:56:20 +0530

    modified

--- LINKED_LIST/InsertAtTheEndOfCircularLinkedList.java
+++ LINKED_LIST/InsertAtTheEndOfCircularLinkedList.java
@@ -3,51 +3,53 @@ package LINKED_LIST;
 public class InsertAtTheEndOfCircularLinkedList {
     private static Node head;
 
-    private static class Node
-    {
+    private static class Node {
         int data;
-        Node prev;
         Node next;
+        Node prev;
 
-        Node(int data)
-        {
-            this.data=data;
-            this.prev=null;
-            this.next=null;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
         }
     }
-    private static void insertAtEnd(int data)
-    {
-        Node newNode =new Node(data);
-        if(head==null)
-        {
-            head=newNode;
-            head.prev=head;
-            head.next=head;
-        }
-        else
-        {
-            Node lastNode=head.prev;
-            newNode.next=head;
-            newNode.prev=lastNode;
-            lastNode.next=newNode;
-            head.prev=lastNode;
 
-
+    private static void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            // List is empty
+            head = newNode;
+            head.next = head;
+            head.prev = head;
+        } else {
+            // List is not empty
+            Node lastNode = head.prev;  // The last node in the list
+            newNode.next = head;
+            newNode.prev = lastNode;
+            lastNode.next = newNode;
+            head.prev = newNode;
         }
     }
-    private static void printList()
-    {
-        Node temp=head;
-        do{
-            System.out.print(temp.data+" ");
-            temp=temp.next;
-        } while(temp!=head);
+
+    private static void printList() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        Node temp = head;
+        do {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        } while (temp != head);
+        System.out.println();
     }
 
     public static void main(String[] args) {
-        for(int i=1; i<=10; ++i)
-            insertAtEnd(i);
+        for (int i = 1; i <= 1000; ++i) {
+            insert(i);
+        }
         printList();
     }
 
