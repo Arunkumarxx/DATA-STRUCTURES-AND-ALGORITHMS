17873bc4a1909bd7f5c469cb85b4513b26c37bc0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-23 12:10:12 +0530

    modified

--- LINKED_LIST/InsertAtBeginOfCircularLinkedList.java
+++ LINKED_LIST/InsertAtBeginOfCircularLinkedList.java
@@ -1,9 +1,25 @@
 package LINKED_LIST;
 
 public class InsertAtBeginOfCircularLinkedList {
+    private static Node head;
+    private static Node tail;
 
+    private static class Node
+    {
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int data)
+         {
+             this.data=data;
+             this.next=null;
+             this.prev=null;
+         }
+    }
+    private static 
     public static void main(String[] args) {
-        
+
     }
 
 }
