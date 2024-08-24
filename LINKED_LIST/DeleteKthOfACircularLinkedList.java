4a601a96030e246cc8254b93e1431d687c29ba04
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-24 12:40:39 +0530

    modified

--- LINKED_LIST/DeleteKthOfACircularLinkedList.java
+++ LINKED_LIST/DeleteKthOfACircularLinkedList.java
@@ -74,7 +74,7 @@ public class DeleteKthOfACircularLinkedList {
     public static void main(String[] args) {
         for(int i=10; i>=1; --i)
             head=insert(head,i);
-       head= deleteKthElement(10,head);
+       head= deleteKthElement(11,head);
         printList(head);
     }
 
