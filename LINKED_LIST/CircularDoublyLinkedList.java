7c0b7893845516130c23d440c293e8f68d898ad3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-24 15:25:43 +0530

    modified

--- LINKED_LIST/CircularDoublyLinkedList.java
+++ LINKED_LIST/CircularDoublyLinkedList.java
@@ -1,4 +1,21 @@
 package LINKED_LIST;
 
 public class CircularDoublyLinkedList {
+    private static Node head;
+
+    private static class Node
+    {
+        int data;
+        Node prev;
+        Node next;
+
+        Node(int data)
+        {
+            this.data=data;
+            this.prev=null;
+            this.next=null;
+        }
+    }
+    
+
 }
