d10a92fa3b759d21a158bfbaede09a998d0713ba
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-24 11:10:44 +0530

    modified

--- LINKED_LIST/DeleteHeadOfCircularLinkedList.java
+++ LINKED_LIST/DeleteHeadOfCircularLinkedList.java
@@ -43,7 +43,7 @@ public class DeleteHeadOfCircularLinkedList {
             System.out.print(temp.data+" ");
         }
         while(temp.next!=head && temp.next!=null);
-        System.out.println(temp.next.data);
+        
         return head;
     }
     public static void main(String[] args) {
