e82b639e0f4083540566226dbd06c9dae1a42d11
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-24 15:22:38 +0530

    modified

--- LINKED_LIST/DeleteWithoutHeadPointer.java
+++ LINKED_LIST/DeleteWithoutHeadPointer.java
@@ -1,21 +1,22 @@
 package LINKED_LIST;
 
-class ListNode {
-    int value;
-    ListNode next;
 
-    ListNode(int value) {
-        this.value = value;
-        this.next = null;
-    }
-}
 
 public class DeleteWithoutHeadPointer {
 
-    public static void deleteNode(ListNode nodeToDelete) {
+   private static class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+    public static void deleteNode(Node nodeToDelete) {
         if (nodeToDelete == null || nodeToDelete.next == null)
             return;
-        ListNode nextNode = nodeToDelete.next;
+        Node nextNode = nodeToDelete.next;
         nodeToDelete.value = nextNode.value;
         nodeToDelete.next = nextNode.next;
         nextNode = null;
@@ -23,11 +24,11 @@ public class DeleteWithoutHeadPointer {
 
     public static void main(String[] args) {
 
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
 
 
         printList(head);
@@ -37,8 +38,8 @@ public class DeleteWithoutHeadPointer {
         printList(head);
     }
 
-    private static void printList(ListNode head) {
-        ListNode current = head;
+    private static void printList(.Node head) {
+        Node current = head;
         while (current != null) {
             System.out.print(current.value + " ");
             current = current.next;
