5d729ecf9a64812fa0dc2a5746fc8b28bb0fa396
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-24 16:52:53 +0530

    modified

--- LINKED_LIST/CheckIdenticalLinkedList.java
+++ LINKED_LIST/CheckIdenticalLinkedList.java
@@ -15,9 +15,23 @@ public class CheckIdenticalLinkedList {
     }
     private static Node insert(int data,Node head)
     {
-        
+
     }
 
+    public boolean areIdentical(Node head1, Node head2) {
+        // write your code here
+        while(head1!=null && head2!=null)
+        {
+            if(head1.data!=head2.data)
+                return false;
+            head1=head1.next;
+            head2=head2.next;
+        }
+        if (head1 != null || head2 != null)
+            return false;
+        return true;
+
+    }
     public static void main(String[] args) {
 
     }
