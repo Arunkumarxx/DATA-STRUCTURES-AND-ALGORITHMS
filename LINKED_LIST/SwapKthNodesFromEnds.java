c27b3f45c19ae4f749e599b0d027b782980bc391
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-24 13:01:29 +0530

    modified

--- LINKED_LIST/SwapKthNodesFromEnds.java
+++ LINKED_LIST/SwapKthNodesFromEnds.java
@@ -1,9 +1,34 @@
 package LINKED_LIST;
 
 public class SwapKthNodesFromEnds {
+    private static class Node
+    {
+        int data;
+        Node next;
+
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+
+    }
+    private static Node insert(int data,Node head)
+    {
+        Node newNode =new Node(data);
+        if(head==null)
+        {
+            head=newNode;
+            return head;
+        }
+        else {
+            head.next=newNode;
+            
+        }
+    }
 
     public static void main(String[] args) {
-        
+
     }
 
 }
