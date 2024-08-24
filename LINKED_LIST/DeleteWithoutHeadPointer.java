637388423396476b60a2492260d3af2a6b9b45c3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-24 15:20:04 +0530

    modified

--- LINKED_LIST/DeleteWithoutHeadPointer.java
+++ LINKED_LIST/DeleteWithoutHeadPointer.java
@@ -1,9 +1,48 @@
 package LINKED_LIST;
 
+class ListNode {
+    int value;
+    ListNode next;
+
+    ListNode(int value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
 public class DeleteWithoutHeadPointer {
+
+    public static void deleteNode(ListNode nodeToDelete) {
+        if (nodeToDelete == null || nodeToDelete.next == null)
+            return;
+        ListNode nextNode = nodeToDelete.next;
+        nodeToDelete.value = nextNode.value;
+        nodeToDelete.next = nextNode.next;
+        nextNode = null;
+    }
 
     public static void main(String[] args) {
 
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+
+        printList(head);
+
+        deleteNode(head.next.next);
+
+        printList(head);
     }
 
+    private static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.value + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
 }
