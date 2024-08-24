6eec2d6539993291874de5b7e699fda9d3271c30
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-24 16:50:52 +0530

    modified

--- LINKED_LIST/CheckIdenticalLinkedList.java
+++ LINKED_LIST/CheckIdenticalLinkedList.java
@@ -1,2 +1,25 @@
-package LINKED_LIST;public class CheckIdenticalLinkedList {
+package LINKED_LIST;
+
+public class CheckIdenticalLinkedList {
+
+    private static class  Node
+    {
+        int data;
+        Node next;
+
+        Node (int data)
+        {
+        this.data=data;
+        this.next=null;
+        }
+    }
+    private static Node insert(int )
+    {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
