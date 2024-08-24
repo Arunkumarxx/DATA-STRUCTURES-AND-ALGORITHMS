af3a81aac773bfe747082941f1d161717848d1b7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-24 17:53:21 +0530

    modified

--- LINKED_LIST/MiddleOfALinkedList.java
+++ LINKED_LIST/MiddleOfALinkedList.java
@@ -4,39 +4,58 @@ public class MiddleOfALinkedList {
 
     private static Node head;
 
-    private static class Node
-    {
+    private static class Node {
         int data;
         Node next;
 
-        Node(int data)
-        {
-            this.data=data;
-            this.next=null;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
-    private static Node insert(int data,)
-    {
 
+    private static Node insert(Node head, int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            return newNode;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+        return head;
     }
-    int getMiddle(Node head) {
-        // Your code here.
-        Node slow=head;
-        Node fast=head;
 
-        while(fast!=null && fast.next!=null)
-        {
-            slow=slow.next;
-            fast=fast.next.next;
+   private  static int getMiddle(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
         return slow.data;
     }
-    private static void printList(Node head)
-    {
 
+    private static void printList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
     }
+
     public static void main(String[] args) {
+        MiddleOfALinkedList list = new MiddleOfALinkedList();
+        head = insert(head, 1);
+        head = insert(head, 2);
+        head = insert(head, 3);
+        head = insert(head, 4);
+        head = insert(head, 5);
 
+        printList(head);
+        System.out.println("Middle :"+getMiddle(head));
     }
-
 }
