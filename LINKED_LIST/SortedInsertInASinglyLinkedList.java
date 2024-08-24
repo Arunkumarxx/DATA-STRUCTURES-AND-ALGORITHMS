7e6f685217b65cfb85dba830683934f72d276a72
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-24 16:00:44 +0530

    modified

--- LINKED_LIST/SortedInsertInASinglyLinkedList.java
+++ LINKED_LIST/SortedInsertInASinglyLinkedList.java
@@ -2,8 +2,28 @@ package LINKED_LIST;
 
 public class SortedInsertInASinglyLinkedList {
 
-    public static void main(String[] args) {
+    private static class Node
+    {
+        int data;
+        Node next;
+
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+    private static Node  insertSorted(int data,Node head)
+    {
+        Node newNode =new Node(data);
+        if(head==null)
+            return newNode;
         
+    }
+
+    public static void main(String[] args) {
+
     }
 
 }
