08e3e453e97e1f60bd49db182f41657477fd6060
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-24 17:51:41 +0530

    modified

--- LINKED_LIST/MiddleOfALinkedList.java
+++ LINKED_LIST/MiddleOfALinkedList.java
@@ -1,2 +1,38 @@
-package LINKED_LIST;public class MiddleOfALinkedList {
+package LINKED_LIST;
+
+public class MiddleOfALinkedList {
+
+    private static Node head;
+
+    private static class Node
+    {
+        int data;
+        Node next;
+
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+    }
+    private static Node insert(int data,)
+    {
+
+    }
+    int getMiddle(Node head) {
+        // Your code here.
+        Node slow=head;
+        Node fast=head;
+
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow.data;
+    }
+    public static void main(String[] args) {
+
+    }
+
 }
