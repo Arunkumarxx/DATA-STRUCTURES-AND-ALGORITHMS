cf2ea7b3fd2f5e4d99442390d5bab6d6fbc82a8d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-25 12:41:44 +0530

    modified

--- LINKED_LIST/ReverseALinkedListInGroupsOfSizeK.java
+++ LINKED_LIST/ReverseALinkedListInGroupsOfSizeK.java
@@ -56,10 +56,13 @@ public class ReverseALinkedListInGroupsOfSizeK {
                 reverese=temp;
                 temp=next;
             }
-            /*for(int i=1; i<=k && temp!=null; ++i )
+            for(int i=1; i<=k && temp!=null; ++i )
             {
-
-            }*/
+                next=temp.next;
+                temp.next=reverese;
+                reverese=temp;
+                temp=next;
+            }
         }
         return reverese;
     }
