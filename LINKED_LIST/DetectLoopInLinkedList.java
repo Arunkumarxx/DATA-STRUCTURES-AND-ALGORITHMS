ba6f025c6a25a68a75135f9c2208bcf77c228958
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-25 13:31:24 +0530

    modified

--- LINKED_LIST/DetectLoopInLinkedList.java
+++ LINKED_LIST/DetectLoopInLinkedList.java
@@ -2,8 +2,32 @@ package LINKED_LIST;
 
 public class DetectLoopInLinkedList {
 
+    private static Node head;
+    private static class Node
+    {
+        int data;
+        Node next;
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+    }
+    private static void printList(Node head)
+    {
+        while(head!=null)
+        {
+            System.out.println(head.data);
+            head=head.next;
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
-        
+     head=new Node(1);
+     head.next=new Node(2);
+     head.next.next=new Node(3);
+     head.next.next.next=new Node(4);
+     head.next=new Node(5);
     }
 
 }
