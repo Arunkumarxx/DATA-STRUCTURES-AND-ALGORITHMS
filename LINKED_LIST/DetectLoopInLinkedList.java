c8172692d1b31c39af551ac8f24ca51f88d90e8e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-25 13:32:10 +0530

    modified

--- LINKED_LIST/DetectLoopInLinkedList.java
+++ LINKED_LIST/DetectLoopInLinkedList.java
@@ -27,7 +27,7 @@ public class DetectLoopInLinkedList {
      head.next=new Node(2);
      head.next.next=new Node(3);
      head.next.next.next=new Node(4);
-     head.next=new Node(5);
+     new Node(5).next=head.next;
      printList(head);
     }
 
