9fab81d8f05f0c1d26a9fc3e1562f176337cafc3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-25 13:42:52 +0530

    modified

--- LINKED_LIST/DetectLoopInLinkedList.java
+++ LINKED_LIST/DetectLoopInLinkedList.java
@@ -17,9 +17,17 @@ public class DetectLoopInLinkedList {
     {
         Node slow=head;
         Node fast=head;
-        
+        while(fast!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast)
+            {
+                return true;
+            }
+        }
+        return false;
     }
-
     public static void main(String[] args) {
 
      head=new Node(1);
@@ -28,7 +36,7 @@ public class DetectLoopInLinkedList {
      head.next.next.next=new Node(4);
      head.next.next.next.next=new Node(5);
      head.next.next.next.next.next=head.next;
-
+        System.out.println(isLoopFound(head));
     }
 
 }
