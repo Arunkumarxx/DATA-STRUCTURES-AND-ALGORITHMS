3b1b7f7e6dd3f1957d798399b763dcac9fb298d9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-25 12:18:41 +0530

    modified

--- LINKED_LIST/RemoveDuplicatesFromASortedSinglyLinkedList.java
+++ LINKED_LIST/RemoveDuplicatesFromASortedSinglyLinkedList.java
@@ -39,11 +39,11 @@ public class RemoveDuplicatesFromASortedSinglyLinkedList {
     }
     private static Node removeDuplicate(Node head)
     {
-        Node temp=head;
-        while(temp!=null && temp.next!=null)
-            if(temp.data== temp.next.data)
-                temp.next=temp.next.next;
-            else temp=temp.next;
+
+        while(head!=null && head.next!=null)
+            if(head.data== head.next.data)
+                head.next=head.next.next;
+            else head=head.next;
         return head;
     }
     public static void main(String[] args) {
