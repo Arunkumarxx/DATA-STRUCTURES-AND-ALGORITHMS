157ee51bdc7569341a5b877b50626e9969213efd
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-25 14:56:41 +0530

    modified

--- LINKED_LIST/DetectAndRemoveLoopInLinkedList.java
+++ LINKED_LIST/DetectAndRemoveLoopInLinkedList.java
@@ -3,19 +3,29 @@ package LINKED_LIST;
 public class DetectAndRemoveLoopInLinkedList {
 
     private static Node head;
-    private static class Node
-    {
+    private static class Node {
         int data;
         Node next;
 
-        Node(int data)
-        {
-            this.data=data;
-            this.next=null;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
-    public static void main(String[] args) {
+    private static Node DetectLoopAndRemoveLoop(Node head)
+    {
 
+    }
+    public static void main(String[] args) {
+        head=new Node(1);
+        head.next=new Node(2);
+        head.next.next=new Node(3);
+        head.next.next.next=new Node(4);
+        head.next.next.next.next=new Node(5);
+        head.next.next.next.next.next=head.next;
+
+        head= DetectLoopAndRemoveLoop(head);
+        
     }
 
 }
