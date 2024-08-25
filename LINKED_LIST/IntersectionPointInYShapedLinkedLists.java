a6fad89125bce480f818a246e77ed1a4bf6a040c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-25 21:24:19 +0530

    modified

--- LINKED_LIST/IntersectionPointInYShapedLinkedLists.java
+++ LINKED_LIST/IntersectionPointInYShapedLinkedLists.java
@@ -1,9 +1,25 @@
 package LINKED_LIST;
 
 public class IntersectionPointInYShapedLinkedLists {
+    private static Node head;
+    private static class Node
+    {
 
-    public static void main(String[] args) {
-        
     }
+    private static Node insertAtBegin(Node head)
+    {
 
+    }
+    public static void main(String[] args) {
+
+    }
+    private static int intersectPoint(Node firstHead, Node secondHead) {
+        Node first = firstHead;
+        Node second = secondHead;
+        while(first!=second){
+            first = (first.next!=null)?first.next:secondHead;
+            second = (second.next!=null)?second.next:firstHead;
+        }
+        return first.data;
+    }
 }
