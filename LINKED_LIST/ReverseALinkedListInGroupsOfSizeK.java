4604a6e10798f9232b99516c3b86605fc628db40
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-25 12:23:59 +0530

    modified

--- LINKED_LIST/ReverseALinkedListInGroupsOfSizeK.java
+++ LINKED_LIST/ReverseALinkedListInGroupsOfSizeK.java
@@ -1,4 +1,23 @@
 package LINKED_LIST;
 
 public class ReverseALinkedListInGroupsOfSizeK {
+    private static Node head;
+    private static class Node
+    {
+        int data;
+        Node next;
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+    }
+    private static Node insert(int data,Node head)
+    {
+        
+    }
+    public static void main(String[] args) {
+
+    }
+
 }
