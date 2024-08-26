d0c06e5aa65da42aa7b437230f974f59ac0d868e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-26 14:02:11 +0530

    modified

--- LINKED_LIST/FindMiddleOfCircularDoublyLinkedList.java
+++ LINKED_LIST/FindMiddleOfCircularDoublyLinkedList.java
@@ -3,7 +3,18 @@ package LINKED_LIST;
 public class FindMiddleOfCircularDoublyLinkedList {
 
     public static void main(String[] args) {
-        
-    }
 
+    }
+    public static int findMiddle(Node head)
+    {
+        Node slow=head;
+        Node fast=head.next;
+
+        while(fast!=head)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow.data;
+    }
 }
