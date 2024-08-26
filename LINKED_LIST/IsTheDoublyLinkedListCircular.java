598e7565212b5d658f432698708ef9b3c667e4d5
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-26 14:17:53 +0530

    modified

--- LINKED_LIST/IsTheDoublyLinkedListCircular.java
+++ LINKED_LIST/IsTheDoublyLinkedListCircular.java
@@ -34,20 +34,32 @@ public class IsTheDoublyLinkedListCircular {
 
         return head;
     }
+    private static void printList(Node head) {
+        if (head == null) return;
+
+        Node current = head;
+        do {
+            System.out.print(current.data + " ");
+            current = current.next;
+        } while (current != head);
+        System.out.println();
+    }
+    private static boolean isCircular(Node head)
+    {
+        return head.prev!=null?true:false;
+    }
     public static void main(String[] args) {
 
         Node head = null;
-
-
         head = insert(head, 1);
         head = insert(head, 2);
         head = insert(head, 3);
 
         printList(head);
+        System.out.println(isCircular(head));
+
     }
-    public static boolean isCircular(Node head)
-    {
-        return head.prev!=null?true:false;
-    }
+
+
 
 }
