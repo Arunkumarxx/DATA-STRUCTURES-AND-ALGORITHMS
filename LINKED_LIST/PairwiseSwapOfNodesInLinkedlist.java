87dbdcc511f82e7ec520ee6cd70eefb31ea733f3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-26 12:12:52 +0530

    modified

--- LINKED_LIST/PairwiseSwapOfNodesInLinkedlist.java
+++ LINKED_LIST/PairwiseSwapOfNodesInLinkedlist.java
@@ -33,12 +33,12 @@ public class PairwiseSwapOfNodesInLinkedlist {
         for(int i=5; i>=1; --i)
             insertAtBegin(i);
 
-        System.out.println(" List:");
+        System.out.println("List:");
         printList(head);
 
         head = pairwiseSwap(head);
 
-        System.out.println(" after pairwise swap:");
+        System.out.println("After pairwise swap:");
         printList(head);
     }
 
