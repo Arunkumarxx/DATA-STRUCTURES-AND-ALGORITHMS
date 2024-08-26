2ed24cd9bfd15de7a367c6e0fad8a349504c05d8
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-26 14:01:09 +0530

    modified

--- LINKED_LIST/IsLinkedListSorted.java
+++ LINKED_LIST/IsLinkedListSorted.java
@@ -1,32 +1,17 @@
 package LINKED_LIST;
 
-class Node {
-    int data;
-    Node next;
 
-    Node(int data) {
-        this.data = data;
-        this.next = null;
-    }
-}
 
 public class IsLinkedListSorted {
-    public static void main(String[] args) {
-        Node head = new Node(1);
-        head.next = new Node(2);
-        head.next.next = new Node(3);
-        head.next.next.next = new Node(4);
+    private static class Node {
+        int data;
+        Node next;
 
-        System.out.println(isSorted(head));
-
-        Node head2 = new Node(1);
-        head2.next = new Node(3);
-        head2.next.next = new Node(2);
-        head2.next.next.next = new Node(4);
-
-        System.out.println(isSorted(head2));
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
     }
-
     public static boolean isSorted(Node head) {
         if (head == null || head.next == null)
             return true;
@@ -44,4 +29,21 @@ public class IsLinkedListSorted {
         }
         return increasing || decreasing;
     }
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+
+        System.out.println(isSorted(head));
+
+        Node head2 = new Node(1);
+        head2.next = new Node(3);
+        head2.next.next = new Node(2);
+        head2.next.next.next = new Node(4);
+
+        System.out.println(isSorted(head2));
+    }
+
+
 }
