dcef38aea2a36452b50e47862497e357eb15f313
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-26 14:04:39 +0530

    modified

--- LINKED_LIST/IsTheDoublyLinkedListCircular.java
+++ LINKED_LIST/IsTheDoublyLinkedListCircular.java
@@ -1,2 +1,9 @@
-package LINKED_LIST;public class IsTheDoublyLinkedListCircular {
+package LINKED_LIST;
+
+public class IsTheDoublyLinkedListCircular {
+
+    public static void main(String[] args) {
+        
+    }
+
 }
