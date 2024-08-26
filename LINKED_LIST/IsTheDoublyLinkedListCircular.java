17b0a5bdb8db9c1f8b216c3eee1b57431aa334e3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-26 14:17:01 +0530

    modified

--- LINKED_LIST/IsTheDoublyLinkedListCircular.java
+++ LINKED_LIST/IsTheDoublyLinkedListCircular.java
@@ -2,24 +2,33 @@ package LINKED_LIST;
 
 public class IsTheDoublyLinkedListCircular {
 
+    private static class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
     private static Node insert(Node head, int data) {
         Node newNode = new Node(data);
 
         if (head == null) {
-            // Create a new circular doubly linked list with the new node
+
             newNode.next = newNode;
             newNode.prev = newNode;
             head = newNode;
         } else {
             Node tail = head.prev;
 
-            // Insert the new node at the beginning
             newNode.next = head;
             newNode.prev = tail;
             head.prev = newNode;
             tail.next = newNode;
 
-            // Update the head to the new node
             head = newNode;
         }
 
