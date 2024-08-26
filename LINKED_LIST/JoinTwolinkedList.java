11b44d762f490e77c879012d020214bb86d456de
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-26 13:37:46 +0530

    modified

--- LINKED_LIST/JoinTwolinkedList.java
+++ LINKED_LIST/JoinTwolinkedList.java
@@ -1,2 +1,35 @@
-package LINKED_LIST;public class JoinTwolinkedList {
+package LINKED_LIST;
+
+public class JoinTwolinkedList {
+
+    private static Node head;
+    private static class Node
+    {
+
+    }
+    private static Node insert(Node head)
+    {
+
+    }
+    private static void PrintList(Node head)
+    {
+
+    }
+
+    public static Node joinTheLists(Node head1, Node head2)
+    {
+        if(head1==null)
+            return head2;
+
+        Node t1=head1;
+        while(t1.next!=null)
+            t1=t1.next;
+
+        t1.next=head2;
+        return head1;
+    }
+    public static void main(String[] args) {
+
+    }
+
 }
