8c5aabb80aed80ac0a692608ac14cdf26556aa4f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-26 13:59:36 +0530

    modified

--- LINKED_LIST/IsLinkedListSorted.java
+++ LINKED_LIST/IsLinkedListSorted.java
@@ -2,6 +2,25 @@ package LINKED_LIST;
 
 public class IsLinkedListSorted {
     public static void main(String[] args) {
-        
+
+    }
+    public static boolean isSorted(Node head)
+    {
+        if(head==null || head.next==null)
+            return true;
+
+        boolean increasing=true;
+        boolean decreasing=true;
+        Node curr=head;
+        while(curr.next!=null)
+        {
+            if(curr.data>curr.next.data)
+                increasing=false;
+
+            if(curr.data<curr.next.data)
+                decreasing=false;
+            curr=curr.next;
+        }
+        return  increasing||decreasing;
     }
 }
