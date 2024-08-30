acdfa0b9fd20bbb99b4f87f8330601a31f4fe5ae
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-30 18:20:54 +0530

    modified

--- LINKED_LIST/MergeKSortedLinkedLists.java
+++ LINKED_LIST/MergeKSortedLinkedLists.java
@@ -1,33 +1,80 @@
 package LINKED_LIST;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MergeKSortedLinkedLists {
 
-    private static class Node
-    {
+    private static class Node {
         int data;
         Node next;
 
-        Node(int data)
-        {
-            this.data=data;
-            this.next=null;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
         }
-
     }
+
     private static Node head;
     private static Node tail;
 
-    private static Node insertAtBegin(int data,Node head)
-    {
-
+    private static Node insertAtBegin(int data, Node head) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            return newNode;
+        }
+        newNode.next = head;
+        return newNode;
     }
-    private static void PrintList()
-    {
 
+    private static void PrintList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    private static List<String> storeInArrayOfSublists(Node head, int groupSize) {
+        List<String> result = new ArrayList<>();
+        Node temp = head;
+
+        while (temp != null) {
+            StringBuilder sublist = new StringBuilder();
+            int count = 0;
+
+
+            while (temp != null && count < groupSize) {
+                if (count > 0) {
+                    sublist.append("->");
+                }
+                sublist.append(temp.data);
+                temp = temp.next;
+                count++;
+            }
+
+            result.add(sublist.toString());
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
 
-    }
+        head = insertAtBegin(8, head);
+        head = insertAtBegin(7, head);
+        head = insertAtBegin(6, head);
+        head = insertAtBegin(5, head);
+        head = insertAtBegin(5, head);
+        head = insertAtBegin(4, head);
+        head = insertAtBegin(3, head);
+        head = insertAtBegin(2, head);
+        head = insertAtBegin(1, head);
 
+        PrintList(head);
+
+        List<String> nodeArray = storeInArrayOfSublists(head, 3);
+        System.out.println("Array of node sublists: " + nodeArray);
+    }
 }
