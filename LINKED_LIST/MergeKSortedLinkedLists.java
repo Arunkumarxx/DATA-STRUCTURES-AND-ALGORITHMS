97cc5196d46f183be9cb165bb14916cf862f71d5
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-08-30 18:16:06 +0530

    modified

--- LINKED_LIST/MergeKSortedLinkedLists.java
+++ LINKED_LIST/MergeKSortedLinkedLists.java
@@ -1,4 +1,25 @@
 package LINKED_LIST;
 
 public class MergeKSortedLinkedLists {
+
+    private static class Node
+    {
+        int data;
+        Node next;
+
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+
+    }
+    private static Node head;
+    private static Node tail;
+
+    private static Node 
+    public static void main(String[] args) {
+
+    }
+
 }
