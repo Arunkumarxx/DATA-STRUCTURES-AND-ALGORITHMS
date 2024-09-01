3fef53acd0c6e9aa1869c5aef87cabb5c2935f98
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-01 18:33:03 +0530

    modified

--- LINKED_LIST/LRUCache.java
+++ LINKED_LIST/LRUCache.java
@@ -1,2 +1,22 @@
-package LINKED_LIST;public class LRUCache {
+package LINKED_LIST;
+
+public class LRUCache {
+
+    private class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+            this.prev=null;
+        }
+    }
+    
+    public static void main(String[] args) {
+
+    }
+
 }
