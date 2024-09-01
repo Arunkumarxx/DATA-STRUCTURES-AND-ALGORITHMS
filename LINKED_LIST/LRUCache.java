94e698f859b89483428b1316dd4f253495705a19
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-01 21:29:53 +0530

    modified

--- LINKED_LIST/LRUCache.java
+++ LINKED_LIST/LRUCache.java
@@ -74,6 +74,9 @@ public class LRUCache {
         LRUCache lruCache = new LRUCache(2);
         lruCache.put(1, 1);
         lruCache.put(2, 2);
-        System.out.println(lruCache.cache.get(1).value);
+        System.out.println(lruCache.get(1));
+        lruCache.put(3, 3);
+        System.out.println(lruCache.get(2)); 
+        lruCache.put(4, 4);
     }
 }
