a070e00582c560604b7547b8515d5cbc50b971f2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-01 21:29:26 +0530

    modified

--- LINKED_LIST/LRUCache.java
+++ LINKED_LIST/LRUCache.java
@@ -2,88 +2,78 @@ package LINKED_LIST;
 
 import java.util.HashMap;
 
+public class LRUCache {
 
- public class LRUCache {
+    private class Node {
+        int value;
+        int key;
+        Node next;
+        Node prev;
 
-     private class Node {
-         int value;
-         int key;
-         Node next;
-         Node prev;
-
-         Node(int key,int value) {
-             this.key = key;
-             this.value=value;
-             this.next = null;
-             this.prev = null;
-         }
-     }
-
-     private HashMap<Integer, Node> cache = new HashMap<>();
-     private int capacity;
-     private Node head = null;
-     private Node tail = null;
-
-     public LRUCache(int capacity)
-     {
-         this.capacity=capacity;
-     }
-
-     private void moveToHead(Node node)
-     {
-         if(head==node)
-             return;
-         if(node.prev!=null) node.prev.next=node.next;
-         if(node.next!=null) node.next.prev=node.prev;
-
-         node.next=head;
-         node.prev=null;
-
-         if(head!=null)
-             head.prev=node;
-         head=node;
-         if(tail==null) tail=head;
-     }
-     private int get(int key)
-     {
-         if(!cache.containsKey(key)) return -1;
-         Node get=cache.get(key);
-         moveToHead(get);
-         return get.value;
-     }
-     private  void put(int key,int value)
-     {
-         if(cache.containsKey(key))
-         {
-             Node node=cache.get(key);
-             node.value=value;
-             moveToHead(node);
-         }
-         else {
-             Node newNode= new Node(key,value);
-             cache.put(key,newNode);
-             if(head==null)
-                 head=tail=newNode;
-             else
-             {
-                 newNode.next=head;
-                 head.prev=newNode;
-                 head=newNode;
-             }
-             if(cache.size()>capacity)
-             {
-                 cache.remove(tail.key);
-                 tail=tail.prev;
-                 if(tail!=null) tail.next=null;
-             }
-         }
-     }
-    public static void main(String[] args) {
-         LRUCache lruCache =new LRUCache(2);
-         lruCache.put(1,1);
-         lruCache.put(2,2);
-        Node temp=lruCache.cache.get(1);
-        System.out.println(temp.value);
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+            this.prev = null;
+        }
     }
 
+    private HashMap<Integer, Node> cache = new HashMap<>();
+    private int capacity;
+    private Node head = null;
+    private Node tail = null;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+    }
+
+    private void moveToHead(Node node) {
+        if (head == node)
+            return;
+        if (node.prev != null) node.prev.next = node.next;
+        if (node.next != null) node.next.prev = node.prev;
+        if (node == tail) tail = tail.prev;
+        node.next = head;
+        node.prev = null;
+        if (head != null) head.prev = node;
+        head = node;
+        if (tail == null) tail = head;
+    }
+
+    public int get(int key) {
+        if (!cache.containsKey(key)) return -1;
+        Node node = cache.get(key);
+        moveToHead(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            node.value = value;
+            moveToHead(node);
+        } else {
+            Node newNode = new Node(key, value);
+            cache.put(key, newNode);
+            if (head == null)
+                head = tail = newNode;
+            else {
+                newNode.next = head;
+                head.prev = newNode;
+                head = newNode;
+            }
+            if (cache.size() > capacity) {
+                cache.remove(tail.key);
+                tail = tail.prev;
+                if (tail != null) tail.next = null;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        LRUCache lruCache = new LRUCache(2);
+        lruCache.put(1, 1);
+        lruCache.put(2, 2);
+        System.out.println(lruCache.cache.get(1).value);
+    }
 }
