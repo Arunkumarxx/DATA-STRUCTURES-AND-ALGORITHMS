e9918cb725c514359070fb2fa8933b3e66d7403f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-03 18:59:34 +0530

    modified

--- STACK/STACK.java
+++ STACK/STACK.java
@@ -1,6 +1,21 @@
 package STACK;
 
 public class STACK {
+    private int [] arr ;
+    private  int size;
+
+    public STACK()
+    {
+        this.size=0;
+        this.arr=new  int [10];
+    }
+    public int add(int value)
+    {
+        if(size==arr.length)
+            
+            else arr[++size]=value;
+    }
+
 
     public static void main(String[] args) {
 
