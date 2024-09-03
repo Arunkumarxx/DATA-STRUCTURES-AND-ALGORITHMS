0f3360f19fdae9253f92e89b74a4eb84267ff106
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-03 19:35:37 +0530

    modified

--- STACK/LinkedListImplementationOfStack.java
+++ STACK/LinkedListImplementationOfStack.java
@@ -1,4 +1,13 @@
 package STACK;
 
 public class LinkedListImplementationOfStack {
+    private class Node
+    {
+        int data;
+        Node next;
+        Node prev;
+    }
+    public static void main(String[] args) {
+
+    }
 }
