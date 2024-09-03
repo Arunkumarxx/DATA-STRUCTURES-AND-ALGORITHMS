5f7271f63893d59031763d0c41ed29c1c55442ac
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-03 22:25:19 +0530

    modified

--- STACK/DeleteMidOfAStack.java
+++ STACK/DeleteMidOfAStack.java
@@ -2,8 +2,23 @@ package STACK;
 
 public class DeleteMidOfAStack {
 
+    public void deleteMid(Stack<Integer> s, int n) {
+
+        delete(s,n/2);
+    }
+    public void delete(Stack<Integer>s,int mid)
+    {
+        if(mid==0)
+        {
+            s.pop();
+            return;
+        }
+        int x= s.pop();
+        delete(s,mid-1);
+        s.push(x);
+    }
     public static void main(String[] args) {
-        
+
     }
 
 }
