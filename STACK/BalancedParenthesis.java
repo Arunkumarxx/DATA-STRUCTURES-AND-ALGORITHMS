1fe62d2a62302d5062a816eaa54e71501274f011
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-04 19:51:58 +0530

    modified

--- STACK/BalancedParenthesis.java
+++ STACK/BalancedParenthesis.java
@@ -5,8 +5,10 @@ import com.sun.security.jgss.GSSUtil;
 public class BalancedParenthesis {
 
     public static void main(String[] args) {
-        for(int i=0; i<=256;++i)
-            System.out.println((char)i);
+        for(int i=0; i<=256;++i) {
+            System.out.println(i);
+            System.out.print((char) i);
+        }
     }
 
 }
