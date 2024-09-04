58eaa9ed92a2f7b4ee053c5f476c2c828795febf
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-04 19:52:37 +0530

    modified

--- STACK/BalancedParenthesis.java
+++ STACK/BalancedParenthesis.java
@@ -6,8 +6,8 @@ public class BalancedParenthesis {
 
     public static void main(String[] args) {
         for(int i=0; i<=256;++i) {
-            System.out.println(i+" ");
-            System.out.print ((char) i);
+            System.out.print(i+" ");
+            System.out.print((char) i);
         }
     }
 
