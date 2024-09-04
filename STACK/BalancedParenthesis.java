b2be4796a062d3970ab5503c79f664671c137937
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-04 20:23:51 +0530

    modified

--- STACK/BalancedParenthesis.java
+++ STACK/BalancedParenthesis.java
@@ -17,20 +17,31 @@ public class BalancedParenthesis {
         newNode.next=head;
         head=newNode;
     }
-    private char pop()
-    {
-        char res=0;
-        if(head!=null)
-        {
-            res=head.data;
-            head=head.next;
+    private char pop() {
+        char res = 0;
+        if (head != null) {
+            res = head.data;
+            head = head.next;
             return res;
         }
-        return -1;
+        return ' ';
+    }
+    private void print()
+    {
+        StackNode temp=head;
+        while(temp!=null)
+        {
+            System.out.println(temp.data+" ");
+            temp=temp.next;
+        }
     }
 
     public static void main(String[] args) {
         BalancedParenthesis stack =new BalancedParenthesis();
-
+        stack.push('(');
+        stack.push('(');
+        stack.push(')');
+        stack.push(')');
+        stack.print();
     }
 }
