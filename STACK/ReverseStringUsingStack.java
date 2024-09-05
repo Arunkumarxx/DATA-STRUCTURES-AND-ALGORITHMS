3a4bc2c070e7b53007c5b8e03a314e4b7248a38f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-05 20:09:27 +0530

    modified

--- STACK/ReverseStringUsingStack.java
+++ STACK/ReverseStringUsingStack.java
@@ -1,2 +1,19 @@
-package STACK;public class ReverseStringUsingStack {
+package STACK;
+
+public class ReverseStringUsingStack {
+    private class StackNode
+    {
+        int data;
+        StackNode next;
+
+        StackNode(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+    }
+    private void 
+    public static void main(String[] args) {
+
+    }
 }
