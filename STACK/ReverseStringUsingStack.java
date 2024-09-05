6c477e933fb6162f2c53af58707cf17c59cdd92a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-05 20:19:25 +0530

    modified

--- STACK/ReverseStringUsingStack.java
+++ STACK/ReverseStringUsingStack.java
@@ -31,6 +31,13 @@ public class ReverseStringUsingStack {
     public static void main(String[] args) {
         String input="arun";
         ReverseStringUsingStack stack =new ReverseStringUsingStack();
-        
+        for(int i=0; i<input.length(); ++i)
+            stack.push(input.charAt(i));
+        String result="";
+        while (stack.head!=null)
+        {
+            result+=stack.pop();
+        }
+        System.out.println(result);
     }
 }
