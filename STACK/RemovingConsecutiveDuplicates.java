1eff4d179a8f92546b310ed1bdaf5a756a9c5aa7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-05 20:33:27 +0530

    modified

--- STACK/RemovingConsecutiveDuplicates.java
+++ STACK/RemovingConsecutiveDuplicates.java
@@ -1,8 +1,48 @@
 package STACK;
 
 public class RemovingConsecutiveDuplicates {
-    
+    private StackNode head;
+    private class StackNode
+    {
+        char data;
+        StackNode next;
+
+        StackNode(char data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+
+    }
+
+    private void push(char data)
+    {
+        if(head==null)
+            insertIntoStack(data);
+        else
+            if(head.data!=data)
+                insertIntoStack(data);
+    }
+
+    private void insertIntoStack(char data)
+    {
+        StackNode newNode =new StackNode(data);
+        newNode.next=head;
+        head=newNode;
+    }
+    private char pop(char data)
+    {
+        char x= head.data;
+        
+    }
+
     public static void main(String[] args) {
+        String input="aaaaabaabcccc";
+        RemovingConsecutiveDuplicates stack =new RemovingConsecutiveDuplicates();
+
+        for(int i=0; i<input.length();++i)
+            stack.push(input.charAt(i));
+        String result="";
 
     }
 
