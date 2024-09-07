fc0f2d1f86233cb19109513d284ef84ae53f8bd0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-07 13:16:57 +0530

    modified

--- STACK/PreviousGreaterElement.java
+++ STACK/PreviousGreaterElement.java
@@ -1,2 +1,22 @@
-package STACK;public class PreviousGreaterElement {
+package STACK;
+
+public class PreviousGreaterElement {
+    private  StackNode head;
+
+    private class StackNode
+    {
+        int data;
+        StackNode next;
+
+        public StackNode(int value)
+        {
+            this.data=value;
+            this.next=null;
+        }
+    }
+    
+    public static void main(String[] args) {
+
+    }
+
 }
