1df6a2f020964fdb9ef07eb5af5314a469504b18
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-07 09:33:25 +0530

    modified

--- STACK/ImplementTwoStacksInAnArray.java
+++ STACK/ImplementTwoStacksInAnArray.java
@@ -1,65 +1,67 @@
 package STACK;
 
 public class ImplementTwoStacksInAnArray {
-
-    private StackNode head;
-    class  StackNode {
-
-    }
-    private int [] stack;
+    private int[] stack;
     private int size1;
     private int size2;
 
-    twoStacks() {
-        this.stack=new int[10];
-        this.size1=0;
-        this.size2=stack.length-1;
+    public ImplementTwoStacksInAnArray() {
+        this.stack = new int[10];
+        this.size1 = 0;
+        this.size2 = stack.length - 1;
     }
 
-    // Function to push an integer into the stack1.
     void push1(int x) {
-        if(size1>size2)
+        if (size1 > size2) {
             resize();
-        stack[size1++]=x;
-    }
-    void resize()
-    {
-        int newSize=stack.length*2;
-        int [] arr =new int [newSize];
-
-        for(int i=0; i<size1;++i)
-            arr[i]=stack[i];
-
-        int newSize2=newSize-1;
-
-        for(int j=stack.length-1;j>size2;--j){
-            arr[newSize2--]=stack[j];
         }
-        size2=newSize2;
-        stack=arr;
+        stack[size1++] = x;
     }
-    // Function to push an integer into the stack2.
+
     void push2(int x) {
-        if(size1>size2)
+        if (size1 > size2) {
             resize();
-        stack[size2--]=x;
+        }
+        stack[size2--] = x;
     }
 
-    // Function to remove an element from top of the stack1.
     int pop1() {
-        if(size1>0)
+        if (size1 > 0) {
             return stack[--size1];
+        }
         return -1;
     }
 
-    // Function to remove an element from top of the stack2.
     int pop2() {
-        if(size2<stack.length-1)
+        if (size2 < stack.length - 1) {
             return stack[++size2];
+        }
         return -1;
     }
-    public static void main(String[] args) {
 
+    void resize() {
+        int newSize = stack.length * 2;
+        int[] arr = new int[newSize];
+
+        for (int i = 0; i < size1; ++i) {
+            arr[i] = stack[i];
+        }
+
+        int newSize2 = newSize - 1;
+
+        for (int j = stack.length - 1; j > size2; --j) {
+            arr[newSize2--] = stack[j];
+        }
+
+        size2 = newSize2;
+        stack = arr;
     }
 
+    public static void main(String[] args) {
+        ImplementTwoStacksInAnArray stacks = new ImplementTwoStacksInAnArray();
+        stacks.push1(5);
+        stacks.push2(10);
+        System.out.println(stacks.pop1());
+        System.out.println(stacks.pop2());
+    }
 }
