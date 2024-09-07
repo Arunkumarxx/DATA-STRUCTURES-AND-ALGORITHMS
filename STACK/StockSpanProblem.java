10751c35b55b399dfb14399fc5d8a0a22b1118b5
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-07 12:49:54 +0530

    modified

--- STACK/StockSpanProblem.java
+++ STACK/StockSpanProblem.java
@@ -5,58 +5,60 @@ import java.util.Stack;
 public class StockSpanProblem {
     private StackNode head;
 
-    private class StackNode
-    {
-        int [] data;
+    private class StackNode {
+        int[] data;
         StackNode next;
 
-        StackNode(int data1,int data2)
-        {
-            this.data[0]=data1;
-            this.data[1]=data2;
-            this.next=null;
+        StackNode(int data1, int data2) {
+            this.data = new int[2]; // Initialize the array
+            this.data[0] = data1;
+            this.data[1] = data2;
+            this.next = null;
         }
-
-    }
-    private void push(int value1,int value2)
-    {
-        StackNode newNode =new StackNode(value1,value2);
-        newNode.next=head;
-        head=newNode;
     }
 
-    private int [] top()
-    {
-        return head.data;
+    private void push(int value1, int value2) {
+        StackNode newNode = new StackNode(value1, value2);
+        newNode.next = head;
+        head = newNode;
     }
-    private int [] pop()
-    {
-            int[] res = head.data;
-            head = head.next;
-            return res;
+
+    private int[] top() {
+        return head != null ? head.data : null; // Check if head is not null
     }
-    private boolean isEmpty()
-    {
-        return head==null;
+
+    private int[] pop() {
+        if (isEmpty()) return null; // Handle empty stack case
+        int[] res = head.data;
+        head = head.next;
+        return res;
+    }
+
+    private boolean isEmpty() {
+        return head == null;
     }
 
     public static int[] calculateSpan(int arr[], int n) {
-        StockSpanProblem stack =new StockSpanProblem();
-        int [] res =new int[n];
-        for(int i=0; i<n; ++i)
-        {
-            while((!stack.isEmpty()) && stack.top()[0]<=arr[i])
-            {
+        StockSpanProblem stack = new StockSpanProblem();
+        int[] res = new int[n];
+
+        for (int i = 0; i < n; ++i) {
+            while (!stack.isEmpty() && stack.top()[0] <= arr[i]) {
                 stack.pop();
             }
-            res[i]=stack.isEmpty()?i+1:i-stack.top()[1];
-            stack.push(arr[i],i);
+            res[i] = stack.isEmpty() ? i + 1 : i - stack.top()[1];
+            stack.push(arr[i], i);
         }
+
         return res;
     }
-    public static void main(String[] args) {
-        int [] arr = {100, 80, 60, 70, 60, 75, 85};
-        System.out.println(calculateSpan(arr, arr.length).toString());
-    }
 
+    public static void main(String[] args) {
+        int[] arr = { 100, 80, 60, 70, 60, 75, 85 };
+        int n = arr.length;
+        int[] spans = calculateSpan(arr, n);
+        for (int span : spans) {
+            System.out.print(span + " ");
+        }
+    }
 }
