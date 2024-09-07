e978a7c75fc46a9a019a7fecdeedb2d7d638b20e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-07 11:30:09 +0530

    modified

--- STACK/StockSpanProblem.java
+++ STACK/StockSpanProblem.java
@@ -1,9 +1,26 @@
 package STACK;
 
 public class StockSpanProblem {
+    private StackNode head;
 
+    private class StackNode
+    {
+        int data;
+        StackNode next;
+
+        StackNode(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+
+    }
+    
+    public static int[] calculateSpan(int arr[], int n) {
+
+    }
     public static void main(String[] args) {
-        
+
     }
 
 }
