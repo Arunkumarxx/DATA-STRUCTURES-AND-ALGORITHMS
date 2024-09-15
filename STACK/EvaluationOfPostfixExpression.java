88cf14b650950c8fd821e410b45b70f4b03496b4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-15 23:04:27 +0530

    modified

--- STACK/EvaluationOfPostfixExpression.java
+++ STACK/EvaluationOfPostfixExpression.java
@@ -52,17 +52,17 @@ public class EvaluationOfPostfixExpression {
                 int second=stack.pop()-48;
                 if(c=='-')
                 {
-                    stack.push(first-second);
+                    stack.push(second-first);
                 }
                 else if(c=='+')
                 {
-                    stack.push(first+second);
+                    stack.push(second+first);
                 }else if(c=='*')
                 {
-                    stack.push(first*second);
+                    stack.push(second*first);
                 }else if(c=='/')
                 {
-                    stack.push(first/second);
+                    stack.push(second/first);
                 }
             }
         }
