4aefcd36adfb1536378c655747f6464c8ff8e3a6
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-15 22:37:02 +0530

    modified

--- STACK/EvaluationOfPostfixExpression.java
+++ STACK/EvaluationOfPostfixExpression.java
@@ -1,10 +1,24 @@
 package STACK;
 
 public class EvaluationOfPostfixExpression {
+    private StackNode head;
     private class StackNode
     {
-        char 
+        char data;
+        StackNode next;
+        StackNode(char value)
+        {
+            this.data=value;
+            this.next=null;
+        }
     }
+    private void push(char value)
+    {
+        StackNode newNode = new StackNode(value);
+        newNode.next=head;
+        head=newNode;
+    }
+    private 
     public static void main(String[] args) {
 
     }
