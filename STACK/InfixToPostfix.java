c3587d6547f7cc6c9e217e7933cdf369041cd6da
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-15 21:12:55 +0530

    modified

--- STACK/InfixToPostfix.java
+++ STACK/InfixToPostfix.java
@@ -1,2 +1,13 @@
-package STACK;public class InfixToPostfix {
+package STACK;
+
+public class InfixToPostfix {
+    private class StackNode {
+        int data;
+        StackNode next;
+    }
+    private 
+    public static void main(String[] args) {
+
+    }
+
 }
