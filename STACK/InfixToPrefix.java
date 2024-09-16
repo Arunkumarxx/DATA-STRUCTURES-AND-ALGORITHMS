d9804f667b012d17573678961b3cd13850d3c34f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-16 11:39:47 +0530

    modified

--- STACK/InfixToPrefix.java
+++ STACK/InfixToPrefix.java
@@ -1,9 +1,38 @@
 package STACK;
 
 public class InfixToPrefix {
-    private class StackNode{
-        char data;
+    private StackNode head;
+
+    private class StackNode {
+        char value;
+        StackNode next;
+        StackNode(char value) {
+            this.value = value;
+            this.next = null;
+        }
     }
+
+    private void push(char value) {
+        StackNode newNode = new StackNode(value);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    private char top() {
+        return head != null ? head.value : '\0';
+    }
+
+    private char pop() {
+        if (head == null) return '\0';
+        char res = head.value;
+        head = head.next;
+        return res;
+    }
+
+    private boolean isEmpty() {
+        return head == null;
+    }
+
     public static void main(String[] args) {
 
     }
