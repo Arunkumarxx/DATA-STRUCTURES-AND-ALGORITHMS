7b6e7778ef03cc128cb5e7792ba20d3bbb4494c7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-16 12:08:27 +0530

    modified

--- STACK/InfixToPrefix.java
+++ STACK/InfixToPrefix.java
@@ -54,7 +54,7 @@ public class InfixToPrefix {
 
     public static void main(String[] args) {
         InfixToPrefix stack = new InfixToPrefix();
-        String str = "A * B + C / D";
+        String str = " (A – B/C) * (A/K-L)";
         int n = str.length();
         StringBuilder rev = new StringBuilder();
 
