81209ce6766ecf5fe8fcf52646005c50e0653c4d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-20 20:46:10 +0530

    modified

--- QUEUE/ImplementationOfQueueUsingLinkedList.java
+++ QUEUE/ImplementationOfQueueUsingLinkedList.java
@@ -1,7 +1,40 @@
 package QUEUE;
 
 public class ImplementationOfQueueUsingLinkedList {
-    
+    QueueNode front, rear;
+    MyQueue()
+    {
+        this.front=null;
+        this.rear=null;
+    }
+    //Function to push an element into the queue.
+    void push(int a)
+    {
+        QueueNode newNode=new QueueNode (a);
+        if(rear==null)
+        {
+            front=rear=newNode;
+        }
+        else  {
+            rear.next=newNode;
+            rear=newNode;
+        }
+    }
+
+    //Function to pop front element from the queue.
+    int pop()
+    {
+        // Your code here
+        if(front!=null)
+        {
+            int x=front.data;
+            front=front.next;
+            if(front==null)
+                rear=null;
+            return x;
+        }
+        return -1;
+    }
     public static void main(String[] args) {
 
     }
