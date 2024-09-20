f9a8a3e499fa2bd108a62f172f3de711f6f278fb
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-20 20:46:55 +0530

    modified

--- QUEUE/ImplementationOfQueueUsingLinkedList.java
+++ QUEUE/ImplementationOfQueueUsingLinkedList.java
@@ -1,52 +1,59 @@
 package QUEUE;
 
 public class ImplementationOfQueueUsingLinkedList {
-    class QueueNode
-    {
+    class QueueNode {
         int data;
         QueueNode next;
-        QueueNode(int a)
-        {
+
+        QueueNode(int a) {
             data = a;
             next = null;
         }
     }
+
     QueueNode front, rear;
-    MyQueue()
-    {
-        this.front=null;
-        this.rear=null;
+
+    ImplementationOfQueueUsingLinkedList() {
+        this.front = null;
+        this.rear = null;
     }
-    //Function to push an element into the queue.
-    void push(int a)
-    {
-        QueueNode newNode=new QueueNode (a);
-        if(rear==null)
-        {
-            front=rear=newNode;
-        }
-        else  {
-            rear.next=newNode;
-            rear=newNode;
+
+    void push(int a) {
+        QueueNode newNode = new QueueNode(a);
+        if (rear == null) {
+            front = rear = newNode;
+        } else {
+            rear.next = newNode;
+            rear = newNode;
         }
     }
 
-    //Function to pop front element from the queue.
-    int pop()
-    {
-        // Your code here
-        if(front!=null)
-        {
-            int x=front.data;
-            front=front.next;
-            if(front==null)
-                rear=null;
+    int pop() {
+        if (front != null) {
+            int x = front.data;
+            front = front.next;
+            if (front == null) {
+                rear = null;
+            }
             return x;
         }
         return -1;
     }
-    public static void main(String[] args) {
 
+    boolean isEmpty() {
+        return front == null;
     }
 
+    public static void main(String[] args) {
+        ImplementationOfQueueUsingLinkedList queue = new ImplementationOfQueueUsingLinkedList();
+
+        queue.push(10);
+        queue.push(20);
+        queue.push(30);
+
+        System.out.println(queue.pop());
+        System.out.println(queue.pop());
+        System.out.println(queue.pop());
+        System.out.println(queue.pop());
+    }
 }
