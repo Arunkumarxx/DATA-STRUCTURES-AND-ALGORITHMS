af4ddec386571674bea7822db1b71d79e0e19ac1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-20 19:57:03 +0530

    modified

--- STACK/STACK.java
+++ STACK/STACK.java
@@ -1,63 +1,64 @@
 package STACK;
-import java.util.Stack;
-public class STACK {
-    private int [] arr ;
-    private  int size;
 
-    public STACK()
-    {
-        this.size=0;
-        this.arr=new  int [10];
+public class STACK {
+    private int[] arr;
+    private int size;
+
+    public STACK() {
+        this.size = 0;
+        this.arr = new int[10];
     }
-    public void push(int value)
-    {
-        if(size==arr.length)
+
+    public void push(int value) {
+        if (size == arr.length) {
             resize();
-            else {
-            arr[size] = value;
-            ++size;
         }
+        arr[size] = value;
+        ++size;
     }
-    public int  pop ()
-    {
-        if(isEmpty())
+
+    public int pop() {
+        if (isEmpty()) {
             return -1;
-        int res= arr[size-1];
-        arr[size-1]=0;
+        }
+        int res = arr[size - 1];
+        arr[size - 1] = 0;
         --size;
         return res;
     }
+
     public boolean isEmpty() {
-        return size==0;
+        return size == 0;
     }
-    public void resize()
-    {
-        int [] newArr =new int[size*2];
-        for(int i=0; i<size;++i)
-        {
-            newArr[i]=arr[i];
+
+    public void resize() {
+        int[] newArr = new int[arr.length * 2];
+        System.arraycopy(arr, 0, newArr, 0, size);
+        arr = newArr;
+    }
+
+    public void print() {
+        for (int i = size - 1; i >= 0; --i) {
+            System.out.println("|" + arr[i] + "|");
         }
-        arr=newArr;
-    }
-    public void print()
-    {
-        for(int i=size-1;i>=0;--i)
-            System.out.println("|"+arr[i]+"|");
     }
 
     public static void main(String[] args) {
-        STACK stack =new STACK();
-        int [] arr ={10, 20, 30, 50, 10, 70, 30};
+        STACK stack = new STACK();
+        int[] arr = {10, 20, 30, 50, 10, 70, 30};
 
-        int n= arr.length;;
-        long res[]= new long[n];
-        for(int i=n-1; i>=0; --i)
-        {
-            while(!stack.isEmpty() && arr[(int)stack.top()]>=arr[i])
+        int n = arr.length;
+        long[] res = new long[n];
+        for (int i = n - 1; i >= 0; --i) {
+            while (!stack.isEmpty() && arr[stack.pop()] >= arr[i]) {
                 stack.pop();
-            res[i]=stack.isEmpty()?n:stack.top();
+            }
+            res[i] = stack.isEmpty() ? n : stack.pop();
             stack.push(i);
         }
-        return res;
+
+        for (long r : res) {
+            System.out.print(r + " ");
+        }
     }
 }
