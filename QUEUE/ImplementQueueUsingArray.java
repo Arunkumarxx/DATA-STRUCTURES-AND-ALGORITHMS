b1f0bf5e27c8770244998ac8582164c092c17892
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-20 19:52:05 +0530

    modified

--- QUEUE/ImplementQueueUsingArray.java
+++ QUEUE/ImplementQueueUsingArray.java
@@ -1,7 +1,34 @@
 package QUEUE;
 
 public class ImplementQueueUsingArray {
-    
+   private class MyQueue {
+
+        int front, rear;
+        int arr[] = new int[100005];
+        int size=arr.length;
+
+        MyQueue()
+        {
+            front=0;
+            rear=0;
+        }
+        void push(int x) {
+            if (rear == size) {
+                return;
+            } else {
+                arr[rear++] = x;
+            }
+        }
+
+
+        int pop() {
+            if (front == rear) {
+                return -1;
+            } else {
+                return arr[front++];
+            }
+        }
+    }
     public static void main(String[] args) {
 
     }
