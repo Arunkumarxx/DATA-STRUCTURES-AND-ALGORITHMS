74ccdd3f1b98481c6484bdd3482fdf066a299701
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-21 14:58:47 +0530

    modified

--- QUEUE/QueueReversal.java
+++ QUEUE/QueueReversal.java
@@ -1,2 +1,33 @@
-package QUEUE;public class QueueReversal {
+package QUEUE;
+
+public class QueueReversal {
+    private QueueNode front;
+    private QueueNode rear;
+    private  class QueueNode
+    {
+        int data;
+        QueueNode next;
+        QueueNode (int value)
+        {
+            this.data=value;
+            this.next=null;
+        }
+    }
+    private void push(int value)
+    {
+        QueueNode newNode =new QueueNode(value);
+        if(rear==null)
+        {
+            front=rear=newNode;
+        }
+        else
+        {
+            newNode.next
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
