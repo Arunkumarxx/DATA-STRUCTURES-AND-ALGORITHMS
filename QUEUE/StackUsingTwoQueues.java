7fc29e1930e36da033f46a549f52b9a2f8304468
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-21 14:00:28 +0530

    modified

--- QUEUE/StackUsingTwoQueues.java
+++ QUEUE/StackUsingTwoQueues.java
@@ -3,34 +3,39 @@ package QUEUE;
 import java.util.LinkedList;
 import java.util.Queue;
 
+public class StackUsingTwoQueues {
+    Queue<Integer> q1 = new LinkedList<>();
+    Queue<Integer> q2 = new LinkedList<>();
 
-public  class StackUsingTwoQueues {
-    Queue<Integer> q1= new LinkedList<>();
-    Queue<Integer> q2 =new LinkedList<>();
-
-    private void push(int value)
-    {
+    public void push(int value) {
         q2.add(value);
 
-            while (!q1.isEmpty()) {
-                q2.add(q1.poll());
-            }
-            Queue<Integer> temp =q1;
-            q1=q2;
-            q2=temp;
+        while (!q1.isEmpty()) {
+            q2.add(q1.poll());
         }
-    }
-    private int pop()
-    {
 
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
     }
+
+    public int pop() {
+        if (!q1.isEmpty()) {
+            return q1.poll();
+        }
+        return -1;
+    }
+
+
     public static void main(String[] args) {
-        StackUsingTwoQueues stack =new StackUsingTwoQueues();
+        StackUsingTwoQueues stack = new StackUsingTwoQueues();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
 
-        stack.push(2);
-        stack.push(2);
-        stack.push(2);
-
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
     }
-
 }
