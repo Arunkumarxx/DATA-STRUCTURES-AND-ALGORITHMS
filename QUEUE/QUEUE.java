32e890ba0f49b8a9f9d665a21f8f119d18582b57
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-21 18:35:40 +0530

    modified

--- QUEUE/QUEUE.java
+++ QUEUE/QUEUE.java
@@ -1,15 +1,11 @@
 package QUEUE;
-
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Stack;
-
 public class QUEUE {
 
     public static void main(String[] args) {
-        Queue<Integer> queue =new LinkedList<>();
-        
+        int n=
     }
+    private void recursive()
+    {
 
+    }
 }
