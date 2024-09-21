d590dc45c28940bca4d05ffbbe04e4c1ee1112fc
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-21 11:02:12 +0530

    modified

--- QUEUE/ImplementingQueueUsingStack.java
+++ QUEUE/ImplementingQueueUsingStack.java
@@ -8,6 +8,7 @@ public class ImplementingQueueUsingStack {
     public static void main(String[] args) {
         Queue<Integer> q1 = new LinkedList<Integer>();
         q1.add(1);
+        q1.add(2);
         System.out.println(q1.peek());
     }
 
