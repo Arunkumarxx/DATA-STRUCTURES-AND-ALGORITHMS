374878ef1505165039a02f92eb71935eae3c32d8
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-21 12:30:38 +0530

    modified

--- QUEUE/ImplementingQueueUsingStack.java
+++ QUEUE/ImplementingQueueUsingStack.java
@@ -13,7 +13,14 @@ public class ImplementingQueueUsingStack {
     }
     private int pop()
     {
-
+        if(stack2.isEmpty())
+        {
+            while(!stack1.isEmpty())
+            {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
     }
 
 
