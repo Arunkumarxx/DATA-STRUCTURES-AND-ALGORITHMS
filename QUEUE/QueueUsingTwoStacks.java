ec014664d2b61cff2923eec7be567bd9a403fe41
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-21 18:17:57 +0530

    modified

--- QUEUE/QueueUsingTwoStacks.java
+++ QUEUE/QueueUsingTwoStacks.java
@@ -50,7 +50,30 @@ public class QueueUsingTwoStacks {
         }
     }
     public static void main(String[] args) {
-        
+
     }
 
+}
+
+class StackQueue {
+    Stack<Integer> s1 = new Stack<Integer>();
+    Stack<Integer> s2 = new Stack<Integer>();
+
+    void Push(int x) {
+        s1.push(x);
+    }
+
+    int Pop() {
+        if (s1.isEmpty() && s2.isEmpty()) {
+            return -1;
+        }
+
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+
+        return s2.pop();
+    }
 }
