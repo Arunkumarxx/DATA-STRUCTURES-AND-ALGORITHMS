4b6d17ca1e4420b8b60754472df13fa3718d0c50
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-21 18:51:20 +0530

    modified

--- QUEUE/QUEUE.java
+++ QUEUE/QUEUE.java
@@ -1,25 +1,6 @@
 package QUEUE;
 public class QUEUE {
-    static StringBuilder res =new StringBuilder();
     public static void main(String[] args) {
-        int n=5;
-        QUEUE q =new QUEUE();
-        q.recursive(n);
-        System.out.println(res);
-        res.setLength(0);
-    }
-    private void recursive(int n)
-    {
-        if(n==0) {
-            res.append(0);
-            return;
-        }
-        if(n==1)
-        {
-            res.append(1);
-            return;
-        }
-        recursive(n/2);
-        res.append(n%2);
+
     }
 }
