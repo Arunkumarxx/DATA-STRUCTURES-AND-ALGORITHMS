0bc96cf4f36f497df9b302f19d78caa8cc56289c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-21 18:20:46 +0530

    modified

--- QUEUE/QueueUsingTwoStacks.java
+++ QUEUE/QueueUsingTwoStacks.java
@@ -1,69 +1,66 @@
 package QUEUE;
 
-public class QueueUsingTwoStacks {
-    private QueueNode front;
-    private QueueNode rear;
-    private  class QueueNode
-    {
+import java.util.Stack;
+
+ class QueueUsingLinkedList {
+    protected QueueNode front;
+    protected QueueNode rear;
+
+    protected class QueueNode {
         int data;
         QueueNode next;
-        QueueNode (int value)
-        {
-            this.data=value;
-            this.next=null;
+
+        QueueNode(int value) {
+            this.data = value;
+            this.next = null;
         }
     }
-    private void add(int value)
-    {
-        QueueNode newNode =new QueueNode(value);
-        if(rear==null)
-        {
-            front=rear=newNode;
-        }
-        else
-        {
-            rear.next=newNode;
-            rear=newNode;
+
+    public void add(int value) {
+        QueueNode newNode = new QueueNode(value);
+        if (rear == null) {
+            front = rear = newNode;
+        } else {
+            rear.next = newNode;
+            rear = newNode;
         }
     }
-    private int poll()
-    {
-        if(isEmpty())
+
+    public int poll() {
+        if (isEmpty()) {
             return -1;
-        int x=front.data;
-        front= front.next;
-        if(front==null)
-            rear=null;
-        return x;
-    }
-    private boolean isEmpty()
-    {
-        return rear==null;
-    }
-    private void print()
-    {
-        QueueNode temp = front;
-        while(temp!=null)
-        {
-            System.out.print(temp.data+" ");
-            temp=temp.next;
         }
+        int value = front.data;
+        front = front.next;
+        if (front == null) {
+            rear = null;
+        }
+        return value;
     }
-    public static void main(String[] args) {
 
+    public boolean isEmpty() {
+        return front == null;
     }
 
+    public void print() {
+        QueueNode temp = front;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
 }
 
-class StackQueue {
-    Stack<Integer> s1 = new Stack<Integer>();
-    Stack<Integer> s2 = new Stack<Integer>();
+ public  class StackQueue extends QueueUsingLinkedList {
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
 
-    void Push(int x) {
+    public void Push(int x) {
         s1.push(x);
     }
 
-    int Pop() {
+    public int Pop() {
         if (s1.isEmpty() && s2.isEmpty()) {
             return -1;
         }
@@ -75,5 +72,24 @@ class StackQueue {
         }
 
         return s2.pop();
+    }
+
+    public static void main(String[] args) {
+        StackQueue queue = new StackQueue();
+
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        System.out.println("Queue using Linked List:");
+        queue.print();
+        System.out.println("Polled: " + queue.poll());
+        queue.print();
+
+        System.out.println("Queue using Two Stacks:");
+        queue.Push(40);
+        queue.Push(50);
+        System.out.println(queue.Pop());
+        System.out.println(queue.Pop());
+        System.out.println(queue.Pop());
     }
 }
