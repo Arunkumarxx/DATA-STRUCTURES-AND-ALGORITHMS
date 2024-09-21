b95863036392b20cfc5e69a045e5c9adcbc64b8a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-21 12:47:17 +0530

    modified

--- QUEUE/QUEUE.java
+++ QUEUE/QUEUE.java
@@ -1,9 +1,12 @@
 package QUEUE;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class QUEUE {
 
     public static void main(String[] args) {
-
+        Queue<Integer> q = new LinkedList<>();
     }
 
 }
