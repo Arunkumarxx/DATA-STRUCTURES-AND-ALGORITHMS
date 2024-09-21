9b9164bc6834dfcf5a94c476eb3da33782a68e24
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-21 12:23:46 +0530

    modified

--- QUEUE/ImplementingQueueUsingStack.java
+++ QUEUE/ImplementingQueueUsingStack.java
@@ -1,15 +1,10 @@
 package QUEUE;
 
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class ImplementingQueueUsingStack {
 
     public static void main(String[] args) {
-        Queue<Integer> q1 = new LinkedList<Integer>();
-        q1.add(1);
-        q1.add(2);
-        System.out.println(q1);
+
     }
 
 }
