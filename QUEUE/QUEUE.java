e76189e0484e899d90273918fca2ddc818c5f293
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-21 12:59:47 +0530

    modified

--- QUEUE/QUEUE.java
+++ QUEUE/QUEUE.java
@@ -2,14 +2,14 @@ package QUEUE;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class QUEUE {
 
     public static void main(String[] args) {
-        Queue<Integer> q = new LinkedList<>();
+        Stack<Integer> q = new Stack<Integer>();
         q.add(1);
         q.add(2);
-        System.out.println(q.poll());
     }
 
 }
