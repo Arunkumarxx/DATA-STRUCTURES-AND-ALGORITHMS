41ff4f57354c0439fb4e9d6e03cc821704361fa0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-21 13:45:09 +0530

    modified

--- QUEUE/StackUsingTwoQueues.java
+++ QUEUE/StackUsingTwoQueues.java
@@ -7,7 +7,23 @@ import java.util.Stack;
 public class StackUsingTwoQueues {
     Queue<Integer> q1= new LinkedList<>();
     Queue<Integer> q2 =new LinkedList<>();
-    
+
+    private void push(int value)
+    {
+        q2.add(value);
+        if(q1.isEmpty())
+        {
+            q1.add(q2.poll());
+        }
+        else
+        {
+            
+        }
+    }
+    private int pop()
+    {
+
+    }
     public static void main(String[] args) {
 
     }
