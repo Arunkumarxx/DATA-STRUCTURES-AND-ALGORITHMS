162ba3c89a6a28475236c5fdbb536144f64ee2f7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-21 13:41:24 +0530

    modified

--- QUEUE/StackUsingTwoQueues.java
+++ QUEUE/StackUsingTwoQueues.java
@@ -1,2 +1,15 @@
-package QUEUE;public class StackUsingTwoQueues {
+package QUEUE;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class StackUsingTwoQueues {
+    Queue<Integer> q1= new LinkedList<>();
+    Queue<Integer> q2 =new LinkedList<>();
+    
+    public static void main(String[] args) {
+
+    }
+
 }
