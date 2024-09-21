fd1ba7bb9201e996074525b299fe9573f7110dda
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-21 18:36:09 +0530

    modified

--- QUEUE/QUEUE.java
+++ QUEUE/QUEUE.java
@@ -2,10 +2,14 @@ package QUEUE;
 public class QUEUE {
 
     public static void main(String[] args) {
-        int n=
+        int n=5;
     }
-    private void recursive()
+    private void recursive(int n)
     {
-
+        if(n==0)
+            return;
+        System.out.println(n%2);
+        n=n/2;
+        recursive(n/2);
     }
 }
