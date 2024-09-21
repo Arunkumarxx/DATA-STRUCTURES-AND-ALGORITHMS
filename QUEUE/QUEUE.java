409143a071d9fd1962d6b05c57b95248113cdfbb
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-21 13:40:29 +0530

    modified

--- QUEUE/QUEUE.java
+++ QUEUE/QUEUE.java
@@ -7,9 +7,7 @@ import java.util.Stack;
 public class QUEUE {
 
     public static void main(String[] args) {
-        Stack<Integer> q = new Stack<Integer>();
-        q.add(1);
-        q.add(2);
+
     }
 
 }
