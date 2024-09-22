d93a720123f708b103674d6b2bbd23a46216eea3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-22 21:00:52 +0530

    modified

--- TREE/TREE.java
+++ TREE/TREE.java
@@ -1,8 +1,14 @@
 package TREE;
 
-public class TREE <> {
-
+public class TREE <Secure> {
+    private Secure data ;
+    TREE(Secure d)
+    {
+     this.data=d;
+    }
     public static void main(String[] args) {
+        TREE tree =new TREE(1);
+        System.out.println(tree.data);
 
     }
 
