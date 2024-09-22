868cdb7e1d20d999030378eda8366bef875b8f55
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-22 14:42:16 +0530

    modified

--- QUEUE/InsertionInDeque.java
+++ QUEUE/InsertionInDeque.java
@@ -19,9 +19,33 @@ public class InsertionInDeque {
     {
         DequeNode newNode =new DequeNode(value);
         if(front==null)
+            front=rear=newNode;
+        else
         {
-
+            front.prev=newNode;
+            newNode.next=front;
+            front=newNode;
         }
+    }
+    private int pop()
+    {
+        
+    }
+    private void addLast(int value)
+    {
+        DequeNode newNode = new DequeNode(value);
+        if(rear==null)
+            front=rear=newNode;
+        else
+        {
+            rear.next=newNode;
+            newNode.prev=rear;
+            rear=newNode;
+        }
+    }
+    private boolean isEmpty()
+    {
+        return rear==null;
     }
     public static void main(String[] args) {
         ArrayDeque<Integer> q =new ArrayDeque<>();
