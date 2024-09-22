aba9adf7f3c187fe2f32a546ac3ebaa1eba02d8d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-22 14:13:35 +0530

    modified

--- QUEUE/InsertionInDeque.java
+++ QUEUE/InsertionInDeque.java
@@ -1,9 +1,28 @@
 package QUEUE;
 
 public class InsertionInDeque {
-
+    private class DequeNode{
+        int data;
+        DequeNode next;
+        DequeNode prev;
+        DequeNode(int value)
+        {
+            this.data=value;
+            this.next=null;
+            this.prev=null;
+        }
+    }
+    DequeNode front,rear;
+    private void addFirst(int value)
+    {
+        DequeNode newNode =new DequeNode(value);
+        if(front==null)
+        {
+            
+        }
+    }
     public static void main(String[] args) {
-        
+
     }
 
 }
