2f74455f111ece4072a631500d82fcb16a79f267
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-22 11:33:52 +0530

    modified

--- SORTING_ALGORITHM_COMPARSION/AlgorithmComparison.java
+++ SORTING_ALGORITHM_COMPARSION/AlgorithmComparison.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class AlgorithmComparison {
     public static void main(String args[]) {
 
-        int size = Integer.MAX_VALUE/2;
+        int size = Integer.MAX_VALUE/3;
         int arr[] = new int[size];
         for (int i = 0; i < size; i++) {
             arr[i] = (int) (Math.random() * 1000);
