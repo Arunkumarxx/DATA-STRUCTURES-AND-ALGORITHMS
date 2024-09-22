b1eddadf8278ae474490a08d4798e50942c471f0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-22 11:21:40 +0530

    modified

--- QUEUE/GasStationOrCircularTour.java
+++ QUEUE/GasStationOrCircularTour.java
@@ -1,2 +1,38 @@
-package QUEUE;public class CircularTour {
+package QUEUE;
+
+public class GasStationOrCircularTour {
+    private class QueueNode
+    {
+        int data;
+        QueueNode next;
+        QueueNode(int x)
+        {
+            this.data=x;
+            this.next=null;
+        }
+    }
+    QueueNode front,rear;
+    private void push(int x)
+    {
+        QueueNode newNode =new QueueNode(x);
+        if(rear==null)
+        {
+            front=rear=newNode;
+        }
+        else
+        {
+            rear.next=newNode;
+            rear=newNode;
+        }
+    }
+    private int pop()
+    {
+        int x=front.data;
+        front=front.next;
+        return x;
+    }
+    public static void main(String[] args) {
+
+    }
+
 }
