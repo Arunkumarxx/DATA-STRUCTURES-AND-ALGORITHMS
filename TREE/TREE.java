b6a2f6023f8f21532c2a36a679410e9490eea19b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-22 21:18:07 +0530

    modified

--- TREE/TREE.java
+++ TREE/TREE.java
@@ -1,13 +1,20 @@
 package TREE;
 
+import com.sun.security.jgss.GSSUtil;
+
 public class TREE <Secure> {
     private Secure data ;
-    TREE(Secure  d )
+    TREE(Secure  d ) {
+        this.data = d;
+    }
+    public void setData(Secure x)
     {
-     this.data=d;
+        this.data=x;
     }
     public static void main(String[] args) {
         TREE<Integer> tree =new TREE("s");
+        System.out.println(tree.data);
+        tree.setData(1023);
         System.out.println(tree.data);
     }
 
