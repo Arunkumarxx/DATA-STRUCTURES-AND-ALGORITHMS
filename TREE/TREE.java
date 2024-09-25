eeb8156e79a25f041bb1e774965e9b0165593105
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-25 12:21:33 +0530

    modified

--- TREE/TREE.java
+++ TREE/TREE.java
@@ -22,7 +22,7 @@ public class TREE  {
         if(root==null)
         {
             root=new Tree(value);
-            return 
+            return root;
         }
     }
     public static void main(String[] args) {
