f36f3f38fc62ac0aad724f7492f431175dcb42e2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-25 21:57:44 +0530

    modified

--- TREE/HeightOfBinaryTree.java
+++ TREE/HeightOfBinaryTree.java
@@ -47,7 +47,13 @@ public class HeightOfBinaryTree {
     }
     public static void main(String[] args) {
         HeightOfBinaryTree tree =new HeightOfBinaryTree();
-        
+        tree.insert(3);
+        tree.insert(2);;
+        tree.insert(1);
+        tree.insert(5);
+        tree.insert(4);
+        tree.insert(6);
+        tree.insert(7);
     }
 
 }
