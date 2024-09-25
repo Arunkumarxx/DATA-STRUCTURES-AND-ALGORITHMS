36d99f1f25ed42d047252258a8fbd4626996db87
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-25 12:27:09 +0530

    modified

--- TREE/TREE.java
+++ TREE/TREE.java
@@ -24,8 +24,30 @@ public class TREE  {
             root=new Tree(value);
             return root;
         }
+        if(value<root.data)
+            root.left=addNode(root.left,value);
+        if(value>root.data)
+            root.right=addNode(root.right,value);
+        return root;
+    }
+    private void print()
+    {
+        traversal(root);
+    }
+    private void traversal(Tree root)
+    {
+        if(root!=null)
+        {
+            System.out.print(root.data+" ");
+            traversal(root.left);
+        }
+        if(root)
     }
     public static void main(String[] args) {
-
+        TREE tree =new TREE();
+        tree.insert(10);
+        tree.insert(20);
+        tree.insert(30);
+        tree.insert(40);
     }
 }
