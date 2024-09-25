fae1918fd0998e31a9b6bed47f30aa260a9e42b1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-25 15:37:38 +0530

    modified

--- TREE/TreeTraversal.java
+++ TREE/TreeTraversal.java
@@ -42,8 +42,8 @@ public class TreeTraversal {
         if(root!=null)
         {
             traversal(root.right);
-            System.out.print(root.data+" ");
             traversal(root.left);
+            System.out.print(root.data+" ");
 
         }
     }
