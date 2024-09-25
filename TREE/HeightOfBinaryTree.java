481f3bf15e214fe8dfa8ae4078c0d9bbdf6c3079
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-25 17:04:47 +0530

    modified

--- TREE/HeightOfBinaryTree.java
+++ TREE/HeightOfBinaryTree.java
@@ -54,6 +54,10 @@ public class HeightOfBinaryTree {
         tree.insert(4);
         tree.insert(6);
         tree.insert(5);
+        System.out.println(tree.root);
+        System.out.println(tree.root.left.data);
+        System.out.println(tree.root.left.left.data);
+//        System.out.println(tree.root.left.data);
         tree.print();
     }
 
