d50431641818e3ec0a08f27262614075b6b52308
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-25 11:58:33 +0530

    modified

--- TREE/TREE.java
+++ TREE/TREE.java
@@ -1,8 +1,21 @@
 package TREE;
 
 public class TREE  {
-
+    private Tree Root;
+    private class Tree
+    {
+        int data;
+        Tree right;
+        Tree left;
+        Tree(int value)
+        {
+            this.data=value;
+            this.right=null;
+            this.left=null;
+        }
+    }
     public static void main(String[] args) {
+        TREE tree =new TREE();
         
     }
 }
--- TREE/TreeTraversal.java
+++ TREE/TreeTraversal.java
@@ -1,2 +1,9 @@
-package TREE;public class TreeTraversal {
+package TREE;
+
+public class TreeTraversal {
+
+    public static void main(String[] args) {
+
+    }
+
 }
