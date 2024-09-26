a6639ade375891d3ff13cc82130c40384e337002
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-26 21:25:54 +0530

    modified

--- TREE/PrintNodesAtKdistance.java
+++ TREE/PrintNodesAtKdistance.java
@@ -49,10 +49,12 @@ public class PrintNodesAtKdistance {
     }
     public static void main(String[] args) {
         PrintNodesAtKdistance tree = new PrintNodesAtKdistance();
-        for(int i=0 ;i<=10;++i)
-        {
-            tree.insert(i);
-        }
+        tree.insert(1);
+        tree.insert(2);
+        tree.insert(3);
+        tree.insert(4);
+        tree.insert(5);
+        tree.insert(8);
         tree.printDistanceK(2);
     }
 
