503c2ab87330a1bb3792c25369ff9bcce5ee652e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-28 14:04:55 +0530

    modified

--- TREE/LevelOrderTraversal.java
+++ TREE/LevelOrderTraversal.java
@@ -1,9 +1,24 @@
 package TREE;
 
+import java.util.ArrayList;
+
 public class LevelOrderTraversal {
+    private class  Tree {
+        int data;
+        Tree right;
+        Tree left;
+
+        Tree(int data)
+        {
+            this.data=data;
+            this.left=right;
+        }
+    }
+    
+    private static ArrayList<ArrayList<Integer>> res= new ArrayList<>();
 
     public static void main(String[] args) {
-        
+
     }
 
 }
