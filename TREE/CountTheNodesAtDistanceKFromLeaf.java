5c0c1d4a7346129a84da4fd71f743fec70e6e7ad
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-28 11:34:46 +0530

    modified

--- TREE/CountTheNodesAtDistanceKFromLeaf.java
+++ TREE/CountTheNodesAtDistanceKFromLeaf.java
@@ -1,9 +1,18 @@
 package TREE;
 
 public class CountTheNodesAtDistanceKFromLeaf {
-
+    private class Tree
+    {
+        int data;
+        Tree left;
+        Tree right;
+        Tree (int data)
+        {
+            
+        }
+    }
     public static void main(String[] args) {
-        
+
     }
 
 }
