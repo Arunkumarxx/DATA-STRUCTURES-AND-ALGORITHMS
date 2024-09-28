d023874302e60e415f6ee20492b0bc577dab9318
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-28 16:11:29 +0530

    modified

--- TREE/LevelOrderLineByLineTraversal.java
+++ TREE/LevelOrderLineByLineTraversal.java
@@ -10,27 +10,27 @@ public class LevelOrderLineByLineTraversal {
 
     private class Tree {
         int data;
-        Tree left,right;
+        Tree left, right;
 
         Tree(int data) {
-            this.data=data;
-            this.left=this.right=null;
+            this.data = data;
+            this.left = this.right = null;
         }
     }
 
     private void insert(int value) {
-        root=addNode(root,value);
+        root = addNode(root, value);
     }
 
-    private Tree addNode(Tree root,int data) {
-        if(root==null) {
-            root=new Tree(data);
+    private Tree addNode(Tree root, int data) {
+        if (root == null) {
+            root = new Tree(data);
             return root;
         }
-        if(data<root.data)
-            root.left=addNode(root.left,data);
-        else if(data>root.data)
-            root.right=addNode(root.right,data);
+        if (data < root.data)
+            root.left = addNode(root.left, data);
+        else if (data > root.data)
+            root.right = addNode(root.right, data);
         return root;
     }
 
@@ -39,33 +39,32 @@ public class LevelOrderLineByLineTraversal {
     }
 
     private void InOrderTraversal(Tree root) {
-        if(root==null)
+        if (root == null)
             return;
         InOrderTraversal(root.left);
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
         InOrderTraversal(root.right);
     }
 
-
     public ArrayList<ArrayList<Integer>> levelOrderLineByLineUsingQueue() {
-        ArrayList<ArrayList<Integer>> res=new ArrayList<>();
-        if(root==null) return res;
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
 
-        Queue<Tree> queue=new LinkedList<>();
+        Queue<Tree> queue = new LinkedList<>();
         queue.add(root);
 
-        while(!queue.isEmpty()) {
-            int levelSize=queue.size();
-            ArrayList<Integer> currentLevel=new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            ArrayList<Integer> currentLevel = new ArrayList<>();
 
-            for(int i=0;i<levelSize;i++) {
-                Tree currentNode=queue.poll();
+            for (int i = 0; i < levelSize; i++) {
+                Tree currentNode = queue.poll();
                 currentLevel.add(currentNode.data);
 
-                if(currentNode.left!=null) {
+                if (currentNode.left != null) {
                     queue.add(currentNode.left);
                 }
-                if(currentNode.right!=null) {
+                if (currentNode.right != null) {
                     queue.add(currentNode.right);
                 }
             }
@@ -76,21 +75,19 @@ public class LevelOrderLineByLineTraversal {
     }
 
     public static void main(String[] args) {
-        LevelOrderTraversal tree=new LevelOrderTraversal();
-        Random random=new Random();
+        LevelOrderLineByLineTraversal tree = new LevelOrderLineByLineTraversal();
+        Random random = new Random();
 
-        // Insert random values into the tree
-        for(int i=0;i<10;++i) {
-            tree.insert(random.nextInt(0,9));
+        for (int i = 0; i < 10; ++i) {
+            tree.insert(random.nextInt(0, 9));
         }
 
-        // Iterative Level Order Traversal using Queue
-        ArrayList<ArrayList<Integer>> queueOutput=tree.levelOrderUsingQueue();
-        for(ArrayList<Integer> level:queueOutput) {
-            for(Integer value:level) {
-                System.out.print(value+" ");
+        ArrayList<ArrayList<Integer>> queueOutput = tree.levelOrderLineByLineUsingQueue();
+        for (ArrayList<Integer> level : queueOutput) {
+            for (Integer value : level) {
+                System.out.print(value + " ");
             }
-            System.out.println();  // Newline after each level
+            System.out.println();
         }
     }
 }
