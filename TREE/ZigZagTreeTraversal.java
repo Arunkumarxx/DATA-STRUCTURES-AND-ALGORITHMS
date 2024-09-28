64e6c274c5608042f052e91610bee9f07e2c7438
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-28 21:24:03 +0530

    modified

--- TREE/ZigZagTreeTraversal.java
+++ TREE/ZigZagTreeTraversal.java
@@ -1,7 +1,50 @@
 package TREE;
 
 public class ZigZagTreeTraversal {
-    
+    private Tree root;
+    private class Tree
+    {
+        int data;
+        Tree left;
+        Tree right;
+
+        Tree(int value)
+        {
+            this.data=value;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    private void insert(int value)
+    {
+        root=addNode(root,value);;
+    }
+    private Tree addNode(Tree root ,int value)
+    {
+        if(root==null)
+        {
+            root=new Tree(value);
+            return root;
+        }
+        if(value< root.data)
+            root.left=addNode(root.left,value);
+        if(value>root.data)
+            root.right=addNode(root.right,value);
+        return root;
+    }
+    private void print()
+    {
+        InOrderTraversal(root);
+    }
+    private void InOrderTraversal(Tree node)
+    {
+        if(node!=null)
+        {
+            InOrderTraversal(node.left);
+            System.out.print(node.data+" ");
+            InOrderTraversal(node.right);
+        }
+    }
     ArrayList<Integer> zigZagTraversal(Node root)
     {
         Queue<Node> q=new LinkedList<>();
