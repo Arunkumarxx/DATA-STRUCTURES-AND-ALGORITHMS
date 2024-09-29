3f2db123348aef381d566c197d1c00e91aaacfd3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-29 11:18:21 +0530

    modified

--- TREE/LeftViewOfBinaryTree.java
+++ TREE/LeftViewOfBinaryTree.java
@@ -33,7 +33,7 @@ public class LeftViewOfBinaryTree {
     }
     private void print()
     {
-
+        inOrderTraversal(root);
     }
     private void inOrderTraversal(Tree root)
     {
@@ -44,7 +44,11 @@ public class LeftViewOfBinaryTree {
         inOrderTraversal(root.right);
     }
     public static void main(String[] args) {
-        
+        LeftViewOfBinaryTree tree =new LeftViewOfBinaryTree();
+        tree.insert(1);
+        tree.insert(2);
+        tree.insert(3);
+        tree.print();
     }
 
 }
