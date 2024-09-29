3964014812f2e6f3cb357c268f9d6641652a6e6b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-29 14:40:01 +0530

    modified

--- TREE/ChildrenSumProperty.java
+++ TREE/ChildrenSumProperty.java
@@ -1,6 +1,7 @@
 package TREE;
 
 public class ChildrenSumProperty {
+    private Tree root;
     private class Tree
     {
         int data;
@@ -12,9 +13,37 @@ public class ChildrenSumProperty {
             this.left=this.right=null;
         }
     }
-    
+    private void insert(int value)
+    {
+        root=addNode(root,value);
+    }
+    private Tree addNode(Tree root,int data)
+    {
+        if(root==null)
+        {
+            root=new Tree(data);
+            return root;
+        }
+        if(data<root.data)
+            root.left=addNode(root.left,data);
+        else if(data>root.data)
+            root.right=addNode(root.right,data);
+        return root;
+    }
+    private void print()
+    {
+        inOrderTraversal(root);
+    }
+    private void inOrderTraversal(Tree root)
+    {
+        if(root==null)
+            return;
+        inOrderTraversal(root.left);
+        System.out.print(root.data+" ");
+        inOrderTraversal(root.right);
+    }
     public static void main(String[] args) {
-
+        
     }
 
 }
