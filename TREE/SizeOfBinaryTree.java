f7e131c7d308eca603cf7fbc7505b2134eecc5c4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-29 10:27:05 +0530

    modified

--- TREE/SizeOfBinaryTree.java
+++ TREE/SizeOfBinaryTree.java
@@ -5,5 +5,14 @@ public class SizeOfBinaryTree {
     public static void main(String[] args) {
 
     }
+    public static int getSize(Node node) {
+        return treeTraversal(node);
+    }
+    private static int treeTraversal(Node root)
+    {
+        if(root==null)
+            return 0;
+        return 1+treeTraversal(root.left)+treeTraversal(root.right);
+    }
 
 }
