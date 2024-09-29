e2eac53a2278b6c0e5e90c2330248615675ae52e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-29 10:38:25 +0530

    modified

--- TREE/MaxAndMinInBinaryTree.java
+++ TREE/MaxAndMinInBinaryTree.java
@@ -1,7 +1,45 @@
 package TREE;
 
 public class MaxAndMinInBinaryTree {
-    
+    private Tree root;
+
+    private class Tree {
+        int data;
+        Tree left, right;
+        Tree(int value) {
+            this.data=value;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+    private void insert(int value) {
+        root=addNode(root,value);
+    }
+
+    private Tree addNode(Tree root, int value) {
+        if (root==null) {
+            root=new Tree(value);
+            return root;
+        }
+        if (value<root.data)
+            root.left=addNode(root.left,value);
+        else if (value>root.data)
+            root.right=addNode(root.right,value);
+        return root;
+    }
+
+    private void print() {
+        InOrderTraversal(root);
+    }
+
+    private void InOrderTraversal(Tree node) {
+        if (node!=null) {
+            InOrderTraversal(node.left);
+            System.out.print(node.data+" ");
+            InOrderTraversal(node.right);
+        }
+    }
     static int max=0;
     static int min=0;
     public static int findMax(Node root){
