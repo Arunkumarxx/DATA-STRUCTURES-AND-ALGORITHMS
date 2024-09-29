ff451eb52fca936d0f08f8ca433f787391682d64
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-29 15:11:16 +0530

    modified

--- TREE/MirrorTree.java
+++ TREE/MirrorTree.java
@@ -1,9 +1,29 @@
 package TREE;
 
-public class MirrorTree {
+public class MirrorTree{
+    private Tree root;
+    private class Tree
+    {
+        int data;
+        Tree left;
+        Tree right;
+        Tree(int value)
+        {
+            this.data=value;
+            this.left=this.right=null;
+        }
+    }
 
+    private void insert()
+    {
+        root=addNode();
+    }
+    private Tree addNode(Tree root)
+    {
+
+    }
     public static void main(String[] args) {
-        
+
     }
 
 }
