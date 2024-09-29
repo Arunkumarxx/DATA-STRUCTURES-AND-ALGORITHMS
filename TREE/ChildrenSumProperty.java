cde6b96305e69ccd64bc11a7e6c3bcba00ac761e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-29 14:35:45 +0530

    modified

--- TREE/ChildrenSumProperty.java
+++ TREE/ChildrenSumProperty.java
@@ -1,9 +1,20 @@
 package TREE;
 
 public class ChildrenSumProperty {
-
+    private class Tree
+    {
+        int data;
+        Tree right;
+        Tree left;
+        Tree(int value)
+        {
+            this.data=value;
+            this.left=this.right=null;
+        }
+    }
+    
     public static void main(String[] args) {
-        
+
     }
 
 }
