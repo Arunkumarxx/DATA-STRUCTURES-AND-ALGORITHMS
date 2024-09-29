adf4a635c4cc1a6d290f037ef078f625bccf08a2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-29 15:15:53 +0530

    modified

--- TREE/MirrorTree.java
+++ TREE/MirrorTree.java
@@ -14,16 +14,37 @@ public class MirrorTree{
         }
     }
 
-    private void insert()
+    private void insert(int data)
     {
-        root=addNode();
+        root=addNode(root,data);
     }
-    private Tree addNode(Tree root)
+    private Tree addNode(Tree root,int value)
     {
-
+        if(root==null)
+        {
+            root=new Tree(value);
+            return root;
+        }
+        if(value<root.data)
+            root.left=addNode(root.left,value);
+        else if (value>root.data)
+            root.right=addNode(root.right,value);
+        return root;
+    }
+    private void print()
+    {
+        inOrderTraversal(root);
+    }
+    private void inOrderTraversal(Tree root)
+    {
+        if(root==null)
+            return;
+        inOrderTraversal(root.left);
+        System.out.print(root.data+" ");
+        inOrderTraversal(root.right);
     }
     public static void main(String[] args) {
-
+        
     }
 
 }
