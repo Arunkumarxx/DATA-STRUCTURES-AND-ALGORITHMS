d3bcc7a201f7cf0032b9d108cf9548ec38e96182
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-09-29 16:01:12 +0530

    modified

--- TREE/CheckForBalancedBinaryTree.java
+++ TREE/CheckForBalancedBinaryTree.java
@@ -1,4 +1,19 @@
 package TREE;
 
 public class CheckForBalancedBinaryTree {
+    private class Tree
+    {
+        int data;
+        Tree left;
+        Tree right;
+        Tree (int value)
+        {
+            this.data=value;
+            this.left=this.right=null;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+
 }
