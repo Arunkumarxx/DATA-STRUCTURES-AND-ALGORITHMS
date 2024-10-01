99b6710772ae8c68a53b18ebc4420c97546a93ca
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-01 20:36:31 +0530

    modified

--- TREE/CheckIsSubtree.java
+++ TREE/CheckIsSubtree.java
@@ -1,7 +1,48 @@
 package TREE;
 
 public class CheckIsSubtree {
+    private Tree root;
+    private class Tree
+    {
+        int data;
+        Tree left;
+        Tree right;
+        Tree(int value)
+        {
+            this.data=value;
+            this.left=this.right=null;
+        }
+    }
 
+    private void insert(int data)
+    {
+        root=addNode(root,data);
+    }
+    private Tree addNode(Tree root,int value)
+    {
+        if(root==null)
+        {
+            root=new Tree(value);
+            return root;
+        }
+        if(value<root.data)
+            root.left=addNode(root.left,value);
+        else if (value>root.data)
+            root.right=addNode(root.right,value);
+        return root;
+    }
+    private void print()
+    {
+        inOrderTraversal(root);
+    }
+    private void inOrderTraversal(Tree root)
+    {
+        if(root==null)
+            return;
+        inOrderTraversal(root.left);
+        System.out.print(root.data+" ");
+        inOrderTraversal(root.right);
+    }
     private static boolean isSubtree(Node root1, Node root2) {
 
         return treeTraversal(root1,root2);
