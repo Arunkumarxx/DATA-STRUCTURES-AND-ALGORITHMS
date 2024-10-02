7a8d3a1aed8fcad807db2bab6f2323a67165be70
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-02 18:03:43 +0530

    modified

--- TREE/VerticalWidthOfBinaryTree.java
+++ TREE/VerticalWidthOfBinaryTree.java
@@ -1,7 +1,46 @@
 package TREE;
 
 public class VerticalWidthOfBinaryTree {
-    
+    private Tree root;
+
+    private class Tree {
+        int data;
+        Tree left;
+        Tree right;
+
+        Tree(int data) {
+            this.data=data;
+            this.left=this.right=null;
+        }
+    }
+
+    private void insert(int value) {
+        root=addNode(root,value);
+    }
+
+    private Tree addNode(Tree root,int data) {
+        if (root==null) {
+            root=new Tree(data);
+            return root;
+        }
+        if (data<root.data)
+            root.left=addNode(root.left,data);
+        else if (data>root.data)
+            root.right=addNode(root.right,data);
+        return root;
+    }
+
+    private void print() {
+        inOrderTraversal(root);
+    }
+
+    private void inOrderTraversal(Tree root) {
+        if (root==null)
+            return;
+        inOrderTraversal(root.left);
+        System.out.print(root.data+" ");
+        inOrderTraversal(root.right);
+    }
     public static void main(String[] args) {
 
     }
