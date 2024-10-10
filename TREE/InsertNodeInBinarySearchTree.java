43808dee7563049a7a3f759f714bc2da92629955
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-10 15:08:47 +0530

    modified

--- TREE/InsertNodeInBinarySearchTree.java
+++ TREE/InsertNodeInBinarySearchTree.java
@@ -1,2 +1,20 @@
-package TREE;public class InsertNodeInBinarySearchTree {
+package TREE;
+
+public class InsertNodeInBinarySearchTree {
+    private class Tree
+    {
+        int data;
+        Tree right;
+        Tree left;
+        Tree(int data)
+        {
+            this.data=data;
+            this.right=left=null;
+        }
+    }
+    
+    public static void main(String[] args) {
+
+    }
+
 }
