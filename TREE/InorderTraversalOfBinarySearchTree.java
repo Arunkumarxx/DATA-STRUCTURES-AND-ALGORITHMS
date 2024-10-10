2573830702daa0bd6058c75c6313d1d7a5915a92
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-10 16:03:22 +0530

    modified

--- TREE/InorderTraversalOfBinarySearchTree.java
+++ TREE/InorderTraversalOfBinarySearchTree.java
@@ -1,7 +1,19 @@
 package TREE;
 
 public class InorderTraversalOfBinarySearchTree {
-    
+    private Tree root;
+    private class Tree {
+        int data;
+        Tree right;
+        Tree left;
+
+        Tree(int value) {
+            this.data = value;
+            this.right = null;
+            this.left = null;
+        }
+    }
+    private void 
     public static void main(String[] args) {
 
     }
