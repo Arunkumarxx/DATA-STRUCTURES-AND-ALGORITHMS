8eb3fcc540fa8d7d5b29d7a091edc8f62ac9a436
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-10 21:18:12 +0530

    modified

--- TREE/MinimumElementInRootBinarySearchTree.java
+++ TREE/MinimumElementInRootBinarySearchTree.java
@@ -1,9 +1,43 @@
 package TREE;
 
 public class MinimumElementInRootBinarySearchTree {
+    private Tree root;
+    private class Tree {
+        int data;
+        Tree right;
+        Tree left;
 
+        Tree(int value) {
+            this.data = value;
+            this.right = null;
+            this.left = null;
+        }
+    }
+    private void insert(int key)
+    {
+        if(root==null)
+        {
+            root=new Tree(key);
+            return;
+        }
+        Tree curr=root;
+        Tree parent=null;
+        while(curr!=null)
+        {
+            parent=curr;
+            if(key<curr.data)
+                curr=curr.left;
+            else if(key>curr.data)
+                curr=curr.right;
+            else return;
+        }
+        if(parent!=null && key<parent.data)
+            parent.left=new Tree(key);
+        else if( parent!=null && key>parent.data)
+            parent.right=new Tree(key);
+    }
     public static void main(String[] args) {
-
+        
     }
 
 }
