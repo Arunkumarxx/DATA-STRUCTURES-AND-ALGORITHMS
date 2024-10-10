3eefe9c9ae15c3981470ef14aadf7b7191800ac9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-10 14:56:36 +0530

    modified

--- TREE/SearchInBinarySearchTree.java
+++ TREE/SearchInBinarySearchTree.java
@@ -1,2 +1,38 @@
-package TREE;public class SearchInBinarySearchTree {
+package TREE;
+
+public class SearchInBinarySearchTree {
+    private Tree root;
+    private class Tree
+    {
+        int data;
+        Tree right;
+        Tree left;
+        Tree(int data)
+        {
+            this.data=data;
+        }
+    }
+    private void insert(int val)
+    {
+        root=addNode(root,val);
+    }
+    private Tree addNode(Tree root,int data)
+    {
+        if(root==null)
+            return new Tree(data);
+        if(data<root.data)
+            root.left=addNode(root.left,data);
+        else if(data>root.data)
+            root.right=addNode(root.right,data);
+        return root;
+    }
+    private boolean search(Tree root)
+    {
+        if(root==null)
+            return false;
+    }
+    public static void main(String[] args) {
+
+    }
+
 }
