d3d0aea9296de7fe1b1fc24c549187f9a9eaafe6
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-11 18:15:46 +0530

    modified

--- TREE/CeilingValueInBinarySearchTree.java
+++ TREE/CeilingValueInBinarySearchTree.java
@@ -1,2 +1,27 @@
-package TREE;public class CeilingValueInBinarySearchTree {
+package TREE;
+
+public class CeilingValueInBinarySearchTree {
+    int findCeil(Node root, int key) {
+        if (root == null) return -1;
+        if(root.left==null && root.right==null && root.data==key)
+            return root.data;
+        Node curr=root;
+        int minCeil=Integer.MAX_VALUE;
+        while(curr!=null)
+        {
+            if(curr.data==key)
+                return curr.data;
+            if(curr.data>key)
+            {
+                minCeil=Math.min(minCeil,curr.data);
+                curr=curr.left;
+            }
+            else if(curr.data<key)
+                curr=curr.right;
+        }
+        return minCeil==Integer.MAX_VALUE?-1:minCeil;
+    }
+    public static void main(String[] args) {
+
+    }
 }
