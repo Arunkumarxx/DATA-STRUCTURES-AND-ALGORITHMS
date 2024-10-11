de19d906487f51bc31c8f27989ec3925d439111f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-11 16:34:49 +0530

    modified

--- TREE/FloorValueOfBinarySearchTree.java
+++ TREE/FloorValueOfBinarySearchTree.java
@@ -1,26 +1,61 @@
 package TREE;
 
 public class FloorValueOfBinarySearchTree {
-    int floor(Node root, int key) {
-        // code here.
+    private Tree root;
+    private class Tree {
+        int data;
+        Tree right,left;
+        Tree(int value) {
+            this.data=value;
+            this.right=this.left=null;
+        }
+    }
+
+    private void insert(int key) {
+        if(root==null) {
+            root=new Tree(key);
+            return;
+        }
+        Tree curr=root;
+        Tree parent=null;
+        while(curr!=null) {
+            parent=curr;
+            if(key<curr.data)
+                curr=curr.left;
+            else if(key>curr.data)
+                curr=curr.right;
+            else return;
+        }
+        if(parent!=null&&key<parent.data)
+            parent.left=new Tree(key);
+        else if(parent!=null&&key>parent.data)
+            parent.right=new Tree(key);
+    }
+
+    int floor(Tree root,int key) {
         int max=Integer.MIN_VALUE;
-        Node curr=root;
-        while(curr!=null)
-        {
+        Tree curr=root;
+        while(curr!=null) {
             if(curr.data==key)
                 return curr.data;
-            if(curr.data<key)
-            {
+            if(curr.data<key) {
                 max=Math.max(curr.data,max);
                 curr=curr.right;
-            }
-            else if(curr.data>key)
+            } else if(curr.data>key)
                 curr=curr.left;
         }
-        return  max==Integer.MIN_VALUE ?-1:max;
+        return max==Integer.MIN_VALUE?-1:max;
     }
+
     public static void main(String[] args) {
+        FloorValueOfBinarySearchTree tree=new FloorValueOfBinarySearchTree();
+        tree.insert(5);
+        tree.insert(3);
+        tree.insert(8);
+        tree.insert(1);
+        tree.insert(4);
 
+        int key=6;
+        System.out.println("Floor value of "+key+": "+tree.floor(tree.root,key));
     }
-
 }
