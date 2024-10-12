e0a7e52d744f4ce0242a5aec5ea85f2ccd9c844f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-12 22:11:34 +0530

    modified

--- TREE/LowestCommonAncestorInABinarySearchTree.java
+++ TREE/LowestCommonAncestorInABinarySearchTree.java
@@ -1,2 +1,19 @@
-package TREE;public class LowestCommonAncestorInABinarySearchTree {
+package TREE;
+
+public class LowestCommonAncestorInABinarySearchTree {
+    Node LCA(Node root, int n1, int n2)
+    {
+        while(root!=null)
+        {
+            if(n1 < root.data  && n2 <root.data)
+                root=root.left;
+            else if(n1 > root.data  && n2 > root.data)
+                root=root.right;
+            else return root;
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+
+    }
 }
