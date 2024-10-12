0738bab712ce035ad1e5f9ed7c8f8154b2635a68
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-12 11:35:45 +0530

    modified

--- TREE/PostOrderTreeTraversal.java
+++ TREE/PostOrderTreeTraversal.java
@@ -38,11 +38,9 @@ public class PostOrderTreeTraversal {
     {
         if(root!=null)
         {
-
             traversal(root.left);
-            System.out.print(root.data+" ");
             traversal(root.right);
-
+            System.out.print(root.data+" ");
         }
     }
     public static void main(String[] args) {
