84717d429c0a88371fe784437d244eb3c50dbd28
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-13 13:05:33 +0530

    modified

--- TREE/AVL_TreeImplementation.java
+++ TREE/AVL_TreeImplementation.java
@@ -3,8 +3,22 @@ public class AVL_TreeImplementation {
     private class Tree
     {
         int data;
+        Tree left;
+        Tree right;
+        int height;
+        private  Tree(int val)
+        {
+            this.data=val;
+            left=right=null;
+            height=0;
+        }
+    }
+    private void insert(int val)
+    {
         
     }
+
+
     public static void main(String[] args) {
 
 
