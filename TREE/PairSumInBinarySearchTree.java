295f276d42cf03f0ec17843d5486fa9fa0efd640
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-15 17:27:50 +0530

    modified

--- TREE/PairSumInBinarySearchTree.java
+++ TREE/PairSumInBinarySearchTree.java
@@ -1,2 +1,34 @@
-package TREE;public class PairSumInBinarySearchTree {
+package TREE;
+
+public class PairSumInBinarySearchTree {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+static List<Integer> ele =new ArrayList();
+static boolean findPair(Node root, int target)
+{
+    ele.clear();
+    inOrderTraversal(root);
+    int start=0 ,end =ele.size()-1;
+    while(start<end)
+    {
+        int sum=ele.get(start)+ele.get(end);
+        if(sum==target)
+            return true;
+        else if(sum<target)
+            ++start;
+        else end--;
+    }
+    return false;
+}
+static void inOrderTraversal(Node root)
+{
+    if(root==null)
+        return;
+    inOrderTraversal(root.left);
+    ele.add(root.data);
+    inOrderTraversal(root.right);
 }
