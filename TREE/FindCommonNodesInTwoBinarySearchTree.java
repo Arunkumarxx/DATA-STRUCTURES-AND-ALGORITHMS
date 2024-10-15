4bc2382fc29cce2afaca334bf451cae3cdef21dc
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-15 17:12:08 +0530

    modified

--- TREE/FindCommonNodesInTwoBinarySearchTree.java
+++ TREE/FindCommonNodesInTwoBinarySearchTree.java
@@ -1,2 +1,37 @@
-package TREE;public class FindCommonNodesInTwoBinarySearchTree {
+package TREE;
+
+public class FindCommonNodesInTwoBinarySearchTree {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+static HashMap<Integer,Integer> freq = new HashMap();
+
+public static ArrayList<Integer> findCommon(Node root1,Node root2)
+{
+    freq.clear();
+    preOrderTraversal(root1);
+    ArrayList<Integer> result =new ArrayList<Integer>();
+    preOrderTraversal(root2,result);
+    Collections.sort(result);
+    return result;
+}
+private static void preOrderTraversal(Node root)
+{
+    if(root==null)
+        return;
+    freq.put(root.data,1);
+    preOrderTraversal(root.left);
+    preOrderTraversal(root.right);
+}
+private static void preOrderTraversal(Node root,ArrayList<Integer> result)
+{
+    if(root==null)
+        return;
+    if(freq.containsKey(root.data))
+        result.add(root.data);
+    preOrderTraversal(root.left,result);
+    preOrderTraversal(root.right,result);
 }
