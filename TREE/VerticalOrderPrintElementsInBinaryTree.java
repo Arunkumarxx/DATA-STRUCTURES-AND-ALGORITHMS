1cfbe099e5e6d86824b403b8b9fd5501513aca6b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-17 21:44:20 +0530

    modified

--- TREE/VerticalOrderPrintElementsInBinaryTree.java
+++ TREE/VerticalOrderPrintElementsInBinaryTree.java
@@ -1,61 +1,75 @@
 package TREE;
 
+import java.util.*;
+
 public class VerticalOrderPrintElementsInBinaryTree {
-    static ArrayList <Integer> verticalOrder(Node Root)
-    {
-        int leftMin=0;
-        int rightMax=0;
-        HashMap<Integer,ArrayList<Integer>> hashMap = new HashMap();
-        ArrayList<Integer> ans = new ArrayList<Integer>();
+    class Node {
+        int data;
+        Node left, right;
+        Node(int value) {
+            this.data = value;
+            left = right = null;
+        }
+    }
 
-        Queue<Pair> queue =new LinkedList<Pair>();
-        queue.add(new Pair(Root,0));
-        while(!queue.isEmpty())
-        {
-            Pair curr= queue.poll();
-            Node root = curr.root;
-            int x_axis= curr.x_axis;
+    private int leftMin = 0;
+    private int rightMax = 0;
+    private HashMap<Integer, ArrayList<Integer>> hashMap = new HashMap<>();
 
-            if(x_axis<leftMin)
-                leftMin=x_axis;
+    public ArrayList<Integer> verticalOrder(Node root) {
+        leftMin = 0;
+        rightMax = 0;
+        hashMap.clear();
+        ArrayList<Integer> ans = new ArrayList<>();
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(root, 0));
 
-            if(x_axis>rightMax)
-                rightMax=x_axis;
+        while (!queue.isEmpty()) {
+            Pair curr = queue.poll();
+            Node node = curr.node;
+            int x_axis = curr.x_axis;
+            if (x_axis < leftMin)
+                leftMin = x_axis;
+            if (x_axis > rightMax)
+                rightMax = x_axis;
+            if (node.left != null)
+                queue.add(new Pair(node.left, x_axis - 1));
+            if (node.right != null)
+                queue.add(new Pair(node.right, x_axis + 1));
+            hashMap.computeIfAbsent(x_axis, k -> new ArrayList<>()).add(node.data);
+        }
 
-            if(root.left!=null)
-                queue.add(new Pair(root.left,x_axis-1));
-
-            if(root.right!=null)
-                queue.add(new Pair(root.right,x_axis+1));
-
-            if(hashMap.containsKey(x_axis))
-                hashMap.get(x_axis).add(root.data);
-
-            else
-            {
-                ArrayList<Integer> res =new ArrayList<Integer>();
-                res.add(root.data);
-                hashMap.put(x_axis,res);
+        for (int i = leftMin; i <= rightMax; ++i) {
+            ArrayList<Integer> temp = hashMap.get(i);
+            if (temp != null) {
+                ans.addAll(temp);
             }
         }
-        while(leftMin<=rightMax)
-        {
-            ArrayList<Integer> temp =hashMap.get(leftMin);
-            for(Integer x:temp)
-                ans.add(x);
-            ++leftMin;
-        }
-        return ans;
 
+        return ans;
     }
-    static class Pair
-    {
-        Node root;
+
+    class Pair {
+        Node node;
         int x_axis;
-        Pair(Node root,int axis)
-        {
-            this.x_axis=axis;
-            this.root=root;
+        Pair(Node node, int axis) {
+            this.x_axis = axis;
+            this.node = node;
         }
+    }
+
+    public static void main(String[] args) {
+        VerticalOrderPrintElementsInBinaryTree tree = new VerticalOrderPrintElementsInBinaryTree();
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+
+        ArrayList<Integer> result = tree.verticalOrder(root);
+        System.out.println("Vertical Order: " + result);
     }
 }
+
+
