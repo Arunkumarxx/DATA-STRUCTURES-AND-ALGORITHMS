87007ad8c1b322ae7274a1eac55b768bc268d230
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-17 21:45:10 +0530

    modified

--- TREE/VerticalOrderPrintElementsInBinaryTree.java
+++ TREE/VerticalOrderPrintElementsInBinaryTree.java
@@ -3,30 +3,28 @@ package TREE;
 import java.util.*;
 
 public class VerticalOrderPrintElementsInBinaryTree {
-    class Node {
+    class Tree {
         int data;
-        Node left, right;
-        Node(int value) {
+        Tree left, right;
+        Tree(int value) {
             this.data = value;
             left = right = null;
         }
     }
-
     private int leftMin = 0;
     private int rightMax = 0;
     private HashMap<Integer, ArrayList<Integer>> hashMap = new HashMap<>();
 
-    public ArrayList<Integer> verticalOrder(Node root) {
+    public ArrayList<Integer> verticalOrder(Tree root) {
         leftMin = 0;
         rightMax = 0;
         hashMap.clear();
         ArrayList<Integer> ans = new ArrayList<>();
         Queue<Pair> queue = new LinkedList<>();
         queue.add(new Pair(root, 0));
-
         while (!queue.isEmpty()) {
             Pair curr = queue.poll();
-            Node node = curr.node;
+            Tree node = curr.node;
             int x_axis = curr.x_axis;
             if (x_axis < leftMin)
                 leftMin = x_axis;
@@ -38,21 +36,19 @@ public class VerticalOrderPrintElementsInBinaryTree {
                 queue.add(new Pair(node.right, x_axis + 1));
             hashMap.computeIfAbsent(x_axis, k -> new ArrayList<>()).add(node.data);
         }
-
         for (int i = leftMin; i <= rightMax; ++i) {
             ArrayList<Integer> temp = hashMap.get(i);
             if (temp != null) {
                 ans.addAll(temp);
             }
         }
-
         return ans;
     }
 
     class Pair {
-        Node node;
+        Tree node;
         int x_axis;
-        Pair(Node node, int axis) {
+        Pair(Tree node, int axis) {
             this.x_axis = axis;
             this.node = node;
         }
@@ -60,16 +56,14 @@ public class VerticalOrderPrintElementsInBinaryTree {
 
     public static void main(String[] args) {
         VerticalOrderPrintElementsInBinaryTree tree = new VerticalOrderPrintElementsInBinaryTree();
-        Node root = new Node(1);
-        root.left = new Node(2);
-        root.right = new Node(3);
-        root.left.left = new Node(4);
-        root.left.right = new Node(5);
-        root.right.left = new Node(6);
+        Tree root = tree.new Tree(1);
+        root.left = tree.new Tree(2);
+        root.right = tree.new Tree(3);
+        root.left.left = tree.new Tree(4);
+        root.left.right = tree.new Tree(5);
+        root.right.left = tree.new Tree(6);
 
         ArrayList<Integer> result = tree.verticalOrder(root);
         System.out.println("Vertical Order: " + result);
     }
 }
-
-
