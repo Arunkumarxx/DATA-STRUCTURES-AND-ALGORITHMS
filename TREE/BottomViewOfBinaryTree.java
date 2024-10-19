0c59ef2d1d3b1db3e8d8fbf8017f216fe34c53d8
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-19 11:55:14 +0530

    modified

--- TREE/BottomViewOfBinaryTree.java
+++ TREE/BottomViewOfBinaryTree.java
@@ -1,35 +1,8 @@
 package TREE;
 
+import java.util.*;
+
 public class BottomViewOfBinaryTree {
-    public ArrayList<Integer> bottomView(Tree root) {
-        Queue<Pair> q=new LinkedList();
-        HashMap<Integer,Integer> hashMap=new HashMap();
-        q.add(new Pair(root,0));
-        int leftMin=0;
-        int rightMax=0;
-        while(!q.isEmpty()) {
-            Pair curr=q.poll();
-            Tree currNode=curr.root;
-            int x_axis=curr.x_axis;
-
-            if(x_axis<leftMin)
-                leftMin=x_axis;
-            if(x_axis>rightMax)
-                rightMax=x_axis;
-
-            hashMap.put(x_axis,currNode.data);
-
-            if(currNode.left!=null)
-                q.add(new Pair(currNode.left,x_axis-1));
-            if(currNode.right!=null)
-                q.add(new Pair(currNode.right,x_axis+1));
-        }
-        ArrayList<Integer> result=new ArrayList<Integer>();
-        for(int i=leftMin;i<=rightMax;++i)
-            result.add(hashMap.get(i));
-        return result;
-    }
-
     class Pair {
         Tree root;
         int x_axis;
@@ -47,9 +20,45 @@ public class BottomViewOfBinaryTree {
             left=right=null;
         }
     }
-
-    public static void main(String[] args) {
-
+    public ArrayList<Integer> bottomView(Tree root) {
+        Queue<Pair> q=new LinkedList<>();
+        HashMap<Integer,Integer> hashMap=new HashMap<>();
+        q.add(new Pair(root,0));
+        int leftMin=0;
+        int rightMax=0;
+        while(!q.isEmpty()) {
+            Pair curr=q.poll();
+            Tree currNode=curr.root;
+            int x_axis=curr.x_axis;
+            if(x_axis<leftMin)
+                leftMin=x_axis;
+            if(x_axis>rightMax)
+                rightMax=x_axis;
+            hashMap.put(x_axis,currNode.data);
+            if(currNode.left!=null)
+                q.add(new Pair(currNode.left,x_axis-1));
+            if(currNode.right!=null)
+                q.add(new Pair(currNode.right,x_axis+1));
+        }
+        ArrayList<Integer> result=new ArrayList<>();
+        for(int i=leftMin;i<=rightMax;++i)
+            result.add(hashMap.get(i));
+        return result;
     }
 
+    public static void main(String[] args) {
+        BottomViewOfBinaryTree tree=new BottomViewOfBinaryTree();
+        Tree root=tree.new Tree(20);
+        root.left=tree.new Tree(8);
+        root.right=tree.new Tree(22);
+        root.left.left=tree.new Tree(5);
+        root.left.right=tree.new Tree(3);
+        root.right.left=tree.new Tree(4);
+        root.right.right=tree.new Tree(25);
+        root.left.right.left=tree.new Tree(10);
+        root.left.right.right=tree.new Tree(14);
+
+        ArrayList<Integer> result=tree.bottomView(root);
+        System.out.println("Bottom View: "+result);
+    }
 }
