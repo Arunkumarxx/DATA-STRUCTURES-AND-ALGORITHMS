a713ea078a3017d4ebbc08a2d2660879086c77b6
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-20 21:05:08 +0530

    modified

--- HEAP/InsertElementInMaxHeap.java
+++ HEAP/InsertElementInMaxHeap.java
@@ -43,7 +43,7 @@ public class InsertElementInMaxHeap {
     }
     public static void main(String[] args) {
         InsertElementInMaxHeap heap =new InsertElementInMaxHeap();
-        for(int i=0;i<=1;++i)
+        for(int i=0;i<=100;++i)
         {
             heap.insert(i);
         }
