cc8d725dda4ef700037dedb518e0aff3e52d2d87
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-20 20:11:49 +0530

    modified

--- HEAP/InsertElementInMaxHeap.java
+++ HEAP/InsertElementInMaxHeap.java
@@ -5,21 +5,31 @@ public class InsertElementInMaxHeap {
     private int size=heap.length;
     private int currentSize=0;
 
-    private void insert(int value)
-    {
-        if(heap[0]==null)
-        {
-            heap[0]= value;
+    private void insert(int value) {
+        if (heap[0] == null) {
+            heap[0] = value;
             ++currentSize;
             return;
         }
-        int i=0;
-        while(i<size)
-        {
-            if(heap[i]==null)
-            {
-                heap[
+        int i = 0;
+        while (i < size) {
+            if (heap[i] == null) {
+                heap[i] = value;
+                ++currentSize;
+                return;
+            }
+            if (heap[i] < value)
+                i = 2 * i + 1;
+            if (heap[i] > value)
+                i = 2 * i + 2;
+            else return;
+            if (i >= size)
+                resize();
         }
+    }
+    private void resize()
+    {
+
     }
     public static void main(String[] args) {
         InsertElementInMaxHeap heap =new InsertElementInMaxHeap();
