169ca41849d691807936ee550bf483e46307992e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-20 16:30:52 +0530

    modified

--- HEAP/ArrayRepresentationOfBinarySearchTreeUnderHeap.java
+++ HEAP/ArrayRepresentationOfBinarySearchTreeUnderHeap.java
@@ -1,69 +1,60 @@
 package HEAP;
 
-public class ArrayRepresentationOfBinarySearchTreeUnderHeap {
-    Integer[] tree = new Integer[100];  // Initial tree size
-    int currentSize = 0;  // Track the current number of inserted elements
-    int size = tree.length;  // Current size of the array
 
-    // Method to insert a value into the binary search tree
-    private void insert(int val) {
-        if (currentSize == size) {
+public class ArrayRepresentationOfBinarySearchTreeUnderHeap {
+    Integer [] tree =new Integer[100];
+    int currentSize=0;
+    int size=tree.length;
+
+    private void insert(int val)
+    {
+        if(currentSize==size)
             resize();
-        }
-        int i = 0;
-        // Traverse the tree to find the correct insertion point
-        while (i < size) {
-            if (tree[i] == null) {
-                tree[i] = val;
+        int i=0;
+        while(i<size)
+        {
+            if(tree[i]==null)
+            {
+                tree[i]=val;
                 currentSize++;
                 return;
             }
-            if (val < tree[i]) {
-                i = 2 * i + 1;
-            } else if (val > tree[i]) {
-                i = 2 * i + 2;
-            } else {
-                return;  // Avoid duplicates
-            }
+            if(val<tree[i])
+                i=2*i+1;
+            else if(val>tree[i])
+                i=2*i+2;
+            else  return;
 
-            if (i >= size) {
+            if(i>=size)
                 resize();
-            }
         }
     }
-
-    // Resizing the array by increasing its size by 3x
-    private void resize() {
-        int newSize = size * 3;  // Triple the current size
-        Integer[] resizedTree = new Integer[newSize];
-        System.arraycopy(tree, 0, resizedTree, 0, size);  // Copy old elements to the new array
-        tree = resizedTree;
-        size = tree.length;  // Update the size variable
-        System.out.println("Resized tree to size: " + size);
+    private void resize()
+    {
+        int newSize= size*3;
+        Integer  [] resizedTree =new Integer[newSize/3];
+        System.arraycopy(tree,0,resizedTree,0,size);
+        tree=resizedTree;
+        size=tree.length;
     }
-
-    // Method to print the tree using in-order traversal
-    private void print() {
+    private void print()
+    {
         inOrderTraversal(0);
     }
 
-    // In-order traversal of the tree
-    private void inOrderTraversal(int ind) {
-        if (ind >= size || tree[ind] == null)
+    private void inOrderTraversal(int ind)
+    {
+        if(ind>=size || tree[ind]==null)
             return;
-        inOrderTraversal(2 * ind + 1);
-        System.out.println(tree[ind] + " ");
-        inOrderTraversal(2 * ind + 2);
+        inOrderTraversal(2*ind+1);
+        System.out.println(tree[ind]+" ");
+        inOrderTraversal(2*ind+2);
     }
-
     public static void main(String[] args) {
-        ArrayRepresentationOfBinarySearchTreeUnderHeap tree = new ArrayRepresentationOfBinarySearchTreeUnderHeap();
-
-        // Insert values into the tree
-        for (int i = 0; i <= 100; ++i)
+        ArrayRepresentationOfBinarySearchTreeUnderHeap tree =new ArrayRepresentationOfBinarySearchTreeUnderHeap();
+        for(int i=0;i<=100;++i)
             tree.insert(i);
-
-        // Print the tree using in-order traversal
         tree.print();
     }
+
 }
