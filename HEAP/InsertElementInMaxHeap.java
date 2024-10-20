88fd0d10cb55898e9c62fba2c94b34a2bf728b18
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-20 20:06:52 +0530

    modified

--- HEAP/InsertElementInMaxHeap.java
+++ HEAP/InsertElementInMaxHeap.java
@@ -1,2 +1,23 @@
-package HEAP;public class InsertElementInMaxHeap {
+package HEAP;
+
+public class InsertElementInMaxHeap {
+    Integer [] heap  = new Integer[100];
+    private int size=heap.length;
+    private int currentSize=0;
+
+    private void insert(int value)
+    {
+        if(heap[0]==null)
+        {
+            heap[0]= value;
+            ++currentSize;
+            return;
+        }
+        
+    }
+    public static void main(String[] args) {
+        InsertElementInMaxHeap heap =new InsertElementInMaxHeap();
+
+    }
+
 }
