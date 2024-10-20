ab2eafe4ba2faee1e00c19d2f8abb704e17ec4f7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-20 16:24:47 +0530

    modified

--- HEAP/ArrayRepresentationOfBinarySearchTreeUnderHeap.java
+++ HEAP/ArrayRepresentationOfBinarySearchTreeUnderHeap.java
@@ -1,59 +1,83 @@
 package HEAP;
 
-
 public class ArrayRepresentationOfBinarySearchTreeUnderHeap {
-    Integer [] tree =new Integer[100];
-    int currentSize=0;
-    int size=tree.length;
+    Integer[] tree = new Integer[1000];  // Initial tree size
+    int currentSize = 0;  // Track current number of elements
+    int size = tree.length;  // Current array size
 
-    private void insert(int val)
-    {
-        if(currentSize==size)
+    // Method to insert a value into the binary search tree
+    private void insert(int val) {
+        if (currentSize == size) {
             resize();
-        int i=0;
-        while(i<size)
-        {
-            if(tree[i]==null)
-            {
-                tree[i]=val;
+        }
+
+        int i = 0;
+        // Traverse the tree to find the correct insertion point
+        while (i < size) {
+            if (tree[i] == null) {
+                tree[i] = val;
                 currentSize++;
                 return;
             }
-            if(val<tree[i])
-                i=2*i+1;
-            else if(val>tree[i])
-                i=2*i+2;
-            else  return;
 
-            if(i>=size)
+            // Move to the left child if the value is smaller
+            if (val < tree[i]) {
+                i = 2 * i + 1;
+            }
+            // Move to the right child if the value is greater
+            else if (val > tree[i]) {
+                i = 2 * i + 2;
+            }
+            // If the value is equal, don't insert (avoid duplicates in BST)
+            else {
+                return;
+            }
+
+            // Resize if the index is about to exceed current size
+            if (i >= size) {
                 resize();
+            }
         }
     }
-    private void resize()
-    {
-        Integer  [] resizedTree =new Integer[size*2];
-        System.arraycopy(tree,0,resizedTree,0,size);
-        tree=resizedTree;
-        size=tree.length;
+
+    // Method to resize the tree array
+    private void resize() {
+        Integer[] resizedTree = new Integer[size * 2];  // Double the size
+        System.arraycopy(tree, 0, resizedTree, 0, size);  // Copy only 'size' elements
+        tree = resizedTree;
+        size = tree.length;  // Update size after resizing
     }
-    private void print()
-    {
+
+    // Method to print the tree using in-order traversal
+    private void print() {
         inOrderTraversal(0);
     }
 
-    private void inOrderTraversal(int ind)
-    {
-        if(ind>=size || tree[ind]==null)
+    // In-order traversal of the tree
+    private void inOrderTraversal(int ind) {
+        // Base case: if the current node is null or out of bounds, return
+        if (ind >= size || tree[ind] == null)
             return;
-        inOrderTraversal(2*ind+1);
-        System.out.println(tree[ind]+" ");
-        inOrderTraversal(2*ind+2);
-    }
-    public static void main(String[] args) {
-        ArrayRepresentationOfBinarySearchTreeUnderHeap tree =new ArrayRepresentationOfBinarySearchTreeUnderHeap();
-        for(int i=0;i<=56;++i)
-            tree.insert(i);
-        tree.print();
+
+        // Traverse the left subtree (2 * ind + 1)
+        inOrderTraversal(2 * ind + 1);
+
+        // Visit the current node
+        System.out.println(tree[ind] + " ");
+
+        // Traverse the right subtree (2 * ind + 2)
+        inOrderTraversal(2 * ind + 2);
     }
 
+    public static void main(String[] args) {
+        ArrayRepresentationOfBinarySearchTreeUnderHeap tree = new ArrayRepresentationOfBinarySearchTreeUnderHeap();
+
+        // Insert values from 0 to 100
+        for (int i = 0; i <= 100; ++i) {
+            tree.insert(i);
+        }
+
+        // Print the tree using in-order traversal
+        tree.print();
+    }
 }
