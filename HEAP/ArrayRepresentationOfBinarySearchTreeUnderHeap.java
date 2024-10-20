c1ec1d8c7702c24068e0546dab0580d3bfa13a90
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-20 16:24:27 +0530

    modified

--- HEAP/ArrayRepresentationOfBinarySearchTreeUnderHeap.java
+++ HEAP/ArrayRepresentationOfBinarySearchTreeUnderHeap.java
@@ -51,7 +51,7 @@ public class ArrayRepresentationOfBinarySearchTreeUnderHeap {
     }
     public static void main(String[] args) {
         ArrayRepresentationOfBinarySearchTreeUnderHeap tree =new ArrayRepresentationOfBinarySearchTreeUnderHeap();
-        for(int i=0;i<=100;++i)
+        for(int i=0;i<=10;++i)
             tree.insert(i);
         tree.print();
     }
