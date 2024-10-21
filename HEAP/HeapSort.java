833220fca8c0fe4ec128e44425c4ac2226ef80e2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-21 13:43:48 +0530

    modified

--- HEAP/HeapSort.java
+++ HEAP/HeapSort.java
@@ -82,10 +82,10 @@ public class HeapSort {
         for(int val:arr)
             insert(val);
         int [] result =new int[arr.length];
-        int i=result.length-1;
+        int i=0;
         while(currentSize>0)
         {
-            result[i--]=delete();
+            result[i++]=delete();
         }
         return result;
     }
