3177fd7e6487c821600bcf194848c295ee17ef9e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-21 14:32:36 +0530

    modified

--- HEAP/HeapSort.java
+++ HEAP/HeapSort.java
@@ -1,7 +1,14 @@
 package HEAP;
 
 public class HeapSort {
+    private void  buildHeap(int [] arr)
+    {
 
+    }
+    private void heapify(int [] arr)
+    {
+
+    }
     public static void main(String[] args) {
         int [] arr ={4,1,3,5,6,9};
 
