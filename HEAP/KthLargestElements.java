255ddb4af386fd49f2ae6b00fffc8a27e7dec5f7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-22 12:41:21 +0530

    modified

--- HEAP/KthLargestElements.java
+++ HEAP/KthLargestElements.java
@@ -3,7 +3,19 @@ package HEAP;
 public class KthLargestElements {
 
     public static void main(String[] args) {
-        
-    }
 
+    }
+    int[] kLargest(int[] arr, int n, int k) {
+        PriorityQueue<Integer> maxHeap =new PriorityQueue(Collections.reverseOrder());
+        for(Integer val:arr)
+        {
+            maxHeap.add(val);
+        }
+        int [] result =new int[k];
+        for(int i=0;i<k;++i)
+        {
+            result[i]=maxHeap.poll();
+        }
+        return result;
+    }
 }
