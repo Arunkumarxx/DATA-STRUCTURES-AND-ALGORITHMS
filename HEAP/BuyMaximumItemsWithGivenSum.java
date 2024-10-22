f5f98ca3d75075ec2c6b4d6d04bec586b724a8db
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-22 11:55:17 +0530

    modified

--- HEAP/BuyMaximumItemsWithGivenSum.java
+++ HEAP/BuyMaximumItemsWithGivenSum.java
@@ -1,2 +1,26 @@
-package HEAP;public class BuyMaximumItemsWithGivenSum {
+package HEAP;
+
+import java.util.PriorityQueue;
+
+public class BuyMaximumItemsWithGivenSum {
+    private int maxItems(int [] arr,int sum)
+    {
+        PriorityQueue<Integer> queue =new PriorityQueue<>();
+        for(Integer val:arr)
+            queue.add(val);
+        int totalItems=0;
+        while(sum!=0 && !queue.isEmpty())
+        {
+            sum-=queue.poll();
+            ++totalItems;
+        }
+        return totalItems;
+    }
+    public static void main(String[] args) {
+        BuyMaximumItemsWithGivenSum obj =new BuyMaximumItemsWithGivenSum();
+        int [] arr =  {1,12,5,111,200};
+        System.out.println( obj.maxItems(arr,0));
+        
+    }
+
 }
