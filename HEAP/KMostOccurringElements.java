88986fcee7ddd6eb0f0e46189a6801fbb7a1fd98
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-22 11:41:50 +0530

    modified

--- HEAP/KMostOccurringElements.java
+++ HEAP/KMostOccurringElements.java
@@ -1,33 +1,34 @@
 package HEAP;
 
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.Collections;
+
 public class KMostOccurringElements {
-
-    public static void main(String[] args) {
-
-    }
-    int kMostFrequent(int arr[], int n, int k) {
-        HashMap<Integer,Integer> hashMap =new HashMap();
-
-        for(int val:arr)
-        {
-            if(hashMap.containsKey(val))
-            {
+    int kMostFrequent(int arr[],int n,int k) {
+        HashMap<Integer,Integer> hashMap=new HashMap();
+        for(int val:arr) {
+            if(hashMap.containsKey(val)) {
                 hashMap.put(val,hashMap.getOrDefault(val,0)+1);
+            } else {
+                hashMap.put(val,1);
             }
-            else hashMap.put(val,1);
         }
-
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-
-        for(Integer val:hashMap.values())
-        {
+        PriorityQueue<Integer> maxHeap=new PriorityQueue<>(Collections.reverseOrder());
+        for(Integer val:hashMap.values()) {
             maxHeap.add(val);
         }
         int sum=0;
-        for(int i=0;i<k;++i)
-        {
+        for(int i=0;i<k;++i) {
             sum+=maxHeap.poll();
         }
         return sum;
+    }
+
+    public static void main(String[] args) {
+        KMostOccurringElements obj=new KMostOccurringElements();
+        int[] arr={1,1,1,2,2,3};
+        int k=2;
+        System.out.println("Sum of frequencies of " + k + " most occurring elements: " + obj.kMostFrequent(arr,arr.length,k));
     }
 }
