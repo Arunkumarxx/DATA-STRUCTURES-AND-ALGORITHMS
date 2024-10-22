0cc119b8aed7dfad012a3790953074e06d20cfca
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-22 12:41:56 +0530

    modified

--- HEAP/KthLargestElements.java
+++ HEAP/KthLargestElements.java
@@ -1,21 +1,26 @@
 package HEAP;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Arrays;
+
 public class KthLargestElements {
-
-    public static void main(String[] args) {
-
-    }
-    int[] kLargest(int[] arr, int n, int k) {
-        PriorityQueue<Integer> maxHeap =new PriorityQueue(Collections.reverseOrder());
-        for(Integer val:arr)
-        {
+    private int[] kLargest(int[] arr, int n, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (Integer val : arr) {
             maxHeap.add(val);
         }
-        int [] result =new int[k];
-        for(int i=0;i<k;++i)
-        {
-            result[i]=maxHeap.poll();
+        int[] result = new int[k];
+        for (int i = 0; i < k; ++i) {
+            result[i] = maxHeap.poll();
         }
         return result;
+    }
+    public static void main(String[] args) {
+        KthLargestElements obj = new KthLargestElements();
+        int[] arr = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+        int[] result = obj.kLargest(arr, arr.length, k);
+        System.out.println("K largest elements: " + Arrays.toString(result));
     }
 }
