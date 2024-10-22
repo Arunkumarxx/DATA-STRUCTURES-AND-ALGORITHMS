d9d542da58fddd95730b7ca9f3da95817d3928b9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-22 13:04:58 +0530

    modified

--- HEAP/KthLargestElements.java
+++ HEAP/KthLargestElements.java
@@ -5,22 +5,38 @@ import java.util.PriorityQueue;
 import java.util.Arrays;
 
 public class KthLargestElements {
-    private int[] kLargest(int[] arr, int n, int k) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        for (Integer val : arr) {
-            maxHeap.add(val);
+
+        int[] kLargest(int[] arr, int n, int k) {
+
+            PriorityQueue<Integer> minHeap =new PriorityQueue<Integer>();
+
+            for(int i=0; i<k;++i)
+                minHeap.add(arr[i]);
+
+            for(int i=k;i<n;++i)
+            {
+                if(arr[i]>minHeap.peek())
+                {
+                    minHeap.poll();
+                    minHeap.add(arr[i]);
+                }
+            }
+            int [] result= new int [k];
+
+            for(int i=0;i<k;++i)
+            {
+                result[i]=minHeap.poll();
+            }
+            int start=0;
+            int end=result.length-1;
+            while(start<end)
+            {
+                int temp=result[start];
+                result[start]=result[end];
+                result[end]=temp;
+                ++start;
+                --end;
+            }
+            return result;
         }
-        int[] result = new int[k];
-        for (int i = 0; i < k; ++i) {
-            result[i] = maxHeap.poll();
-        }
-        return result;
     }
-    public static void main(String[] args) {
-        KthLargestElements obj = new KthLargestElements();
-        int[] arr = {3, 2, 1, 5, 6, 4};
-        int k = 2;
-        int[] result = obj.kLargest(arr, arr.length, k);
-        System.out.println("K largest elements: " + Arrays.toString(result));
-    }
-}
