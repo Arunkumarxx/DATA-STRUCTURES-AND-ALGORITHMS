5a497042d8d408ac049dfdddc4a2c09a19064719
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-22 11:56:44 +0530

    modified

--- HEAP/BuyMaximumItemsWithGivenSum.java
+++ HEAP/BuyMaximumItemsWithGivenSum.java
@@ -9,11 +9,12 @@ public class BuyMaximumItemsWithGivenSum {
         for(Integer val:arr)
             queue.add(val);
         int totalItems=0;
-        while(sum>0 && !queue.isEmpty())
+        while(!queue.isEmpty())
         {
             sum-=queue.poll();
             if(sum>=0)
             ++totalItems;
+            else break;
         }
         return totalItems;
     }
