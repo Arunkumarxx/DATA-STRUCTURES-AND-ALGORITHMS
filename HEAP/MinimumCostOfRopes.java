6ba65388d67bfe729d90438eee2ed25f0fb649f5
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-22 12:25:16 +0530

    modified

--- HEAP/MinimumCostOfRopes.java
+++ HEAP/MinimumCostOfRopes.java
@@ -1,2 +1,30 @@
-package HEAP;public class MinimumCostOfRopes {
+package HEAP;
+
+public class MinimumCostOfRopes {
+
+    public static void main(String[] args) {
+
+    }
+    public long minCost(long[] arr) {
+        PriorityQueue<Long> minHeap =new PriorityQueue();
+
+        for(Long val:arr)
+        {
+            minHeap.add(val);
+        }
+        long totalCost=0;
+
+        while(minHeap.size()>1)
+        {
+            long min1=minHeap.poll();
+            long min2=minHeap.poll();
+
+            long newRope= min1+min2;
+
+            totalCost+=newRope;
+
+            minHeap.add(newRope);
+        }
+        return totalCost;
+    }
 }
