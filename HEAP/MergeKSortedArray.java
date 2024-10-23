a20334d415034b0a4dfaf228f3505d39c544f9a3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-23 15:40:28 +0530

    modified

--- HEAP/MergeKSortedArray.java
+++ HEAP/MergeKSortedArray.java
@@ -1,27 +1,30 @@
 package HEAP;
 
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Arrays;
+
 public class MergeKSortedArray {
 
-    public static void main(String[] args) {
-
-    }
-    public static ArrayList<Integer> mergeKArrays(int[][] arr,int k)
-    {
-        PriorityQueue<Integer> MinHeap =new PriorityQueue();
-        for(int i=0;i<k;++i)
-        {
-            for(int j=0;j<k;++j)
-            {
-                MinHeap.add(arr[i][j]);
+    private static ArrayList<Integer> mergeKArrays(int[][] arr,int k) {
+        PriorityQueue<Integer> minHeap=new PriorityQueue<>();
+        for(int i=0;i<k;++i) {
+            for(int j=0;j<k;++j) {
+                minHeap.add(arr[i][j]);
             }
         }
-        ArrayList<Integer> res =new ArrayList<Integer>();
-        int size=MinHeap.size();
-        for(int i=0;i<size;++i)
-        {
-            res.add(MinHeap.poll());
+        ArrayList<Integer> res=new ArrayList<>();
+        int size=minHeap.size();
+        for(int i=0;i<size;++i) {
+            res.add(minHeap.poll());
         }
-        MinHeap.clear();
+        minHeap.clear();
         return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{1, 3, 5}, {2, 4, 6}, {0, 8, 9}};
+        int k = 3;
+        System.out.println("Merged array: " + Arrays.toString(mergeKArrays(arr, k).toArray()));
     }
 }
