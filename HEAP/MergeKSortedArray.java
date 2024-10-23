ad885814b72f4f4c86a2909c23588e630acd2639
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-23 15:38:31 +0530

    modified

--- HEAP/MergeKSortedArray.java
+++ HEAP/MergeKSortedArray.java
@@ -3,7 +3,25 @@ package HEAP;
 public class MergeKSortedArray {
 
     public static void main(String[] args) {
-        
-    }
 
+    }
+    public static ArrayList<Integer> mergeKArrays(int[][] arr,int k)
+    {
+        PriorityQueue<Integer> MinHeap =new PriorityQueue();
+        for(int i=0;i<k;++i)
+        {
+            for(int j=0;j<k;++j)
+            {
+                MinHeap.add(arr[i][j]);
+            }
+        }
+        ArrayList<Integer> res =new ArrayList<Integer>();
+        int size=MinHeap.size();
+        for(int i=0;i<size;++i)
+        {
+            res.add(MinHeap.poll());
+        }
+        MinHeap.clear();
+        return res;
+    }
 }
