0361d7c117a83e08a683e4894e2aaf26782e15b4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-23 12:45:41 +0530

    modified

--- HEAP/KthSmallestElement.java
+++ HEAP/KthSmallestElement.java
@@ -1,2 +1,27 @@
-package HEAP;public class KthSmallestElement {
+package HEAP;
+
+public class KthSmallestElement {
+
+    public static void main(String[] args) {
+
+    }
+    public static int kthSmallest(int arr[], int n, int k)
+    {
+        PriorityQueue<Integer> maxHeap =new PriorityQueue<Integer>(Collections.reverseOrder());
+
+        for(int i=0;i<k;++i)
+        {
+            maxHeap.add(arr[i]);
+        }
+        for(int i=k;i<n;++i)
+        {
+            if(arr[i]<maxHeap.peek())
+            {
+                maxHeap.poll();
+                maxHeap.add(arr[i]);
+            }
+        }
+        return maxHeap.poll();
+    }
 }
+
