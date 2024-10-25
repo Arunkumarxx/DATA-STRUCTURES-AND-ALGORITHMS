c4eadcc0cd7c3e2cb14019ecfd683431752e1c63
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-25 19:56:35 +0530

    modified

--- HEAP/FindMedianInAStream.java
+++ HEAP/FindMedianInAStream.java
@@ -1,9 +1,29 @@
 package HEAP;
 
 public class FindMedianInAStream {
+    private  static PriorityQueue<Integer> small = new PriorityQueue<>(Collections.reverseOrder());
+    private  static PriorityQueue<Integer> large = new PriorityQueue<>();
+    private static boolean even = true;
+    public static void insertHeap(int num)
+    {
+        if (even) {
+            large.offer(num);
+            small.offer(large.poll());
+        } else {
+            small.offer(num);
+            large.offer(small.poll());
+        }
+        even = !even;
+    }
 
+    public static double getMedian() {
+        if (even)
+            return (small.peek() + large.peek()) / 2.0;
+        else
+            return small.peek();
+    }
     public static void main(String[] args) {
-        
+
     }
 
 }
