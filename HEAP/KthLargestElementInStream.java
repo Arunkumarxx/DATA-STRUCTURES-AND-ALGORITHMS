8a17ff4266ca471ce51062ff8652db447eda715d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-25 20:14:50 +0530

    modified

--- HEAP/KthLargestElementInStream.java
+++ HEAP/KthLargestElementInStream.java
@@ -1,25 +1,24 @@
 package HEAP;
 
+import java.util.PriorityQueue;
+
 public class KthLargestElementInStream {
-    private void kthLargest(int arr[], int n, int k){
-        // Your code here\
-        PriorityQueue<Integer> minHeap =new PriorityQueue<Integer>();
-        for(int i=0;i<n;++i)
-        {
+    private void kthLargest(int arr[], int n, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int i = 0; i < n; ++i) {
             minHeap.add(arr[i]);
             if (minHeap.size() > k)
                 minHeap.poll();
-
-
             if (minHeap.size() < k)
                 System.out.print("-1 ");
             else
                 System.out.print(minHeap.peek() + " ");
-
         }
     }
     public static void main(String[] args) {
-
+        KthLargestElementInStream obj = new KthLargestElementInStream();
+        int[] arr = {3, 2, 1, 5, 6, 4};
+        int k = 3;
+        obj.kthLargest(arr, arr.length, k);
     }
-
 }
