e250936c540d199d3868f129177de34ca4898ffc
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-25 19:57:02 +0530

    modified

--- HEAP/FindMedianInAStream.java
+++ HEAP/FindMedianInAStream.java
@@ -1,29 +1,36 @@
 package HEAP;
 
+import java.util.PriorityQueue;
+import java.util.Collections;
+
 public class FindMedianInAStream {
-    private  static PriorityQueue<Integer> small = new PriorityQueue<>(Collections.reverseOrder());
-    private  static PriorityQueue<Integer> large = new PriorityQueue<>();
-    private static boolean even = true;
-    public static void insertHeap(int num)
-    {
-        if (even) {
+    private static PriorityQueue<Integer> small=new PriorityQueue<>(Collections.reverseOrder());
+    private static PriorityQueue<Integer> large=new PriorityQueue<>();
+    private static boolean even=true;
+
+    public static void insertHeap(int num) {
+        if(even) {
             large.offer(num);
             small.offer(large.poll());
         } else {
             small.offer(num);
             large.offer(small.poll());
         }
-        even = !even;
+        even=!even;
     }
 
     public static double getMedian() {
-        if (even)
-            return (small.peek() + large.peek()) / 2.0;
+        if(even)
+            return (small.peek()+large.peek())/2.0;
         else
             return small.peek();
     }
+
     public static void main(String[] args) {
-
+        insertHeap(1);
+        insertHeap(2);
+        System.out.println("Current median: "+getMedian());
+        insertHeap(3);
+        System.out.println("Current median: "+getMedian());
     }
-
 }
