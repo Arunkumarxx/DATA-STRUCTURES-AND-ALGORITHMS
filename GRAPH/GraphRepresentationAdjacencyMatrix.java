6b545ff451f371bf9b60451c0cc7300995e401f8
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-27 15:21:10 +0530

    modified

--- GRAPH/GraphRepresentationAdjacencyMatrix.java
+++ GRAPH/GraphRepresentationAdjacencyMatrix.java
@@ -16,5 +16,18 @@ public class GraphRepresentationAdjacencyMatrix
     {
         GraphRepresentationAdjacencyMatrix Graph1 =new GraphRepresentationAdjacencyMatrix(5);
         Graph1.addAdjacent(0,1);
+        Graph1.addAdjacent(0,4);
+        Graph1.addAdjacent(1,0);
+        Graph1.addAdjacent(1,4);
+        Graph1.addAdjacent(1,3);
+        Graph1.addAdjacent(1,2);
+        Graph1.addAdjacent(2,1);
+        Graph1.addAdjacent(2,3);
+        Graph1.addAdjacent(4,0);
+        Graph1.addAdjacent(4,1);
+        Graph1.addAdjacent(4,3);
+        Graph1.addAdjacent(3,4);
+        Graph1.addAdjacent(3,1);
+        Graph1.addAdjacent(3,2);
     }
 }
