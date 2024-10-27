c6a7030ea1a6598542ff0b19a9ce321e563b9f23
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-27 14:52:28 +0530

    modified

--- GRAPH/GraphRepresentationAdjacencyMatrix.java
+++ GRAPH/GraphRepresentationAdjacencyMatrix.java
@@ -4,9 +4,14 @@ public class GraphRepresentationAdjacencyMatrix
 {
     private boolean [][]  Graph;
 
-    private GraphRepresentationAdjacencyMatrix()
+    private GraphRepresentationAdjacencyMatrix(int size)
     {
-        
+        Graph=new boolean[size][size];
+
+    }
+    private void addAdjacent(int v1,int v2)
+    {
+        Graph[][]
     }
     public static void main(String[] args)
     {
