6dc7e616d8342ae45607bd9f36955ba2898f77f9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-27 16:39:22 +0530

    modified

--- GRAPH/PrintAdjacencyList.java
+++ GRAPH/PrintAdjacencyList.java
@@ -1,2 +1,29 @@
-package GRAPH;public class PrinAdjacencyList {
+package GRAPH;
+
+public class PrintAdjacencyList
+{
+    List<List<Integer>> adjacent;
+    private void createAdjacent(int vetrices)
+    {
+        adjacent= new ArrayList();
+
+        for(int i=0;i<vetrices;++i)
+        {
+            adjacent.add(new ArrayList<Integer>());
+        }
+    }
+    public List<List<Integer>> printGraph(int vetrices, int edges[][]) {
+        if (adjacent == null || adjacent.size() != vetrices)
+            createAdjacent(vetrices);
+
+        for(int rows=0;rows<edges.length;++rows)
+            addAdjacent(edges[rows][0],edges[rows][1]);
+        return adjacent;
+    }
+
+    private void addAdjacent(int s,int d)
+    {
+        adjacent.get(s).add(d);
+        adjacent.get(d).add(s);
+    }
 }
