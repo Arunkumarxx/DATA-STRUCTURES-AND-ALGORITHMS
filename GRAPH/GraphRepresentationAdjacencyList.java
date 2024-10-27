ab1734892c94074a046a0f41c61d61c598b2aab2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-27 15:50:54 +0530

    modified

--- GRAPH/GraphRepresentationAdjacencyList.java
+++ GRAPH/GraphRepresentationAdjacencyList.java
@@ -4,7 +4,12 @@ import java.util.ArrayList;
 
 public class GraphRepresentationAdjacencyList
 {
-    private ArrayList<>
+    private ArrayList<ArrayList<Integer>> AdjacencyList;
+
+    private void addAdjacent(int s,int d)
+    {
+        
+    }
     public static void main(String[] args)
     {
         GraphRepresentationAdjacencyList graph1=new GraphRepresentationAdjacencyList();
