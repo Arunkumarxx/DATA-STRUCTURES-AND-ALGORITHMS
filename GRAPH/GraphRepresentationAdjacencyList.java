0add8914d537bfb290c963ffe2c2c27a7128abb6
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-27 16:02:22 +0530

    modified

--- GRAPH/GraphRepresentationAdjacencyList.java
+++ GRAPH/GraphRepresentationAdjacencyList.java
@@ -6,21 +6,22 @@ public class GraphRepresentationAdjacencyList
 {
     private ArrayList<ArrayList<Integer>> AdjacencyList;
 
-    GraphRepresentationAdjacencyList()
+    GraphRepresentationAdjacencyList(int vertices)
     {
-        AdjacencyList=new ArrayList<>();
+        AdjacencyList =new ArrayList<>(vertices);
+        for(int i=0;i<vertices;++i)
+        {
+            AdjacencyList.add(new ArrayList<>());
+        }
     }
     private void addAdjacent(int s,int d)
     {
-        if(AdjacencyList.get(s)==null)
-        {
-            AdjacencyList.add(s, new ArrayList<Integer>());
-            AdjacencyList.add(s,d);
-        }
+        AdjacencyList.get(s).add(d);
+        AdjacencyList.get(d).add(s);
     }
     public static void main(String[] args)
     {
         GraphRepresentationAdjacencyList graph1=new GraphRepresentationAdjacencyList();
-
+        graph1.addAdjacent();
     }
 }
