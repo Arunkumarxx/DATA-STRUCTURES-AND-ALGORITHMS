8627dbacb9b5ec41907d890d6329d266cd771d28
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-28 12:31:23 +0530

    modified

--- GRAPH/GraphBreadthFirstSearchAdjacencyList.java
+++ GRAPH/GraphBreadthFirstSearchAdjacencyList.java
@@ -1,2 +1,25 @@
-package GRAPH;public class GraphBreadthFirstSearchAdjacencyList {
+package GRAPH;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GraphBreadthFirstSearchAdjacencyList
+{
+    List<List<Character>> adjacencyList ;
+    GraphBreadthFirstSearchAdjacencyList(int vertices)
+    {
+        adjacencyList=new ArrayList<>();
+        for(int i=0;i<vertices;++i)
+            adjacencyList.add(new ArrayList<>());
+    }
+    private void addAdjacency(char source,char destination)
+    {
+        adjacencyList.get(source).add(destination);
+        adjacencyList.get(destination).add(source);
+    }
+    public static void main(String[] args)
+    {
+        GraphBreadthFirstSearchAdjacencyList Graph1 =new GraphBreadthFirstSearchAdjacencyList(8);
+        Graph1.addAdjacency('a','e');
+    }
 }
