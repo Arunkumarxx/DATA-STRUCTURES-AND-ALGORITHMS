7539b22d38f8db86fbba84802cc35220c09d9744
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-28 13:06:25 +0530

    modified

--- GRAPH/GraphBreadthFirstSearchAdjacencyList.java
+++ GRAPH/GraphBreadthFirstSearchAdjacencyList.java
@@ -39,7 +39,6 @@ public class GraphBreadthFirstSearchAdjacencyList
                     System.out.print(val + " ");
                 }
             }
-            System.out.println();
         }
     }
     public static void main(String[] args)
