d701907c3cc29cfe4cfaf1377f08951bac3eeb28
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-28 16:28:49 +0530

    modified

--- GRAPH/BreadthFirstSearchUsingNumberAdjacencyList.java
+++ GRAPH/BreadthFirstSearchUsingNumberAdjacencyList.java
@@ -1,34 +1,44 @@
 package GRAPH;
 
-public class BreadthFirstSearchUsingNumberAdjacencyList
-{
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BreadthFirstSearchUsingNumberAdjacencyList {
     private ArrayList<Integer> bfsOfGraph(ArrayList<ArrayList<Integer>> adj) {
-        ArrayList<Integer> result= new ArrayList<Integer>();
-        boolean visited [] =new boolean [adj.size()];
-
-        Queue<Integer> queue =new LinkedList<Integer>();
-
+        ArrayList<Integer> result=new ArrayList<>();
+        boolean visited[]=new boolean[adj.size()];
+        Queue<Integer> queue=new LinkedList<>();
         queue.add(0);
         visited[0]=true;
-
-        while(!queue.isEmpty())
-        {
+        while(!queue.isEmpty()) {
             int curr=queue.poll();
             result.add(curr);
-            for(Integer val:adj.get(curr))
-            {
-                if(!visited[val])
-                {
+            for(Integer val:adj.get(curr)) {
+                if(!visited[val]) {
                     visited[val]=true;
                     queue.add(val);
-
                 }
             }
         }
         return result;
     }
-    public static void main(String[] args)
-    {
 
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int vertices = 5;
+        for (int i = 0; i < vertices; ++i) {
+            adj.add(new ArrayList<>());
+        }
+        adj.get(0).add(1);
+        adj.get(0).add(2);
+        adj.get(1).add(3);
+        adj.get(1).add(4);
+        adj.get(2).add(4);
+        adj.get(3).add(4);
+
+        BreadthFirstSearchUsingNumberAdjacencyList obj = new BreadthFirstSearchUsingNumberAdjacencyList();
+        ArrayList<Integer> bfsResult = obj.bfsOfGraph(adj);
+        System.out.println("BFS traversal of the graph: " + bfsResult);
     }
 }
