b2f132557c6d83f5f50deb7139b1202365ef561d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-28 18:14:08 +0530

    modified

--- GRAPH/DistanceOfNearestCellHaving1.java
+++ GRAPH/DistanceOfNearestCellHaving1.java
@@ -8,6 +8,12 @@ public class DistanceOfNearestCellHaving1
     }
     public static void main(String[] args)
     {
-
+        DistanceOfNearestCellHaving1 obj =new DistanceOfNearestCellHaving1();
+        int [][] grid = {
+                {0,1,1,0},
+                {1,1,0,0},
+                {0,0,1,1}
+        };
+        obj.nearest(grid);
     }
 }
