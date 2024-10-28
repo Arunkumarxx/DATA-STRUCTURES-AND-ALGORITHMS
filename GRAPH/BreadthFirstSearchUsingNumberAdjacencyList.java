61292ca4310fe8f3ccd980af6bce7bb496bb8d27
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-28 16:28:07 +0530

    modified

--- GRAPH/BreadthFirstSearchUsingNumberAdjacencyList.java
+++ GRAPH/BreadthFirstSearchUsingNumberAdjacencyList.java
@@ -1,2 +1,34 @@
-package GRAPH;public class BreadthFirstSearchUsingNumberAdjacencyList {
+package GRAPH;
+
+public class BreadthFirstSearchUsingNumberAdjacencyList
+{
+    private ArrayList<Integer> bfsOfGraph(ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> result= new ArrayList<Integer>();
+        boolean visited [] =new boolean [adj.size()];
+
+        Queue<Integer> queue =new LinkedList<Integer>();
+
+        queue.add(0);
+        visited[0]=true;
+
+        while(!queue.isEmpty())
+        {
+            int curr=queue.poll();
+            result.add(curr);
+            for(Integer val:adj.get(curr))
+            {
+                if(!visited[val])
+                {
+                    visited[val]=true;
+                    queue.add(val);
+
+                }
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args)
+    {
+
+    }
 }
