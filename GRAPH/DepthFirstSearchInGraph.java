e1368ea7366e4bf0da3317760c2add379c1a6288
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-29 12:39:43 +0530

    modified

--- GRAPH/DepthFirstSearchInGraph.java
+++ GRAPH/DepthFirstSearchInGraph.java
@@ -1,10 +1,23 @@
 package GRAPH;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DepthFirstSearchInGraph
 {
-
-    public static void main(String[] args)
+    List<List<Integer>> Adjacency ;
+    DepthFirstSearchInGraph()
+    {
+        Adjacency =new ArrayList<>();
+        for(int i=0;i<52;++i)
+            Adjacency.add(new ArrayList<>());
+    }
+    private void addAdjacency()
     {
         
+    }
+    public static void main(String[] args)
+    {
+
     }
 }
