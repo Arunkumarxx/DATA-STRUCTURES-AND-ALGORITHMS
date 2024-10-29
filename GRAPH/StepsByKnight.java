8295efe386740dc8f0d49f4ef44d70aa5c407893
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-29 22:19:15 +0530

    modified

--- GRAPH/StepsByKnight.java
+++ GRAPH/StepsByKnight.java
@@ -2,7 +2,10 @@ package GRAPH;
 
 public class StepsByKnight
 {
-    
+    private int minStepToReachTarget(int []KnightPos, int []TargetPos, int n)
+    {
+        
+    }
     public static void main(String[] args)
     {
 
