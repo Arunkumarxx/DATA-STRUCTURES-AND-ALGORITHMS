3cfb5c40739e8cc372ec4f1a2c624d028266e47f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-29 22:49:45 +0530

    modified

--- GRAPH/StepsByKnight.java
+++ GRAPH/StepsByKnight.java
@@ -11,6 +11,16 @@ public class StepsByKnight
         int[] dy = {1, 2, 2, 1, -1, -2, -2, -1};
         Queue<int []> queue =new LinkedList<>();
         queue.add(new int[]{knightPos[0]-1,knightPos[1]-1});
+
+        while(!queue.isEmpty())
+        {
+            int [] curr= queue.poll();
+
+        }
+    }
+    private boolean isValid(int x,int y,)
+    {
+        if(x>=0 && x<=)
     }
     public static void main(String[] args)
     {
