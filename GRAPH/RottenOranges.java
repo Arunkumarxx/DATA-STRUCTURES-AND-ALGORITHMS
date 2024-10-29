c0e727eb9781d49c6ced480292f7a571687c8acf
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-29 15:26:58 +0530

    modified

--- GRAPH/RottenOranges.java
+++ GRAPH/RottenOranges.java
@@ -7,24 +7,33 @@ public class RottenOranges
 {
     private int orangesRotting(int [][] grid)
     {
-        int row=grid.length;
-        int col=grid[0].length;
+
         int rowSize=grid.length;
         int colSize=grid[0].length;
         Queue<int [] > queue =new LinkedList<>();
+
         boolean[][] visited =new boolean[rowSize][colSize];
-        for(int i=0;i<rowSize;++i)
+        int [][] result =new int[rowSize][colSize];
+
+        for(int row=0;row<rowSize;++row)
         {
-            for(int j=0;j<colSize;++j)
+            for(int col=0;col<colSize;++col)
             {
-                if(grid[i][j]==2)
+                if(grid[row][col]==2)
                 {
-                    visited[i][j] = true;
+                    visited[row][col] = true;
                     queue.add(new int[]{row,col, 0});
                 }
             }
         }
-        
+
+        while(!queue.isEmpty())
+        {
+            int [] curr=queue.poll();
+            int row=curr[0], col=curr[1],time=curr[2];
+
+            // check for left
+        }
     }
     public static void main(String[] args)
     {
