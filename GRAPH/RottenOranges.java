fa83acdc1263f110bb0182f098facf4f6b1c8775
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-29 15:10:46 +0530

    modified

--- GRAPH/RottenOranges.java
+++ GRAPH/RottenOranges.java
@@ -8,7 +8,14 @@ public class RottenOranges
     }
     public static void main(String[] args)
     {
-        
+        RottenOranges rottenOranges =new RottenOranges();
+        int [][] matrix  =
+                {
+                        {0,1,2},
+                        {0,1,2},
+                        {2,1,1}
+                };
+        rottenOranges.orangesRotting(matrix);
     }
 
 }
