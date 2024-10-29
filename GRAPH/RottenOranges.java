b67987cd1380705a936bbba0c80570c70b782b4a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-29 16:13:16 +0530

    modified

--- GRAPH/RottenOranges.java
+++ GRAPH/RottenOranges.java
@@ -71,14 +71,10 @@ public class RottenOranges
     {
         RottenOranges rottenOranges =new RottenOranges();
         int[][] matrix = {
-                {2, 1, 1, 0, 1, 1, 0, 1},
-                {1, 0, 1, 1, 0, 2, 1, 1},
-                {1, 1, 0, 2, 1, 1, 0, 1},
-                {0, 1, 1, 1, 1, 0, 1, 1},
-                {1, 2, 0, 1, 1, 1, 0, 2},
-                {1, 1, 1, 0, 1, 1, 1, 1},
-                {0, 1, 0, 1, 2, 1, 1, 0},
-                {1, 1, 1, 1, 0, 1, 2, 1}
+                {2, 1, 1, 0, 1},
+                {1, 0, 1, 1, 0},
+                {1, 1, 0, 2, 1},
+
         };
         System.out.println( rottenOranges.orangesRotting(matrix));
     }
