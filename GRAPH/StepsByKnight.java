463872c99cede9d141191557ca57b84b6b60290f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-29 22:46:18 +0530

    modified

--- GRAPH/StepsByKnight.java
+++ GRAPH/StepsByKnight.java
@@ -1,14 +1,16 @@
 package GRAPH;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class StepsByKnight
 {
-    private int minStepToReachTarget(int []KnightPos, int []TargetPos, int n)
+    private int minStepToReachTarget(int []knightPos, int []targetPos, int n)
     {
-
         int[] dx = {-2, -1, 1, 2, 2, 1, -1, -2};
         int[] dy = {1, 2, 2, 1, -1, -2, -2, -1};
-        
-
+        Queue<int []> queue =new LinkedList<>();
+        queue.add(new int[]{knightPos[0]-1,knightPos[1]-1});
     }
     public static void main(String[] args)
     {
