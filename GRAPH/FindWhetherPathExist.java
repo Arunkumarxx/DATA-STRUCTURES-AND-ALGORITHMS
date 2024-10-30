f9ae348888d69f7c2c3eb495e468d0a1ec65dd31
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-30 17:52:42 +0530

    modified

--- GRAPH/FindWhetherPathExist.java
+++ GRAPH/FindWhetherPathExist.java
@@ -5,7 +5,15 @@ public class FindWhetherPathExist
 
     public static void main(String[] args)
     {
-        
+        one:for(int i=0;i<10;++i)
+        {
+            two:for(int j=0;j<10;++j)
+            {
+                if(j==5)
+                break one;
+                System.out.println(j);
+            }
+        }
     }
 
 }
