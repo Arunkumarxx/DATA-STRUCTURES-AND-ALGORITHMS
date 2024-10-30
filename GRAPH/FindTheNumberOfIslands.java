9be81a2fd3965a3d1570b31ee83ee87ad44861d4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-30 14:39:28 +0530

    modified

--- GRAPH/FindTheNumberOfIslands.java
+++ GRAPH/FindTheNumberOfIslands.java
@@ -7,42 +7,21 @@ public class FindTheNumberOfIslands
 {
     private int numIsIslands(char [][] grid,int n,int m)
     {
+        int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+
         int rowSize=n;
         int colsSize=m;
+        int res=0;
         Queue<int []> queue =new LinkedList<>();
         for(int row=0;row<rowSize;++row)
         {
             for(int col=0;col<colsSize;++col)
             {
-                if(grid[row][col]=='1')
-                {
-                    queue.add(new int[]{row, col});
-                }
+
             }
         }
-        int res=0;
-        int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
-        int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
-        while(!queue.isEmpty())
-        {
-            int [] curr =queue.poll();
-            int currX=curr[0];
-            int currY=curr[1];
-            int count=0;
-            if(grid[currX][currY]=='1')
-            {
-                for (int i = 0; i < 8; ++i)
-                {
-                    if (isValid(currX - dx[i], currY - dy[i], n, m) && grid[currX][currY]=='0')
-                    {
-                        ++count;
-                        queue.add(new int[]{currX - dx[i], currY - dx[i], n, m});
-                    } else ++count;
-                }
-                grid[currX][currY] = '0';
-            }
-            if(count==8)++res;
-        }
+
         return res;
     }
     private boolean isValid(int x,int y,int n,int m)
