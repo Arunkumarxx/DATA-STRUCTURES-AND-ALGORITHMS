bcd8e0c956f2fa237c02b2426adfee9aa30cc92b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-30 13:43:12 +0530

    modified

--- GRAPH/FindTheNumberOfIslands.java
+++ GRAPH/FindTheNumberOfIslands.java
@@ -6,7 +6,11 @@ public class FindTheNumberOfIslands
     public static void main(String[] args)
     {
         char [][] grid ={
-                {'0','1','1','0'}
+                {'0','1','1','0'},
+                {'0','1','1','0'},
+                {'0','0','1','0'},
+                {'0','0','0','0'},
+                {'0','0','0','0'},
         };
     }
 }
