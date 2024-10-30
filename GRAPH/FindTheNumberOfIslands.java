0ad3b13c6e5807e3cb032b3d9c98420d904692d8
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-30 14:33:56 +0530

    modified

--- GRAPH/FindTheNumberOfIslands.java
+++ GRAPH/FindTheNumberOfIslands.java
@@ -3,57 +3,62 @@ package GRAPH;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class FindTheNumberOfIslands {
-    private int numIsIslands(char[][] grid, int n, int m) {
-        int rowSize = n;
-        int colsSize = m;
-        int res = 0;
-
-        int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
-        int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
-
-        for (int row = 0; row < rowSize; ++row) {
-            for (int col = 0; col < colsSize; ++col) {
-                if (grid[row][col] == '1') {
-                    res++;
-                    Queue<int[]> queue = new LinkedList<>();
+public class FindTheNumberOfIslands
+{
+    private int numIsIslands(char [][] grid,int n,int m)
+    {
+        int rowSize=n;
+        int colsSize=m;
+        Queue<int []> queue =new LinkedList<>();
+        for(int row=0;row<rowSize;++row)
+        {
+            for(int col=0;col<colsSize;++col)
+            {
+                if(grid[row][col]=='1')
+                {
                     queue.add(new int[]{row, col});
-                    grid[row][col] = '0'; // Mark as visited
-
-                    while (!queue.isEmpty()) {
-                        int[] curr = queue.poll();
-                        int currX = curr[0];
-                        int currY = curr[1];
-
-                        for (int i = 0; i < 8; ++i) {
-                            int newX = currX + dx[i];
-                            int newY = currY + dy[i];
-
-                            if (isValid(newX, newY, n, m) && grid[newX][newY] == '1') {
-                                queue.add(new int[]{newX, newY});
-                                grid[newX][newY] = '0'; // Mark as visited
-                            }
-                        }
-                    }
                 }
             }
         }
+        int res=0;
+        int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+        while(!queue.isEmpty())
+        {
+            int [] curr =queue.poll();
+            int currX=curr[0];
+            int currY=curr[1];
+            int count=0;
+            if(grid[currX][currY]=='1')
+            {
+                for (int i = 0; i < 8; ++i)
+                {
+                    if (isValid(currX - dx[i], currY - dy[i], n, m) && grid[currX][currY]=='0')
+                    {
+                        ++count;
+                        queue.add(new int[]{currX - dx[i], currY - dx[i], n, m});
+                    } else ++count;
+                }
+                grid[currX][currY] = '0';
+            }
+            if(count==8)++res;
+        }
         return res;
     }
-
-    private boolean isValid(int x, int y, int n, int m) {
-        return x >= 0 && x < n && y >= 0 && y < m;
+    private boolean isValid(int x,int y,int n,int m)
+    {
+        return  x>=0 && x<=n-1 && y>=0 && y<=m-1?true:false;
     }
-
-    public static void main(String[] args) {
-        char[][] grid = {
-                {'0', '1', '1', '0'},
-                {'0', '1', '1', '0'},
-                {'0', '0', '1', '0'},
-                {'0', '0', '0', '0'},
-                {'1', '1', '0', '1'},
+    public static void main(String[] args)
+    {
+        char [][] grid ={
+                {'0','1','1','0'},
+                {'0','1','1','0'},
+                {'0','0','1','0'},
+                {'0','0','0','0'},
+                {'1','1','0','1'},
         };
-        FindTheNumberOfIslands findTheNumberOfIslands = new FindTheNumberOfIslands();
-        System.out.println(findTheNumberOfIslands.numIsIslands(grid, grid.length, grid[0].length));
+        FindTheNumberOfIslands findTheNumberOfIslands =new FindTheNumberOfIslands();
+        System.out.println(findTheNumberOfIslands.numIsIslands(grid,grid.length,grid[0].length));
     }
 }
