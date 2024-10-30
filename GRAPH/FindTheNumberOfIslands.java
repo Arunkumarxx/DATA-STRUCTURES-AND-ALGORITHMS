62a775347d99dfe1e26eb2b5c4b47188d771f0b5
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-30 14:19:31 +0530

    modified

--- GRAPH/FindTheNumberOfIslands.java
+++ GRAPH/FindTheNumberOfIslands.java
@@ -1,10 +1,42 @@
 package GRAPH;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class FindTheNumberOfIslands
 {
-    private int numIsIslands(char [][] grid)
+    private int numIsIslands(char [][] grid,int n,int m)
     {
-        
+        int rowSize=n;
+        int colsSize=m;
+        Queue<int []> queue =new LinkedList<>();
+        for(int row=0;row<rowSize;++row)
+        {
+            for(int col=0;col<colsSize;++col)
+            {
+                if(grid[row][col]==1)
+                {
+                    queue.add(new int[]{row, col});
+                }
+            }
+        }
+        int [] dx ={-1,-1,-1,     0,0,0   ,1,1,1 };
+        int [] dy ={-1, 0, 1,    -1,0,1   ,-1,0,1};
+        while(!queue.isEmpty())
+        {
+            int [] curr =queue.poll();
+            int currX=curr[0];
+            int currY=curr[1];
+            int count=0;
+            for(int i=0;i<8;++i)
+            {
+                if(isValid(currX-dx[i],currY-dy[i],n,m) && )
+            }
+        }
+    }
+    private boolean isValid(int x,int y,int n,int m)
+    {
+        return  x>=0 && x<=n-1 && y>=0 && y<=m-1?true:false;
     }
     public static void main(String[] args)
     {
