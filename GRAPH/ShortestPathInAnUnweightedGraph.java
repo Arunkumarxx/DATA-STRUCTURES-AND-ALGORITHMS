2a405a7d05b4f122d70cc71c1d418b826f574aa8
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-31 13:15:59 +0530

    modified

--- GRAPH/ShortestPathInAnUnweightedGraph.java
+++ GRAPH/ShortestPathInAnUnweightedGraph.java
@@ -34,11 +34,12 @@ public class ShortestPathInAnUnweightedGraph
         for(int i=0;i< adj.size();++i)
         {
             System.out.print(i);
-            
+
             for(Integer val:adj.get(i))
             {
-                System.out.println(val);
+                System.out.print(val);
             }
+            System.out.println();
         }
     }
     public static void main(String[] args)
