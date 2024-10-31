e38cc3a416fc09d0f896c07734307ff8573870b1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-31 15:23:00 +0530

    modified

--- GRAPH/ShortestPathInUndirectedGraph.java
+++ GRAPH/ShortestPathInUndirectedGraph.java
@@ -2,10 +2,22 @@ package GRAPH;
 
 public class ShortestPathInUndirectedGraph
 {
-    
+
     public static void main(String[] args)
     {
-
+        int [][] edges =
+                {
+                        {0,1},
+                        {0,3},
+                        {3,4},
+                        {4,5},
+                        {5,6},
+                        {1,2},
+                        {2,6},
+                        {6,7},
+                        {6,7},
+                        {7,8},
+                        {6,8},
+                };
     }
-
 }
