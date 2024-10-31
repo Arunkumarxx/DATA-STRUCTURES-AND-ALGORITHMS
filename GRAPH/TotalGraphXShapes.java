65deba51643f504d87dddb8723ec202aadab2591
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-31 11:40:40 +0530

    modified

--- GRAPH/TotalGraphXShapes.java
+++ GRAPH/TotalGraphXShapes.java
@@ -6,7 +6,16 @@ public class TotalGraphXShapes
 {
     private int xShape(char [][] grid)
     {
-        
+        int n=grid.length;
+        int m=grid[0].length;
+        int count=0;
+        for(int row=0;row<n;++row)
+        {
+            for(int col=0;col<m;++col)
+            {
+                if()
+            }
+        }
     }
     public static void main(String[] args)
     {
