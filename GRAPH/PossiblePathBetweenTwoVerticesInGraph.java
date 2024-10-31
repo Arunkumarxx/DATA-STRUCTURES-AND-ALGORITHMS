0d0c363dbcc0ea2fa2aaec6bc34f392b7db179db
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-31 11:24:27 +0530

    modified

--- GRAPH/PossiblePathBetweenTwoVerticesInGraph.java
+++ GRAPH/PossiblePathBetweenTwoVerticesInGraph.java
@@ -1,8 +1,23 @@
 package GRAPH;
-
+import java.util.*;
 public class PossiblePathBetweenTwoVerticesInGraph
 {
-
+    public int countPaths(int V, ArrayList<ArrayList<Integer>> adj, int source, int destination) {
+        Queue<Integer> queue =new LinkedList<>();
+        queue.add(source);
+        int pathCount=0;
+        while(!queue.isEmpty())
+        {
+            int src= queue.poll();
+            if(src==destination)
+                ++pathCount;
+            for(Integer newSrc:adj.get(src))
+            {
+                queue.add(newSrc);
+            }
+        }
+        return pathCount;
+    }
     public static void main(String[] args)
     {
         
