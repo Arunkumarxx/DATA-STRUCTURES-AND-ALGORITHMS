7bf3407e1d1aaa07d188b32e74620c26d33f4153
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-31 11:39:16 +0530

    modified

--- GRAPH/TotalGraphXShapes.java
+++ GRAPH/TotalGraphXShapes.java
@@ -1,2 +1,22 @@
-package GRAPH;public class TotalGraphXShapes {
+package GRAPH;
+
+import GREEDY.GREEDY;
+
+public class TotalGraphXShapes
+{
+    private int xShape(char [][] grid)
+    {
+        
+    }
+    public static void main(String[] args)
+    {
+        char [][] grid ={
+                {'X','O','X'},
+                {'O','X','O'},
+                {'X','X','X'}
+        };
+        TotalGraphXShapes totalGraphXShapes =new TotalGraphXShapes();
+        System.out.println(totalGraphXShapes.xShape(grid));
+    }
+
 }
