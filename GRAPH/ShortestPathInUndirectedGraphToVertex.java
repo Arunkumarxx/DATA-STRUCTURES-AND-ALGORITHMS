024b47864215ecd7de59b588071dd587ce3aa681
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-10-31 15:31:39 +0530

    modified

--- GRAPH/ShortestPathInUndirectedGraphToVertex.java
+++ GRAPH/ShortestPathInUndirectedGraphToVertex.java
@@ -1,10 +1,26 @@
 package GRAPH;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ShortestPathInUndirectedGraphToVertex
 {
-    private int[] shortestPath(int[][] edges,int n,int m ,int src) {
+    private int[] shortestPath(int[][] edges,int n,int m ,int source) {
         boolean [] visited =new boolean[n];
-        
+        Queue<int []> queue =new LinkedList<>();
+        queue.add(new int[]{source,0});
+        visited[source]=true;
+
+        while(!queue.isEmpty())
+        {
+            int [] curr=queue.poll();
+            int  src=curr[0];
+            int dis=curr[1];
+            for(int :edges[src])
+            {
+                int newSrc=
+            }
+        }
     }
     public static void main(String[] args)
     {
