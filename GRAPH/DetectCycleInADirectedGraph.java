68ac9feae824ace3303edd6f8089712fc52af49b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-01 13:42:19 +0530

    modified

--- GRAPH/DetectCycleInADirectedGraph.java
+++ GRAPH/DetectCycleInADirectedGraph.java
@@ -1,8 +1,23 @@
 package GRAPH;
 
+import java.util.ArrayList;
+
 public class DetectCycleInADirectedGraph
 {
-    private boolean detectCycleInDirectedGraph()
+    private boolean isCyclic(int v, ArrayList<ArrayList<Integer>> adj)
+    {
+        boolean [] visited =new boolean[v];
+        boolean [] pathVisited = new boolean[v];
+        for(int i=0;i<v;++i)
+        {
+            if(!visited[i])
+            {
+                if(detectCycle())
+                    return true;
+            }
+        }
+    }
+    private boolean detectCycle(boolean[] visited,boolean[] pathVisited,)
     {
 
     }
