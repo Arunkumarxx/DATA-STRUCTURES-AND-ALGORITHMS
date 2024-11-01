5715cfe7ff19c82c17731bc62ad6260d19c0d049
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-01 13:34:54 +0530

    modified

--- GRAPH/DetectCycleInADirectedGraph.java
+++ GRAPH/DetectCycleInADirectedGraph.java
@@ -2,11 +2,14 @@ package GRAPH;
 
 public class DetectCycleInADirectedGraph
 {
-    
+    private boolean detectCycleInDirectedGraph()
+    {
+
+    }
     public static void main(String[] args)
     {
         DetectCycleInADirectedGraph detectCycleInADirectedGraph =new DetectCycleInADirectedGraph();
-
+        System.out.println(detectCycleInADirectedGraph.detectCycleInDirectedGraph());
     }
 
 }
