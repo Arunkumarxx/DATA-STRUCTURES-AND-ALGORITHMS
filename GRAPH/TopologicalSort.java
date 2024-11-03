d4dad9f565fe6803aae1c975ebeba7e00dd334a3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-03 13:52:05 +0530

    modified

--- GRAPH/TopologicalSort.java
+++ GRAPH/TopologicalSort.java
@@ -1,2 +1,36 @@
-package GRAPH;public class TopologicalSort {
+package GRAPH;
+
+public class TopologicalSort
+{
+    private ArrayList<Integer> topologicalSort(ArrayList<ArrayList<Integer>> adj) {
+        int n=adj.size();
+        int [] degree =new int [adj.size()];
+        for(int i=0;i<n;++i)
+            for(Integer edges:adj.get(i))
+                ++degree[edges];
+
+        Queue<Integer> queue = new LinkedList<Integer>();
+        for (int i = 0; i < n; i++)
+            if (degree[i] == 0)
+                queue.add(i);
+
+        ArrayList<Integer> result =new ArrayList<Integer>();
+        while(!queue.isEmpty())
+        {
+            int curr= queue.poll();
+            result.add(curr);
+            for(int edge:adj.get(curr))
+            {
+                --degree[edge];
+                if(degree[edge]==0)
+                    queue.add(edge);
+
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args)
+    {
+        
+    }
 }
