5f1d6a0ddf7a4311d51e619fef6590beeeb8e75e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-03 13:54:31 +0530

    modified

--- GRAPH/MinimumSwapsToSort.java
+++ GRAPH/MinimumSwapsToSort.java
@@ -1,24 +1,22 @@
 package GRAPH;
 
-public class MinimumSwapsToSort
-{
-    public int minSwaps(int nums[])
-    {
-        int n=nums.length;
-        ArrayList<Pair> list= new ArrayList(n);
-        for(int i=0;i<n;++i)
-            list.add(new Pair(nums[i],i));
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
-        Collections.sort(list,
-                Comparator.comparingInt(pair -> pair.value));
+public class MinimumSwapsToSort {
+    public int minSwaps(int nums[]) {
+        int n = nums.length;
+        ArrayList<Pair> list = new ArrayList<>(n);
+        for (int i = 0; i < n; ++i)
+            list.add(new Pair(nums[i], i));
 
-        int swapCount=0;
-        for(int i=0;i<n;++i)
-        {
-            if(list.get(i).ind!=i)
-            {
-                while(list.get(i).ind!=i)
-                {
+        Collections.sort(list, Comparator.comparingInt(pair -> pair.value));
+
+        int swapCount = 0;
+        for (int i = 0; i < n; ++i) {
+            if (list.get(i).ind != i) {
+                while (list.get(i).ind != i) {
                     int correctIndex = list.get(i).ind;
                     Pair temp = list.get(i);
                     list.set(i, list.get(correctIndex));
@@ -29,18 +27,20 @@ public class MinimumSwapsToSort
         }
         return swapCount;
     }
-    class Pair
-    {
+
+    class Pair {
         int value;
         int ind;
-        Pair(int value,int ind)
-        {
-            this.value=value;
-            this.ind=ind;
+
+        Pair(int value, int ind) {
+            this.value = value;
+            this.ind = ind;
         }
     }
-    public static void main(String[] args)
-    {
 
+    public static void main(String[] args) {
+        MinimumSwapsToSort obj = new MinimumSwapsToSort();
+        int[] nums = {4, 3, 2, 1};
+        System.out.println("Minimum number of swaps to sort: " + obj.minSwaps(nums));
     }
 }
