39c182c127f7af68cf0f79b79de707aeab5d2c06
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-04 16:45:08 +0530

    modified

--- GRAPH/ShortestPathInDirectedAcyclicGraph.java
+++ GRAPH/ShortestPathInDirectedAcyclicGraph.java
@@ -2,62 +2,17 @@ package GRAPH;
 
 import java.util.*;
 
+
 public class ShortestPathInDirectedAcyclicGraph
 {
-    class Pair
-    {
-        int value;
-        int distance;
-        Pair(int value,int distance)
-        {
-            this.value=value;
-            this.distance=distance;
-        }
-    }
-    private void ShortestPathInDirectedAcyclicGraph(ArrayList<ArrayList<Integer>> adj)
-    {
-        int n=adj.size();
-        boolean [] visited =new boolean[n];
-        Stack<Integer> stack =new Stack<>();
-        for(int i=0;i<n;++i)
-        {
-            if(!visited[i])
-            {
-                dfs(adj,visited,stack,i);
-            }
-        }
-        int [] dis =new int[n];
-        Arrays.fill(dis,-1);
-        dis[stack.peek()]=0;
-        Queue<Pair> queue =new LinkedList<>();
-        queue.add(new Pair(stack.peek(),0));
-        while(stack.isEmpty())
-        {
-            while(!queue.isEmpty())
-            {
-                Pair pair =queue.poll();
 
-            }
-        }
-
-    }
-    private void dfs(ArrayList<ArrayList<Integer>> adj,boolean[] visited,Stack<Integer> stack,int source)
-    {
-        visited[source]=true;
-        for(int vertices:adj.get(source))
-        {
-            if(!visited[vertices])
-                dfs(adj,visited,stack,vertices);
-        }
-        stack.push(source);
-    }
     public static void main(String[] args)
     {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
         for (int i = 0; i < 7; i++) {
-            adj.add(new ArrayList<Integer>());
+            adj.add(new ArrayList<>());
         }
-        adj.get(6).add(new Pair(5,0));
+        adj.get(6).add(new int{1,0});
         adj.get(6).add(5);
         adj.get(5).add(4);
         adj.get(4).add(0);
