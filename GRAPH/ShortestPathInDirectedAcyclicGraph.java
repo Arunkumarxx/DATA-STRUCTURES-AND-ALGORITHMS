54ce4d589a0dfaf553b0975f301c27d685032a47
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-04 16:49:30 +0530

    modified

--- GRAPH/ShortestPathInDirectedAcyclicGraph.java
+++ GRAPH/ShortestPathInDirectedAcyclicGraph.java
@@ -12,14 +12,14 @@ public class ShortestPathInDirectedAcyclicGraph
         for (int i = 0; i < 7; i++) {
             adj.add(new ArrayList<>());
         }
-        adj.get(6).add(new int[] {1,0});
-        adj.get(6).add(new int []{});
-        adj.get(5).add(4);
-        adj.get(4).add(0);
-        adj.get(4).add(2);
-        adj.get(2).add(3);
-        adj.get(0).add(1);
-        adj.get(1).add(3);
+        adj.get(6).add(new int []{4,2});
+        adj.get(6).add(new int []{5,3});
+        adj.get(5).add(new int []{4,1});
+        adj.get(4).add(new int []{0,3});
+        adj.get(4).add(new int []{2,1});
+        adj.get(2).add(new int []{3,3});
+        adj.get(0).add(new int []{1,2});
+        adj.get(1).add(new int []{3,1});
     }
 
 }
