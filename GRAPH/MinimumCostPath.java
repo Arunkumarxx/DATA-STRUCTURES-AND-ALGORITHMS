e03d399962f18be372ca12514194661effaeb81e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-04 20:39:47 +0530

    modified

--- GRAPH/MinimumCostPath.java
+++ GRAPH/MinimumCostPath.java
@@ -49,7 +49,6 @@ public class MinimumCostPath
                         if(minValue!=Integer.MAX_VALUE)
                         {
                             queue.add(new int[]{minValueRowAndCol[0], minValueRowAndCol[1]});
-                            visited[minValueRowAndCol[0]][minValueRowAndCol[1]] = true;
                         }
                     }
                 }
