41013c85b54ae523c30ae36b8bf5a3901c6114bd
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-05 18:56:17 +0530

    modified

--- GRAPH/UnitAreaOfLargestRegionOfOnes.java
+++ GRAPH/UnitAreaOfLargestRegionOfOnes.java
@@ -5,14 +5,14 @@ import java.util.Queue;
 
 public class UnitAreaOfLargestRegionOfOnes
 {
-    private void findMaxAreaOfOnes(int[][] grid)
+    private int  findMaxAreaOfOnes(int[][] grid)
     {
         int n=grid.length;
         int m=grid[0].length;
 
         int [] x ={-1, 0, 1,-1,1,1,0,-1};
         int [] y ={-1,-1,-1, 0, 0, 1, 1, 1};
-
+        int maxAreaOfOnes=0;
         boolean[][] visited =new boolean[n][m];
         for(int row=0;row<n;++row)
         {
@@ -20,17 +20,35 @@ public class UnitAreaOfLargestRegionOfOnes
             {
                 if(!visited[row][col] && grid[row][col]==1)
                 {
+                    int currMaxArea=0;
                     Queue<int[]> queue =new LinkedList<>();
                     queue.add(new int[]{row,col});
                     while(!queue.isEmpty())
                     {
                         int [] curr=queue.poll();
-                        int currX=curr[0];
-                        int currY=curr[1];
+                        int currR=curr[0];
+                        int currC=curr[1];
+                        visited[currR][currC]=true;
+                        ++currMaxArea;
+                        for(int i=0;i<8;++i)
+                        {
+                            int newCurrR=currR+x[i];
+                            int newCurrC=currC+y[i];
+                            if(isValid(newCurrC,newCurrR,n,m) && !visited[newCurrC][newCurrR] && grid[newCurrC][newCurrR]==1)
+                            {
+
+                            }
+                        }
                     }
+                    maxAreaOfOnes=Math.max(currMaxArea,maxAreaOfOnes);
                 }
             }
         }
+        return maxAreaOfOnes;
+    }
+    private boolean isValid(int x,int y,int n,int m)
+    {
+        return x>=0 && y>=0 && x<n && y<m?true:false;
     }
     public static void main(String[] args)
     {
@@ -40,6 +58,6 @@ public class UnitAreaOfLargestRegionOfOnes
                 {0,0,0,1},
         };
         UnitAreaOfLargestRegionOfOnes unitAreaOfLargestRegionOfOnes =new UnitAreaOfLargestRegionOfOnes();
-        unitAreaOfLargestRegionOfOnes.findMaxAreaOfOnes(grid);
+        System.out.println(unitAreaOfLargestRegionOfOnes.findMaxAreaOfOnes(grid));
     }
 }
