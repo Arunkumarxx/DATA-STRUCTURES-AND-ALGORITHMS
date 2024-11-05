9e90477c73c3ce1dba44ac59fff0c9505252f4ed
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-05 18:25:26 +0530

    modified

--- GRAPH/UnitAreaOfLargestRegionOfOnes.java
+++ GRAPH/UnitAreaOfLargestRegionOfOnes.java
@@ -2,10 +2,18 @@ package GRAPH;
 
 public class UnitAreaOfLargestRegionOfOnes
 {
-
-    public static void main(String[] args)
+    private void findMaxAreaOfOnes(int[][] grid)
     {
         
     }
-
+    public static void main(String[] args)
+    {
+        int [] [] grid ={
+                {1,1,1,0},
+                {0,0,1,0},
+                {0,0,0,1},
+        };
+        UnitAreaOfLargestRegionOfOnes unitAreaOfLargestRegionOfOnes =new UnitAreaOfLargestRegionOfOnes();
+        unitAreaOfLargestRegionOfOnes.findMaxAreaOfOnes(grid);
+    }
 }
