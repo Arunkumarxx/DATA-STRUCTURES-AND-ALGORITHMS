6bd948f20467b8f9014e06d3868aaf54e152d6b0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-07 20:07:54 +0530

    modified

--- GRAPH/MinimumSpanningTree.java
+++ GRAPH/MinimumSpanningTree.java
@@ -6,21 +6,21 @@ public class MinimumSpanningTree {
     private int spanningTree(int v, int e, List<List<int[]>> adj) {
         boolean[] visited = new boolean[v];
         PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
-        int sum = 0;
+        int sum =0;
         minHeap.add(new int[]{0, 0});
         while (!minHeap.isEmpty()) {
-            int[] edge = minHeap.poll();
-            int weight = edge[0];
-            int node = edge[1];
+            int[] edge=minHeap.poll();
+            int weight=edge[0];
+            int node=edge[1];
 
             if (visited[node])
                 continue;
 
-            visited[node] = true;
+            visited[node] =true;
             sum += weight;
             for (int[] neighbor : adj.get(node)) {
-                int neighborNode = neighbor[0];
-                int neighborWeight = neighbor[1];
+                int neighborNode=neighbor[0];
+                int neighborWeight=neighbor[1];
 
                 if (!visited[neighborNode])
                     minHeap.add(new int[]{neighborWeight, neighborNode});
@@ -28,7 +28,6 @@ public class MinimumSpanningTree {
         }
         return sum;
     }
-
     public static void main(String[] args) {
         MinimumSpanningTree obj = new MinimumSpanningTree();
         List<List<int[]>> adj = new ArrayList<>();
