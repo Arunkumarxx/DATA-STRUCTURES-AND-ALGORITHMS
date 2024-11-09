f0970c8aedd077ff79cf527fe3ded5bb39bc9732
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-09 21:26:31 +0530

    modified

--- GRAPH/KosaRajuAlgorithmStronglyConnectedComponents.java
+++ GRAPH/KosaRajuAlgorithmStronglyConnectedComponents.java
@@ -56,7 +56,19 @@ public class KosaRajuAlgorithmStronglyConnectedComponents
     }
     public static void main(String[] args)
     {
+        KosaRajuAlgorithmStronglyConnectedComponents obj =new KosaRajuAlgorithmStronglyConnectedComponents();
+        ArrayList<ArrayList<Integer>> adj =new ArrayList<ArrayList<Integer>>();
+        int v=5;
+        for(int i=0;i<v;++i)
+            adj.add(new ArrayList<Integer>());
 
+        adj.get(0).add(2);
+        adj.get(0).add(3);
+        adj.get(1).add(0);
+        adj.get(2).add(1);
+        adj.get(3).add(4);
+
+        System.out.println("Number of strongly connected components: " + obj.kosaraju(v,adj));
     }
 
 }
