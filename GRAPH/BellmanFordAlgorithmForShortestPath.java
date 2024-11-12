9168f725df6befcea2e937dd99fbd8c43acc71f3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-12 15:53:22 +0530

    modified

--- GRAPH/BellmanFordAlgorithmForShortestPath.java
+++ GRAPH/BellmanFordAlgorithmForShortestPath.java
@@ -1,5 +1,6 @@
 package GRAPH;
 
+import java.lang.foreign.PaddingLayout;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -11,10 +12,22 @@ public class BellmanFordAlgorithmForShortestPath
         ArrayList<ArrayList<Integer>> adj =new ArrayList<>();
         for(int i=0;i<v;++i)
             adj.add(new ArrayList<>());
-        
+        for(int row=0;row<v;++row)
+        {
+            for(int col=0;col<edges[row].length;++col)
+            {
+                adj.get(edges[row][col])
+            }
+        }
     }
     public static void main(String[] args)
     {
-
+        int [][] edges =
+                {
+                        {0,1,5},
+                        {1,0,3},
+                        {1,2,-1},
+                        {2,0,1},
+                };
     }
 }
