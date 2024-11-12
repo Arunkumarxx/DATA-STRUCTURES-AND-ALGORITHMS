826e1e7fd295b9594cea8007dfdb95cc5eb30f2d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-12 15:49:48 +0530

    modified

--- GRAPH/BellmanFordAlgorithmForShortestPath.java
+++ GRAPH/BellmanFordAlgorithmForShortestPath.java
@@ -1,5 +1,6 @@
 package GRAPH;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -7,17 +8,10 @@ public class BellmanFordAlgorithmForShortestPath
 {
     private int [] bellsManFord(int v,int [][] edges,int src)
     {
-        Queue<int []> queue =new LinkedList<>();
-        int n=v;
-        queue.add(new int[]{0,0});
-        int [] dis =new int[n];
-        while(!queue.isEmpty())
-        {
-            int [] curr = queue.poll();
-            int currSrc=curr[0];
-            int currDis=curr[1];
-            int nextCurr=edges[currSrc][0];
-        }
+        ArrayList<ArrayList<Integer>> adj =new ArrayList<>();
+        for(int i=0;i<v;++i)
+            adj.add(new ArrayList<>());
+        
     }
     public static void main(String[] args)
     {
