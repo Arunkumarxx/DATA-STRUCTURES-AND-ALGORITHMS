4aff86f0a4ce4d1e7fe791b9e88e2a7c63ae434d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-12 16:41:03 +0530

    modified

--- GRAPH/BellmanFordAlgorithmForShortestPath.java
+++ GRAPH/BellmanFordAlgorithmForShortestPath.java
@@ -26,7 +26,7 @@ public class BellmanFordAlgorithmForShortestPath
             }
             System.out.println();
         }
-        int [] dis =new int[v];
+        int [] dis =new int[v-1];
         Arrays.fill(dis,Integer.MAX_VALUE);
         Queue<Integer> queue =new LinkedList<>();
         dis[src]=0;
