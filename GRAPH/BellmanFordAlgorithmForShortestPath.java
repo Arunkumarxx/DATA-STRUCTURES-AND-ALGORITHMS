c3d06ddeb8d3e1a9e789231d84937f2758872155
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-12 17:07:54 +0530

    modified

--- GRAPH/BellmanFordAlgorithmForShortestPath.java
+++ GRAPH/BellmanFordAlgorithmForShortestPath.java
@@ -39,10 +39,8 @@ public class BellmanFordAlgorithmForShortestPath
     {
         int [][] edges =
                 {
-                        {0,1,5},
-                        {1,0,3},
-                        {1,2,-1},
-                        {2,0,1},
+                        {0,1,-1},
+                        {1,0,-1};
                 };
         BellmanFordAlgorithmForShortestPath bellmanFordAlgorithmForShortestPath = new BellmanFordAlgorithmForShortestPath();
         System.out.println(Arrays.toString(bellmanFordAlgorithmForShortestPath.bellsManFord(edges.length,edges,2)));
