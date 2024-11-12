2cf0c7ad5f24e3a83bb618d8c794f0e3e0de559e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-12 15:30:20 +0530

    modified

--- GRAPH/BellmanFordAlgorithmForShortestPath.java
+++ GRAPH/BellmanFordAlgorithmForShortestPath.java
@@ -1,10 +1,19 @@
 package GRAPH;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BellmanFordAlgorithmForShortestPath
 {
     private int [] bellsManFord(int v,int [][] edges,int src)
     {
-        
+        Queue<int []> queue =new LinkedList<>();
+        queue.add(new int[]{0,0});
+        while(!queue.isEmpty())
+        {
+            int [] curr = queue.poll();
+            
+        }
     }
     public static void main(String[] args)
     {
