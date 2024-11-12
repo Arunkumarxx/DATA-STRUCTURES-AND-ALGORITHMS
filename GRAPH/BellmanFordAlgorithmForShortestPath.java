47a11732377701546e50e24459d1682eb23a9b20
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-12 16:10:35 +0530

    modified

--- GRAPH/BellmanFordAlgorithmForShortestPath.java
+++ GRAPH/BellmanFordAlgorithmForShortestPath.java
@@ -14,11 +14,10 @@ public class BellmanFordAlgorithmForShortestPath
         for(int i=0;i<=v;++i)
             adj.add(new ArrayList<>());
         for (int row = 0; row < v; ++row) {
-            for (int col = 0; col < edges[row].length; ++col) {
-                int[] curr = new int[]{edges[row][col]};
-                adj.get(curr[0]).add(new int[]{curr[1], curr[2]});
-            }
+            int[] curr = new int[]{edges[row][0], edges[row][1], edges[row][2]};
+            adj.get(curr[0]).add(new int[]{curr[1], curr[2]});
         }
+
         for(int i=0;i<v;++i)
         {
             for(int [] edg:adj.get(i))
