38b83eb507928a9a100857156961ce4402654b3c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-12 15:39:24 +0530

    modified

--- GRAPH/BellmanFordAlgorithmForShortestPath.java
+++ GRAPH/BellmanFordAlgorithmForShortestPath.java
@@ -14,7 +14,12 @@ public class BellmanFordAlgorithmForShortestPath
         while(!queue.isEmpty())
         {
             int [] curr = queue.poll();
-            int 
+            int currSrc=curr[0];
+            int currVal=curr[1];
+            for(int neighbor:edges[currSrc])
+            {
+                queue.add(new int[]{edges[neighbor][0],edges[neighbor][1]})
+            }
         }
     }
     public static void main(String[] args)
