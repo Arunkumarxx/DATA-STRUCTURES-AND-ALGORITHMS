f7822e22d61aa8eb5919b9874380b02c29cde25e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-13 20:20:38 +0530

    modified

--- GREEDY/ActivitySelection.java
+++ GREEDY/ActivitySelection.java
@@ -2,14 +2,18 @@ package GREEDY;
 
 public class ActivitySelection
 {
-
+    private int getMaximumNumberOfActivity()
+    {
+        
+        return -1;
+    }
     public static void main(String[] args)
     {
         ActivitySelection obj =new ActivitySelection();
          Pair [] arr ={
                  obj.new Pair(3,8),
                  obj.new Pair(2, 4),
-                 obj.new Pair(2, 4),
+                 obj.new Pair(1, 3),
                  obj.new Pair(10,11)
          };
     }
