3e5054682c46c5f2f9975cadced118b73873e7ee
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-13 20:45:13 +0530

    modified

--- GREEDY/ActivitySelection.java
+++ GREEDY/ActivitySelection.java
@@ -1,10 +1,12 @@
 package GREEDY;
 
+import java.util.Collections;
+
 public class ActivitySelection
 {
     private int getMaximumNumberOfActivity(int [] [] PairArray)
     {
-        
+        Collections.sort(PairArray,(a,b)->Integer.compare());
         return -1;
     }
     public static void main(String[] args)
