a522b12fa7611e0c689b87c883e6500022e4d293
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-13 20:15:14 +0530

    modified

--- GREEDY/ActivitySelection.java
+++ GREEDY/ActivitySelection.java
@@ -2,10 +2,19 @@ package GREEDY;
 
 public class ActivitySelection
 {
-    
+
     public static void main(String[] args)
     {
-
+         Pair [] arr ={new Pair()};
     }
-
+    class Pair
+    {
+        int start;
+        int end;
+        Pair(int start,int end )
+        {
+            this.start=start;
+            this.end=end;
+        }
+    }
 }
