5d5c52057a2134b6cf87ff4ea8533f8ea548cd80
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-15 15:21:10 +0530

    modified

--- GREEDY/FractionalKnapsack.java
+++ GREEDY/FractionalKnapsack.java
@@ -2,48 +2,45 @@ package GREEDY;
 
 import java.util.*;
 
-public class FractionalKnapsack
-{
-    private class Pair
-    {
+public class FractionalKnapsack {
+    private class Pair {
         int value;
         int weight;
-        Pair(int value,int weight)
-        {
+        Pair(int value, int weight) {
             this.value=value;
             this.weight=weight;
         }
     }
 
-    private double fractionalKnapsack(List<Integer> val, List<Integer> wt, int capacity)
-    {
-        int n = val.size();
-        Pair[] pair = new Pair[n];
+    private double fractionalKnapsack(List<Integer> val, List<Integer> wt, int capacity) {
+        int n=val.size();
+        Pair[] pair=new Pair[n];
 
-        for (int i = 0; i < n; ++i)
-            pair[i] = new Pair(val.get(i), wt.get(i));
+        for(int i=0;i<n;++i)
+            pair[i]=new Pair(val.get(i), wt.get(i));
 
-        Arrays.sort(pair, (a, b) -> Double.compare((double) b.value / b.weight, (double) a.value / a.weight));
-        double result = 0;
-        int ind = 0;
-        while (capacity > 0 && ind < n)
-        {
-            if (capacity>=pair[ind].weight)
-            {
-                capacity -= pair[ind].weight;
-                result += pair[ind].value;
-            } else if (ind < n)
-            {
-                double newValue = ((double) pair[ind].value / pair[ind].weight) * capacity;
-                result += newValue;
+        Arrays.sort(pair, (a, b)->Double.compare((double)b.value/b.weight, (double)a.value/a.weight));
+        double result=0;
+        int ind=0;
+        while(capacity>0&&ind<n) {
+            if(capacity>=pair[ind].weight) {
+                capacity-=pair[ind].weight;
+                result+=pair[ind].value;
+            } else if(ind<n) {
+                double newValue=((double)pair[ind].value/pair[ind].weight)*capacity;
+                result+=newValue;
                 break;
             }
             ++ind;
         }
         return result;
     }
-    public static void main(String[] args)
-    {
 
+    public static void main(String[] args) {
+        FractionalKnapsack obj=new FractionalKnapsack();
+        List<Integer> val=Arrays.asList(60, 100, 120);
+        List<Integer> wt=Arrays.asList(10, 20, 30);
+        int capacity=50;
+        System.out.println("Maximum value in Knapsack: "+obj.fractionalKnapsack(val, wt, capacity));
     }
 }
