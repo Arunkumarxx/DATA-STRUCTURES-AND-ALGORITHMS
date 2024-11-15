4b0fb8278d2a286c83ebb8cbb02fc349ee8dd6a3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-15 23:36:52 +0530

    modified

--- GREEDY/LargestNumberWithGivenSum.java
+++ GREEDY/LargestNumberWithGivenSum.java
@@ -4,6 +4,6 @@ public class LargestNumberWithGivenSum
 {
     public static void main(String[] args)
     {
-        System.out.println(Math.pow(5,5));
+        System.out.println(Math.pow(5,100));
     }
 }
