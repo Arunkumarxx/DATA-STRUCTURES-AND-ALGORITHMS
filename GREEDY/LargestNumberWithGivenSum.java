cf8e3a503a8f8243265c92eee06eda97ca418c76
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-16 00:05:05 +0530

    modified

--- GREEDY/LargestNumberWithGivenSum.java
+++ GREEDY/LargestNumberWithGivenSum.java
@@ -4,40 +4,17 @@ public class LargestNumberWithGivenSum
 {
     private String largestNumber(int n, int sum)
     {
-        StringBuilder result =new StringBuilder();
-        int targetSum=sum;
-        while(sum>0 && n>0)
-        {
-            if(sum>=9)
-            {
-                result.append(9);
-                targetSum-=9;
-                sum-=9;
-                --n;
-            }
-            else
-            {
-                result.append(sum);
-                targetSum-=sum;
-                --n;
-                while(n>0)
-                {
-                    result.append(0);
-                    --n;
-                }
-            }
-        }
-
-        if(targetSum!=0)
+        if(sum>9*n)
             return "-1";
-        return result.toString();
+        StringBuilder result =new StringBuilder();
+
+        
     }
     public static void main(String[] args)
     {
         LargestNumberWithGivenSum largestNumberWithGivenSum =new LargestNumberWithGivenSum();
         int n=5;
         int sum=12;
-        System.out.println(2*n);
         System.out.println( largestNumberWithGivenSum.largestNumber(n,sum));
     }
 }
