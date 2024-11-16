efec3179a4229fd7fc114fcf4229aff8d75300f9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-16 16:19:24 +0530

    modified

--- GREEDY/HuffManAlgorithm.java
+++ GREEDY/HuffManAlgorithm.java
@@ -5,18 +5,14 @@ public class HuffManAlgorithm
 
     public static void main(String[] args)
     {
-        System.out.println("A");
-        HuffManAlgorithm.main(args);
+
     }
 
 }
 
 class HuffManEncodingAlgorithm
 {
-    public static void main(String [] args)
-    {
-        System.out.println("Arun");
-    }
+    
 }
 
 class HuffManDecodingAlgorithm
