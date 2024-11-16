cdbab59e94115fb1982765664997763dfe24f804
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-16 21:00:36 +0530

    modified

--- GREEDY/HuffManAlgorithm.java
+++ GREEDY/HuffManAlgorithm.java
@@ -1,143 +1,124 @@
 package GREEDY;
 
+import java.util.*;
 
-import java.util.Comparator;
-
-import java.util.PriorityQueue;
-
-class Pair
-{
+class Pair {
     char value;
     Pair left, right;
     int freq;
-    Pair(char value,int freq,Pair left,Pair right)
-    {
+    Pair(char value, int freq, Pair left, Pair right) {
         this.value=value;
         this.freq=freq;
         this.left=left;
         this.right=right;
     }
 }
-public class HuffManAlgorithm
-{
-    public static void main(String[] args)
-    {
-        String string  ="AAAAAABCCCCCCDDEEEEE";
+
+public class HuffManAlgorithm {
+    public static void main(String[] args) {
+        String string="AAAAAABCCCCCCDDEEEEE";
         int n=string.length();
-        HuffManEncodingAlgorithm encodingAlgorithm =new HuffManEncodingAlgorithm();
-        String  [] table=new String[52];
-        System.out.println(encodingAlgorithm.enCodingString(string,n,table));
-        HuffManDecodingAlgorithm decodingAlgorithm =new HuffManDecodingAlgorithm();
+        HuffManEncodingAlgorithm encodingAlgorithm=new HuffManEncodingAlgorithm();
+        String[] table=new String[52];
+        System.out.println("Encoded String: "+encodingAlgorithm.enCodingString(string, n, table));
+        HuffManDecodingAlgorithm decodingAlgorithm=new HuffManDecodingAlgorithm();
+        Pair root=encodingAlgorithm.getRoot();
+        String encodedString=encodingAlgorithm.enCodingString(string, n, table);
+        System.out.println("Decoded String: "+decodingAlgorithm.decodeHuffmanData(root, encodedString));
     }
 }
 
+class HuffManEncodingAlgorithm {
+    int[] freq=new int[52];
+    private Pair root;
 
-class HuffManEncodingAlgorithm
-{
-    int  [] freq =new int [52];
-    protected void print(Pair root,String  code,String [] table)
-    {
+    protected Pair getRoot() {
+        return root;
+    }
+
+    protected void print(Pair root, String code, String[] table) {
         if(root==null)
             return;
-        if(Character.isLetter(root.value))
-        {
-            if(Character.isLowerCase(root.value))
-            {
+        if(Character.isLetter(root.value)) {
+            if(Character.isLowerCase(root.value)) {
                 int ind=root.value-'a';
                 table[ind]=code;
-            }
-            else
-            {
+            } else {
                 int ind=root.value-'A'+26;
                 table[ind]=code;
             }
         }
-        print(root.left,code+'0',table);
-        print(root.right,code +'1',table);
+        print(root.left, code+'0', table);
+        print(root.right, code+'1', table);
     }
-    protected String  enCodingString(String string,int n,String [] table)
-    {
-        for (int i = 0; i < n; ++i) {
-            char c = string.charAt(i);
 
-            if (Character.isLowerCase(c)) {
-                freq[c - 'a']++;
+    protected String enCodingString(String string, int n, String[] table) {
+        for(int i=0;i<n;++i) {
+            char c=string.charAt(i);
+            if(Character.isLowerCase(c)) {
+                freq[c-'a']++;
             } else {
-                freq[c - 'A' + 26]++;
+                freq[c-'A'+26]++;
             }
         }
 
-        PriorityQueue<Pair> minheap =new PriorityQueue<>(Comparator.comparing((a)->a.freq));
-        for(int i=0;i<52;++i)
-        {
-            if(freq[i]!=0)
-            {
-                minheap.add(new Pair(intToChar(i),freq[i],null,null));
+        PriorityQueue<Pair> minheap=new PriorityQueue<>(Comparator.comparing((a)->a.freq));
+        for(int i=0;i<52;++i) {
+            if(freq[i]!=0) {
+                minheap.add(new Pair(intToChar(i), freq[i], null, null));
             }
         }
-        while(minheap.size()>1)
-        {
-            Pair pair1 =minheap.poll();
-            Pair pair2 =minheap.poll();
+        while(minheap.size()>1) {
+            Pair pair1=minheap.poll();
+            Pair pair2=minheap.poll();
             int freqSum=pair1.freq+pair2.freq;
-            Pair newPair= new Pair('*',freqSum, pair1,pair2);
+            Pair newPair=new Pair('*', freqSum, pair1, pair2);
             minheap.add(newPair);
         }
-        print(minheap.poll(),"",table);
-        StringBuilder stringBuilder =new StringBuilder();
-        for (int i = 0; i < n; ++i) {
-            char currentChar = string.charAt(i);
-            if (Character.isLowerCase(currentChar))
-            {
-                int index = currentChar - 'a';
-                String value = table[index];
+        root=minheap.poll();
+        print(root, "", table);
+        StringBuilder stringBuilder=new StringBuilder();
+        for(int i=0;i<n;++i) {
+            char currentChar=string.charAt(i);
+            if(Character.isLowerCase(currentChar)) {
+                int index=currentChar-'a';
+                String value=table[index];
                 stringBuilder.append(value);
-            }
-            else
-            {
-                int index = currentChar - 'A' + 26;
-                String value = table[index];
+            } else {
+                int index=currentChar-'A'+26;
+                String value=table[index];
                 stringBuilder.append(value);
             }
         }
-
         return stringBuilder.toString();
     }
-    protected char intToChar(int i)
-    {
+
+    protected char intToChar(int i) {
         if(i<=25)
             return ((char)('a'+i));
         else return (char)('A'+(i-26));
     }
 }
 
-class HuffManDecodingAlgorithm
-{
+class HuffManDecodingAlgorithm {
     public String decodeHuffmanData(Pair root, String binStr) {
-        int n = binStr.length();
-        StringBuilder result = new StringBuilder();
-        Pair temp = root;
-
-        for (int i = 0; i < n; ++i) {
-            // Traverse left for '0' and right for '1'
-            if (binStr.charAt(i) == '0') {
-                temp = temp.left;
+        int n=binStr.length();
+        StringBuilder result=new StringBuilder();
+        Pair temp=root;
+        for(int i=0;i<n;++i) {
+            if(binStr.charAt(i)=='0') {
+                temp=temp.left;
             } else {
-                temp = temp.right;
+                temp=temp.right;
             }
-
-            // If we reach a leaf node, append the character to the result
-            if (Character.isLetter(temp.value)) {
+            if(Character.isLetter(temp.value)) {
                 result.append(temp.value);
-                temp = root; // Reset to the root for the next character
+                temp=root;
             }
         }
-
-        // Check if we ended at a leaf node after the loop
-        if (Character.isLetter(temp.value)) {
+        if(Character.isLetter(temp.value)) {
             result.append(temp.value);
         }
-
         return result.toString();
     }
 }
