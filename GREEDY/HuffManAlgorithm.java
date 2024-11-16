0edac9e5c0c82e05f3c992e5e1a0d49430abda59
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-16 16:39:39 +0530

    modified

--- GREEDY/HuffManAlgorithm.java
+++ GREEDY/HuffManAlgorithm.java
@@ -1,14 +1,16 @@
 package GREEDY;
 
+import java.util.HashMap;
+
 public class HuffManAlgorithm
 {
 
     public static void main(String[] args)
     {
         String string  ="AAAAAABCCCCCCDDEEEEE";
-
+        int n=string.length();
         HuffManEncodingAlgorithm encodingAlgorithm =new HuffManEncodingAlgorithm();
-        encodingAlgorithm.enCodingString(string);
+        encodingAlgorithm.enCodingString(string,n);
         HuffManDecodingAlgorithm decodingAlgorithm =new HuffManDecodingAlgorithm();
 
     }
@@ -16,9 +18,14 @@ public class HuffManAlgorithm
 
 class HuffManEncodingAlgorithm
 {
-    protected String enCodingString(String string)
+    int [] freq =new int[52];
+    protected String enCodingString(String string,int n)
     {
-        
+        for(int i=0;i<n;++i)
+        {
+            int  ind=string.charAt(i);
+            if(ind<=65)
+        }
     }
 }
 
