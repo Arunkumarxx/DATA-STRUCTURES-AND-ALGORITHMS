0e26a83411908fa84922d9be736d843add0e9deb
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-16 22:28:32 +0530

    modified

--- BACKTRACKING/PermutationsOfGivenStringUsingBackTracking.java
+++ BACKTRACKING/PermutationsOfGivenStringUsingBackTracking.java
@@ -2,27 +2,33 @@ package BACKTRACKING;
 
 public class PermutationsOfGivenStringUsingBackTracking
 {
-    private void permutations(String string)
-    {
-        recursion(string,"");
-    }
-    private void recursion(String str1,String str2)
+    HashSet<String> hashSet =new HashSet<String>();
+    private void permutations(String str1, StringBuilder str2)
     {
         if(str1.length()==0)
         {
-            System.out.println(str2);
+            hashSet.add(str2.toString());
             return;
         }
         // backTracking
         for(int i=0;i<str1.length();++i)
         {
-            char curr=str1.charAt(i);
+            char curr = str1.charAt(i);
 
-            String  newStr2 = str2+curr;
+            str2.append(curr);
+            String newStr1 = str1.substring(0, i) + str1.substring(i + 1);
 
-            String newStr1 = str1.substring(0,i)+str1.substring(i+1);
-            recursion(newStr1,newStr2);
+            permutations(newStr1, str2);
+
+            str2.deleteCharAt(str2.length() - 1);
         }
+    }
+
+    public List<String> findPermutation(String s) {
+        permutations(s,new StringBuilder());
+        ArrayList<String> result =new ArrayList(hashSet);
+        Collections.sort(result);
+        return result;
     }
     public static void main(String[] args)
     {
