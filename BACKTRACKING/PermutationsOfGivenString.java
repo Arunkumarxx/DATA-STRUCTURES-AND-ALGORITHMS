e15162e9e5e6dc58d6b8a716a8d7eb9659f13bdf
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-16 21:57:24 +0530

    modified

--- BACKTRACKING/PermutationsOfGivenString.java
+++ BACKTRACKING/PermutationsOfGivenString.java
@@ -16,7 +16,7 @@ public class PermutationsOfGivenString
         for(int i=0;i<str1.length();++i)
         {
             String newStr2=str1.substring(i);
-            String newStr1=str1.substring(0,i-1)+str1.substring(i+1,str1.length());
+            String newStr1=str1.substring(0,i)+str1.substring(i+1,str1.length());
             recursion(newStr1,newStr2);
         }
     }
