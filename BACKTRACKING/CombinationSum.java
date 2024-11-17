5570c43c047bd2ab4440a116ad6e2076cb32d16c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-17 14:31:21 +0530

    modified

--- BACKTRACKING/CombinationSum.java
+++ BACKTRACKING/CombinationSum.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class CombinationSum
 {
     ArrayList<ArrayList<Integer>> result =new ArrayList<>();
-    private void combinationSum(int [] arr,ArrayList<Integer> list,int target,int sum,int ind)
+    private void cbmSum(int [] arr, ArrayList<Integer> list, int target, int sum, int ind)
     {
         if(sum==target)
             result.add(new ArrayList<>(list));
@@ -16,7 +16,7 @@ public class CombinationSum
         for(int i =ind; i<arr.length;++i)
         {
             list.add(arr[i]);
-            combinationSum(arr,list,target,sum+arr[i],i);
+            cbmSum(arr,list,target,sum+arr[i],i);
             list.remove(list.size()-1);
         }
     }
@@ -26,7 +26,7 @@ public class CombinationSum
         int target=7;
         CombinationSum sum =new CombinationSum();
         ArrayList<Integer> arrayList =new ArrayList<>();
-        sum.combinationSum(arr,arrayList,target,0,0);
+        sum.cbmSum(arr,arrayList,target,0,0);
         System.out.println(sum.result);
     }
 }
