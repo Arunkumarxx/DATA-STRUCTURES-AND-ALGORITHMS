9e15227d7fd5dc04de938e50014d95d9423d38e9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-17 13:00:32 +0530

    modified

--- BACKTRACKING/CombinationSum.java
+++ BACKTRACKING/CombinationSum.java
@@ -2,10 +2,16 @@ package BACKTRACKING;
 
 public class CombinationSum
 {
-
-    public static void main(String[] args)
+    private void CombinationSum(int [] arr,int target)
     {
         
+    }
+    public static void main(String[] args)
+    {
+        int [] arr = {2,3,6,7};
+        int target=7;
+        CombinationSum sum =new CombinationSum();
+        sum.CombinationSum(arr,target);
     }
 
 }
