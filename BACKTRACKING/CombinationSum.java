e5f5bcd275badb0cfa193aefdc602198a3c4e0d7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-17 14:56:15 +0530

    modified

--- BACKTRACKING/CombinationSum.java
+++ BACKTRACKING/CombinationSum.java
@@ -1,19 +1,13 @@
 package BACKTRACKING;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class CombinationSum
 {
-    static ArrayList<ArrayList<Integer>> combinationSum(ArrayList<Integer> A, int B)
-    {
-        result.clear();
-        Collections.sort(A);
-        ArrayList<Integer> list =new ArrayList<Integer>();
-        cbmSum(A,list,B,0,0);
-        return result;
-    }
-    private static ArrayList<ArrayList<Integer>> result =new ArrayList<>();
-    private static void cbmSum(ArrayList<Integer>arr, ArrayList<Integer> list, int target, int sum, int ind)
+    private ArrayList<ArrayList<Integer>> result=new ArrayList<>();
+
+    private void cbmSum(ArrayList<Integer> arr, ArrayList<Integer> list, int target, int sum, int ind)
     {
         if(sum==target)
             result.add(new ArrayList<>(list));
@@ -21,23 +15,36 @@ public class CombinationSum
         if(sum>target)
             return;
 
-        for(int i =ind; i<arr.size();++i)
+
+        for(int i=ind;i<arr.size();++i)
         {
-            if (i > ind && arr.get(i) == arr.get(i - 1))
+            if(i>ind&&arr.get(i)==arr.get(i-1))
                 continue;
 
             list.add(arr.get(i));
-            cbmSum(arr,list,target,sum+arr.get(i),i);
+            cbmSum(arr, list, target, sum+arr.get(i), i);
             list.remove(list.size()-1);
         }
     }
+
+    public ArrayList<ArrayList<Integer>> combinationSum(ArrayList<Integer> A, int B)
+    {
+        result.clear();
+        Collections.sort(A);
+        ArrayList<Integer> list=new ArrayList<>();
+        cbmSum(A, list, B, 0, 0);
+        return result;
+    }
+
     public static void main(String[] args)
     {
-        int [] arr = {2,3,6,7};
+        CombinationSum sum=new CombinationSum();
+        ArrayList<Integer> arr=new ArrayList<>();
+        arr.add(2);
+        arr.add(3);
+        arr.add(6);
+        arr.add(7);
         int target=7;
-        CombinationSum sum =new CombinationSum();
-        ArrayList<Integer> arrayList =new ArrayList<>();
-        sum.cbmSum(arr,arrayList,target,0,0);
-        System.out.println(sum.result);
+        System.out.println(sum.combinationSum(arr, target));
     }
 }
