4aebf4b284833a9118ed82206246c8620b486873
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-19 17:05:16 +0530

    modified

--- BACKTRACKING/M_Coloring.java
+++ BACKTRACKING/M_Coloring.java
@@ -1,62 +1,73 @@
 package BACKTRACKING;
 
+import java.util.*;
+
 public class M_Coloring
 {
-    int [] color;
+    int[] color;
     ArrayList<ArrayList<Integer>> list;
     int v;
     int m;
-    boolean graphColoring(int v, List<int[]> edges, int m) {
 
-        this.color=new int[v];
-        this.list=new ArrayList<>();
-        this.v=v;
-        this.m=m;
+    boolean graphColoring(int v, List<int[]> edges, int m)
+    {
+        this.color = new int[v];
+        this.list = new ArrayList<>();
+        this.v = v;
+        this.m = m;
 
-        for(int i=0;i<v;++i)
+        for (int i = 0; i < v; ++i)
             list.add(new ArrayList<Integer>());
 
-        int size=edges.size();
-        for(int i=0;i<size;++i)
+        for (int[] edge : edges)
         {
-            int first=edges.get(i)[0];
-            int second=edges.get(i)[1];
-            list.get(first).add(second);
-            list.get(second).add(first);
+            list.get(edge[0]).add(edge[1]);
+            list.get(edge[1]).add(edge[0]);
         }
-        return  mColoring(0);
+        return mColoring(0);
     }
+
     boolean mColoring(int src)
     {
-        if(src==v)
+        if (src == v)
             return true;
 
-        for(int i=1;i<=m;++i)
+        for (int i = 1; i <= m; ++i)
         {
-            if(isPossible(i,src))
+            if (isPossible(i, src))
             {
-                color[src]=i;
-
-                if(mColoring(src+1))
+                color[src] = i;
+                if (mColoring(src + 1))
                     return true;
-
-                color[src]=0;
+                color[src] = 0;
             }
         }
         return false;
     }
-    boolean isPossible(int colorCode,int src)
+
+    boolean isPossible(int colorCode, int src)
     {
-        for(int val:list.get(src))
-        {
-            if(colorCode==color[val])
+        for (int val : list.get(src))
+            if (colorCode == color[val])
                 return false;
-        }
         return true;
     }
+
     public static void main(String[] args)
     {
+        M_Coloring obj = new M_Coloring();
+        int v = 4;
+        List<int[]> edges = Arrays.asList(
+                new int[]{0, 1},
+                new int[]{0, 2},
+                new int[]{1, 2},
+                new int[]{1, 3}
+        );
+        int m = 3;
 
+        if (obj.graphColoring(v, edges, m))
+         System.out.println("The graph can be colored with " + m + " colors.");
+        else
+            System.out.println("The graph cannot be colored with " + m + " colors.");
     }
-
 }
