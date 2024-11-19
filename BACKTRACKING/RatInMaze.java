335694e99a483fc8059c5194a832755fab4f28f6
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-19 18:53:54 +0530

    modified

--- BACKTRACKING/RatInMaze.java
+++ BACKTRACKING/RatInMaze.java
@@ -1,8 +1,60 @@
 package BACKTRACKING;
-
+import java.util.*;
 public class RatInMaze
 {
-    
+    int n;
+    int m;
+
+    ArrayList<String> result =new ArrayList<String>();
+    boolean [][] visited;
+
+
+    int [] x ={-1,0,1,0};
+    int [] y ={0,-1,0,1};
+
+    char [] directions ={'U','L','D','R'};
+
+    StringBuilder string =new StringBuilder();
+
+
+    public ArrayList<String> findPath(int[][] mat) {
+        this.n=mat.length;
+        this.m=mat[0].length;
+        this.visited =new boolean [n][m];
+        if(mat[0][0]==1 && mat[n-1][m-1]==1)
+            dfs(mat,0,0);
+
+        return result;
+    }
+    public void dfs(int [][]mat,int row,int col)
+    {
+        if(row==n-1 && col==m-1)
+        {
+            result.add(string.toString());
+            return;
+        }
+        visited[row][col]=true;
+
+        for(int i=0;i<4;++i)
+        {
+            int newRow=row+x[i];
+            int newCol=col+y[i];
+            if(isValid(newRow,newCol,mat))
+            {
+                string.append(directions[i]);
+                dfs(mat,newRow,newCol);
+                string.deleteCharAt(string.length()-1);
+            }
+        }
+        visited[row][col]=false;
+    }
+    public boolean isValid(int x,int y,int[][]mat)
+    {
+        if(x>=0 &&  y>=0 && x<n && y<m && !visited[x][y] && mat[x][y]==1)
+            return true;
+        return false;
+    }
+
     public static void main(String[] args)
     {
 
