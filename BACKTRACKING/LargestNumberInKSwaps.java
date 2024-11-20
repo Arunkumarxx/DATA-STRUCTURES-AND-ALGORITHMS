cdc220491017c772f7e40d66608acde30f4f93b4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-20 16:17:40 +0530

    modified

--- BACKTRACKING/LargestNumberInKSwaps.java
+++ BACKTRACKING/LargestNumberInKSwaps.java
@@ -4,15 +4,45 @@ class Res {
     static String max = "";
 }
 
-
 public class LargestNumberInKSwaps
 {
+    public static void findMaximumNumUtil(char ar[], int k, Res r)
+    {
+        if (k == 0) return;
+        int n = ar.length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+
+                if (ar[j] > ar[i])
+                {
+                    char temp = ar[i];
+                    ar[i] = ar[j];
+                    ar[j] = temp;
+
+                    String st = new String(ar);
+
+                    if (r.max.compareTo(st) < 0)
+                    {
+                        r.max = st;
+                    }
+                    findMaximumNumUtil(ar, k - 1, r);
+
+                    temp = ar[i];
+                    ar[i] = ar[j];
+                    ar[j] = temp;
+                }
+            }
+        }
+    }
 
     public static void main(String[] args)
     {
-        char c1='a';
-        char c2='s';
-        System.out.println(c1<c2);
-    }
 
+        Res r = new Res();
+        r.max = str;
+        findMaximumNumUtil(str.toCharArray(), k, r);
+        System.out.println();
+    }
 }
