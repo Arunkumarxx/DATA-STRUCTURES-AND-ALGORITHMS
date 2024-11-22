71458e76fe6c8d4bc0b8fe1bd6278e02ced67b01
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-22 19:48:00 +0530

    modified

--- BACKTRACKING/UniqueSubsets.java
+++ BACKTRACKING/UniqueSubsets.java
@@ -12,11 +12,25 @@ public class UniqueSubsets
     }
     private void generateUniqueSubSets(int [] arr,int n,int ind)
     {
+        if(ind==n-1)
+            return;
+
         ArrayList<Integer> innerList =new ArrayList<>();
-        for(int i=0;i<ind;++i)
-        {
+
+        for(int i=0;i<=ind;++i)
             innerList.add(arr[i]);
-        }
+
+        list.add(innerList);
+
+        generateUniqueSubSets(arr,n,ind+1);
+
+        innerList.clear();
+
+        for(int i=0;i<ind;++i)
+            innerList.add(arr[i]);
+
+        for(int i=ind+1;i<n;++i)
+            innerList.add(arr[i]);
     }
 
     public static void main(String[] args)
@@ -24,6 +38,7 @@ public class UniqueSubsets
         UniqueSubsets uniqueSubsets =new UniqueSubsets();
         int [] arr ={1,2,3};
         System.out.println(uniqueSubsets.uniqueSubsets(arr,arr.length));
+        System.out.println(uniqueSubsets.list);
     }
 
 }
