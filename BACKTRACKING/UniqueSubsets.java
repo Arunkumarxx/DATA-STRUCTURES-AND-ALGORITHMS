2410f069a615aa4fb1030a3d6ea4a4f8f091d765
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-22 20:14:54 +0530

    modified

--- BACKTRACKING/UniqueSubsets.java
+++ BACKTRACKING/UniqueSubsets.java
@@ -1,38 +1,38 @@
 package BACKTRACKING;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class UniqueSubsets
 {
-    private static ArrayList<ArrayList<Integer>> list =new ArrayList<>();
-    public static ArrayList <ArrayList <Integer>> AllSubsets(int arr[], int n)
-    {
+    private static ArrayList<ArrayList<Integer>> list=new ArrayList<>();
 
+    public static ArrayList<ArrayList<Integer>> AllSubsets(int arr[], int n)
+    {
         list.clear();
         Arrays.sort(arr);
-        generateUniqueSubSets(arr,new ArrayList<>(),n,0);
+        generateUniqueSubSets(arr, new ArrayList<>(), n, 0);
         return list;
     }
-    private static void generateUniqueSubSets(int [] arr,ArrayList<Integer> arrayList,int n,int ind)
+
+    private static void generateUniqueSubSets(int[] arr, ArrayList<Integer> arrayList, int n, int ind)
     {
         list.add(new ArrayList<>(arrayList));
         for(int i=ind;i<n;++i)
         {
-            if(i>ind  && arr[i-1]==arr[i])
+            if(i>ind&&arr[i-1]==arr[i])
                 continue;
 
             arrayList.add(arr[i]);
-
-            generateUniqueSubSets(arr,arrayList,n,i+1);
-
+            generateUniqueSubSets(arr, arrayList, n, i+1);
             arrayList.remove(arrayList.size()-1);
         }
     }
+
     public static void main(String[] args)
     {
-        UniqueSubsets uniqueSubsets =new UniqueSubsets();
-        int [] arr ={1,2,3};
-        System.out.println(uniqueSubsets.uniqueSubsets(arr,arr.length));
+        UniqueSubsets uniqueSubsets=new UniqueSubsets();
+        int[] arr={1, 2, 3};
+        System.out.println(AllSubsets(arr, arr.length));
     }
-
 }
