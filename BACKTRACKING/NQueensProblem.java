f7ba9331aad2057f0ed80851c963f5fec7a36af7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-23 14:34:42 +0530

    modified

--- BACKTRACKING/NQueensProblem.java
+++ BACKTRACKING/NQueensProblem.java
@@ -2,9 +2,17 @@ package BACKTRACKING;
 
 public class NQueensProblem
 {
-    
+    boolean [][] chessBoardTrack ;
+
+    char [][] chessBoard;
+    private void nQueensProblem(int n)
+    {
+        chessBoardTrack= new boolean[n][n];
+        
+    }
     public static void main(String[] args)
     {
+        int n=4;
 
     }
 
