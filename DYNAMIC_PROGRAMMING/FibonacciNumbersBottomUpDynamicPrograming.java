00e2dd511a9adf2d0f73ea48fd89f05dfae8b9a9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-24 15:53:35 +0530

    modified

--- DYNAMIC_PROGRAMMING/FibonacciNumbersBottomUpDynamicPrograming.java
+++ DYNAMIC_PROGRAMMING/FibonacciNumbersBottomUpDynamicPrograming.java
@@ -30,6 +30,6 @@ public class FibonacciNumbersBottomUpDynamicPrograming
     public static void main(String[] args)
     {
         FibonacciNumbersBottomUpDynamicPrograming obj =new FibonacciNumbersBottomUpDynamicPrograming();
-        System.out.println(obj.findNthFibonacci(15));
+        System.out.println(obj.findNthFibonacci());
     }
 }
