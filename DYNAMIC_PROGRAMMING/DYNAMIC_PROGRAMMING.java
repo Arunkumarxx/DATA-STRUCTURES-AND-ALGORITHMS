7adef701e72571d15ffcc67ebf1d690da4f153f9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-24 14:34:22 +0530

    modified

--- DYNAMIC_PROGRAMMING/DYNAMIC_PROGRAMMING.java
+++ DYNAMIC_PROGRAMMING/DYNAMIC_PROGRAMMING.java
@@ -4,17 +4,6 @@ public class DYNAMIC_PROGRAMMING {
 
     public static void main(String[] args)
     {
-        int i=0;
-        int j=1;
-        int n=34;
-        System.out.print("Fibonacci series: "+i+" "+j);
-        while(i+j<=n)
-        {
-            int sum=i+j;
-            System.out.print(" "+sum);
-            i=j;
-            j=sum;
-        }
     }
 
 }
