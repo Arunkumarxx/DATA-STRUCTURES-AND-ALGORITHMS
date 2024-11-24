4f8fbad9db631ce499314036765f4a747d34afb6
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-24 15:52:35 +0530

    modified

--- DYNAMIC_PROGRAMMING/FibonacciNumbersBottomUpDynamicPrograming.java
+++ DYNAMIC_PROGRAMMING/FibonacciNumbersBottomUpDynamicPrograming.java
@@ -1,11 +1,35 @@
 package DYNAMIC_PROGRAMMING;
 
+import javax.sound.sampled.EnumControl;
+import java.util.*;
 public class FibonacciNumbersBottomUpDynamicPrograming
 {
-    
-    public static void main(String[] args)
-    {
 
+    long [] arr;
+    public long findNthFibonacci(int number)
+    {
+        arr=new long [number+1];
+        Arrays.fill(arr,-1);
+        return  recursion(number);
     }
 
+    public long recursion(long n)
+    {
+        if(arr[(int)n]==-1L)
+        {
+            long res;
+            if(n==0L || n==1L)
+                return n;
+            else
+                res=recursion(n-1L)+recursion(n-2L);
+            arr[(int)n]=res;
+        }
+        return arr[(int)n];
+    }
+
+    public static void main(String[] args)
+    {
+        FibonacciNumbersBottomUpDynamicPrograming obj =new FibonacciNumbersBottomUpDynamicPrograming();
+        System.out.println(obj.findNthFibonacci(100));
+    }
 }
