21d4221429dbd649034860fd28781400acdb36ac
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-25 21:19:28 +0530

    modified

--- DYNAMIC_PROGRAMMING/MaximumContiguousSubArraySumAndElements.java
+++ DYNAMIC_PROGRAMMING/MaximumContiguousSubArraySumAndElements.java
@@ -2,7 +2,28 @@ package DYNAMIC_PROGRAMMING;
 
 public class MaximumContiguousSubArraySumAndElements
 {
-    
+    public long maximumSum(int arr[], int n)
+    {
+        int maxSum=Integer.MIN_VALUE;
+        int currSum=0;
+        StringBuilder str = new StringBuilder();
+        for(int i=0;i<n;++i)
+        {
+            if(currSum<0)
+                currSum=0;
+
+            currSum+=arr[i];
+
+            if(currSum>maxSum)
+                maxSum=currSum;
+
+            str.append(currSum).append(" ");
+        }
+
+        System.out.println(str.toString());
+        return (long)maxSum;
+    }
+
     public static void main(String[] args)
     {
 
