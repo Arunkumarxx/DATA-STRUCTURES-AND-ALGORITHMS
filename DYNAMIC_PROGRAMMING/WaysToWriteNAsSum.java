e36c84a00db953a2437dc1901026ee5e85eb9b43
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-25 19:21:37 +0530

    modified

---
+++ DYNAMIC_PROGRAMMING/WaysToWriteNAsSum.java
@@ -0,0 +1,33 @@
+package DYNAMIC_PROGRAMMING;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class WaysToWriteNAsSum
+{
+    int countWays(int n)
+    {
+        int table[]=new int[n+1];
+        int mod=1000000007;
+
+        for(int i=0;i<=n;i++)
+        {
+            table[i]=0;
+        }
+
+        table[0]=1;
+
+        for(int i=1;i<n;i++)
+            for(int j=i;j<=n;j++)
+                table[j]=(table[j]%mod+table[j-i]%mod)%mod; 
+
+        return table[n]%mod;
+    }
+
+    public static void main(String[] args)
+    {
+        WaysToWriteNAsSum obj=new WaysToWriteNAsSum();
+        int n=5;
+        System.out.println("Number of ways to write "+n+" as sum: "+obj.countWays(n));
+    }
+}
