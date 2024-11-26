d4905135482c0a5acc10939eb72cad0878ea4f1b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-26 20:12:25 +0530

    modified

--- DYNAMIC_PROGRAMMING/CountWaysToNthStair.java
+++ DYNAMIC_PROGRAMMING/CountWaysToNthStair.java
@@ -2,13 +2,12 @@ package DYNAMIC_PROGRAMMING;
 
 public class CountWaysToNthStair
 {
-
     private Long countWays(int n) {
         return (long)(n/2) +1L;
     }
     public static void main(String[] args)
     {
         CountWaysToNthStair obj =new CountWaysToNthStair();
-        obj.countWays(5);
+        System.out.println(obj.countWays(5));
     }
 }
