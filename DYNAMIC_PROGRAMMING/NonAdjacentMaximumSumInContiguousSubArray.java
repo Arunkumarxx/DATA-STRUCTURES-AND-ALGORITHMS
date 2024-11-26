d35092a9592d4a981e9fe34a2077e2e07c72de32
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-26 15:17:33 +0530

    modified

--- DYNAMIC_PROGRAMMING/NonAdjacentMaximumSumInContiguousSubArray.java
+++ DYNAMIC_PROGRAMMING/NonAdjacentMaximumSumInContiguousSubArray.java
@@ -2,11 +2,28 @@ package DYNAMIC_PROGRAMMING;
 
 public class NonAdjacentMaximumSumInContiguousSubArray
 {
+    public long maximumSum(int arr[], int sizeOfArray)
+    {
 
+        arr[1] = Math.max(arr[0],arr[1]);
+        for(int i = 2;i<sizeOfArray;i++)
+
+            arr[i] = Math.max(arr[i],Math.max(arr[i-1],arr[i]+arr[i-2]));
+
+
+        long max = arr[0];
+
+        for(int i =0;i<sizeOfArray;i++)
+            max = Math.max(max,arr[i]);
+
+        return max;
+
+    }
     public static void main(String[] args)
     {
         int [] arr = {4,5,6,7,8};
-        
+        NonAdjacentMaximumSumInContiguousSubArray obj =new NonAdjacentMaximumSumInContiguousSubArray();
+        System.out.println(obj.maximumSum(arr, arr.length));
     }
 
 }
