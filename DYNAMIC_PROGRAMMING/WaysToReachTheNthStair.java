250b4f8405afd6b2e8a51b20e5779e21acc7aa32
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-26 17:26:32 +0530

    modified

--- DYNAMIC_PROGRAMMING/WaysToReachTheNthStair.java
+++ DYNAMIC_PROGRAMMING/WaysToReachTheNthStair.java
@@ -2,10 +2,20 @@ package DYNAMIC_PROGRAMMING;
 
 public class WaysToReachTheNthStair
 {
-    
+    int countWays(int n)
+    {
+        int [] dp =new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;++i)
+            dp[i]=dp[i-2]+dp[i-1];
+        return dp[n];
+    }
     public static void main(String[] args)
     {
-
+        WaysToReachTheNthStair obj =new WaysToReachTheNthStair();
+        System.out.println(obj.countWays(5));
     }
 
 }
