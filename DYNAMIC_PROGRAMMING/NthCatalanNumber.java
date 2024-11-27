96ffeeba99d27576400e26c02f0098d2a2fd77f9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-27 14:57:53 +0530

    modified

--- DYNAMIC_PROGRAMMING/NthCatalanNumber.java
+++ DYNAMIC_PROGRAMMING/NthCatalanNumber.java
@@ -2,5 +2,28 @@ package DYNAMIC_PROGRAMMING;
 
 public class NthCatalanNumber
 {
-    
+    private int findCatalan(int n)
+    {
+        if(n==0 || n==1)
+            return 1;
+
+        long dp[]=new long[n+1],mod=(long)(1e9)+7;
+        dp[0]=dp[1]=1;
+
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=0;
+            for(int j=0;j<i;j++)
+            {
+                dp[i] = (dp[i]+(dp[j]*dp[i-j-1])%mod)%mod;
+            }
+        }
+
+        return (int)dp[n];
+    }
+    public static void main(String[] args)
+    {
+        NthCatalanNumber obj =new NthCatalanNumber();
+        System.out.println(obj.findCatalan(5));
+    }
 }
