5427fc55327933b2ea1d7be6ce9e22e4680f27ff
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-27 16:16:33 +0530

    modified

--- DYNAMIC_PROGRAMMING/LongestCommonSubsequence.java
+++ DYNAMIC_PROGRAMMING/LongestCommonSubsequence.java
@@ -5,7 +5,16 @@ public class LongestCommonSubsequence
 
     private int getLCS(String s1,String s2)
     {
-        
+        // a b d a c e
+        // b a b c e
+
+        int n1=s1.length();
+        int n2=s2.length();
+
+        for(int i=0;i<n1;++i)
+        {
+            
+        }
     }
     public static void main(String[] args)
     {
