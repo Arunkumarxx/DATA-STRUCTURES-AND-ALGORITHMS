516cdbd27659f4d4637f720d1b45a64cd77caa04
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-27 16:40:13 +0530

    modified

--- DYNAMIC_PROGRAMMING/LongestCommonSubsequence.java
+++ DYNAMIC_PROGRAMMING/LongestCommonSubsequence.java
@@ -1,47 +1,40 @@
 package DYNAMIC_PROGRAMMING;
 
-public class LongestCommonSubsequence
-{
+public class LongestCommonSubsequence {
 
-    private int getLCS(String s1,String s2)
-    {
-        // a b d a c e
-        // b a b c e
+    private int getLCS(String s1, String s2) {
+        int n1 = s1.length();
+        int n2 = s2.length();
+        int max = 0;
 
-        int n1=s1.length();
-        int n2=s2.length();
-        int max=0;
-        for(int i=0;i<n2;++i)
-        {
-            for(int j=0;j<n1;++j)
-            {
-                int x=i;
-                int y=j;
-                int count=0;
-                while(x<n2 && y<n1)
-                {
-                    while (x < n2 && y < n1 && s2.charAt(x) != s1.charAt(y))
-                        ++y;
-                    if(x<n2 && y<n1 )
-                    {
-                        if(s2.charAt(x)==s1.charAt(y))
-                        {
-                            ++count;
-                            ++x;
-                            
-                        }
-                        else ++y;
+        // Iterate over all starting indices in s2
+        for (int i = 0; i < n2; ++i) {
+            // Iterate over all starting indices in s1
+            for (int j = 0; j < n1; ++j) {
+                int x = i;
+                int y = j;
+                int count = 0;
+
+                // Check for common subsequence from these starting indices
+                while (x < n2 && y < n1) {
+                    if (s2.charAt(x) == s1.charAt(y)) {
+                        count++; // Characters match, increment count
+                        x++;
                     }
+                    y++; // Move to the next character in s1 regardless
                 }
-                max=Math.max(count,max);
+
+                // Update max length of LCS found so far
+                max = Math.max(max, count);
             }
         }
         return max;
     }
-    public static void main(String[] args)
-    {
-        LongestCommonSubsequence obj=new LongestCommonSubsequence();
-        System.out.println(obj.getLCS("ABCDGH","AEDFHR"));
-    }
 
+    public static void main(String[] args) {
+        LongestCommonSubsequence obj = new LongestCommonSubsequence();
+        System.out.println("LCS Length: " + obj.getLCS("ABCDGH", "AEDFHR")); // Expected: 3
+        System.out.println("LCS Length: " + obj.getLCS("ABC", "AC"));        // Expected: 2
+        System.out.println("LCS Length: " + obj.getLCS("XYZW", "XYWZ"));    // Expected: 3
+    }
 }
