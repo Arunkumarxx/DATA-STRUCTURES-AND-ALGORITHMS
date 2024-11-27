8c7235580d28f3da7f99ae74e04caea98e664b4b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-11-27 15:48:19 +0530

    modified

--- DYNAMIC_PROGRAMMING/LongestCommonSubsequence.java
+++ DYNAMIC_PROGRAMMING/LongestCommonSubsequence.java
@@ -5,32 +5,28 @@ public class LongestCommonSubsequence
 
     private int getLCS(String s1,String s2)
     {
-        int n1 = s1.length();
-        int n2 = s2.length();
-        int max = 0;
-
-        for (int i = 0; i < n2; ++i) {
-            int indOne = i;
-            int indTwo = 0;
-            int count = 0;
-
-            while (indOne < n2 && indTwo < n1) {
-                if (s2.charAt(indOne) == s1.charAt(indTwo)) {
+        int n1=s1.length();
+        int n2=s2.length();
+        int max=0;
+        for(int i=0;i<n2;++i)
+        {
+            int indOne=i;
+            int indTwo=0;
+            int count=0;
+            while (indOne<n2 && indTwo<n1)
+            {
+                if(s2.charAt(indOne)==s1.charAt(indTwo))
+                {
                     ++count;
                     ++indOne;
                     ++indTwo;
-                } else {
-                    ++indOne;  // Move indOne forward to try to find a match
                 }
+                else ++indTwo;
             }
-
-            if (count > max) {
-                max = count;
-            }
+            if(count>max)
+                max=count;
         }
-
         return max;
-
     }
     public static void main(String[] args)
     {
