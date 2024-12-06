0fb4b3a1e9d2fb226666bc057f6f5b432cff8d62
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-12-06 14:20:14 +0530

    modified

--- TREE/SearchQueryForStrings.java
+++ TREE/SearchQueryForStrings.java
@@ -2,10 +2,21 @@ package TREE;
 
 public class SearchQueryForStrings
 {
-    
+    private final int alphabetSize=52;
+    private class TrieNode
+    {
+        TrieNode [] children;
+        boolean isEndOfWord;
+        TrieNode()
+        {
+            this.children =new TrieNode[alphabetSize];
+        }
+    }
+    TrieNode root;
+
     public static void main(String[] args)
     {
-
+        
     }
 
 }
