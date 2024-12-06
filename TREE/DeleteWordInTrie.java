5c41ed152c50b44385f356a4af381832b6f53281
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-12-06 15:54:53 +0530

    modified

--- TREE/DeleteWordInTrie.java
+++ TREE/DeleteWordInTrie.java
@@ -2,10 +2,23 @@ package TREE;
 
 public class DeleteWordInTrie
 {
-    private final int 
-    public static void main(String[] args)
+    private final int alphabetSize=26;
+    private class TrieNode
+    {
+        TrieNode[] children = new TrieNode[alphabetSize];
+    }
+    private void insert(String word)
     {
 
+    }
+    private void search(String word)
+    {
+
+    }
+    public static void main(String[] args)
+    {
+        DeleteWordInTrie Trie =new DeleteWordInTrie();
+        
     }
 
 }
