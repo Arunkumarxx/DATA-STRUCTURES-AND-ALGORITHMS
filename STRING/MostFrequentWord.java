748cd0eb303044b0daa539a47c71b1d5eb939665
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-12-09 19:36:48 +0530

    modified

--- STRING/MostFrequentWord.java
+++ STRING/MostFrequentWord.java
@@ -1,2 +1,34 @@
-package STRING;public class MostFrequentWord {
+package STRING;
+import java.util.*;
+public class MostFrequentWord
+{
+    HashMap<String,Integer> hashMap =new HashMap<>();
+    StringBuilder result=new StringBuilder();
+    int freq=0;
+    public String mostFrequentWord(String arr[],int n)
+    {
+        mostFreqWord(arr,n,0);
+        return result.toString();
+    }
+    public void mostFreqWord(String [] arr,int n,int ind)
+    {
+        if(ind==n)
+            return;
+
+        if(hashMap.containsKey(arr[ind]))
+            hashMap.put(arr[ind],hashMap.get(arr[ind])+1);
+        else hashMap.put(arr[ind],1);
+        mostFreqWord(arr,n,ind+1);
+        int newFreq=hashMap.get(arr[ind]);
+        if(newFreq>freq)
+        {
+            result.setLength(0);
+            result.append(arr[ind]);
+            freq=newFreq;
+        }
+    }
+    public static void main(String[] args)
+    {
+        
+    }
 }
