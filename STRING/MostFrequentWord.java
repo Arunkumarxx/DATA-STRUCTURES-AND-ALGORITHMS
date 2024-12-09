814ee3692c4fc1d726c1e1a26be3cf3c43a7fb08
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-12-09 19:57:35 +0530

    modified

--- STRING/MostFrequentWord.java
+++ STRING/MostFrequentWord.java
@@ -31,7 +31,8 @@ public class MostFrequentWord
     public static void main(String[] args)
     {
         MostFrequentWord obj =new MostFrequentWord();
-        String [] arr ={"xejdcj","xejdcj"," lvjpb","tmyuiu","lvjpb","tmyuiu","ovoba"," lvjpb","lvjpb","fqhyu"," fqhyu","tmyuiu","xejdcj","tmyuiu"," fqhyu","ovoba","xejdcj"};
-        System.out.println(obj.mostFrequentWord(arr,arr.length));
+        String[] arr = {"xejdcj", "xejdcj", "lvjpb", "tmyuiu", "lvjpb", "tmyuiu",
+                "ovoba", "lvjpb", "lvjpb", "fqhyu", "fqhyu", "tmyuiu",
+                "xejdcj", "tmyuiu", "fqhyu", "ovoba", "xejdcj"};        System.out.println(obj.mostFrequentWord(arr,arr.length));
     }
 }
